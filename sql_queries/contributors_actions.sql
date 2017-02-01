01d62e16e26df66ce7d3b3dcc11ffdef6ba87c76
Derek Howard <doombreakr@gmail.com>
2017-02-01 13:37:01 -0600

    Update contributors_actions.sql

--- sql_queries/contributors_actions.sql
+++ sql_queries/contributors_actions.sql
@@ -11,7 +11,7 @@ SET @proj = (SELECT projects.id FROM projects INNER JOIN users ON projects.owner
 SELECT * FROM
 
    (
-   SELECT users.id        as "user_id", 
+   SELECT       users.id        as "user_id", 
 	        users.login     as "login",
 	        com.count       as "commits",
 	        pulls.count     as "pull_requests",
