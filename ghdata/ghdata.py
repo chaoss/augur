29798664223642c876c6cd99d7f86bf0afd60445
Derek Howard <derek@howderek.com>
2017-02-04 16:20:53 -0600

    Move to Flask, ready to merge into dev and delete exploratory

--- ghdata/ghdata.py
+++ ghdata/ghdata.py
@@ -1,11 +1,14 @@
 #SPDX-License-Identifier: MIT
 
 import sqlalchemy as s
-import records
+import pandas as pd
 
-class GHData:
+
+class GHData(object):
     def __init__(self, dbstr):
-        self.db = records.Database(dbstr)
+        self.db = s.create_engine(dbstr)
+        self.__schema = s.MetaData()
+        self.__schema.reflect(bind=self.db)
 
 
     def __generate_predicate_dates(table, start=None, end=None):
@@ -19,12 +22,23 @@ class GHData:
 
     # Gets information about users
     def user(self, username=None, start=None, end=None):
-        meta = s.MetaData()
-        users = s.Table('users', meta, autoload=True, autoload_with=self.db.db)
+        users = self.__schema.table['users']
         q = s.select([users])
         if (start or end):
             q = q.where(s.sql.text(self.__generate_predicate_dates(start, end)))
         if (username):
             q = q.where(users.c.login == username)
-            return self.db.query(str(q), login_1=username)
-        return self.db.query(str(q))
+            return self.db.execute(str(q), login_1=username)
+        return self.db.execute(str(q))
+
+    def stargazers(self, repo=None, start=None, end=None):
+        stargazers = self.__schema['watchers']
+        q = s.select([stargazers])
+        if (start or end):
+            q = q.where(s.sql.text(self.__generate_predicate_dates(start, end)))
+        if (repo):
+            projects = self.__schema['projects']
+            q = q.join(projects)
+            q = q.where(projects.c.name == repo)
+            return self.db.execute(str(q), name_1=repo)
+
--- ghdata/server.py
+++ ghdata/server.py
@@ -1,6 +1,5 @@
 #SPDX-License-Identifier: MIT
-
-import click
+from flask import Flask, Response, json
 import os
 import sys
 import datetime
@@ -15,56 +14,52 @@ from ghdata import GHData
 class GHDataClient:
     """Stores configuration of the CLI, which can be set using options at the command line"""
     def __init__(self, db_host='127.0.0.1', db_port=3306, db_user='root', db_pass='', db_name='ghtorrent', file=None, dataformat=None, start=None, end=None, connect=False):
-        self.db_host = db_host
-        self.db_port = db_port
-        self.db_user = db_user
-        self.db_pass = db_pass
-        self.db_name = db_name
-        self.file = file
-        self.dataformat = dataformat
+        self.__db_host = db_host
+        self.__db_port = db_port
+        self.__db_user = db_user
+        self.__db_pass = db_pass
+        self.__db_name = db_name
+        self.__file = file
+        self.__dataformat = dataformat
 
         if (connect):
-            self.connect()
+            self.__connect()
         # Parse start time
+        """
         if (start == 'earliest'):
-            self.start = None
+            self.__start = None
         elif (start != None):
-            self.start = parser.parse(start, fuzzy=True)
-            self.start = self.start
+            self.__start = parser.parse(start, fuzzy=True)
+            self.__start = self.start
         
         # Parse end time
         if (end == 'latest'):
-            self.end = None
+            self.__end = None
         else:
-            self.end = parser.parse(end, fuzzy=True)
+            self.__end = parser.parse(end, fuzzy=True)
+        """
 
-    def connect(self):
-        self.dbstr = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(self.db_user, self.db_pass, self.db_host, self.db_port, self.db_name)
-        self.ghdata = GHData(self.dbstr)
 
-    def output(self, obj):
-        click.echo(obj.export(self.dataformat))
+    def __connect(self):
+        if (self.__ghdata):
+            self.__dbstr = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(self.db_user, self.db_pass, self.db_host, self.db_port, self.db_name)
+            self.__ghdata = GHData(self.dbstr)
+         
 
-    def user(self, username):
-        self.output(self.ghdata.user(username, self.start, self.end))
+    """If an attribute doesn't exist, assume it's a GHData method and handle that"""
+    def get(self, key, **args):
+        self.__connect()
+        print(self.__ghdata.__class__.__dict__)
+        self.__output(getattr(self.__ghdata, key)(start=self.__start, end=self.__end, **args))
 
 
 
 # Globals
 client = None # Initalized in the base group function below
+app = Flask(__name__)
 # Flags and Initialization
-@click.group()
-@click.option('--host', default='127.0.0.1', help='Database host (default: localhost)')
-@click.option('--port', default='3306', help='Database port (default: 3306)')
-@click.option('--db', default='ghtorrent', help='Database name (default: ghtorrent)')
-@click.option('--user', default='root', help='Database user (default: root)')
-@click.option('--password', default='root', help='Database pass (default: root)')
-@click.option('--file', type=click.File('wb'), default=sys.stdout, help='Output file')
-@click.option('--config', type=click.File('rb'), help='Configuration file')
-@click.option('--format', 'dataformat', default='csv', help='csv (default), json, yaml, json, xls, xlsx, human')
-@click.option('--start', default='earliest', help='First date to get data from. Keyword \'earliest\' includes oldest data (default).')
-@click.option('--end', default='latest', help='Last date to get data from. Keyword \'latest\' includes newest data (default)' )
-def cli(host, port, db, user, password, file, config, dataformat, start, end):
+
+def init():
     """Tool to gather data about GitHub repositories.
 
     Requires the GHTorrent MySQL database (http://ghtorrent.org/).
@@ -74,101 +69,56 @@ def cli(host, port, db, user, password, file, config, dataformat, start, end):
     To get help on subcommands, type the command with --help.
     """
     # Read config file if passed
-    if (config):
+    try:
         parser = configparser.ConfigParser()
-        parser.readfp(config)
+        parser.read('ghdata.cfg')
         host = parser.get('Database', 'host')
         port = parser.get('Database', 'port')
         user = parser.get('Database', 'user')
         password = parser.get('Database', 'pass')
         db = parser.get('Database', 'name')
         dataformat = parser.get('Format', 'format')
-
-    global client
+        global client
+        client = GHDataClient(db_host=host, db_port=port, db_user=user, db_pass=password, db_name=db, file=file, dataformat=dataformat)
+        app.run()
+    except:
+        print('Failed to open config file.')
+        config = configparser.RawConfigParser()
+        config.add_section('Database')
+        config.set('Database', 'host', '127.0.0.1')
+        config.set('Database', 'port', '3306')
+        config.set('Database', 'user', 'root')
+        config.set('Database', 'pass', 'root')
+        config.set('Database', 'name', 'ghtorrent')
+        config.add_section('Format')
+        config.set('Format', 'format', 'csv')
+        # Writing our configuration file to 'example.cfg'
+        with open('ghdata.cfg', 'wb') as configfile:
+            config.write(configfile)
+        print('Default config saved to ghdata.cfg')
     
-    client = GHDataClient(db_host=host, db_port=port, db_user=user, db_pass=password, db_name=db, file=file, dataformat=dataformat, start=start, end=end)
+
+"""
+
+"""
 
 
+@app.route('/')
+def root():
+    info = Response(response='{"status": "active"}',
+                    status=200,
+                    mimetype="application/json")
+    return info
 
-
-# Get data about a user
-@cli.command()
-@click.argument('username', default='')
+@app.route('/user/:username')
 def user(username):
-    """Events for a given user"""
-    client.connect()
-    client.user(username)
-
-
-# Get information about repos, includes subcommands
-@cli.group()
-def repo():
-    """Events for a given repository"""
-    # @todo: All events related to a repo
-    return
-
-@repo.command()
-@click.argument('repo', default='')
-def commits(username):
-    # @todo: Releases
-    return
-
-@repo.command()
-@click.argument('repo', default='')
-def starring(username):
-    # @todo: Stargazers and when they started starring
-    return
-
-@repo.command()
-@click.argument('repo', default='')
-def forks(username):
-    # @todo: Fork timeseries
-    return
-
-@repo.command()
-@click.option('--with-comments', 'comments', is_flag=True, default=False)
-@click.argument('repo', default='')
-def pullrequests(comments, username):
-    # @todo: Pull requests and their events
-    return
-
-@repo.command()
-@click.argument('repo', default='')
-def issues(username):
-    # @todo: Issues and their metadata
-    return
-
-@repo.command()
-@click.argument('repo', default='')
-def statistics(username):
-    # @todo: Statistics from the Statistics API
-    return
-
-@repo.command()
-@click.argument('repo', default='')
-def releases(username):
-    # @todo: Releases
-    return
+    return ''
 
 # Generates a default config file
-@cli.command(name="create-default-config")
-@click.argument('username', default='')
 def create_default_config(username):
     """Generates default .cfg file"""
-    config = configparser.RawConfigParser()
-    config.add_section('Database')
-    config.set('Database', 'host', '127.0.0.1')
-    config.set('Database', 'port', '3306')
-    config.set('Database', 'user', 'root')
-    config.set('Database', 'pass', 'root')
-    config.set('Database', 'name', 'ghtorrent')
-    config.add_section('Format')
-    config.set('Format', 'format', 'csv')
-    # Writing our configuration file to 'example.cfg'
-    with open('default.cfg', 'wb') as configfile:
-        config.write(configfile)
-    click.echo('Default config saved to default.cfg')
+    
 
 
 if __name__ == '__main__':
-    cli(auto_envvar_prefix='GHDATA') #Says that we want environment variables prefixed with GHDATA
+    init()
