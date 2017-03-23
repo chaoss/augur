6847bccfb5adefeee890943d4289f48790851628
Derek Howard <derek@howderek.com>
2017-03-23 10:52:39 -0500

    Add __init__.py for new arch

---
+++ ghdata/__init__.py
@@ -0,0 +1,9 @@
+#SPDX-License-Identifier: MIT
+"""
+GHData is a library for researchers who wants to work with metrics related to
+the health and sustainability of GitHub repositories.
+
+Most functions in the library return Pandas DataFrames.
+"""
+from .services.ghtorrent import GHTorrent
+from .services.publicwww import PublicWWW
