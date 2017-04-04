82f689dfda2e049925367145d0560be8aebb63d8
ChristianCme <cmehil.warn@gmail.com>
2017-04-04 15:55:32 -0500

    Added rough version of contributions by file

--- ghdata/services/githubapi.py
+++ ghdata/services/githubapi.py
@@ -14,3 +14,21 @@ class GitHubAPI(object):
       import github
       self.GITUB_API_KEY = api_key
       self.__api = github.Github(api_key)
+
+    def contributions_by_file(self, owner, repo, start=None, end=None):
+        """
+        Gets number of addtions and deletions in each file by user
+
+        Puts it in dataframe with columns:
+        User    File    Num of Additions    Num of Deletions
+
+        WORK IN PROGRESS
+        """
+        df = []
+        for commit in self.g.get_repo((owner + "/" + repo)).get_commits(since=start,until=end):
+            for file in commit.files:
+                df.append({'User': commit.author, 'File': file.filename, 'Number of Additions': file.additions, 'Number of Deletions': file.deletions, 'Total': file.changes})
+
+        pd.DataFrame(df)
+
+        return df
