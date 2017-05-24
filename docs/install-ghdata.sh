c7b07339e5c9b100f20dcf73a1c96fd77d8c4194
Derek Howard <derek@howderek.com>
2017-05-24 11:43:52 -0500

    Add unzip dependency

--- docs/install-ghdata.sh
+++ docs/install-ghdata.sh
@@ -5,17 +5,19 @@ PACKAGE_MANAGER="sudo apt-get -y install"
 MYSQL_PACKAGE="mysql-server"
 NODE_PACKAGE="nodejs"
 CURL_PACKAGE="curl"
+UNZIP_PACKAGE="unzip"
 INSTALL_CURL=0
 INSTALL_NODE_PPA=0
 PYTHON_PACKAGE="python python-pip"
 DEPENDENCY_INSTALL_COMMAND="$PACKAGE_MANAGER"
+SCRIPT_DEPENDENCY_INSTALL_COMMAND="$PACKAGE_MANAGER"
 
 function yes_or_no {
     read -p "$1 [y/n]: " -n 1 -r
   if [[ ! $REPLY =~ ^[Yy]$ ]]
   then
     printf "$2\n"
-      return 1
+    return 1
   fi
   echo
   return 0
@@ -42,44 +44,51 @@ echo "| Dependency  |  Status  |"
 echo "+-------------+----------+" 
 
 if hash mysql 2>/dev/null; then
-    echo "| MySQL       |    found |"
+  echo "| MySQL       |    found |"
 else
-    echo "| MySQL       |  missing |"
+  echo "| MySQL       |  missing |"
   DEPENDENCY_INSTALL_COMMAND+=" $MYSQL_PACKAGE"
 fi
 
 if hash node 2>/dev/null; then
   # TODO: Check node version
-    echo "| Node        |    found |"
+  echo "| Node        |    found |"
 else
-    echo "| Node        |  missing |"
+  echo "| Node        |  missing |"
   DEPENDENCY_INSTALL_COMMAND+=" $NODE_PACKAGE"
   INSTALL_NODE_PPA=1
 fi
 
 if hash pip 2>/dev/null; then
-    echo "| Python      |    found |"
+  echo "| Python      |    found |"
 else
-    echo "| Python      |  missing |"
+  echo "| Python      |  missing |"
   DEPENDENCY_INSTALL_COMMAND+=" $PYTHON_PACKAGE"
 fi
 
 if hash curl 2>/dev/null; then
-    echo "| cURL        |    found |"
+  echo "| cURL        |    found |"
 else
-    echo "| cURL        |  missing |"
-  INSTALL_CURL=1
+  echo "| cURL        |  missing |"
+  SCRIPT_DEPENDENCY_INSTALL_COMMAND+=" $CURL_PACKAGE"
+fi
+
+if hash unzip 2>/dev/null; then
+  echo "| unzip       |    found |"
+else
+  echo "| unzip       |  missing |"
+  SCRIPT_DEPENDENCY_INSTALL_COMMAND+=" $UNZIP_PACKAGE"
 fi
 
 echo "+-------------+----------+"
 
 # Install cURL
-if [[ "$INSTALL_NODE_PPA" == "1" ]]
+if [[ "$PACKAGE_MANAGER" != "$SCRIPT_DEPENDENCY_INSTALL_COMMAND"  ]]
 then
-  echo "This installation requires cURL."
-  if yes_or_no_critical "$PACKAGE_MANAGER $CURL_PACKAGE" "Aborted."
+  echo "This installation requires curl and unzip to work."
+  if yes_or_no_critical "$SCRIPT_DEPENDENCY_INSTALL_COMMAND" "Installation aborted."
   then
-      $PACKAGE_MANAGER $CURL_PACKAGE
+      $SCRIPT_DEPENDENCY_INSTALL_COMMAND
   fi
 fi
 
