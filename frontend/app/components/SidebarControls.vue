b8985b3bd66a697a49f17cc404262b26250ca411
ChristianCme <cmehil.warn@gmail.com>
2018-02-05 16:35:34 -0600

    Add default selection

--- frontend/app/components/SidebarControls.vue
+++ frontend/app/components/SidebarControls.vue
@@ -87,7 +87,7 @@
                   <option value="2015">2015</option>
                   <option value="2016">2016</option>
                   <option value="2017">2017</option>
-                  <option value="2018">2018</option>
+                  <option value="2018" selected="selected">2018</option>
                 </select>
                 <div class="desc">Year</div>
               </div>
