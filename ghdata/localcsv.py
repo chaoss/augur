53e7310c1d28bcc2ee83b866aad98c78b7f05c8e
Gourav Sardana <gsardana001@gmail.com>
2018-03-18 16:15:56 +0530

    Update localcsv.py

--- ghdata/localcsv.py
+++ ghdata/localcsv.py
@@ -7,4 +7,6 @@ class LocalCSV(object):
     def __init__(self):
         return
 
-    name_gender = pd.read_csv(get_data_path('name_gender.csv'), index_col=0)
+    name_gender = pd.read_csv(get_data_path('name_gender.csv'), index_col=0) #read the data from name_gender.csv
+    
+    
