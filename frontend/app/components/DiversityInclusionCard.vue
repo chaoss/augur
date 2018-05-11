536ffdf8170e769bfb9da4a21dd1171d5dcfa000
Derek Howard <derek@howderek.com>
2018-05-11 14:51:07 -0500

    Add cards

---
+++ frontend/app/components/DiversityInclusionCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <h1>Diversity and Inclusion</h1>
+    <h2>{{ $store.state.baseRepo }}</h2>
+      <div class="row">
+        <p>These metrics have not yet been developed.</p>
+      </div>
+    </div>
+    
+  </section>
+</template>
+
+<script>
+
+import LineChart from './charts/LineChart'
+import BubbleChart from './charts/BubbleChart'
+import StackedBarChart from './charts/StackedBarChart'
+
+module.exports = {
+  components: {
+    LineChart,
+    BubbleChart,
+    StackedBarChart
+  }
+}
+
+</script>
