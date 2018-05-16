3e1ca3dd9fe8517fd197d5912f0596d04b02b281
Keanu Nichols <nichols.keanu9@gmail.com>
2018-05-16 08:30:12 -0400

    Update ghtorrent.py

--- augur/ghtorrent.py
+++ augur/ghtorrent.py
@@ -150,6 +150,20 @@ class GHTorrent(object):
         commitsSQL = s.sql.text(self.__single_table_count_by_date('commits', group_by=group_by))
         return pd.read_sql(commitsSQL, self.db, params={"repoid": str(repoid)})
 
+    def commits1(self, owner, repo=None, group_by="week"):
+        """
+        Timeseries of all the commits on a repo
+
+        :param owner: The name of the project owner or the id of the project in the projects table of the project in the projects table. Use repoid() to get this.
+        :param repo: The name of the repo. Unneeded if repository id was passed as owner.
+        :return: DataFrame with commits/day
+        """
+        repoid = self.repoid(owner, repo)
+        commitsSQL = s.sql.text(self.__single_table_count_by_date('commits', group_by=group_by))
+        temp = pd.read_sql(commitsSQL, self.db, params={"repoid": str(repoid)})
+        tem = temp['commits'] > 100
+        return temp[tem].reset_index(drop=True)
+
     def commit_comments(self, owner, repo=None, group_by="week"):
         """
         Timeseries of commit comments
@@ -461,6 +475,10 @@ class GHTorrent(object):
             GROUP BY users.id
             ORDER BY commits DESC
         """)
+        print("HERE!!!")
+        temp = pd.read_sql(rawContributionsSQL, self.db, params={"repoid": str(repoid)})
+        print(temp['location'][0:5])
+        print("END!!!!")
         return pd.read_sql(rawContributionsSQL, self.db, params={"repoid": str(repoid)})
 
     def classify_contributors(self, owner, repo=None):
