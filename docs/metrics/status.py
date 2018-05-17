b78dd97fbb72acaa727d941ca2c57380eeec85d0
Derek Howard <derek@howderek.com>
2018-05-17 11:01:27 -0500

    Add git data source (WIP), issues open/closed notebook

--- docs/metrics/status.py
+++ docs/metrics/status.py
@@ -2,6 +2,8 @@ import os
 import re
 import json
 import glob
+import webbrowser
+from flask import Flask, request, Response
 
 metric_files = ['upstream/1_Diversity-Inclusion.md', 'upstream/2_Growth-Maturity-Decline.md', 'upstream/3_Risk.md', 'upstream/4_Value.md']
 
@@ -13,30 +15,36 @@ metric_type_by_file = {
 }
 
 color_by_status = {
-    'unimplemented': '\033[31mu',
-    'in_progress': '\033[33mp',
-    'implemented': '\033[32mi'
+    'unimplemented': '<span style="color: #C00">unimplemented</span>',
+    'in_progress': '<span style="color: #CC0">in progress</span>',
+    'implemented': '<span style="color: #0C0">implemented</span>'
 }
 
-
-print('+----- STATUS ------+')
-print('| \033[31mu\033[0m - unimplemented |')
-print('| \033[33mp\033[0m - in_progress   |')
-print('| \033[32mi\033[0m - implemented   |')
-print('+-------------------+\n\n')
 statusMap = json.loads(open('status.json', 'r').read())
+statusHTML = """
+<html>
+<head>
+    <title>Augur Metrics Status</title>
+    <style>
+        td { padding: 5px }
+    </style>
+</head>
+<body>
+    <h1>Augur Metrics Status</h1>
+    
+"""
 
 def getFileID(path):
     return os.path.splitext(os.path.basename(path))[0]
 
 def printMetric(title, path):
+    global statusHTML
     status = 'unimplemented'
     fileID = getFileID(path)
     if fileID in statusMap:
         status = statusMap[fileID]
     if status != 'printed':
-        print('{} {} ({})\033[0m'.format(color_by_status[status], title, fileID))
-        print('  https://github.com/chaoss/metrics/tree/master/{}\n'.format(path))
+        statusHTML += '<tr><td>{}</td><td><a href="https://github.com/chaoss/metrics/tree/master/{}"> {} ({})</td></tr>'.format(color_by_status[status], path, title, fileID)
     statusMap[fileID] = 'printed'
     return fileID
 
@@ -45,12 +53,33 @@ for filename in metric_files:
     file = open(filename, 'r')
     matches = re.findall(r'\[(.*?)\]\((.*?\.md)\)', file.read())
     if len(matches) > 0:
-        print('\033[4m' + metric_type_by_file[filename] + '\033[0m\n')
+        statusHTML +=  '<h2>' + metric_type_by_file[filename] + '</h2><table><tr><td>status</td><td>metric</td></tr>'
     for match in matches:
         printMetric(match[0], match[1])
+    statusHTML +=  '</table>'
     
 
 # Iterate through the files in activity-metrics to find uncategorized metrics
-print('\033[4mUncategorized\033[0m\n')
+statusHTML +=  '<h2>Uncategorized</h2><table><tr><td>status</td><td>metric</td></tr>'
 for filename in glob.iglob('upstream/activity-metrics/*.md'):
     printMetric(getFileID(filename).replace('-', ' ').title(), 'activity-metrics/' + getFileID(filename) + '.md')
+
+
+statusHTML += """
+    </table>
+</body>
+</html>
+"""
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return statusHTML
+
+def run():
+    webbrowser.open_new_tab('http://localhost:5001/')
+    app.run(port=5001)
+
+if __name__ == "__main__":
+     run()
