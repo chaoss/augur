169724a035c94a32fdfa752f0a23335c1ef027ed
Carter Landis <ccarterlandis@gmail.com>
2018-06-13 23:41:21 -0500

    updated existing API docs

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-06-06T23:55:47.067Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-06-14T04:18:12.737Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
