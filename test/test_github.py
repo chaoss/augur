6e895d3662a621e0e5c348ad9f185834fa90a43b
Carter Landis <ccarterlandis@gmail.com>
2018-06-17 15:28:05 -0500

    updated metric function tests

--- test/test_github.py
+++ test/test_github.py
@@ -6,7 +6,7 @@ import pandas
 def github():
     import augur
     augurApp = augur.Application()
-    return augurApp.github()
+    return augurApp.githubapi()
 
 """
 Pandas testing format
@@ -19,7 +19,6 @@ The tests check if a value is anywhere in the dataframe
 # *** DIVERSITY AND INCLUSION *** #
 
 # *** GROWTH, MATURITY, AND DECLINE *** #
-# lines changed
 
 # *** RISK *** #
 
@@ -28,18 +27,13 @@ The tests check if a value is anywhere in the dataframe
 # *** ACTIVITY *** #
 
 # *** EXPERIMENTAL *** #
-# def test_bus_factor(github):
+
+# def test_bus_factor(github): #METRIC FUNCTION IS BROKEN
 #     assert github.bus_factor("OSSHealth", "augur",start="1-1-17", end="5-12-17").isin(["9"]).any
 
-# def test_tags(github):
-#     assert github.tags("OSSHealth", "augur").isin(["v0.2"]).any
+def test_tags(github):
+    assert github.tags("rails", "rails").isin(["2008-04-10T17:25:06-07:00"]).any
 
-# major tags
-
-
-
-
-
-
-
+def test_major_tags(github):
+    assert github.major_tags("rails", "rails").isin(["2016-06-30T17:20:36-04:00"]).any
 
