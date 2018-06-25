c56ac2c94369cde9a25abd33c23bb0a06f69d0b3
Derek Howard <derek@howderek.com>
2018-06-25 18:37:26 -0500

    Add data_sources function

--- augur/__init__.py
+++ augur/__init__.py
@@ -6,12 +6,15 @@ from .util import logger
 
 # Classes
 from .application import Application
-# from .downloads import Downloads
-# from .ghtorrent import GHTorrent
-# from .ghtorrentplus import GHTorrentPlus
-# from .git import Git
-# from .githubapi import GitHubAPI
-# from .librariesio import LibrariesIO
-# from .localcsv import LocalCSV
-# from .publicwww import PublicWWW
-# from .server import Server
+
+def data_sources():
+    from .downloads import Downloads
+    from .ghtorrent import GHTorrent
+    from .ghtorrentplus import GHTorrentPlus
+    from .git import Git
+    from .githubapi import GitHubAPI
+    from .librariesio import LibrariesIO
+    from .localcsv import LocalCSV
+    from .publicwww import PublicWWW
+    from .server import Server
+    return [Downloads, GHTorrent, GHTorrentPlus, Git, GitHubAPI, LibrariesIO, LocalCSV, PublicWWW, Server]
