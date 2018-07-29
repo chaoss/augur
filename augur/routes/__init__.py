56d543b7ec5087158a795574fa380f80e9a58c39
Carter Landis <ccarterlandis@gmail.com>
2018-07-29 13:07:54 -0500

    renamed route files

--- augur/routes/__init__.py
+++ augur/routes/__init__.py
@@ -16,8 +16,6 @@ route_files = getRouteFiles()
 
 def create_all_routes(server):
     for route_file in route_files:
-        try:
-            module = importlib.import_module('.' + route_file, 'augur.routes')
-            module.create_routes(server)
-        except Exception as e:
-           print(e) 
+        module = importlib.import_module('.' + route_file, 'augur.routes')
+        module.create_routes(server)
+
