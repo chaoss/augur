f75df659acdfc1edcdd763f44a442b44fd54b3dd
Carter Landis <ccarterlandis@gmail.com>
2018-08-01 19:44:10 -0500

    added metric status table formatting and styling

--- frontend/app/components/MetricStatusCard.vue
+++ frontend/app/components/MetricStatusCard.vue
@@ -1,50 +1,63 @@
 <template>
-	  <ul>
-	      <li>
-		    <b>Name: {{ metric.name }}</b>
-		  </li>
-			  <ul>
-			      <li>
-				    Tag: {{ metric.tag }}
-				  </li>
-			      <li v-if="metric.url != '/'">
-				    URL: {{ metric.url }}
-				  </li>
-			      <li>
-		 			Group: {{ metric.group }}
-				  </li>
-			      <li v-if="metric.endpoint != 'n/a'">
-		 			Endpoint: {{ metric.endpoint }}
-				  </li>
-			      <li v-if="metric.source != 'n/a'">
-		 			Source: {{ metric.source }}
-				  </li>
-			      <li>
-		 			Backend Status: {{ metric.backend_status }}
-				  </li>
-			      <li>
-		 			Frontend Status: {{ metric.frontend_status }}
-				  </li>
-			      <li v-if="metric.metric_type != 'n/a'">
-		 			Metric Type: {{ metric.metric_type }}
-				  </li>
-			</ul>
-	</ul>
+	<div>
+		<tr>
+			<td>backend status</td>
+			<td>frontend status</td>
+		    <td>name</td>
+		    <td>endpoint</td>
+		    <td>source</td>
+			<td>metric type</td>
+		</tr>
+		<tr>
+			<td v-bind:style="{ color: backend_color }">{{ metric.backend_status }}</td>
+			<td v-bind:style="{ color: frontend_color }">{{ metric.frontend_status }}</td>
+			<td>{{ metric.name }}</td>
+			<td>{{ metric.endpoint }}</td>
+			<td>{{ metric.source }}</td>
+			<td>{{ metric.metric_type }}</td>
+		</tr>
+	</div>
 </template>
 
 <script>
-
 export default {
 
-  name: 'MetricsStatusCard',
-  props: [ 'metric' ],
+  name: "MetricStatusCard",
+
+  props: [ "metric" ],
+
   data () {
     return {
+    	backend_color: "#0c0",
+    	frontend_color: "#0c0"
     }
   },
+  methods: {
+  	getBackendStatus() {
+  		if (this.metric["backend_status"] == "unimplemented") {
+  			this.backend_color = "#c00"
+  		} 
+  		else if (this.metric["backend_status"] == "undefined") {
+  			this.backend_color = "#cc0"
+  		} 
+  		else if (this.metric["backend_status"] == "implemented") {
+  			this.backend_color = "0c0"
+  		}
+  	},
+  	getFrontendStatus() {
+  		if (this.metric["frontend_status"] == "unimplemented") {
+  			this.frontend_color = "#c00"
+  		}   
+  		else if (this.metric["frontend_status"] == "implemented") {
+  			this.frontend_color = "#0c0"
+  		}   
+  	}
+  },
+  mounted() {
+  	this.getBackendStatus()
+  	this.getFrontendStatus()
+  }
 }
-
-
 </script>
 
 <style lang="css" scoped>
