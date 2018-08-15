e22d558516f2515afa16c054a504ddd70f52a347
Derek Howard <derek@howderek.com>
2018-08-14 19:19:02 -0500

    Update docs

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-08-12T21:02:34.832Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-08-15T00:18:52.452Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
