2a7d239dd3c3073a7726e8fbec5d3c4a1e346c15
Carter Landis <ccarterlandis@gmail.com>
2018-12-21 16:22:08 -0600

    remove outdated docs

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1462,7 +1462,7 @@ if (module.hot) {(function () {  var hotAPI = require("vue-hot-reload-api")
   if (!module.hot.data) {
     hotAPI.createRecord("data-v-6becaf40", __vue__options__)
   } else {
-    hotAPI.rerender("data-v-6becaf40", __vue__options__)
+    hotAPI.reload("data-v-6becaf40", __vue__options__)
   }
 })()}
 });
