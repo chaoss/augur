06254025456de006c8b50d8d2d813f4c0bb1320f
Parth Sharma <parth261297@gmail.com>
2019-03-10 00:50:17 +0530

    Add route for 'githubapi.open_issues'

--- augur/datasources/githubapi/routes.py
+++ augur/datasources/githubapi/routes.py
@@ -111,6 +111,30 @@ def create_routes(server):
     """
     server.addMetric(github.lines_of_code_changed, 'lines_changed')
 
+    """
+    @api {get} /:owner/:repo/timeseries/githubapi/issues Issues Opened
+    @apiName githubapi-issues-opened
+    @apiGroup Growth-Maturity-Decline
+    @apiDescription <a href="https://github.com/chaoss/wg-gmd/blob/master/metrics/issues-open.md">CHAOSS Metric Definition</a>
+
+    @apiGroup Growth-Maturity-Decline
+    @apiParam {String} owner Username of the owner of the GitHub repository
+    @apiParam {String} repo Name of the GitHub repository
+
+    @apiSuccessExample {json} Success-Response:
+                        [
+                            {
+                                "created_at": "2019-02-28T00:00:00.000Z",
+                                "count": 4
+                            },
+                            {
+                                "created_at": "2019-03-01T00:00:00.000Z",
+                                "count": 1
+                            },
+                        ]
+    """
+    server.addTimeseries(github.open_issues, 'githubapi/issues')
+
     #####################################
     ###            RISK               ###
     #####################################
