546756f3637e1a05a0eb67a23edf20c71e135a5d
bing0ne <root@bing0ne.com>
2019-03-31 20:34:16 -0400

    add metric pull request closed in ghtorrent

--- augur/datasources/ghtorrent/routes.py
+++ augur/datasources/ghtorrent/routes.py
@@ -318,6 +318,29 @@ def create_routes(server):
     """
     server.addTimeseries(ghtorrent.pull_requests_open, 'pulls')
 
+    """
+    @api {get} /:owner/:repo/timeseries/pulls/closed Pull Requests Open
+    @apiName pull-requests-closed
+    @apiGroup Growth-Maturity-Decline
+    @apiDescription <a href="https://github.com/chaoss/metrics/blob/master/activity-metrics/pull-requests-closed.md">CHAOSS Metric Definition</a>
+    
+    @apiParam {String} owner Username of the owner of the GitHub repository
+    @apiParam {String} repo Name of the GitHub repository
+    
+    @apiSuccessExample {json} Success-Response:
+                        [
+                            {
+                                "date": "2013-01-09T00:00:00.000Z",
+                                "pull_requests": 3
+                            },
+                            {
+                                "date": "2016-01-14T00:00:00.000Z",
+                                "pull_requests": 1
+                            }
+                        ]
+    """
+    server.addTimeseries(ghtorrent.pull_requests_closed, 'pulls/closed')
+
     #####################################
     ###            RISK               ###
     #####################################
