8460ea0926261e83a72fbe03dfab0cf1917e0d12
Carter Landis <ccarterlandis@gmail.com>
2019-05-18 20:23:05 -0500

    Add broker ability to send worker to compatible workers
    The broker can now identify a request's "given" parameter, find
    workers that can accept this type of given parameter, and
    distribute the work to each accordingly.
    
    Signed-off-by: Carter Landis <ccarterlandis@gmail.com>

--- augur/broker_test/broker_test/broker.py
+++ augur/broker_test/broker_test/broker.py
@@ -1,18 +1,43 @@
+import logging
+import requests
+logging.basicConfig(filename='logs/server.log')
+logger = logging.getLogger(name="broker_logger")
 
-class Worker(object):
+class Worker():
     """docstring for Worker"""
     def __init__(self, ID, location, qualifications):
         self.ID = ID
         self.location = location
         self.qualifications = qualifications
-        print(self.ID, self.location, self.qualifications)
+        self.given = qualifications[0]['given'][0][0]
+        print("Worker given: " + self.given)
+
+class Job():
+    """docstring for Job"""
+    def __init__(self, job_type, models, given):
+        self.job_type = job_type
+        self.models = models
+        self.given = given
 
 class Broker(object):
     """docstring for Broker"""
     def __init__(self):
-        print("Broker is alive!")
         self.connected_workers = []
+        self.created_jobs = []
 
     def add_new_worker(self, worker):
-        self.connected_workers.append(Worker(worker['id'], worker['location'], worker['qualifications']))
-        print(self.connected_workers)
+        self.connected_workers.append(Worker(ID=worker['id'], location=worker['location'], qualifications=worker['qualifications']))
+
+    def create_job(self, job):
+        self.created_jobs.append(Job(job_type=job['job_type'], models=job['models'], given=job["given"]))
+
+        for job in self.created_jobs:
+            job_given = list(job.given.keys())[0]
+            compatible_workers = [worker for worker in self.connected_workers if worker.given == job_given]
+
+            for worker in compatible_workers:
+                task = {'task': job.given}
+                requests.post(worker.location + '/AUGWOP/task', json=task)
+
+
+
--- workers/augur_worker_github/augur_worker_github/runtime.py
+++ workers/augur_worker_github/augur_worker_github/runtime.py
@@ -1,19 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import click
 
 def create_server(app, gw):
+    tasks = []
     @app.route("/")
     def hello():
         return "Hello World!"
 
-    @app.route("/AUGWOP/task")
+    @app.route("/AUGWOP/task", methods=['POST', 'GET'])
     def augwop_task():
-        return jsonify({
-            "status": "IDLE",
-            "tasks": [{
-                "given": []
-            }]
-        })
+        if request.method == 'POST':
+            print(request.json)
+            tasks.append(request.json)
+            return jsonify({"success": "sucess"})
+        if request.method == 'GET':
+            return jsonify({
+                "status": "IDLE",
+                "tasks": [{
+                    "given": []
+                }],
+                "test": tasks
+            })
 
     @app.route("/AUGWOP/config")
     def augwop_config():
@@ -22,7 +29,7 @@ def create_server(app, gw):
 @click.command()
 @click.option('--augur-url', default='http://localhost:5000/', help='Augur URL')
 @click.option('--host', default='localhost', help='Host')
-@click.option('--port', default='51232', help='Port')
+@click.option('--port', default=51232, help='Port')
 def main(augur_url, host, port):
     app = Flask(__name__)
     create_server(app, None)
