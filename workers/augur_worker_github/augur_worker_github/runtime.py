cf05c6247a8782124e52f685bbffc2e96609745e
gabe-heim <gabe.heim@yahoo.com>
2019-05-19 12:46:01 -0500

    Added plenty of styling/comments
    Comments were added in places where there could be potential confusion or where elaboration could aid in understanding.

--- workers/augur_worker_github/augur_worker_github/runtime.py
+++ workers/augur_worker_github/augur_worker_github/runtime.py
@@ -3,6 +3,11 @@ import click
 from augur_worker_github.worker import GitHubWorker
 
 def create_server(app, gw):
+    """ Consists of AUGWOP endpoints for the broker to communicate to this worker
+    Can post a new task to be added to the workers queue
+    Can retrieve current status of the worker
+    Can retrieve the workers config object
+    """
     
     @app.route("/")
     def hello():
@@ -10,11 +15,13 @@ def create_server(app, gw):
 
     @app.route("/AUGWOP/task", methods=['POST', 'GET'])
     def augwop_task():
-        if request.method == 'POST':
+        """ AUGWOP endpoint that gets hit to add a task to the workers queue
+        """
+        if request.method == 'POST': #will post a task to be added to the queue
             print(request.json)
             app.gh_worker._queue.put(request.json)
             return jsonify({"success": "sucess"})
-        if request.method == 'GET':
+        if request.method == 'GET': #will retrieve the current tasks/status of the worker
             return jsonify({
                 "status": "IDLE",
                 "tasks": [{
@@ -25,16 +32,20 @@ def create_server(app, gw):
 
     @app.route("/AUGWOP/config")
     def augwop_config():
-        return "Hey!"
+        """ Retrieve worker's config
+        """
+        return app.gh_worker.config
 
 @click.command()
 @click.option('--augur-url', default='http://localhost:5000/', help='Augur URL')
 @click.option('--host', default='localhost', help='Host')
 @click.option('--port', default=51232, help='Port')
 def main(augur_url, host, port):
+    """ Declares singular worker and creates the server and flask app that it will be running on
+    """
     app = Flask(__name__)
 
-    config = {
+    config = { 
             'database_connection_string': 'psql://localhost:5432/augur',
             "key": "2759b561575060cce0d87c0f8d7f72f53fe35e14",
             "display_name": "GitHub API Key",
@@ -42,7 +53,8 @@ def main(augur_url, host, port):
             "required": 1,
             "type": "string"
         }
-    app.gh_worker = GitHubWorker(config)
+
+    app.gh_worker = GitHubWorker(config) # declares the worker that will be running on this server with specified config
     
     create_server(app, None)
     app.run(debug=app.debug, host=host, port=port)
--- workers/augur_worker_github/augur_worker_github/worker.py
+++ workers/augur_worker_github/augur_worker_github/worker.py
@@ -3,12 +3,19 @@ from urllib.parse import urlparse
 import requests
 
 class CollectorTask:
+    """ Worker's perception of a task in its queue
+    Holds a message type (EXIT, TASK, etc) so the worker knows how to process the queue entry
+    and the github_url given that it will be collecting data for
+    """
     def __init__(self, message_type='TASK', github_url=None):
         self.type = message_type
         self.github_url = github_url
 
 
 def collect(queue, config):
+    """ Function to process each entry in the worker's task queue
+    Determines what action to take based off the message type
+    """
     while True:
         try:
             message = queue.get()
@@ -25,6 +32,9 @@ def collect(queue, config):
             query(message.github_url)
 
 def query(git_url):
+    """ Data collection function
+    Query the github api for contributors and issues (not yet implemented)
+    """
     path = urlparse(url)
     split = path[2].split('/')
 
@@ -43,6 +53,12 @@ def query(git_url):
 
 
 class GitHubWorker:
+    """ Worker that collects data from the Github API and stores it in our database
+    task: current task being worked on
+    child: current process of the queue being ran
+    queue: queue of tasks to be fulfilled
+    config: holds info like api keys, descriptions, and database connection strings
+    """
     def __init__(self, config, task=None):
         self._task = task
         self._child = None
@@ -50,7 +66,6 @@ class GitHubWorker:
         self.config = config
         # self.update_config(self, config)
         self.API_KEY = self.config['key']
-        print("initing")
         
         specs = {
             "id": "com.augurlabs.core.github_worker",
@@ -64,11 +79,12 @@ class GitHubWorker:
             "config": [self.config]
         }
 
-
         requests.post('http://localhost:5000/api/workers', json=specs)
         
 
     def update_config(self, config):
+        """ Method to update config and set a default
+        """
         self.config = {
             'database_connection_string': 'psql://localhost:5432/augur',
             "key": "2759b561575060cce0d87c0f8d7f72f53fe35e14",
@@ -82,10 +98,15 @@ class GitHubWorker:
 
     @property
     def task(self):
+        """ Property that is returned when the worker's current task is referenced
+        """
         return self._task
     
     @task.setter
     def task(self, value):
+        """ Method to set or update the current task property's value
+        Adds this task to the queue, and calls method to process queue
+        """
         if not self._queue.empty(): 
             git_url = value[0]['given']['git_url']
             if 'github.com' in git_url:
@@ -96,8 +117,13 @@ class GitHubWorker:
             raise ValueError('Queue is already full')
 
     def cancel(self):
+        """ Delete/cancel current task
+        """
         self._task = None
 
     def run(self):
+        """ Kicks off the processing of the queue if it is not already being processed
+        Gets run whenever a new task is added
+        """
         if not self._child:
             self._child = Process(target=collect, args=((self._queue, self.config),))
