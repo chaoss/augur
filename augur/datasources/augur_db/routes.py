1f31694efa65efb7c9971a2b019c7eec7dce6e73
Parth Sharma <parth261297@gmail.com>
2019-06-04 18:58:21 +0530

    add route for 'code_changes' metric
    Signed-off-by: Parth Sharma <parth261297@gmail.com>

--- augur/datasources/augur_db/routes.py
+++ augur/datasources/augur_db/routes.py
@@ -5,7 +5,7 @@ Creates routes for the Augur database data source plugin
 
 from flask import request, Response
 
-def create_routes(server):  
+def create_routes(server):
 
     augur_db = server._augur['augur_db']()
 
@@ -24,3 +24,104 @@ def create_routes(server):
                         status=200,
                         mimetype="application/json")
     server.updateMetricMetadata(function=augur_db.downloaded_repos, endpoint='/{}/repos'.format(server.api_version), metric_type='git')
+
+    #####################################
+    ###           EVOLUTION           ###
+    #####################################
+
+    """
+    @api {get} /repo-groups/:repo_group_id/code-changes
+    @apiName Code Changes
+    @apiGroup Evolution
+    @apiDescription <a href="https://github.com/chaoss/wg-evolution/blob/master/metrics/Code_Changes.md">CHAOSS Metric Definition</a>
+
+    @apiParam {String} repo_group_id Repository Group ID
+    @apiParam {string} period Periodicity specification. Possible values: 'day', 'week', 'month', 'year'. Defaults to 'day'
+    @apiParam {string} begin_date Beginning date specification. Possible values: '2018', '2018-05', '2019-05-01', ..., ' 2017-03-02 05:34:19'. Defaults to '1970-1-1 0:0:0'
+    @apiParam {string} end_date Ending date specification. Possible values: '2018', '2018-05', '2019-05-01', ..., ' 2017-03-02 05:34:19'. Defaults to current date & time.
+
+    @apiSuccessExample {json} Success-Response:
+                    [
+                        {
+                            "commit_date": "2018-01-01T00:00:00.000Z",
+                            "repo_id": 1,
+                            "commit_count": 5140
+                        },
+                        {
+                            "commit_date": "2019-01-01T00:00:00.000Z",
+                            "repo_id": 1,
+                            "commit_count": 711
+                        },
+                        {
+                            "commit_date": "2015-01-01T00:00:00.000Z",
+                            "repo_id": 25001,
+                            "commit_count": 1071
+                        }
+                    ]
+    """
+    server.addRepoGroupMetric(augur_db.code_changes, 'code-changes')
+
+    """
+    @api {get} /repo-groups/:repo_group_id/repos/:repo_id/code-changes
+    @apiName Code Changes
+    @apiGroup Evolution
+    @apiDescription <a href="https://github.com/chaoss/wg-evolution/blob/master/metrics/Code_Changes.md">CHAOSS Metric Definition</a>
+
+    @apiParam {String} repo_group_id Repository Group ID.
+    @apiParma {String} repo_id Repository ID.
+    @apiParam {string} period Periodicity specification. Possible values: 'day', 'week', 'month', 'year'. Defaults to 'day'
+    @apiParam {string} begin_date Beginning date specification. Possible values: '2018', '2018-05', '2019-05-01', ..., ' 2017-03-02 05:34:19'. Defaults to '1970-1-1 0:0:0'
+    @apiParam {string} end_date Ending date specification. Possible values: '2018', '2018-05', '2019-05-01', ..., ' 2017-03-02 05:34:19'. Defaults to current date & time.
+
+    @apiSuccessExample {json} Success-Response:
+                    [
+                        {
+                            "commit_date": "2018-01-01T00:00:00.000Z",
+                            "commit_count": 2287
+                        },
+                        {
+                            "commit_date": "2018-02-01T00:00:00.000Z",
+                            "commit_count": 1939
+                        },
+                        {
+                            "commit_date": "2018-03-01T00:00:00.000Z",
+                            "commit_count": 1979
+                        },
+                        {
+                            "commit_date": "2018-04-01T00:00:00.000Z",
+                            "commit_count": 2159
+                        }
+                    ]
+    """
+    server.addRepoMetric(augur_db.code_changes, 'code-changes')
+
+    # @server.app.route('/{}/repo-groups/<repo_group_id>/code-changes'.format(server.api_version))
+    # def code_changes_repo_group_route(repo_group_id):
+    #     period = request.args.get('period', 'day')
+    #     begin_date = request.args.get('begin_date')
+    #     end_date = request.args.get('end_date')
+
+    #     kwargs = {'repo_group_id': repo_group_id, 'period': period,
+    #               'begin_date': begin_date, 'end_date': end_date}
+
+    #     data = server.transform(augur_db.code_changes,
+    #                             args=[],
+    #                             kwargs=kwargs)
+
+    #     return Response(response=data, status=200, mimetype='application/json')
+
+    # @server.app.route('/{}/repo-groups/<repo_group_id>/repo/<repo_id>/code-changes'.format(server.api_version))
+    # def code_changes_repo_route(repo_group_id, repo_id):
+    #     period = request.args.get('period', 'day')
+    #     begin_date = request.args.get('begin_date')
+    #     end_date = request.args.get('end_date')
+
+    #     kwargs = {'repo_group_id': repo_group_id, 'repo_id': repo_id,
+    #               'period': period, 'begin_date': begin_date,
+    #               'end_date': end_date}
+
+    #     data = server.transform(augur_db.code_changes,
+    #                             args=[],
+    #                             kwargs=kwargs)
+
+    #     return Response(response=data, status=200, mimetype='application/json')
