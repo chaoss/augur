39ff51eb7d3852e2678c5afef2d5ac9b219e4b7c
Parth Sharma <parth261297@gmail.com>
2019-06-11 21:34:34 +0530

    add unit test for code_changes
    Signed-off-by: Parth Sharma <parth261297@gmail.com>

--- augur/datasources/augur_db/test_augur_db.py
+++ augur/datasources/augur_db/test_augur_db.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import pandas as pd
 
 @pytest.fixture(scope="module")
 def augur_db():
@@ -34,3 +35,16 @@ def test_issues_first_time_opened(augur_db):
     # repo_gorup_id
     assert augur_db.issues_first_time_opened(1, period='day').isin([
         "2019-05-23 00:00:00+00:00"]).any
+
+def test_code_changes(augur_db):
+    #repo_id
+    assert augur_db.code_changes(23, 21350, period='year').isin([pd.Timestamp('2009-01-01T00:00:00+00:00'), 2]).any().any()
+
+    # repo_group_id
+    assert augur_db.code_changes(23, period='year').isin([pd.Timestamp('2009-01-01T00:00:00+00:00'), 21350, 2]).any().any()
+
+    #begin_date & end_date
+    assert augur_db.code_changes(23, 21350, period='month', begin_date='2009',
+                                 end_date='2011-05').isin([pd.Timestamp('2009-02-01T00:00:00+00:00'), 2]).any().any()
+    assert augur_db.code_changes(23, period='month', begin_date='2009',
+                                 end_date='2011-05').isin([pd.Timestamp('2011-02-01T00:00:00+00:00'), 21420, 4]).any().any()
