bdc2c7b801d01cde7aad39cf79a96f770e5306e4
Parth Sharma <parth261297@gmail.com>
2019-06-11 21:35:11 +0530

    add unit test for code_changes_lines
    Signed-off-by: Parth Sharma <parth261297@gmail.com>

--- augur/datasources/augur_db/test_augur_db.py
+++ augur/datasources/augur_db/test_augur_db.py
@@ -48,3 +48,16 @@ def test_code_changes(augur_db):
                                  end_date='2011-05').isin([pd.Timestamp('2009-02-01T00:00:00+00:00'), 2]).any().any()
     assert augur_db.code_changes(23, period='month', begin_date='2009',
                                  end_date='2011-05').isin([pd.Timestamp('2011-02-01T00:00:00+00:00'), 21420, 4]).any().any()
+
+def test_code_changes_lines(augur_db):
+    #repo_id
+    assert augur_db.code_changes_lines(22, 21331, period='year').isin([pd.Timestamp('2016-01-01T00:00:00+00:00'), 27190, 3163]).any().any()
+
+    #repo_group_id
+    assert augur_db.code_changes_lines(23, period='year').isin([pd.Timestamp('2016-01-01T00:00:00+00:00'), 21420, 31, 3]).any().any()
+
+    #begin_date & end_date
+    assert augur_db.code_changes_lines(22, 21331, period='month', begin_date='2016',
+                                       end_date='2016-05').isin([pd.Timestamp('2016-02-01T00:00:00+00:00'), 196, 108]).any().any()
+    assert augur_db.code_changes_lines(22, period='month', begin_date='2016-05',
+                                       end_date='2016-08-15').isin([pd.Timestamp('2016-06-01T00:00:00+00:00'), 21331, 70, 20]).any().any()
