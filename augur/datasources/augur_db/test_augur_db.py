07f7a23254764184ccb69b6c5eba609c1fb2284e
bing0n3 <root@bing0ne.com>
2019-06-11 15:35:20 +0800

    add unit test for metric 'sub_project'
    Signed-off-by: bing0n3 <root@bing0ne.com>

--- augur/datasources/augur_db/test_augur_db.py
+++ augur/datasources/augur_db/test_augur_db.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import pandas as pd
 
 @pytest.fixture(scope="module")
 def augur_db():
@@ -34,3 +35,20 @@ def test_issues_first_time_opened(augur_db):
     # repo_gorup_id
     assert augur_db.issues_first_time_opened(1, period='day').isin([
         "2019-05-23 00:00:00+00:00"]).any
+
+
+def test_sub_projects(augur_db):
+
+    # repo group
+    assert augur_db.sub_projects(24).iloc[0]['sub_protject_count'] > 50
+
+    # repo id
+    assert augur_db.sub_projects(
+        24, repo_id=21477).iloc[0]['sub_protject_count'] > 50
+
+    # test begin_date and end_date
+    assert augur_db.sub_projects(24, begin_date='2019-6-1 00:00:01',
+                                 end_date='2019-06-10 23:59:59').iloc[0]['sub_protject_count'] < 5
+
+    assert augur_db.sub_projects(24, repo_id=21441, begin_date='2019-6-1 00:00:01',
+                                 end_date='2019-06-10 23:59:59').iloc[0]['sub_protject_count'] < 5
