614a42f83f322a47635dc3b8e4027ad84da3becc
Carter Landis <ccarterlandis@gmail.com>
2019-06-24 17:16:47 -0500

    Fix broken make api-docs command
    There was a blank @apiDescription tag in the pull_requests_merged route
    under the GitHub API datasource. This blank tag was causing the documentation
    generation process to error out, so placeholder text was added until the proper
    documentation link was located.
    
    Signed-off-by: Carter Landis <ccarterlandis@gmail.com>

---
+++ frontend/public/api_docs/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-06-24T22:16:25.282Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
