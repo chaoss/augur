213af2336d1c3748ee633d7cf5ea0537064e2092
Bingwen Ma <mbwpony@gmail.com>
2019-06-30 13:25:24 +0800

    refactor metric 'contributors'
    Signed-off-by: Bingwen Ma <mbwpony@gmail.com>

--- augur/datasources/augur_db/test_augur_db_routes.py
+++ augur/datasources/augur_db/test_augur_db_routes.py
@@ -158,7 +158,7 @@ def test_sub_projects_by_repo(augur_db_routes):
     assert data[0]["sub_protject_count"] > 0
 
 def test_contributors_by_group(augur_db_routes):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/24/contributors')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/20/contributors')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -166,7 +166,7 @@ def test_contributors_by_group(augur_db_routes):
 
 
 def test_contributors_by_repo(augur_db_routes):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/24/repos/21477/contributors')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/20/repos/21000/contributors')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
