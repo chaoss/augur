b96157c22135cb329f296792c4de6f6c99da5f0f
gabe-heim <gabe.heim@yahoo.com>
2019-06-30 17:15:45 -0500

    New http broker and housekeeper with manager
    Our new design for the broker to live on a mp manager and its processes to live within http routes is implemented and working, housekeeper is fully functional with http communication. GH worker has been slightly altered to work with these processes

--- augur/routes/broker.py
+++ augur/routes/broker.py
@@ -4,19 +4,59 @@ Creates routes for the broker
 """
 from flask import request, jsonify, Response
 import logging
+import json
+import requests
 
 def create_broker_routes(server):
 
-    @server.app.route('/{}/job'.format(server.api_version), methods=['POST'])
-    def job():
+    @server.app.route('/{}/task'.format(server.api_version), methods=['POST'])
+    def task():
         """ AUGWOP route that is hit when data needs to be added to the database
-        Retrieves a json consisting of job specifications that the broker will use to assign a worker
+        Retrieves a json consisting of task specifications that the broker will use to assign a worker
         """
-        job = request.json
-        logging.info("Recieved a new job to distribute for model: " + job['models'][0])
-        # logging.info(job['given'])
-        # server.broker.create_job(job)
-        return jsonify({"status": "success", "job": job})
+        task = request.json
+
+        given = list(task['given'].keys())[0]
+        model = task['models'][0]
+        if task['job_type'] != 'MAINTAIN':
+            logging.info("Broker recieved a new user task ... checking for compatible workers for given: " + str(given) + " and model(s): " + str(model) + "")
+
+        worker_found = False
+        for worker in list(server.broker._getvalue().keys()):
+            models = server.broker[worker]['models']
+            givens = server.broker[worker]['given']
+            user_queue = server.broker[worker]['user_queue']
+            maintain_queue = server.broker[worker]['maintain_queue']
+
+            if model in models and given in givens:
+                if task['job_type'] == "UPDATE":
+                    server.broker[worker]['user_queue'].append(task)
+                    logging.info("New length of worker {}'s user queue: {}".format(worker, str(len(server.broker[worker]['user_queue']))))
+                elif task['job_type'] == "MAINTAIN":
+                    server.broker[worker]['maintain_queue'].append(task)
+                    # logging.info("New length of worker {}'s maintain queue: {}".format(worker, str(len(server.broker[worker]['maintain_queue']))))
+
+                if server.broker[worker]['status'] == 'Idle':
+                    if len(user_queue) > 0:
+                        new_task = user_queue.pop(0)
+                        logging.info("Worker {} is idle, preparing to send the {} task to {}".format(worker, new_task['given']['git_url'], str(server.broker[worker]['location'])))
+                        requests.post(server.broker[worker]['location'] + '/AUGWOP/task', json=new_task)
+                        server.broker[worker]['status'] = 'Working'
+                    elif len(maintain_queue) > 0:
+                        new_task = maintain_queue.pop(0)
+                        # logging.info("Worker {} is idle, preparing to send the {} task to {}".format(worker, new_task['given']['git_url'], str(server.broker[worker]['location'])))
+                        requests.post(server.broker[worker]['location'] + '/AUGWOP/task', json=new_task)
+                        server.broker[worker]['status'] = 'Working'
+                    else:
+                        logging.info("Both queues are empty for worker {} and it is idle".format(worker))
+                worker_found = True
+        # Otherwise, let the frontend know that the request can't be served
+        if not worker_found:
+            logging.info(f"Augur does not have knowledge of any workers that are capable of handing the request: " + str(task))
+
+        return Response(response=task,
+                        status=200,
+                        mimetype="application/json")
 
     @server.app.route('/{}/workers'.format(server.api_version), methods=['POST'])
     def worker():
@@ -25,21 +65,65 @@ def create_broker_routes(server):
         """
         worker = request.json
         logging.info("Recieved HELLO message from worker: " + worker['id'])
-        # server.broker.add_new_worker(worker, server.broker.connected_workers)
+        server.broker[worker['id']] = server.manager.dict()
+        server.broker[worker['id']]['user_queue'] = server.manager.list()
+        server.broker[worker['id']]['maintain_queue'] = server.manager.list()
+        server.broker[worker['id']]['given'] = server.manager.list()
+        server.broker[worker['id']]['models'] = server.manager.list()
+        server.broker[worker['id']]['given'].append(worker['qualifications'][0]['given'][0][0])
+        server.broker[worker['id']]['models'].append(worker['qualifications'][0]['models'][0])
+        server.broker[worker['id']]['status'] = 'Idle'
+        server.broker[worker['id']]['location'] = worker['location']
+
         return Response(response=worker['id'],
                         status=200,
                         mimetype="application/json")
 
     @server.app.route('/{}/completed_task'.format(server.api_version), methods=['POST'])
     def sync_queue():
-        #job = request.json
-        # logging.info("Message recieved that worker " + job['worker_id'] + " completed task: " + str(job))
-        # server.broker.completed_job(job)
-        return jsonify({"status": "success"})#, "job": job})
+        task = request.json
+        worker = task['worker_id']
+        logging.info("Message recieved that worker " + worker + " completed task: " + str(task))
+        models = server.broker[worker]['models']
+        givens = server.broker[worker]['given']
+        user_queue = server.broker[worker]['user_queue']
+        maintain_queue = server.broker[worker]['maintain_queue']
 
-    @server.app.route('/{}/status/<worker>'.format(server.api_version), methods=['POST'])
-    def get_status(worker):
-        # status = server.broker.get_status(worker)
+        try:
+            new_task = user_queue.pop(0)
+            logging.info("Worker {}'s user queue is not empty, preparing to send it the next 'user' task: {}".format(worker, new_task['given']['git_url']))
+            logging.info("Remaining length of user queue: {}".format(str(len(user_queue))))
+            requests.post(server.broker[worker]['location'] + '/AUGWOP/task', json=new_task)
+        except:
+            try:
+                new_task = maintain_queue.pop(0)
+                logging.info("Worker {}'s user queue is empty but the maintain queue is not, preparing to send it the next 'maintain' task: {}".format(worker, new_task['given']['git_url']))
+                logging.info("Remaining length of maintain queue: {}".format(str(len(maintain_queue))))
+                requests.post(server.broker[worker]['location'] + '/AUGWOP/task', json=new_task)
+            except:
+                logging.info("Both queues are empty for worker {}".format(worker))
+                server.broker[worker]['status'] = 'Idle'
+
+        return Response(response=task,
+                        status=200,
+                        mimetype="application/json")
+
+    @server.app.route('/{}/workers/status'.format(server.api_version), methods=['GET'])
+    def get_status():
+        status = {}
+        for worker in list(server.broker._getvalue().keys()):
+            status[worker] = worker
+            status[worker]['user_queue'] = server.broker[worker]['user_queue']._getvalue()
+            status[worker]['maintain_queue'] = server.broker[worker]['maintain_queue']._getvalue()
+
         return Response(response=status,
+                        status=200,
+                        mimetype="application/json")
+
+    @server.app.route('/{}/workers/remove'.format(server.api_version), methods=['POST'])
+    def remove_worker():
+        worker = request.json
+        del server.broker[worker['id']]
+        return Response(response=worker,
                         status=200,
                         mimetype="application/json")
