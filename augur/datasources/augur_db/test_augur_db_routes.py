d64607c135e16de05ce0aa5fce152c0b65c9ff64
Bingwen Ma <mbwpony@gmail.com>
2019-06-30 15:15:38 +0800

    update test routes
    Signed-off-by: Bingwen Ma <mbwpony@gmail.com>

--- augur/datasources/augur_db/test_augur_db_routes.py
+++ augur/datasources/augur_db/test_augur_db_routes.py
@@ -192,14 +192,14 @@ def test_issues_open_age_by_group(augur_db_routes):
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
-    assert data[0]["datedifference"] > 1
+    assert data[0]["open_date"] > 1
 
 def test_issues_open_age_by_repo(augur_db_routes):
     response = requests.get('http://localhost:5000/api/unstable/repo-groups/20/repos/21000/issues-open-age/')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
-    assert data[0]["datedifference"] > 1
+    assert data[0]["open_date"] > 1
 
 def test_issues_closed_resolution_duration_by_group(augur_db_routes):
     response = requests.get('http://localhost:5000/api/unstable/repo-groups/24/issues-closed-resolution-duration/')
