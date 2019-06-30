e7641870f9286bc82119d98f6557277ea17243e8
Bingwen Ma <mbwpony@gmail.com>
2019-06-30 13:25:24 +0800

    refactor metric 'contributors'
    Signed-off-by: Bingwen Ma <mbwpony@gmail.com>

--- augur/datasources/augur_db/test_augur_db.py
+++ augur/datasources/augur_db/test_augur_db.py
@@ -119,17 +119,17 @@ def test_pull_requests_merge_contributor_new(augur_db):
 
 def test_contributors(augur_db):
     # repo group
-    assert augur_db.contributors(24).iloc[0]['total'] > 5
+    assert augur_db.contributors(20).iloc[0]['total'] > 5
 
     # repo id
     assert augur_db.contributors(
-        24, repo_id=21524).iloc[0]['total'] > 5
+        24, repo_id=21000).iloc[0]['total'] > 5
 
     # test begin_date and end_date
-    assert augur_db.contributors(24, begin_date='2019-6-1 00:00:01',
+    assert augur_db.contributors(20, begin_date='2019-6-1 00:00:01',
                                  end_date='2019-06-10 23:59:59').iloc[0]['total'] < 5
 
-    assert augur_db.contributors(24, repo_id=21524, begin_date='2019-6-1 00:00:01',
+    assert augur_db.contributors(20, repo_id=21000, begin_date='2019-6-1 00:00:01',
                                  end_date='2019-06-10 23:59:59').iloc[0]['total'] < 5
 
 
