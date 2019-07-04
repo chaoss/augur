c1436d0038c084d3bd3f8334e8e148da43576537
Parth Sharma <parth261297@gmail.com>
2019-07-04 19:03:38 +0530

    add 'fork_count' metric implementation
    Signed-off-by: Parth Sharma <parth261297@gmail.com>

--- augur/datasources/augur_db/augur_db.py
+++ augur/datasources/augur_db/augur_db.py
@@ -1242,6 +1242,41 @@ class Augur(object):
             results = pd.read_sql(forks_SQL, self.db, params={'repo_id': repo_id})
             return results
 
+    @annotate(tag='fork-count')
+    def fork_count(self, repo_group_id, repo_id=None):
+        """
+        Returns the latest fork count
+
+        :param repo_group_id: The repository's repo_group_id
+        :param repo_id: The repository's repo_id, defaults to None
+        :return: Fork count
+        """
+        if not repo_id:
+            fork_count_SQL = s.sql.text("""
+                SELECT a.repo_id, repo_name, a.fork_count AS forks
+                FROM repo_info a LEFT JOIN repo_info b
+                ON (a.repo_id = b.repo_id AND a.repo_info_id < b.repo_info_id), repo
+                WHERE b.repo_info_id IS NULL
+                AND a.repo_id = repo.repo_id
+                AND a.repo_id IN
+                    (SELECT repo_id FROM repo
+                     WHERE  repo_group_id = :repo_group_id)
+            """)
+
+            results = pd.read_sql(fork_count_SQL, self.db, params={'repo_group_id': repo_group_id})
+            return results
+        else:
+            fork_count_SQL = s.sql.text("""
+                SELECT repo_name, fork_count AS forks
+                FROM repo_info JOIN repo ON repo_info.repo_id = repo.repo_id
+                WHERE repo_info.repo_id = :repo_id
+                ORDER BY repo_info.data_collection_date DESC
+                LIMIT 1
+            """)
+
+            results = pd.read_sql(fork_count_SQL, self.db, params={'repo_id': repo_id})
+            return results
+
     @annotate(tag='languages')
     def languages(self, repo_group_id, repo_id=None):
         """Returns the implementation languages
