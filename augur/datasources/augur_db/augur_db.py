f69ef3b00faf0758abca8a1eb6d2ce4e7f31b79e
Bingwen Ma <mbwpony@gmail.com>
2019-07-05 21:02:27 +0800

    add metric annual-commit-count-ranked-by-repo-in-repo-group
    Signed-off-by: Bingwen Ma <mbwpony@gmail.com>

--- augur/datasources/augur_db/augur_db.py
+++ augur/datasources/augur_db/augur_db.py
@@ -1478,7 +1478,101 @@ class Augur(object):
         "repo_id": repo_id, "calendar_year": calendar_year})
         return results
 
-    
+    @annotate(tag='annual-commit-count-ranked-by-repo-in-repo-group')
+    def annual_commit_count_ranked_by_repo_in_repo_group(self, repo_group_id, repo_id=None, timeframe=None):
+        """
+        For each repository in a collection of repositories being managed, each REPO's total commits during the current Month, 
+        Year or Week. Result ranked from highest number of commits to lowest by default. 
+        :param repo_group_id: The repository's repo_group_id
+        :param repo_id: The repository's repo_id, defaults to None
+        :param calendar_year: the calendar year a repo is created in to be considered "new"
+        """    
+        if timeframe == None:
+            timeframe = 'all'
+        
+        cdRgTpRankedCommitsSQL = None
+
+        if repo_id:
+            if timeframe == 'all':
+                cdRgTpRankedCommitsSQL = s.sql.text("""
+                    SELECT repo.repo_id, repo_name as name, SUM(added - removed - whitespace) as net, patches
+                    FROM dm_repo_annual, repo, repo_groups
+                    WHERE repo.repo_group_id = (select repo.repo_group_id from repo where repo.repo_id = :repo_id)
+                    AND repo.repo_group_id = repo_groups.repo_group_id
+                    AND dm_repo_annual.repo_id = repo.repo_id
+                    group by repo.repo_id, patches
+                    order by net desc
+                    LIMIT 10
+                """)
+            elif timeframe == 'year':
+                cdRgTpRankedCommitsSQL = s.sql.text("""
+                    SELECT repo.repo_id, repo_name as name, SUM(added - removed - whitespace) as net, patches
+                    FROM dm_repo_annual, repo, repo_groups
+                    WHERE repo.repo_group_id = (select repo.repo_group_id from repo where repo.repo_id = :repo_id)
+                    AND repo.repo_group_id = repo_groups.repo_group_id
+                    AND dm_repo_annual.repo_id = repo.repo_id
+                    AND date_part('year', repo_added) = date_part('year', CURRENT_DATE)
+                    group by repo.repo_id, patches
+                    order by net desc
+                    LIMIT 10
+                """)
+            elif timeframe == 'month':
+                cdRgTpRankedCommitsSQL = s.sql.text("""
+                    SELECT repo.repo_id, repo_name as name, SUM(added - removed - whitespace) as net, patches
+                    FROM dm_repo_monthly, repo, repo_groups
+                    WHERE repo.repo_group_id = (select repo.repo_group_id from repo where repo.repo_id = :repo_id)
+                    AND repo.repo_group_id = repo_groups.repo_group_id
+                    AND dm_repo_monthly.repo_id = repo.repo_id
+                    AND date_part('year', repo_added) = date_part('year', CURRENT_DATE)
+                    AND date_part('month', repo_added) = date_part('month', CURRENT_DATE)
+                    group by repo.repo_id, patches
+                    order by net desc
+                    LIMIT 10
+                """)
+        else: 
+            if timeframe == 'all':
+                cdRgTpRankedCommitsSQL = s.sql.text("""
+                    SELECT repo.repo_id, repo_name as name, SUM(added - removed - whitespace) as net, patches
+                    FROM dm_repo_annual, repo, repo_groups
+                    WHERE repo.repo_group_id = :repo_group_id
+                    AND repo.repo_group_id = repo_groups.repo_group_id
+                    AND dm_repo_annual.repo_id = repo.repo_id
+                    group by repo.repo_id, patches
+                    order by net desc
+                    LIMIT 10
+                """)
+            elif timeframe == "year":
+                cdRgTpRankedCommitsSQL = s.sql.text(
+                    """
+                    SELECT repo.repo_id, repo_name as name, SUM(added - removed - whitespace) as net, patches
+                    FROM dm_repo_annual, repo, repo_groups
+                    WHERE repo.repo_group_id = :repo_group_id
+                    AND repo.repo_group_id = repo_groups.repo_group_id
+                    AND dm_repo_annual.repo_id = repo.repo_id
+                    AND date_part('year', repo_added) = date_part('year', CURRENT_DATE)
+                    group by repo.repo_id, patches
+                    order by net desc
+                    LIMIT 10
+                    """
+                )
+            elif timeframe == 'month':
+                cdRgTpRankedCommitsSQL = s.sql.text("""
+                    SELECT repo.repo_id, repo_name as name, SUM(added - removed - whitespace) as net, patches
+                    FROM dm_repo_annual, repo, repo_groups
+                    WHERE repo.repo_group_id = :repo_group_id
+                    AND repo.repo_group_id = repo_groups.repo_group_id
+                    AND dm_repo_annual.repo_id = repo.repo_id
+                    AND date_part('year', repo_added) = date_part('year', CURRENT_DATE)
+                    AND date_part('month', repo_added) = date_part('month', CURRENT_DATE)
+                    group by repo.repo_id, patches
+                    order by net desc
+                    LIMIT 10
+                """)
+
+
+        results = pd.read_sql(cdRgTpRankedCommitsSQL, self.db, params={ "repo_group_id": repo_group_id,
+        "repo_id": repo_id})
+        return results
 
 
 #####################################
