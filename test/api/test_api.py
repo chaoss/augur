38abefee3a41d43a48a38533321d4dadd07940eb
Carter Landis <ccarterlandis@gmail.com>
2019-07-13 17:54:26 -0500

    Update Travis build config
    Signed-off-by: Carter Landis <ccarterlandis@gmail.com>

--- test/api/test_api.py
+++ test/api/test_api.py
@@ -7,9 +7,9 @@ SOURCE = "**"
 if len(sys.argv) == 2:
     SOURCE = str(sys.argv[1])
 
-subprocess.Popen(['make', 'backend-restart'])
+start = subprocess.Popen(['make', 'backend'])
 time.sleep(5)
-process = subprocess.run([f'pytest augur/datasources/{SOURCE}/test_{SOURCE}_routes.py'], shell=True)
+process = subprocess.run("pytest augur/datasources/{}/test_{}_routes.py".format(SOURCE, SOURCE), shell=True)
 time.sleep(2)
 subprocess.Popen(['make', 'backend-stop'])
 
