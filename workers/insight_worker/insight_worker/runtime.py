6081476f9e293a1c5a5d1e0af2384454cc464044
Paul Orton <ortonpaul18@gmail.com>
2019-07-16 13:54:03 -0500

    Add insight creation

--- workers/insight_worker/insight_worker/runtime.py
+++ workers/insight_worker/insight_worker/runtime.py
@@ -3,6 +3,7 @@ import click
 from insight_worker.worker import InsightWorker
 import os
 import json
+import logging
 
 def create_server(app, gw):
     """ Consists of AUGWOP endpoints for the broker to communicate to this worker
@@ -16,10 +17,12 @@ def create_server(app, gw):
         """ AUGWOP endpoint that gets hit to add a task to the workers queue or is used to get the heartbeat/status of worker
         """
         if request.method == 'POST': #will post a task to be added to the queue
-            print(request.json)
+            logging.info("Sending to work on task: {}".format(str(request.json)))
             app.gh_worker.task = request.json
+            
             #set task
             return jsonify({"success": "sucess"})
+
         if request.method == 'GET': #will retrieve the current tasks/status of the worker
             return jsonify({
                 "status": gh_worker._queue if condition else condition_if_false,
@@ -44,9 +47,11 @@ def main(augur_url, host, port):
     app = Flask(__name__)
 
     #load credentials
-    credentials = read_config("Database")
+    credentials = read_config("Database", use_main_config=1)
+    server = read_config("Server", use_main_config=1)
 
     config = { 
+            "broker_port": server["port"],
             "host": credentials["host"],
             "password": credentials["password"],
             "port": credentials["port"],
@@ -70,54 +75,58 @@ def main(augur_url, host, port):
     
 
 
-def read_config(section, name=None, environment_variable=None, default=None, config_file='augur.config.json', no_config_file=0):
-        """
-        Read a variable in specified section of the config file, unless provided an environment variable
+def read_config(section, name=None, environment_variable=None, default=None, config_file='augur.config.json', no_config_file=0, use_main_config=0):
+    """
+    Read a variable in specified section of the config file, unless provided an environment variable
 
-        :param section: location of given variable
-        :param name: name of variable
-        """
+    :param section: location of given variable
+    :param name: name of variable
+    """
 
-        __config_bad = False
+
+    __config_bad = False
+    if use_main_config == 0:
         __config_file_path = os.path.abspath(os.getenv('AUGUR_CONFIG_FILE', config_file))
-        __config_location = os.path.dirname(__config_file_path)
-        __export_env = os.getenv('AUGUR_ENV_EXPORT', '0') == '1'
-        __default_config = { 'Database': {"host": "nekocase.augurlabs.io"} }
+    else:        
+        __config_file_path = os.path.abspath(os.path.dirname(os.path.dirname(os.getcwd())) + '/augur.config.json')
 
-        if os.getenv('AUGUR_ENV_ONLY', '0') != '1' and no_config_file == 0:
-            try:
-                __config_file = open(__config_file_path, 'r+')
-            except:
-                # logger.info('Couldn\'t open {}, attempting to create. If you have a augur.cfg, you can convert it to a json file using "make to-json"'.format(config_file))
-                if not os.path.exists(__config_location):
-                    os.makedirs(__config_location)
-                __config_file = open(__config_file_path, 'w+')
-                __config_bad = True
+    __config_location = os.path.dirname(__config_file_path)
+    __export_env = os.getenv('AUGUR_ENV_EXPORT', '0') == '1'
+    __default_config = { 'Database': {"host": "nekocase.augurlabs.io"} }
+
+    if os.getenv('AUGUR_ENV_ONLY', '0') != '1' and no_config_file == 0:
+        try:
+            __config_file = open(__config_file_path, 'r+')
+        except:
+            # logger.info('Couldn\'t open {}, attempting to create. If you have a augur.cfg, you can convert it to a json file using "make to-json"'.format(config_file))
+            if not os.path.exists(__config_location):
+                os.makedirs(__config_location)
+            __config_file = open(__config_file_path, 'w+')
+            __config_bad = True
 
 
-            # Options to export the loaded configuration as environment variables for Docker
-           
-            if __export_env:
-                
-                export_filename = os.getenv('AUGUR_ENV_EXPORT_FILE', 'augur.cfg.sh')
-                __export_file = open(export_filename, 'w+')
-                # logger.info('Exporting {} to environment variable export statements in {}'.format(config_file, export_filename))
-                __export_file.write('#!/bin/bash\n')
+        # Options to export the loaded configuration as environment variables for Docker
+       
+        if __export_env:
+            
+            export_filename = os.getenv('AUGUR_ENV_EXPORT_FILE', 'augur.cfg.sh')
+            __export_file = open(export_filename, 'w+')
+            # logger.info('Exporting {} to environment variable export statements in {}'.format(config_file, export_filename))
+            __export_file.write('#!/bin/bash\n')
 
-            # Load the config file and return [section][name]
-            try:
-                config_text = __config_file.read()
-                __config = json.loads(config_text)
-                if name is not None:
-                    return(__config[section][name])
-                else:
-                    return(__config[section])
-
-            except json.decoder.JSONDecodeError as e:
-                if not __config_bad:
-                    __using_config_file = False
-                    # logger.error('%s could not be parsed, using defaults. Fix that file, or delete it and run this again to regenerate it. Error: %s', __config_file_path, str(e))
-
-                __config = __default_config
+        # Load the config file and return [section][name]
+        try:
+            config_text = __config_file.read()
+            __config = json.loads(config_text)
+            if name is not None:
                 return(__config[section][name])
+            else:
+                return(__config[section])
 
+        except json.decoder.JSONDecodeError as e:
+            if not __config_bad:
+                __using_config_file = False
+                # logger.error('%s could not be parsed, using defaults. Fix that file, or delete it and run this again to regenerate it. Error: %s', __config_file_path, str(e))
+
+            __config = __default_config
+            return(__config[section][name])
