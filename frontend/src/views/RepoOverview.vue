9aa72d68197ba748f486c18c3c8f0ccd7cea26eb
Bingwen Ma <mbwpony@gmail.com>
2019-07-24 13:18:09 +0800

    translate RepoOverview component
    Signed-off-by: Bingwen Ma <mbwpony@gmail.com>

--- frontend/src/views/RepoOverview.vue
+++ frontend/src/views/RepoOverview.vue
@@ -184,21 +184,23 @@
   </d-container>
 </template>
 
-<script>
+<script lang="ts">
+import  { Component, Vue } from 'vue-property-decorator';
+import {mapActions, mapGetters} from "vuex";
 import SparkChart from '../components/charts/SparkChart.vue';
 import InsightChart from '../components/charts/InsightChart.vue';
-import TickChart from '../components/charts/TickChart'
-import LinesOfCodeChart from '../components/charts/LinesOfCodeChart'
-import NormalizedStackedBarChart from '../components/charts/NormalizedStackedBarChart'
-import OneDimensionalStackedBarChart from '../components/charts/OneDimensionalStackedBarChart'
-import HorizontalBarChart from '../components/charts/HorizontalBarChart'
-import GroupedBarChart from '../components/charts/GroupedBarChart'
-import StackedBarChart from '../components/charts/StackedBarChart'
-import DynamicLineChart from '../components/charts/DynamicLineChart'
-import DualLineChart from '../components/charts/DualLineChart'
-import Spinner from '../components/Spinner'
+import TickChart from '../components/charts/TickChart.vue'
+import LinesOfCodeChart from '../components/charts/LinesOfCodeChart.vue'
+import NormalizedStackedBarChart from '../components/charts/NormalizedStackedBarChart.vue'
+import OneDimensionalStackedBarChart from '../components/charts/OneDimensionalStackedBarChart.vue'
+import HorizontalBarChart from '../components/charts/HorizontalBarChart.vue'
+import GroupedBarChart from '../components/charts/GroupedBarChart.vue'
+import StackedBarChart from '../components/charts/StackedBarChart.vue'
+import DynamicLineChart from '../components/charts/DynamicLineChart.vue'
+import DualLineChart from '../components/charts/DualLineChart.vue'
+import Spinner from '../components/Spinner.vue'
 
-export default {
+@Component({
   components: {
     SparkChart,
     InsightChart,
@@ -213,42 +215,46 @@ export default {
     DualLineChart,
     Spinner
   },
-  computed: {
-    repo () {
-      return this.$store.state.baseRepo
-    },
-    gitRepo () {
-      return this.$store.state.gitRepo
-    },
-    values () {
-      console.log("getting values")
-      let values = {}
-      let repo = window.AugurAPI.Repo({ gitURL: this.gitRepo })
-      repo.issuesClosed().then((data) => {
-        values['issuesClosed'] = data
-        this.loaded_overview = true
-      })
-      return values
-    }
-  },
-  data() {
-    return {
-      colors: ["#343A40", "#24a2b7", "#159dfb", "#FF3647", "#4736FF","#3cb44b","#ffe119","#f58231","#911eb4","#42d4f4","#f032e6"],
-      testEndpoints: ['closedIssues', 'openIssues', 'codeCommits'],
-      testTimeframes: ['past 1 month', 'past 3 months', 'past 2 weeks'],
-      repos: {},
-      projects: [],
-      themes: ['dark', 'info', 'royal-blue', 'warning'],
-      project: null,
-      loaded_overview: false,
-      loaded_evolution: false,
-      loaded_issues: false,
-      loaded_experimental: false,
-      loaded_activity: false
-    };
-  },
   methods: {
-    getOwner(url) {
+    ...mapActions('common',[
+      'endpoint', // map `this.endpoint({...})` to `this.$store.dispatch('endpoint', {...})`
+                  // uses: this.endpoint({endpoints: [], repos (optional): [], repoGroups (optional): []})
+      'getRepoRelations',
+    ])
+  },
+  computed: {
+    ...mapGetters('common',[
+      'repoRelationsInfo',
+      'groupsInfo',
+      'gitRepo',
+      'repo',
+    ])
+  },
+})
+
+export default class RepoOverview extends Vue{
+  colors =  ["#343A40", "#24a2b7", "#159dfb", "#FF3647", "#4736FF","#3cb44b","#ffe119","#f58231","#911eb4","#42d4f4","#f032e6"]
+  testEndpoints = ['closedIssues', 'openIssues', 'codeCommits']
+  testTimeframes = ['past 1 month', 'past 3 months', 'past 2 weeks']
+  repos = {}
+  projects = []
+  themes= ['dark', 'info', 'royal-blue', 'warning']
+  project= null
+  loaded_overview= false
+  loaded_evolution= false
+  loaded_issues= false
+  loaded_experimental= false
+  loaded_activity= false
+
+  // deflare vuex action, getter, mutations
+  repoRelationsInfo!: any;
+  groupsInfo!:any;
+  getRepoRelations!: any;
+  getRepoGroups!:any;
+  repo_groups!:any[];
+  sorted_repo_groups!:any[];
+
+  getOwner(url) {
       let first = url.indexOf(".")
       let last = url.lastIndexOf(".")
       let domain = null
@@ -273,124 +279,183 @@ export default {
         owner = null //url.substring(url.indexOf('/') + 1, url.lastIndexOf('/'))
         repo = url.slice(url.lastIndexOf('/') + 1)
         return domain
-      }
-    },
-    getRepo(url){
-      let first = url.indexOf(".")
-      let last = url.lastIndexOf(".")
-      let domain = null
-      let owner = null
-      let repo = null
-      let extension = false
-
-      if (first == last){ //normal github
-        domain = url.substring(0, first)
-        owner = url.substring(url.indexOf('/') + 1, url.lastIndexOf('/'))
-        repo = url.slice(url.lastIndexOf('/') + 1)
-        return repo
-      } else if (url.slice(last) == '.git'){ //github with extension
-        domain = url.substring(0, first)
-        extension = true
-        owner = url.substring(url.indexOf('/') + 1, url.lastIndexOf('/'))
-        repo = url.substring(url.lastIndexOf('/') + 1, url.length - 4)
-        return repo
-      } else { //gluster
-        domain = url.substring(first + 1, last)
-        owner = null //url.substring(url.indexOf('/') + 1, url.lastIndexOf('/'))
-        repo = url.slice(url.lastIndexOf('/') + 1)
-        return repo
-      }
-    },
-    getColor (idx) {
-      if (idx % 2 == 0)
-        return 'color: green'
-      else
-        return 'color: red'
-    },
-    getDirection (idx) {
-      if (idx % 2 == 0)
-        return 'arrow_upward'
-      else
-        return 'arrow_downward'
-    },
-    getPhrase (idx) {
-      if (idx % 2 == 0)
-        return 'increased'
-      else
-        return 'declined'
-    },
-    onRepo (e) {
-      this.$store.commit('setRepo', {
-        githubURL: e.target.value
-      })
-    },
-    onGitRepo (e) {
-      let first = e.url.indexOf(".")
-      let last = e.url.lastIndexOf(".")
-      let domain = null
-      let owner = null
-      let repo = null
-      let extension = false
-
-      if (first == last){ //normal github
-        domain = e.url.substring(0, first)
-        owner = e.url.substring(e.url.indexOf('/') + 1, e.url.lastIndexOf('/'))
-        repo = e.url.slice(e.url.lastIndexOf('/') + 1)
-      } else if (e.url.slice(last) == '.git'){ //github with extension
-        domain = e.url.substring(0, first)
-        extension = true
-        owner = e.url.substring(e.url.indexOf('/') + 1, e.url.lastIndexOf('/'))
-        repo = e.url.substring(e.url.lastIndexOf('/') + 1, e.url.length - 4)
-      } else { //gluster
-        domain = e.url.substring(first + 1, last)
-        owner = null //e.url.substring(e.url.indexOf('/') + 1, e.url.lastIndexOf('/'))
-        repo = e.url.slice(e.url.lastIndexOf('/') + 1)
-      }
-      this.$store.commit('setRepo', {
-        gitURL: e.url
-      })
-
-      this.$store.commit('setTab', {
-        tab: 'git'
-      })
-
-      this.$router.push({
-        name: 'git',
-        params: {repo: e.url}
-      })
-    },
-    getDownloadedRepos() {
-      this.downloadedRepos = []
-      window.AugurAPI.getDownloadedGitRepos().then((data) => {
-        $(this.$el).find('.spinner').removeClass('loader')
-        $(this.$el).find('.spinner').removeClass('relative')
-        this.repos = window._.groupBy(data, 'project_name')
-        this.projects = Object.keys(this.repos)
-        let impRepos = []
-        for (let i = 0; i < this.projects.length; i++) {
-          impRepos.push(this.repos[this.projects[i]][0])
-        }
-        console.log("LOADED")
-        this.loaded = true
-        // window.AugurAPI.batchMapped(impRepos, ['codeCommits']).then((data) => {
-        //   console.log("DATA", data)
-        // }, () => {
-        //   //this.renderError()
-        // }) // end batch request
-      })
-    },
-    btoa(s) {
-      return window.btoa(s)
     }
-  },
-  created() {
-    // this.getDownloadedRepos()
-    let repo = window.AugurAPI.Repo({ gitURL: this.gitRepo })
-    this.project = repo.rg_name
-    // repo.facadeProject().then((data) => {
-      // this.loaded=true
-    // })
-  },
+  }
+    
+  getRepo(url){
+    let first = url.indexOf(".")
+    let last = url.lastIndexOf(".")
+    let domain = null
+    let owner = null
+    let repo = null
+    let extension = false
+
+    if (first == last){ //normal github
+      domain = url.substring(0, first)
+      owner = url.substring(url.indexOf('/') + 1, url.lastIndexOf('/'))
+      repo = url.slice(url.lastIndexOf('/') + 1)
+      return repo
+    } else if (url.slice(last) == '.git'){ //github with extension
+      domain = url.substring(0, first)
+      extension = true
+      owner = url.substring(url.indexOf('/') + 1, url.lastIndexOf('/'))
+      repo = url.substring(url.lastIndexOf('/') + 1, url.length - 4)
+      return repo
+    } else { //gluster
+      domain = url.substring(first + 1, last)
+      owner = null //url.substring(url.indexOf('/') + 1, url.lastIndexOf('/'))
+      repo = url.slice(url.lastIndexOf('/') + 1)
+      return repo
+    }
+  }
+
+  getColor (idx) {
+    if (idx % 2 == 0)
+      return 'color: green'
+    else
+      return 'color: red'
+  }
+  
+  getDirection (idx) {
+    if (idx % 2 == 0)
+      return 'arrow_upward'
+    else
+      return 'arrow_downward'
+  }
+
+  getPhrase (idx) {
+    if (idx % 2 == 0)
+      return 'increased'
+    else
+      return 'declined'
+  }
+
+  onRepo (e) {
+    this.$store.commit('setRepo', {
+      githubURL: e.target.value
+    })
+  }
+
+  onGitRepo (e) {
+    let first = e.url.indexOf(".")
+    let last = e.url.lastIndexOf(".")
+    let domain = null
+    let owner = null
+    let repo = null
+    let extension = false
+
+    if (first == last){ //normal github
+      domain = e.url.substring(0, first)
+      owner = e.url.substring(e.url.indexOf('/') + 1, e.url.lastIndexOf('/'))
+      repo = e.url.slice(e.url.lastIndexOf('/') + 1)
+    } else if (e.url.slice(last) == '.git'){ //github with extension
+      domain = e.url.substring(0, first)
+      extension = true
+      owner = e.url.substring(e.url.indexOf('/') + 1, e.url.lastIndexOf('/'))
+      repo = e.url.substring(e.url.lastIndexOf('/') + 1, e.url.length - 4)
+    } else { //gluster
+      domain = e.url.substring(first + 1, last)
+      owner = null //e.url.substring(e.url.indexOf('/') + 1, e.url.lastIndexOf('/'))
+      repo = e.url.slice(e.url.lastIndexOf('/') + 1)
+    }
+    this.$store.commit('setRepo', {
+      gitURL: e.url
+    })
+
+    this.$store.commit('setTab', {
+      tab: 'git'
+    })
+
+    this.$router.push({
+      name: 'git',
+      params: {repo: e.url}
+    })
+  }
 }
+
+// export default {
+//   components: {
+//     SparkChart,
+//     InsightChart,
+//     TickChart,
+//     LinesOfCodeChart,
+//     NormalizedStackedBarChart,
+//     OneDimensionalStackedBarChart,
+//     HorizontalBarChart,
+//     GroupedBarChart,
+//     DynamicLineChart,
+//     StackedBarChart,
+//     DualLineChart,
+//     Spinner
+//   },
+//   computed: {
+//     repo () {
+//       return this.$store.state.baseRepo
+//     },
+//     gitRepo () {
+//       return this.$store.state.gitRepo
+//     },
+//     values () {
+//       console.log("getting values")
+//       let values = {}
+//       let repo = window.AugurAPI.Repo({ gitURL: this.gitRepo })
+//       repo.issuesClosed().then((data) => {
+//         values['issuesClosed'] = data
+//         this.loaded_overview = true
+//       })
+//       return values
+//     }
+//   },
+//   data() {
+//     return {
+//       colors: ["#343A40", "#24a2b7", "#159dfb", "#FF3647", "#4736FF","#3cb44b","#ffe119","#f58231","#911eb4","#42d4f4","#f032e6"],
+//       testEndpoints: ['closedIssues', 'openIssues', 'codeCommits'],
+//       testTimeframes: ['past 1 month', 'past 3 months', 'past 2 weeks'],
+//       repos: {},
+//       projects: [],
+//       themes: ['dark', 'info', 'royal-blue', 'warning'],
+//       project: null,
+//       loaded_overview: false,
+//       loaded_evolution: false,
+//       loaded_issues: false,
+//       loaded_experimental: false,
+//       loaded_activity: false
+//     };
+//   },
+//   methods: {
+//     
+//     getDownloadedRepos() {
+//       this.downloadedRepos = []
+//       window.AugurAPI.getDownloadedGitRepos().then((data) => {
+//         $(this.$el).find('.spinner').removeClass('loader')
+//         $(this.$el).find('.spinner').removeClass('relative')
+//         this.repos = window._.groupBy(data, 'project_name')
+//         this.projects = Object.keys(this.repos)
+//         let impRepos = []
+//         for (let i = 0; i < this.projects.length; i++) {
+//           impRepos.push(this.repos[this.projects[i]][0])
+//         }
+//         console.log("LOADED")
+//         this.loaded = true
+//         // window.AugurAPI.batchMapped(impRepos, ['codeCommits']).then((data) => {
+//         //   console.log("DATA", data)
+//         // }, () => {
+//         //   //this.renderError()
+//         // }) // end batch request
+//       })
+//     },
+//     btoa(s) {
+//       return window.btoa(s)
+//     }
+//   },
+//   created() {
+//     // this.getDownloadedRepos()
+//     let repo = window.AugurAPI.Repo({ gitURL: this.gitRepo })
+//     this.project = repo.rg_name
+//     // repo.facadeProject().then((data) => {
+//       // this.loaded=true
+//     // })
+//   },
+// }
 </script>
 
