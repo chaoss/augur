3d7e403dc9c54fa4bf879bbcee94f96e629fa653
Bingwen Ma <mbwpony@gmail.com>
2019-08-06 20:34:04 +0800

    add test license releated metric
    Signed-off-by: Bingwen Ma <mbwpony@gmail.com>

--- augur/datasources/augur_db/test_augur_db_routes.py
+++ augur/datasources/augur_db/test_augur_db_routes.py
@@ -365,21 +365,16 @@ def test_languages_by_repo(augur_db_routes):
     pass
 
 def test_license_declared_by_group(augur_db_routes):
-    # TODO need more daa
-    pass
-    # response = requests.get('http://localhost:5000/api/unstable/repo-groups/21/license-declared')
-    # data = response.json()
-    # assert response.status_code == 200
-    # assert len(data) >= 1
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/21/license-declared')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) >= 1
 
 def test_license_declared_by_repo(augur_db_routes):
-    # TODO need more daa
-    pass
-    # response = requests.get('http://localhost:5000/api/unstable/repo-groups/21/repos/21252/license-declared')
-    # data = response.json()
-    # assert response.status_code == 200
-    # assert len(data) >= 1
-    # assert data[0]["license"] == 'Apache-2.0'
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/21/repos/21116/license-declared')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) >= 1
 
 def test_annual_commit_count_ranked_by_new_repo_in_repo_group_by_repo(augur_db_routes):
     response = requests.get('http://localhost:5000/api/unstable/repo-groups/20/repos/21000/annual-commit-count-ranked-by-new-repo-in-repo-group')
@@ -477,3 +472,27 @@ def test_aggregate_summary_by_group(augur_db_routes):
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1    
+
+def test_license_coverage_by_group(augur_db_routes):
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/21/license-coverage')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) >= 1
+
+def test_license_coverage_by_repo(augur_db_routes):
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/21/repos/21116/license-coverage')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) >= 1
+
+def test_license_count_by_group(augur_db_routes):
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/21/license-count')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) >= 1
+
+def test_license_count_by_repo(augur_db_routes):
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/21/repos/21116/license-count')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) >= 1
