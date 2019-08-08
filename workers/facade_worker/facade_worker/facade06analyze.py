0cac5f3b4cd93851e092d2892e6ff3bfb371b646
gabe-heim <gabe.heim@yahoo.com>
2019-08-08 11:37:36 -0500

    indentation fix

--- workers/facade_worker/facade_worker/facade06analyze.py
+++ workers/facade_worker/facade_worker/facade06analyze.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# SPDX-License-Identifier:	Apache-2.0
+# SPDX-License-Identifier:  Apache-2.0
 
 # Git repo maintenance
 #
@@ -40,10 +40,10 @@ from facade_worker.facade02utilitymethods import update_repo_log, trim_commit, s
 from facade_worker.facade03analyzecommit import analyze_commit
 
 # if platform.python_implementation() == 'PyPy':
-# 	import pymysql
+#   import pymysql
 # else:
-# 	import MySQLdb
-	
+#   import MySQLdb
+    
 def analysis(cfg, multithreaded):
 
 # Run the analysis by looping over all active repos. For each repo, we retrieve
@@ -57,130 +57,133 @@ def analysis(cfg, multithreaded):
 
 ### Local helper functions ###
 
-	def update_analysis_log(repos_id,status):
+    def update_analysis_log(repos_id,status):
 
-	# Log a repo's analysis status
+    # Log a repo's analysis status
 
-		log_message = ("INSERT INTO analysis_log (repos_id,status) "
-			"VALUES (%s,%s)")
+        log_message = ("INSERT INTO analysis_log (repos_id,status) "
+            "VALUES (%s,%s)")
 
-		cfg.cursor.execute(log_message, (repos_id,status))
-		cfg.db.commit()
+            try:
+                cfg.cursor.execute(log_message, (repos_id,status))
+                cfg.db.commit()
+            except:
+                pass
 
 ### The real function starts here ###
 
-	cfg.update_status('Running analysis')
-	cfg.log_activity('Info','Beginning analysis')
+    cfg.update_status('Running analysis')
+    cfg.log_activity('Info','Beginning analysis')
 
-	start_date = cfg.get_setting('start_date')
+    start_date = cfg.get_setting('start_date')
 
-	repo_list = "SELECT repo_id,repo_group_id,repo_path,repo_name FROM repo WHERE repo_status='Analyze'"
-	cfg.cursor.execute(repo_list)
-	repos = list(cfg.cursor)
+    repo_list = "SELECT repo_id,repo_group_id,repo_path,repo_name FROM repo WHERE repo_status='Analyze'"
+    cfg.cursor.execute(repo_list)
+    repos = list(cfg.cursor)
 
 
-	for repo in repos:
-		update_analysis_log(repo[0],'Beginning analysis')
-		cfg.log_activity('Verbose','Analyzing repo: %s (%s)' % (repo[0],repo[3]))
+    for repo in repos:
+        update_analysis_log(repo[0],'Beginning analysis')
+        cfg.log_activity('Verbose','Analyzing repo: %s (%s)' % (repo[0],repo[3]))
 
-		# First we check to see if the previous analysis didn't complete
+        # First we check to see if the previous analysis didn't complete
 
-		get_status = ("SELECT working_commit FROM working_commits WHERE repos_id=%s")
+        get_status = ("SELECT working_commit FROM working_commits WHERE repos_id=%s")
 
-		cfg.cursor.execute(get_status, (repo[0], ))
-		working_commits = list(cfg.cursor)
-		#cfg.cursor.fetchone()[1]
+        cfg.cursor.execute(get_status, (repo[0], ))
+        working_commits = list(cfg.cursor)
+        #cfg.cursor.fetchone()[1]
 
-		# If there's a commit still there, the previous run was interrupted and
-		# the commit data may be incomplete. It should be trimmed, just in case.
-		for commit in working_commits:
-			trim_commit(cfg, repo[0],commit[0])
+        # If there's a commit still there, the previous run was interrupted and
+        # the commit data may be incomplete. It should be trimmed, just in case.
+        for commit in working_commits:
+            trim_commit(cfg, repo[0],commit[0])
 
-			# Remove the working commit.
-			remove_commit = ("DELETE FROM working_commits "
-				"WHERE repos_id = %s AND working_commit = %s")
-			cfg.cursor.execute(remove_commit, (repo[0],commit[0]))
-			cfg.db.commit()
+            # Remove the working commit.
+            remove_commit = ("DELETE FROM working_commits "
+                "WHERE repos_id = %s AND working_commit = %s")
+            cfg.cursor.execute(remove_commit, (repo[0],commit[0]))
+            cfg.db.commit()
 
-			cfg.log_activity('Debug','Removed working commit: %s' % commit[0])
+            cfg.log_activity('Debug','Removed working commit: %s' % commit[0])
 
-		# Start the main analysis
+        # Start the main analysis
 
-		update_analysis_log(repo[0],'Collecting data')
+        update_analysis_log(repo[0],'Collecting data')
 
-		repo_loc = ('%s%s/%s%s/.git' % (cfg.repo_base_directory,
-			repo[1], repo[2],
-			repo[3]))
-		# Grab the parents of HEAD
+        repo_loc = ('%s%s/%s%s/.git' % (cfg.repo_base_directory,
+            repo[1], repo[2],
+            repo[3]))
+        # Grab the parents of HEAD
 
-		parents = subprocess.Popen(["git --git-dir %s log --ignore-missing "
-			"--pretty=format:'%%H' --since=%s" % (repo_loc,start_date)],
-			stdout=subprocess.PIPE, shell=True)
+        parents = subprocess.Popen(["git --git-dir %s log --ignore-missing "
+            "--pretty=format:'%%H' --since=%s" % (repo_loc,start_date)],
+            stdout=subprocess.PIPE, shell=True)
 
-		parent_commits = set(parents.stdout.read().decode("utf-8",errors="ignore").split(os.linesep))
+        parent_commits = set(parents.stdout.read().decode("utf-8",errors="ignore").split(os.linesep))
 
-		# If there are no commits in the range, we still get a blank entry in
-		# the set. Remove it, as it messes with the calculations
+        # If there are no commits in the range, we still get a blank entry in
+        # the set. Remove it, as it messes with the calculations
 
-		if '' in parent_commits:
-			parent_commits.remove('')
+        if '' in parent_commits:
+            parent_commits.remove('')
 
-		# Grab the existing commits from the database
+        # Grab the existing commits from the database
 
-		existing_commits = set()
+        existing_commits = set()
 
-		find_existing = ("SELECT DISTINCT cmt_commit_hash FROM commits WHERE repo_id=%s")
+        find_existing = ("SELECT DISTINCT cmt_commit_hash FROM commits WHERE repo_id=%s")
 
-		cfg.cursor.execute(find_existing, (repo[0], ))
+        cfg.cursor.execute(find_existing, (repo[0], ))
 
-		for commit in list(cfg.cursor):
-			existing_commits.add(commit[0])
+        for commit in list(cfg.cursor):
+            existing_commits.add(commit[0])
 
-		# Find missing commits and add them
+        # Find missing commits and add them
 
-		missing_commits = parent_commits - existing_commits
+        missing_commits = parent_commits - existing_commits
 
-		cfg.log_activity('Debug','Commits missing from repo %s: %s' %
-			(repo[0],len(missing_commits)))
+        cfg.log_activity('Debug','Commits missing from repo %s: %s' %
+            (repo[0],len(missing_commits)))
 
-		if multithreaded:
+        if multithreaded:
 
-			from multiprocessing import Pool
+            from multiprocessing import Pool
 
-			pool = Pool()
+            pool = Pool()
 
-			for commit in missing_commits:
+            for commit in missing_commits:
 
-				result = pool.apply_async(analyze_commit(cfg, repo[0], repo_loc, commit, multithreaded))
+                result = pool.apply_async(analyze_commit(cfg, repo[0], repo_loc, commit, multithreaded))
 
-			pool.close()
-			pool.join()
+            pool.close()
+            pool.join()
 
-		else:
-			for commit in missing_commits:
-				analyze_commit(cfg, repo[0], repo_loc, commit, multithreaded)
+        else:
+            for commit in missing_commits:
+                analyze_commit(cfg, repo[0], repo_loc, commit, multithreaded)
 
-		update_analysis_log(repo[0],'Data collection complete')
+        update_analysis_log(repo[0],'Data collection complete')
 
-		update_analysis_log(repo[0],'Beginning to trim commits')
+        update_analysis_log(repo[0],'Beginning to trim commits')
 
-		# Find commits which are out of the analysis range
+        # Find commits which are out of the analysis range
 
-		trimmed_commits = existing_commits - parent_commits
+        trimmed_commits = existing_commits - parent_commits
 
-		cfg.log_activity('Debug','Commits to be trimmed from repo %s: %s' %
-			(repo[0],len(trimmed_commits)))
+        cfg.log_activity('Debug','Commits to be trimmed from repo %s: %s' %
+            (repo[0],len(trimmed_commits)))
 
-		for commit in trimmed_commits:
+        for commit in trimmed_commits:
 
-			trim_commit(cfg, repo[0],commit)
+            trim_commit(cfg, repo[0],commit)
 
-		set_complete = "UPDATE repo SET repo_status='Complete' WHERE repo_id=%s and repo_status != 'Empty'"
+        set_complete = "UPDATE repo SET repo_status='Complete' WHERE repo_id=%s and repo_status != 'Empty'"
 
-		cfg.cursor.execute(set_complete, (repo[0], ))
+        cfg.cursor.execute(set_complete, (repo[0], ))
 
-		update_analysis_log(repo[0],'Commit trimming complete')
+        update_analysis_log(repo[0],'Commit trimming complete')
 
-		update_analysis_log(repo[0],'Complete')
+        update_analysis_log(repo[0],'Complete')
 
-	cfg.log_activity('Info','Running analysis (complete)')
+    cfg.log_activity('Info','Running analysis (complete)')
