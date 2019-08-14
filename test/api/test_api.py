2597cdcb13961a4abe3fab481818562078bb7ac9
Carter Landis <ccarterlandis@gmail.com>
2019-08-14 17:09:59 -0500

    Refactor API testing script to be consistent with Travis build
    Signed-off-by: Carter Landis <ccarterlandis@gmail.com>

--- test/api/test_api.py
+++ test/api/test_api.py
@@ -1,5 +1,6 @@
 import time
 import subprocess
+import os
 import pytest
 import sys
 
@@ -7,10 +8,12 @@ SOURCE = "**"
 if len(sys.argv) == 2:
     SOURCE = str(sys.argv[1])
 
-start = subprocess.Popen(['make', 'backend'])
-time.sleep(5)
+FNULL = open(os.devnull, 'w')
+
+start = subprocess.Popen(['augur', 'run'], stdout=FNULL, stderr=subprocess.STDOUT)
+time.sleep(20)
 process = subprocess.run("pytest augur/datasources/{}/test_{}_routes.py".format(SOURCE, SOURCE), shell=True)
-time.sleep(2)
+time.sleep(5)
 subprocess.Popen(['make', 'backend-stop'])
 
 sys.exit(process.returncode)
