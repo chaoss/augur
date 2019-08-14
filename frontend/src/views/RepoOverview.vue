b58ff9c53e37d0fa7de93b0b7fe6ff559b5e04a9
Paul Orton <ortonpaul18@gmail.com>
2019-08-14 11:32:54 -0500

    Add charts placeholders for endpoint data

--- frontend/src/views/RepoOverview.vue
+++ frontend/src/views/RepoOverview.vue
@@ -43,9 +43,25 @@
             field="loc"> -->
             <!-- :data="values['annualLinesOfCodeCountRankedByRepoInRepoGroup']"> -->
           <!-- </grouped-bar-chart>
+        </div> -->
+
+      <div class="row">
+        <div class="col col-6">
+          <grouped-bar-chart source="cdRgNewrepRankedCommits"
+            title="Top New Repos in 2018 by Commits with Baseline Averages - Sorted"
+            field="commit">
+          </grouped-bar-chart>
         </div>
 
-    </div> -->
+        <div class="col col=6">
+          <grouped-bar-chart source="cdRgNewrepRankedLoc"
+            title="Top New Repos in 2018 by Net LoC with Baseline Averages - Sorted"
+            field="loc">
+          </grouped-bar-chart>
+        </div>
+      </div>
+
+    </div>
  
     <!-- Evolution section -->
     <div class="page-header row no-gutters py-4">
@@ -77,6 +93,89 @@
         <!-- </dynamic-line-chart>
       </div>
  -->
+      <div class="row">
+        <div class="col col-6">
+          <dynamic-line-chart source=""
+            title="Code Commits / Week"
+            cite-url="https://github.com/augurlabs/wg-gmd/blob/master/activity-metrics/commits.md"
+            cite-text="Commits">
+            <!-- :data="values[]"> -->
+          </dynamic-line-chart>
+        </div>
+
+        <div class="col col-6">
+          <dynamic-line-chart source="codeReviewIteration"
+            title="Number of Code Review Iterations"
+            cite-url="https://github.com/chaoss/metrics/blob/master/activity-metrics/code-review-iteration.md"
+            cite-text="Code Review Iterations">
+          </dynamic-line-chart>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-6">
+          <dynamic-line-chart source="contributionAcceptance"
+            title="Contribution Acceptance"
+            cite-url="https://github.com/chaoss/metrics/blob/master/activity-metrics/contribution-acceptance.md"
+            cite-text="Contribution Acceptance">
+          </dynamic-line-chart>
+        </div>
+
+        <div class="col col-6">
+          <dynamic-line-chart source="forks"
+            title="Forks / Week"
+            cite-url="https://github.com/augurlabs/wg-gmd/blob/master/activity-metrics/forks.md"
+            cite-text="Forks">
+          </dynamic-line-chart>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-6">
+          <dynamic-line-chart source="maintainerResponseToMergeRequestDuration"
+            title="Time to First Maintainer Response to Merge Request"
+            cite-url="https://github.com/chaoss/metrics/blob/master/activity-metrics/maintainer-response-to-merge-request-duration.md"
+            cite-text="Time to First Maintainer Response to Merge Request">
+          </dynamic-line-chart>
+        </div>
+
+        <div class="col col-6">
+          <dynamic-line-chart source="newContributingGithubOrganizations"
+            title="New Contributing Github Organizations"
+            cite-url="https://github.com/chaoss/metrics/blob/master/activity-metrics/new-contributing-organizations.md"
+            cite-text="New Contributing Organizations">
+          </dynamic-line-chart>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-6">
+          <dynamic-line-chart source="openIssues"
+            title="Open Issues / Week"
+            cite-url="https://github.com/augurlabs/wg-gmd/blob/master/activity-metrics/open-issues.md"
+            cite-text="Issues Open">
+          </dynamic-line-chart>
+        </div>
+
+        <div class="col col-6">
+          <dynamic-line-chart source="pullRequestComments"
+            title="Pull Request Comments / Week"
+            cite-url="https://github.com/augurlabs/wg-gmd/blob/master/activity-metrics/pull-request-comments.md"
+            cite-text="Pull Request Comments">
+          </dynamic-line-chart>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-12">
+          <dynamic-line-chart source="pullRequestsOpen"
+            title="Pull Requests Open / Week"
+            cite-url="https://github.com/augurlabs/wg-gmd/blob/master/activity-metrics/pull-requests-open.md"
+            cite-text="Open Pull Requests">
+          </dynamic-line-chart>
+        </div>
+      </div>
+ 
     </div>
 
     <!-- Issues Section -->
@@ -132,6 +231,75 @@
                     disable-rolling-average=1>
                     <!-- :data="values['totalCommitters']"> -->
         </dynamic-line-chart>
+      </div>
+
+      <div class="row">
+        <div class="col col-6">
+          <dynamic-line-chart source="contributionAcceptance"
+            title="Contribution Acceptance Rate"
+            cite-url=""
+            cite-text="Contribution Acceptance">
+          </dynamic-line-chart>
+        </div>
+
+        <div class="col col-6">
+          <dynamic-line-chart source="communityEngagement:issues_open"
+            title="Community Engagement: Open Issues"
+            cite-url="https://github.com/augurlabs/wg-gmd/blob/master/activity-metrics/open-issues.md"
+            cite-text="Open Issues"
+            disable-rolling-average=1>
+          </dynamic-line-chart>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-6">
+          <dynamic-line-chart source="communityEngagement:issues_closed_total"
+            title="Community Engagement: Closed Issues"
+            cite-url="https://github.com/augurlabs/wg-gmd/blob/master/activity-metrics/closed-issues.md"
+            cite-text="Closed Issues"
+            disable-rolling-average=1>
+          </dynamic-line-chart>
+        </div>
+
+        <div class="col col-6">
+          <dynamic-line-chart source="fakes"
+            title="Fakes"
+            cite-url=""
+            cite-text="Fakes"
+            disable-rolling-average=1>
+          </dynamic-line-chart>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-6">
+          <dynamic-line-chart source="newWatchers"
+            title="New Watchers / Week"
+            cite-url=""
+            cite-text="New Watchers">
+          </dynamic-line-chart>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-12">
+          <stacked-bar-chart source="issueActivity"
+            title="Issue Activity"
+            cite-url=""
+            cite-text="Issue Activity">
+          </stacked-bar-chart>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-12">
+          <bubble-chart source="contributors"
+            title="Contributor Overview"
+            cite-url=""
+            cite-text="Contributors">
+          </bubble-chart>
+        </div>
       </div>
 
     </div>
