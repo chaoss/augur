e5dc8418687ddb3a3164fac37efc9161b84b7a4c
Carter Landis <ccarterlandis@gmail.com>
2019-08-27 19:16:37 -0500

    Finish skeleton of new comprehensive install script
    This commit marks the first attempt at a all-in-one installation
    process for new users of Augur. To run the script, users will need
    execute the `augurinstall.sh` file in the main directory. The following
    items will then be configured:
    
    0. Virtual environment (if the user is not found to already be in one)
    1. Backend dependencies
    2. Worker dependecies
    3. Database configuration (user will be prompted)
    4. Configuration file defaults (user will be prompted for GitHub API key)
    5. Frontend dependencies (optional)
    
    At this point, the user should have a *mostly* functioning version of Augur,
    provided there was nothing abnormal about the dependency installation process
    or the provided credentials.
    
    Signed-off-by: Carter Landis <ccarterlandis@gmail.com>

--- augurinstall.sh
+++ augurinstall.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 ## Back End
 
+PS3="Your choice: "
+
 if [[ -z $VIRTUAL_ENV ]]; then
   echo "*** We noticed you're not using a virutal environment. It is STRONGLY recommended to install Augur in its own virutal environment. ***"
   echo "*** Would you like to create a virtual environment using virtualenv? ***"
-  read  -n 1 -p "[y/n]: " mainmenuinput
+  select choice in "Yes" "No"
+  do
+    case $choice in
+      Yes )
+          virtualenv -p python3.6 augur_venv
+          echo "*** Your environment was installed as \`augur_venv\`. Please activate and restart the installation using your shell's appropriate command. ***"
+          echo "*** For example, if you're using bash run 'source augur_venv/bin/activate'. ***"
+          exit 0
+        ;;
+      No )
+          echo "Resuming installation..."
+          break
+        ;;
+    esac
   echo
-
-  if [[ $mainmenuinput == 'y' ]]; then
-    # virtualenv venv/augur
-    echo "*** Your environment was installed to venv/augur. Please activate and restart the installation. ***"
-    echo "*** For example, if you're using bash run 'source venv/augur/bin/activate'. ***"
-    exit 0
-  fi
-
-else
-  echo "Virtual environment detected. Resuming installation."
+done
 fi
 
 echo "Installing backend dependencies..."
@@ -37,12 +43,96 @@ do
     fi
 done
 
-echo "Installing frontend dependencies..."
-cd frontend/;
-yarn install;
-yarn global add apidoc brunch newman @vue/cli; 
-yarn run build;
-cd ../;
+echo "Setting up the database configuration."
 
+installpostgreslocally="Use a pre-existing Postgres 10 or 11 installation to which you can install the Augur schema?"
+installpostgresremotely="Use a pre-existing Postgres 10 or 11 installation to which someone else can install the Augur schema?"
+postgresalreadyinstalled="Use a pre-existing Postgres 10 or 11 installation with the Augur schema already installed? "
+QUIT="Quit"
+
+select haveinstalledpostgres in "$installpostgreslocally" "$installpostgresremotely" "$postgresalreadyinstalled" "$QUIT"
+do
+  case $haveinstalledpostgres in
+      $QUIT )
+      echo "Quitting..."
+      break
+    ;;
+    $installpostgreslocally )
+        echo "After you have installed the Augur schema to your database, please return to this point in the installation."
+        echo "Please enter the credentials for your database."
+        read -p "Host: " host
+        read -p "name: " name
+        read -p "password: " password
+        read -p "port: " port
+        read -p "schema: " schema
+        read -p "Key: " key
+        read -p "user: " user
+      ;;
+    $installpostgresremotely )
+        echo "Once the Augur schema has been installed on to your database for you, please return to this point in the installation."
+        echo "Please enter the credentials for your database."
+        read -p "Host: " host
+        read -p "name: " name
+        read -p "password: " password
+        read -p "port: " port
+        read -p "schema: " schema
+        read -p "Key: " key
+        read -p "user: " user
+        break
+      ;;
+    $postgresalreadyinstalled )
+        echo "Please enter the credentials for your database."
+        read -p "Database: " database
+        read -p "Name: " name
+        read -p "Host: " host
+        read -p "Port: " port
+        read -p "User: " user
+        read -p "Password: " password
+        read -p "Schema: " schema
+        read -p "Key: " key
+        read -p "Zombie ID: " zombie_id
+        break
+      ;;
+  esac
+done
+echo
+
+config="
+{
+  \"database\": \"$database\",
+  \"name\": \"$name\",
+  \"host\": \"$host\",
+  \"port\": \"$port\",
+  \"user\": \"$user\",
+  \"password\": \"$password\",
+  \"schema\": \"$schema\",
+  \"key\": \"$key\",
+  \"zombie_id\": \"$zombie_id\"
+}"
+
+
+rm temp.config.json
+echo $config > temp.config.json
+
+echo "Generating configuration file..."
+python util/make-config.py
+
+echo "Would you like to install Augur's frontend dependencies?"
+select choice in Yes No
+do
+  case $choice in
+    Yes )
+      echo "Installing frontend dependencies..."
+      cd frontend/;
+      yarn install;
+      yarn global add apidoc brunch newman @vue/cli; 
+      yarn run build;
+      cd ../;
+      ;;
+    No )
+      echo "Skipping frontend dependencies..."
+      ;;
+  esac
 echo "Setting up API documentation..."
 cd docs && apidoc --debug -f "\.py" -i ../augur/ -o api/; rm -rf ../frontend/public/api_docs; mv api ../frontend/public/api_docs;
+done
--- util/make-config.py
+++ util/make-config.py
@@ -2,54 +2,37 @@ import json
 import os
 import sys
 
-
 def configure_cache(config):
     print("==Setting up Cache configuration==")
     config['Cache'] = {'config': {}}
-    c_data_dir = input("Enter Cache Data Directory [Default: runtime/cache/]: ") or "runtime/cache/"
-    config['Cache']['config']['cache.data_dir'] = c_data_dir
-    c_lock_dir = input("Enter Cache Lock Directory [Default: runtime/cache/]: ") or "runtime/cache/"
-    config['Cache']['config']['cache.lock_dir'] = c_lock_dir
-    c_type = input("Enter Cache Type [Default: file]: ") or "file"
-    config['Cache']['config']['cache.type'] = c_type
-    print()
+    config['Cache']['config']['cache.data_dir'] = "runtime/cache/"
+    config['Cache']['config']['cache.lock_dir'] = "runtime/cache/"
+    config['Cache']['config']['cache.type'] = "file"
 
-def configure_database(config):
+def configure_database(config, credentials):
     print("==Setting up Augur Database==")
     config['Database'] = {}
-    config['Database']['connection_string'] = "sqlite:///:memory:"
-    db_name = input("Enter Database Name [Default: augur]: ") or "augur"
-    config['Database']['database'] = db_name
-    config['Database']['name'] = db_name
-    db_host = input("Enter Database Host [Default: localhost]: ") or "localhost"
-    config['Database']['host'] = db_host
-    db_port = input("Enter Database Port [Default: 5432]: ") or "5432"
-    config['Database']['port'] = db_port
-    db_user = input("Enter Database User [Default: augur]: ") or "augur"
-    config['Database']['user'] = db_user
-    db_password = input("Enter Database Password: ") or "YOUR PASSWORD"
-    config['Database']['password'] = db_password
-    db_schema = input("Enter Database Schema [Default: augur_data]: ") or "augur_data"
-    config['Database']['schema'] = db_schema
-    db_key = input("Enter GitHub API key/token: ") or "YOUR KEY"
-    config['Database']['key'] = db_key
-    config['GitHub'] = {'apikey': db_key}
-    db_zombie = input("Enter Zombie ID [Default: 22]:") or "22"
-    config['Database']['zombie_id'] = db_zombie
+    config['Database']['database'] = credentials['database'] or "augur"
+    config['Database']['name'] = credentials['name']
+    config['Database']['host'] = credentials['host'] or "localhost"
+    config['Database']['port'] = credentials['port'] or "5432"
+    config['Database']['user'] = credentials['user'] or "augur"
+    config['Database']['password'] = credentials['password'] or "YOUR PASSWORD"
+    config['Database']['schema'] = credentials['schema'] or "augur_data"
+    config['Database']['key'] = credentials['key'] or "YOUR KEY"
+    config['Database']['zombie_id'] = credentials['zombie_id'] or "22"
+
+    github_api_key = input("Please enter your GitHub API key: ")
+    config['GitHub'] = {'apikey': github_api_key}
     print()
 
 def configure_server(config):
     print("==Setting up Augur Server==")
     config['Server'] = {}
-    s_host = input("Enter Server Host [Default: 0.0.0.0]: ") or "0.0.0.0"
-    config['Server']['host'] = s_host
-    s_port = input("Enter Server Host [Default: 5000]: ") or "5000"
-    config['Server']['port'] = s_port
-    s_workers = input("Enter Number of Workers [Default: 4]: ") or "4"
-    config['Server']['workers'] = s_workers
-    s_cache = input("Enter Cache Expiry Duration [Default: 3600]: ") or "3600"
-    config['Server']['cache_expire'] = s_cache
-    print()
+    config['Server']['host'] = "0.0.0.0"
+    config['Server']['port'] = "5000"
+    config['Server']['workers'] = "4"
+    config['Server']['cache_expire'] = "3600"
 
 def configure_facade(config):
     print("==Setting up Facade==")
@@ -83,23 +66,8 @@ def configure_ghtorrent(config):
     config['GHTorrent']['pass'] = gh_pass
     print()
 
-def configure_ghtorrent_plus(config):
-    print("==Setting up GHTorrent Plus==")
-    config['GHTorrentPlus'] = {}
-    gh_host = input("Enter GHTorrentPlus Host [Default: localhost]: ") or "localhost"
-    config['GHTorrentPlus']['host'] = gh_host
-    gh_port = input("Enter GHTorrentPlus Port [Default: 3306]: ") or "3306"
-    config['GHTorrentPlus']['port'] = gh_port
-    gh_name = input("Enter GHTorrentPlus Name [Default: ghtorrentplus]: ") or "ghtorrentplus"
-    config['GHTorrentPlus']['name'] = gh_name
-    gh_user = input("Enter GHTorrentPlus Username [Default: augur]: ") or "augur"
-    config['GHTorrentPlus']['user'] = gh_user
-    gh_pass = input("Enter GHTorrentPlus Password: ") or "password"
-    config['GHTorrentPlus']['pass'] = gh_pass
-    print()
-
 def configure_defaults(config):
-    print("\n==Setting up defaults==")
+    print("==Setting up defaults==")
 
     if not 'Facade' in config:
         config["Facade"] = {
@@ -174,27 +142,23 @@ def main():
     print("Beginning 'augur.config.json' creation process...\n")
     config = {}
 
+
     configure_cache(config)
-    configure_database(config)
+    with open('temp.config.json', 'r') as db_credentials_file:
+        configure_database(config, json.load(db_credentials_file))
     configure_server(config)
 
-    inp = input("Would you like to setup GHTorrent? (Y/N): ")
-    if inp.lower() == 'y':
-        configure_ghtorrent(config)
-    else:
-        print("Skipping GHTorrent configuration...")
+    # inp = input("Would you like to setup GHTorrent? (Y/N): ")
+    # if inp.lower() == 'y':
+    #     configure_ghtorrent(config)
+    # else:
+    #     print("Skipping GHTorrent configuration...")
 
-    inp = input("Would you like to setup GHTorrentPlus? (Y/N): ")
-    if inp.lower() == 'y':
-        configure_ghtorrent_plus(config)
-    else:
-        print("Skipping GHTorrent Plus configuration...")
-
-    inp = input("Would you like to setup Facade? (Y/N): ")
-    if inp.lower() == 'y':
-        configure_facade(config)
-    else:
-        print("Skipping Facade configuration...")
+    # inp = input("Would you like to setup Facade? (Y/N): ")
+    # if inp.lower() == 'y':
+    #     configure_facade(config)
+    # else:
+    #     print("Skipping Facade configuration...")
 
     configure_defaults(config)
 
