7bf52388e743f9a35f02b7d0ee1b4268542852ad
Matt Snell <msnell@unomaha.edu>
2019-09-07 17:55:04 -0500

    Progress toward SBOM API
    Signed-off-by: Matt Snell <msnell@unomaha.edu>

---
+++ frontend/src/components/charts/DownloadCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <d-card>
+    <d-card-body :title="title" class="text-center">
+      <p v-if="values === undefined">There is no SBOM Download available for this repository.</p>
+    </d-card-body>
+  </d-card>
+</template>
+
+<script lang="ts">
+  import  { Component, Vue } from 'vue-property-decorator';
+  import {mapActions, mapGetters} from "vuex";
+
+  const AppProps = Vue.extend({
+    props: {
+      title: String,
+      data: Object,
+      source: String,
+      headers: Array,
+      fields: Array,
+    }
+  })
+
+  @Component({
+    computed: {
+      ...mapGetters('compare',[
+        'comparedRepos',
+        'base'
+      ]),
+    },
+  })
+  export default class CountBlock extends AppProps{
+
+    // compare.getter
+    base!:any
+    comparedRepos!:any
+
+
+    get values(){
+      return this.data[this.source]
+    }
+
+  }
+</script>
