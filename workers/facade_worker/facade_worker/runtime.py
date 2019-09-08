f2e59501220388ee3e99d7c607a2b86c4e9d22fa
gabe-heim <gabe.heim@yahoo.com>
2019-09-08 14:38:51 -0500

    correct config reference to server block

--- workers/facade_worker/facade_worker/runtime.py
+++ workers/facade_worker/facade_worker/runtime.py
@@ -56,7 +56,7 @@ def main(augur_url, host, port):
             "id": "com.augurlabs.core.facade_worker.{}".format(worker_port),
             "broker_port": server['port'],
             "broker_host": server['host'],
-            "location": "http://{}:{}".format(server['broker_host'], worker_port),
+            "location": "http://{}:{}".format(server['host'], worker_port),
             "zombie_id": 22,
             "host": credentials["host"],
             "key": credentials["key"],
