c05fc3ef98eead69e16b24ea9faf15d148a6b57e
Matt Snell <msnell@unomaha.edu>
2019-09-08 17:38:48 -0500

    Get SBOM to risk page (not stable)
    Signed-off-by: Matt Snell <msnell@unomaha.edu>

--- frontend/src/components/charts/DownloadCard.vue
+++ frontend/src/components/charts/DownloadCard.vue
@@ -2,11 +2,15 @@
   <d-card>
     <d-card-body :title="title" class="text-center">
       <p v-if="values === undefined">There is no SBOM Download available for this repository.</p>
+      <button v-if="values" @click="download">
+        Download SBOM
+      </button>
     </d-card-body>
   </d-card>
 </template>
 
 <script lang="ts">
+
   import  { Component, Vue } from 'vue-property-decorator';
   import {mapActions, mapGetters} from "vuex";
 
@@ -27,6 +31,17 @@
         'base'
       ]),
     },
+    methods: {
+      download(e) {
+      console.log(this.values[0])
+      let blob = new Blob([this.values[0].sbom_scan], { type: 'text/json' })
+      let link = document.createElement('a')
+      console.log(blob)
+      link.href = window.URL.createObjectURL(blob)
+      link.download = 'sbom.json'
+      link.click()
+      }
+    }
   })
   export default class CountBlock extends AppProps{
 
