c049eb69af3581eff4e3eb1d1c75c8bd72f87006
gabe-heim <gabe.heim@yahoo.com>
2019-09-18 16:40:32 -0500

    housekeeper can accept a task with a single repo, broker checks heartbeat before sending task, added heartbeat to all workers, possible gh worker fix

--- workers/facade_worker/facade_worker/runtime.py
+++ workers/facade_worker/facade_worker/runtime.py
@@ -29,6 +29,13 @@ def create_server(app, gw):
                         status=200,
                         mimetype="application/json")
 
+    @app.route("/AUGWOP/heartbeat", methods=['GET'])
+    def heartbeat():
+        if request.method == 'GET':
+            return jsonify({
+                "status": "alive"
+            })
+            
     @app.route("/AUGWOP/config")
     def augwop_config():
         """ Retrieve worker's config
