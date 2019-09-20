39a481daca2983192f7e04ea9ec1cfbec4ad2e34
Carter Landis <ccarterlandis@gmail.com>
2019-09-20 18:32:16 -0500

    Add automatic database creation to install script
    The installation script will now automatically install
    the augur database schemas if prompted by the user.
    
    Signed-off-by: Carter Landis <ccarterlandis@gmail.com>

--- util/scripts/install/setup_db.sh
+++ util/scripts/install/setup_db.sh
@@ -1,29 +1,119 @@
-echo "If you need to install Postgres, the downloads can be found here: https://www.postgresql.org/download/"
-installpostgreslocally="Would you like to use a pre-existing Postgres 10 or 11 installation to which you can install the Augur schema?"
-installpostgresremotely="Would you like to use a pre-existing Postgres 10 or 11 installation to which someone else can install the Augur schema?"
-postgresalreadyinstalled="Would you like to use a pre-existing Postgres 10 or 11 installation with the Augur schema already installed? "
-SKIP="Skip this section"
+#!/bin/bash
 
-select haveinstalledpostgres in "$installpostgreslocally" "$installpostgresremotely" "$postgresalreadyinstalled" "$SKIP"
+PS3="
+Please type the number corresponding to your selection and then press the Enter/Return key.
+Your choice: "
+
+echo
+echo "**********************************"
+echo "Setting up the database configuration..."
+echo "**********************************"
+echo
+
+function generate_config_file() {
+
+  echo
+  echo "**********************************"
+  echo "Generating configuration file..."
+  echo "**********************************"
+  echo
+
+  INSTALL_SCRIPT_HOME="util/scripts/install"
+  cd $INSTALL_SCRIPT_HOME
+
+  rm temp.config.json
+  touch temp.config.json
+
+  echo $1 > temp.config.json
+
+  python make_config.py
+
+  rm temp.config.json
+
+  cd ../../.. #get back to augur root
+}
+
+function set_remote_db_credentials() {
+
+  read -p "database: " database
+  read -p "host: " host
+  read -p "port: " port
+  read -p "user: " user
+  read -p "password: " password
+  read -p "key: " key
+
+  config="
+  {
+    \"database\": \"$database\",
+    \"host\": \"$host\",
+    \"port\": \"$port\",
+    \"user\": \"$user\",
+    \"password\": \"$password\",
+    \"key\": \"$key\"
+  }"
+
+  # generate_config_file $config
+}
+
+function set_local_db_credentials() {
+
+  read -p "database: " database
+  read -p "user: " user
+  read -p "port: " port
+  read -p "password: " password
+  read -p "key: " key
+
+  config="
+  {
+    \"database\": \"$database\",
+    \"host\": \"localhost\",
+    \"port\": \"$port\",
+    \"user\": \"$augur\",
+    \"password\": \"$password\",
+    \"key\": \"$key\"
+  }"
+
+  # generate_config_file $config
+}
+
+echo "If you need to install Postgres, the downloads can be found here: https://www.postgresql.org/download/"
+echo
+install_locally="Would you like to use a LOCAL Postgres 10 or 11 installation without the database already installed?"
+install_remotely="Would you like to use a REMOTE Postgres 10 or 11 installation without the database already installed?"
+already_installed="Would you like to use a pre-existing Postgres 10 or 11 installation with the Augur database ALREADY installed? "
+SKIP="Skip this section"
+echo
+
+select install_location in "$install_locally" "$install_remotely" "$already_installed" "$SKIP"
 do
-  case $haveinstalledpostgres in
+  case $install_location in
       $SKIP )
+      echo
       echo "Skipping database configuration..."
+      echo
       break
     ;;
-    $installpostgreslocally )
-        echo "After you have installed the Augur schema to your database, please return to this point in the installation."
-        echo "Please enter the credentials for your database."
-        enter_db_credentials
+    $install_locally )
+        echo "Please set the credentials for your database."
+        set_local_db_credentials
+        psql -c "create database $database;"
+        psql -c "create user $user with encrypted password '$password';"
+        psql -c "alter database $database owner to $user;"
+        psql -c "grant all privileges on database $database to $user;"
+        psql -h "localhost" -d $database -U $user -p $port -a -w -f augur/persistence_schema/0-all.sql
         break
       ;;
-    $installpostgresremotely )
-        echo "Once the Augur schema has been installed on to your database for you, please return to this point in the installation."
-        echo "Please enter the credentials for your database."
-        enter_db_credentials
+    $install_remotely )
+        echo "Please set the credentials for your database."
+        set_remote_db_credentials
+        psql -h $host -p $port -c "create database $database;"
+        psql -h $host -p $port -c "create user $user with encrypted password '$password';"
+        psql -h $host -p $port -c "alter database $database owner to $user;"
+        psql -h $host -p $port -c "grant all privileges on database $database to $user;"
+        psql -h $host -d $database -U $user -p $port -a -w -f augur/persistence_schema/0-all.sql
         break
       ;;
-    $postgresalreadyinstalled )
+    $already_installed )
         echo "Please enter the credentials for your database."
         enter_db_credentials
         break
