7a208c68ba8486bc551edf544ec9f74dda68e3fc
gabe-heim <gabe.heim@yahoo.com>
2019-10-05 19:17:45 -0500

    working pie charts for commits and lines but no legend yet

--- frontend/src/components/charts/PieChart.vue
+++ frontend/src/components/charts/PieChart.vue
@@ -1,8 +1,11 @@
 <template>
 
-  <div ref="holder">
-    <div id="chart"></div>
-  </div>
+  <d-card-body :title="title" ref="holder">
+    <div v-if="!loaded">
+      <spinner></spinner>
+    </div>
+    <div :id="'chart' + field"></div>
+  </d-card-body>
 </template>
 
 <script lang="ts">
@@ -11,10 +14,14 @@ import Vue from 'vue';
 import vegaEmbed from 'vega-embed'
 import Component from 'vue-class-component';
 import AugurStats from '@/AugurStats.ts'
+import Spinner from '../../components/Spinner.vue'
 import {mapActions, mapGetters, mapMutations} from "vuex";
 
 @Component({
-  props: ['source', 'citeUrl', 'citeText', 'title', 'disableRollingAverage', 'alwaysByDate', 'domain', 'data'], 
+  components: {
+    Spinner,
+  },
+  props: ['source', 'citeUrl', 'citeText', 'title', 'disableRollingAverage', 'alwaysByDate', 'domain', 'data', 'field'], 
   methods: {
     ...mapActions('common',[
       'endpoint', // map `this.endpoint({...})` to `this.$store.dispatch('endpoint', {...})`
@@ -32,6 +39,8 @@ import {mapActions, mapGetters, mapMutations} from "vuex";
 })
 export default class PieChart extends Vue {
 
+  //data props
+  loaded: boolean = false
   // getters
   base!:any
 
@@ -54,23 +63,23 @@ export default class PieChart extends Vue {
       "signals": [
         {
           "name": "startAngle", "value": 0,
-          "bind": {"input": "range", "min": 0, "max": 6.29, "step": 0.01}
+          // "bind": {"input": "range", "min": 0, "max": 6.29, "step": 0.01}
         },
         {
           "name": "endAngle", "value": 6.29,
-          "bind": {"input": "range", "min": 0, "max": 6.29, "step": 0.01}
+          // "bind": {"input": "range", "min": 0, "max": 6.29, "step": 0.01}
         },
         {
           "name": "padAngle", "value": 0,
-          "bind": {"input": "range", "min": 0, "max": 0.1}
+          // "bind": {"input": "range", "min": 0, "max": 0.1}
         },
         {
-          "name": "innerRadius", "value": 90,
-          "bind": {"input": "range", "min": 0, "max": 90, "step": 1}
+          "name": "innerRadius", "value": 60,
+          // "bind": {"input": "range", "min": 0, "max": 90, "step": 1}
         },
         {
           "name": "cornerRadius", "value": 0,
-          "bind": {"input": "range", "min": 0, "max": 10, "step": 0.5}
+          // "bind": {"input": "range", "min": 0, "max": 10, "step": 0.5}
         },
         {
           "name": "sort", "value": false,
@@ -81,18 +90,10 @@ export default class PieChart extends Vue {
       "data": [
         {
           "name": "table",
-          // "values": [
-          //   {"id": 1, "field": 4},
-          //   {"id": 2, "field": 6},
-          //   {"id": 3, "field": 10},
-          //   {"id": 4, "field": 3},
-          //   {"id": 5, "field": 7},
-          //   {"id": 6, "field": 8}
-          // ],
           "transform": [
             {
               "type": "pie",
-              "field": "commits",
+              "field": this.field,
               "startAngle": {"signal": "startAngle"},
               "endAngle": {"signal": "endAngle"},
               "sort": {"signal": "sort"}
@@ -101,12 +102,35 @@ export default class PieChart extends Vue {
         }
       ],
 
-      "scales": [
+      "legends": [
+        {
+          "stroke": "color",
+          "title": "Author Email",
+          "padding": 4,
+          "encode": {
+            "symbols": {
+              "enter": {
+                "strokeWidth": {"value": 2},
+                "size": {"value": 50}
+              }
+            }
+          }
+        }
+      ],
+
+      "scales": [ //{"scale": "r", "field": "data"},
         {
           "name": "color",
           "type": "ordinal",
           "domain": {"data": "table", "field": "email"},
           "range": {"scheme": "category20"}
+        },
+        {
+          "name": "r",
+          "type": "sqrt",
+          "domain": {"data": "table", "field": this.field},
+          "zero": true,
+          "range": [20, 100]
         }
       ],
 
@@ -125,28 +149,57 @@ export default class PieChart extends Vue {
               "endAngle": {"field": "endAngle"},
               "padAngle": {"signal": "padAngle"},
               "innerRadius": {"signal": "innerRadius"},
-              "outerRadius": {"signal": "width / 2"},
+              "outerRadius": {"signal": "width / 2"},//{"scale": "r", "field": "data"},
               "cornerRadius": {"signal": "cornerRadius"}
             }
+          }
+        },
+        {
+          "type": "text",
+          "from": {"data": "table"},
+          "encode": {
+            "x": {"field": {"group": "width"}, "mult": 0.5},
+            "y": {"field": {"group": "height"}, "mult": 0.5},
+            "radius": {"scale": "r", "field": this.field, "offset": 8},
+            "theta": {"signal": "(datum.startAngle + datum.endAngle)/2"},
+            "fill": {"value": "#030"},
+            "align": {"value": "center"},
+            "baseline": {"value": "middle"},
+            "text": {"field": this.field}
           }
         }
       ]
     }
 
-    // hitting endpoint
-    this.base[this.source]().then((data: any) => {
-      //data handler (what gets returned)
-      
-      console.log(data)
+    //define function that does any operation needed on data before sending it to the chart
+    let processData = (data) => {
+
+      console.log("PIECHART data",data)
+
+      let values = []
+      for (let i = 0; i < 10; i++) {
+        data[i].id = data[i].email
+      }
 
       //data reorganizing
       config.data[0].values = data
-
+      this.loaded = true
       this.reloadImage(config)
+    }
 
-    }, () => {
-      //this.renderError()
-    }) // end batch request
+    //condition handler to retrieve data if needed and call processData with it
+    if (!this.data) {
+      // hitting endpoint
+      this.base[this.source]().then((data: any) => {
+        //data handler (what gets returned)
+        
+        processData(data)
+      }, () => {
+        //this.renderError()
+      })
+    } else {
+      processData(this.data)
+    }
 
     
     
@@ -157,7 +210,8 @@ export default class PieChart extends Vue {
 
   reloadImage (config:any) {
     //,tooltip: {theme: 'dark'}
-    vegaEmbed('#chart', config, {actions: false}) 
+    console.log("piechart", JSON.stringify(config))
+    vegaEmbed('#chart' + this.field, config, {actions: false}) 
   }
 
   mounted () {
