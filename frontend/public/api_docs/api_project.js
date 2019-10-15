78578eca619f605f3b5737a822b52d3041631453
Michael Woodruff <michaelwoodruff@mwc-021085.dhcp.missouri.edu>
2019-10-15 18:30:58 -0500

    implement dynamic year range in LinesOfCodeChart.vue

--- frontend/public/api_docs/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-10-03T02:37:16.735Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
