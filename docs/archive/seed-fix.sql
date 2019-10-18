0a0e9c71f41cf309d89d22d18086359bca5f89a6
Carter Landis <ccarterlandis@gmail.com>
2019-10-18 12:58:39 -0500

    Remove docs/archive
    Signed-off-by: Carter Landis <ccarterlandis@gmail.com>

--- docs/archive/seed-fix.sql
+++
@@ -1,2 +0,0 @@
-DELETE from "augur_operations"."gh_worker_job";
-INSERT INTO "augur_operations"."gh_worker_job" VALUES ('issues', 5, 0, '0', 'The issues model consists of contributors, issues, issue events, issue labels, and issue comments.', 0, '2019-06-17 06:51:53', 1, 22);
