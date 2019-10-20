be400365d7ff347107a2fb21da37c9c27670169e
Sean Goggins <s@goggins.com>
2019-10-20 21:00:10 +0000

    unmessing up the augurkill.sh script

--- util/scripts/control/augurkill.sh
+++ util/scripts/control/augurkill.sh
@@ -3,4 +3,5 @@
 echo "augur processes"
 ps aux | grep -ie $VIRTUAL_ENV/ |   awk '{print "kill -9 " $2}'
 echo "worker processes"
-kill $(ps aux | grep -ie $VIRTUAL_ENV/ | grep -ie bin | grep worker  |  awk '{print $2}')
+ps aux | grep -ie $VIRTUAL_ENV/ | grep -ie bin | grep worker  |  awk '{print "kill -9 " $2}'
+
