25f3787fb1b172e018e9e9ed75e2db006513f013
Sean Goggins <s@goggins.com>
2019-10-29 10:28:36 -0500

    Updated install to specify `settings` table schema as `augur_data`

--- util/scripts/install/setup_db.sh
+++ util/scripts/install/setup_db.sh
@@ -126,7 +126,7 @@ function create_db_schema() {
     psql -h $host -d $database -U $db_user -p $port -a -w -f persistence_schema/3-augur_operations.sql
     psql -h $host -d $database -U $db_user -p $port -a -w -f persistence_schema/4-spdx.sql
     psql -h $host -d $database -U $db_user -p $port -a -w -f persistence_schema/5-seed-data.sql
-    psql -h $host -d $database -U $db_user -p $port -a -w -c "UPDATE settings SET VALUE = '$facade_repo_path' WHERE setting='repo_directory';"
+    psql -h $host -d $database -U $db_user -p $port -a -w -c "UPDATE augur_data.settings SET VALUE = '$facade_repo_path' WHERE setting='repo_directory';"
     echo "Schema created"
 
     echo "Would you like to load your database with some sample data provided by Augur?"
