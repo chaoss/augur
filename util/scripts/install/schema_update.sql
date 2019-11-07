a65811790f3b9d0334984f541df4f7facb63da90
Sean P. Goggins <s@goggins.com>
2019-11-07 08:48:40 -0600

    Sketch of schema update script we will need from here forward. Next master release needs to include this. We will need to start tracking schema versions after the next release.

---
+++ util/scripts/install/schema_update.sql
@@ -0,0 +1,29 @@
+ALTER TABLE "augur_data"."contributors_aliases" ALTER COLUMN "data_collection_date" SET DEFAULT CURRENT_TIMESTAMP;
+
+CREATE SEQUENCE "spdx"."projects_package_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+ALTER TABLE "spdx"."files" DROP CONSTRAINT "files_project_id_fkey";
+
+ALTER TABLE "spdx"."files" ADD COLUMN "package_id" int4;
+
+ALTER TABLE "spdx"."files" DROP COLUMN "project_id";
+
+ALTER TABLE "spdx"."projects" DROP CONSTRAINT "projects_pkey";
+
+ALTER TABLE "spdx"."projects" ADD COLUMN "package_id" int4 NOT NULL DEFAULT nextval('"spdx".projects_package_id_seq'::regclass);
+
+ALTER TABLE "spdx"."projects" DROP COLUMN "project_id";
+
+ALTER TABLE "spdx"."projects" ADD CONSTRAINT "projects_pkey" PRIMARY KEY ("package_id");
+
+SELECT setval('"spdx"."projects_package_id_seq"', 1, false);
+
+ALTER SEQUENCE "spdx"."projects_package_id_seq"
+OWNED BY "spdx"."projects"."package_id";
+
+ALTER SEQUENCE "spdx"."projects_package_id_seq" OWNER TO "augur";
