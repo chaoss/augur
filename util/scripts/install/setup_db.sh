af751f3b52a8bb2e1520af19b73fc646ca571b2e
Sean P. Goggins <s@goggins.com>
2019-11-22 18:30:34 -0600

    Added schema update to install script:     psql -h $host -d $database -U $db_user -p $port -a -w -f ./schema_update.sql

--- util/scripts/install/setup_db.sh
+++ util/scripts/install/setup_db.sh
@@ -143,6 +143,7 @@ function create_db_schema() {
     psql -h $host -d $database -U $db_user -p $port -a -w -f persistence_schema/3-augur_operations.sql
     psql -h $host -d $database -U $db_user -p $port -a -w -f persistence_schema/4-spdx.sql
     psql -h $host -d $database -U $db_user -p $port -a -w -f persistence_schema/5-seed-data.sql
+    psql -h $host -d $database -U $db_user -p $port -a -w -f ./schema_update.sql
     psql -h $host -d $database -U $db_user -p $port -a -w -c "UPDATE augur_data.settings SET VALUE = '$facade_repo_path' WHERE setting='repo_directory';"
     echo "Schema created"
 
