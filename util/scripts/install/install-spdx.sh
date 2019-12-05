20c65e0bc53864d85762ab03c90e5e3bf79a5576
Sean Goggins <s@goggins.com>
2019-12-05 23:12:14 +0000

    updates to put sudo commands in a special file

--- util/scripts/install/install-spdx.sh
+++ util/scripts/install/install-spdx.sh
@@ -9,8 +9,5 @@ else
   echo "Augur-SPDX exists"
 fi
 cd augur-spdx
-sudo apt-get install git python3-pip postgresql
-sudo apt-get install libpq-dev
-sudo apt-get install libglib2.0-dev libjsoncpp-dev libjson-c-dev
 pip3 install .
 ./scripts/install-nomos.sh
