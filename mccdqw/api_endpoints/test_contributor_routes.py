2091d0e5bafa51e939d2c4f6f67cb1b37a0bcfc1
mccdqw <54416592+mccdqw@users.noreply.github.com>
2019-12-14 15:54:37 -0600

    Create test_contributor_routes.py

---
+++ mccdqw/api_endpoints/test_contributor_routes.py
@@ -0,0 +1,12 @@
+import requests
+import pytest
+
+@pytest.fixture(scope="session")
+def metrics():
+    pass
+
+def test_contributor_affiliation(metrics):
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/20/contributor-affiliation')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) >= 1
