eb968fc17c499e4214b3628aff7533daeb0a16ea
mccdqw <54416592+mccdqw@users.noreply.github.com>
2019-12-14 18:55:53 -0600

    Create test_insight_routes.py

---
+++ mccdqw/api_endpoints/test_insight_routes.py
@@ -0,0 +1,5 @@
+def test_testing_coverage(metrics):
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/20/testing-coverage')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) >= 1
