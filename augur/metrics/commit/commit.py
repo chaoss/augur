6943f592aab891752f62696fd2c2946160748921
Sean Goggins <s@goggins.com>
2019-12-16 19:42:19 +0900

    fixed a committer counting issues in the API. It was counting the total commits, not the number of committers. This API call only affected the risk page.

--- augur/metrics/commit/commit.py
+++ augur/metrics/commit/commit.py
@@ -30,35 +30,63 @@ def committers(self, repo_group_id, repo_id=None, begin_date=None, end_date=None
     if repo_id:
         committersSQL = s.sql.text(
             """
-                SELECT
-                    date_trunc(:period, commits.cmt_author_date::date) as date,
+                SELECT DATE,
                     repo_name,
                     rg_name,
-                    count(cmt_author_name)
+                    COUNT ( author_count ) 
                 FROM
-                    commits, repo, repo_groups
-                WHERE
-                    commits.repo_id = :repo_id AND commits.repo_id = repo.repo_id
-                    AND repo.repo_group_id = repo_groups.repo_group_id
-                    AND commits.cmt_author_date BETWEEN :begin_date and :end_date
-                GROUP BY date, repo_name, rg_name
-                ORDER BY date DESC
+                    (
+                    SELECT
+                        date_trunc(:period, commits.cmt_author_date::date) as date,
+                        repo_name,
+                        rg_name,
+                        cmt_author_name,
+                        cmt_author_email,
+                        COUNT ( cmt_author_name ) AS author_count 
+                    FROM
+                        commits, repo, repo_groups
+                    WHERE
+                        commits.repo_id = :repo_id AND commits.repo_id = repo.repo_id
+                        AND repo.repo_group_id = repo_groups.repo_group_id
+                        AND commits.cmt_author_date BETWEEN :begin_date and :end_date
+                    GROUP BY date, repo_name, rg_name, cmt_author_name, cmt_author_email 
+                    ORDER BY date DESC
+                    ) C
+                GROUP BY
+                    C.DATE,
+                    repo_name,
+                    rg_name 
+                ORDER BY C.DATE desc 
             """
         )
     else:
         committersSQL = s.sql.text(
             """
-            SELECT
-                date_trunc(:period, commits.cmt_author_date::date) as date,
-                rg_name,
-                count(cmt_author_name)
-            FROM
-                commits, repo, repo_groups
-            WHERE
-                repo.repo_group_id = repo_groups.repo_group_id AND repo.repo_group_id = :repo_group_id
-                AND repo.repo_id = commits.repo_id
-                AND commits.cmt_author_date BETWEEN :begin_date and :end_date
-            GROUP BY date, rg_name
+                SELECT DATE,
+                    rg_name,
+                    COUNT ( author_count ) 
+                FROM
+                    (
+                    SELECT
+                        date_trunc(:period, commits.cmt_author_date::date) as date,
+                        rg_name,
+                        cmt_author_name,
+                        cmt_author_email,
+                        COUNT ( cmt_author_name ) AS author_count 
+                    FROM
+                        commits, repo, repo_groups
+                    WHERE
+                        commits.repo_id = repo.repo_id
+                        AND repo.repo_group_id = repo_groups.repo_group_id
+                        AND commits.cmt_author_date BETWEEN :begin_date and :end_date
+                        AND repo.repo_group_id = :repo_group_id
+                    GROUP BY date, rg_name, cmt_author_name, cmt_author_email 
+                    ORDER BY date DESC
+                    ) C
+                GROUP BY
+                    C.DATE,
+                    rg_name 
+                ORDER BY C.DATE desc 
             """
         )
 
