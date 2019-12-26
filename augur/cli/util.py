4d49eb88abd1ce58b0103f300aa64a577f358a68
Carter Landis <ccarterlandis@pm.me>
2019-12-26 14:42:04 -0600

    Remove outdated util commands
    Signed-off-by: Carter Landis <ccarterlandis@pm.me>

--- augur/cli/util.py
+++ augur/cli/util.py
@@ -14,50 +14,6 @@ def cli():
 def shell(app):
     app.shell()
 
-@cli.command('edit-config', short_help='Edit your config file')
-@pass_application
-def edit_config(app):
-    """
-    Edit your config file
-    """
-    click.edit(filename=app._config_file_path)
-
-
-@cli.command('python-location', short_help='Print the location of the interpreter that is running this')
-def interpreter():
-    """
-    Print the location of the interpreter that is running this
-    """
-    print(sys.executable)
-
-
-@cli.command('except', short_help='Test logging and raise an exception')
-@pass_application
-def excpt(app):
-    """
-    Print the location of the interpreter that is running this
-    """
-    print('Logging tests... ', file=sys.stderr)
-    app.log.info('Hello')
-    app.log.warn('Things are looking scary')
-    app.log.error('Things are bad!')
-    app.log.fatal('Now I am dying')
-    print('Exception handling...', file=sys.stderr)
-    raise Exception('is dead')
-
-
-@cli.command('upgrade', short_help='Upgrade Augur')
-@click.option('--from-directory', '-f', type=click.Path(), help='Upgrade from a provided directory rather than git.')
-@pass_application
-def upgrade(app, from_directory):
-    """
-    Print the location of the interpreter that is running this
-    """
-    pass
-    # app.log.info(pyrcss.util.self_upgrade(from_directory=from_directory, dry_run=true))
-    # pyrcss.util.self_upgrade(from_directory=from_directory)
-
-
 @cli.command('kill', short_help='Kill Augur')
 @pass_application
 def kill(app):
