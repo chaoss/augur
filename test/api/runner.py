c0ad7ed32c546782a06555f977be150926c330d8
Carter Landis <ccarterlandis@pm.me>
2020-01-01 12:19:21 -0600

    Test first implementation of tox
    Signed-off-by: Carter Landis <ccarterlandis@pm.me>

--- test/api/runner.py
+++ test/api/runner.py
@@ -4,18 +4,12 @@ import os
 import pytest
 import sys
 
-SOURCE = "**"
-if len(sys.argv) == 2:
-    SOURCE = str(sys.argv[1])
-
-test_location_string = "augur/metrics/{source}/test_{source}_routes.py".format(source=SOURCE)
-
 FNULL = open(os.devnull, "w")
 
 start = subprocess.Popen(["augur", "run", "--disable-housekeeper"], stdout=FNULL, stderr=subprocess.STDOUT)
 print("Waiting for the server to start...")
 time.sleep(15)
-process = subprocess.run(["pytest", "-ra", test_location_string])
+process = subprocess.run(["pytest", "-ra", "test/api/"])
 time.sleep(2)
 subprocess.Popen(["augur", "util", "kill"], stdout=FNULL, stderr=subprocess.STDOUT)
 print("Server successfully shutdown.")
