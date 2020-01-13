10dead6163830875664ab352f65d30da8e74fdea
gabe-heim <gabe.heim@yahoo.com>
2020-01-13 11:08:09 -0600

    remove reference to self.working_on when a completed task is registered

--- workers/standard_methods.py
+++ workers/standard_methods.py
@@ -176,6 +176,63 @@ def paginate(self, logging, url, duplicate_col_map, update_col_map, table, table
 
     return tuples
 
+def read_config(section, name=None, environment_variable=None, default=None, config_file='augur.config.json', no_config_file=0, use_main_config=0):
+    """
+    Read a variable in specified section of the config file, unless provided an environment variable
+
+    :param section: location of given variable
+    :param name: name of variable
+    """
+
+    __config_bad = False
+    if use_main_config == 0:
+        __config_file_path = os.path.abspath(os.getenv('AUGUR_CONFIG_FILE', config_file))
+    else:        
+        __config_file_path = os.path.abspath(os.path.dirname(os.path.dirname(os.getcwd())) + '/augur.config.json')
+
+    __config_location = os.path.dirname(__config_file_path)
+    __export_env = os.getenv('AUGUR_ENV_EXPORT', '0') == '1'
+    __default_config = { 'Database': {"host": "nekocase.augurlabs.io"} }
+
+    if os.getenv('AUGUR_ENV_ONLY', '0') != '1' and no_config_file == 0:
+        try:
+            __config_file = open(__config_file_path, 'r+')
+        except:
+            # logger.info('Couldn\'t open {}, attempting to create. If you have a augur.cfg, you can convert it to a json file using "make to-json"'.format(config_file))
+            if not os.path.exists(__config_location):
+                os.makedirs(__config_location)
+            __config_file = open(__config_file_path, 'w+')
+            __config_bad = True
+
+
+        # Options to export the loaded configuration as environment variables for Docker
+        if __export_env:
+            
+            export_filename = os.getenv('AUGUR_ENV_EXPORT_FILE', 'augur.cfg.sh')
+            __export_file = open(export_filename, 'w+')
+            # logger.info('Exporting {} to environment variable export statements in {}'.format(config_file, export_filename))
+            __export_file.write('#!/bin/bash\n')
+
+        # Load the config file and return [section][name]
+        try:
+            config_text = __config_file.read()
+            __config = json.loads(config_text)
+            if name is not None:
+                return(__config[section][name])
+            else:
+                return(__config[section])
+
+        except json.decoder.JSONDecodeError as e:
+            if not __config_bad:
+                __using_config_file = False
+                # logger.error('%s could not be parsed, using defaults. Fix that file, or delete it and run this again to regenerate it. Error: %s', __config_file_path, str(e))
+
+            __config = __default_config
+            try:
+                return(__config[section][name])
+            except:
+                return(__config[section])
+
 def record_model_process(self, logging, repo_id, model):
 
     task_history = {
@@ -200,7 +257,7 @@ def register_task_completion(self, logging, task, repo_id, model):
     # Task to send back to broker
     task_completed = {
         'worker_id': self.config['id'],
-        'job_type': self.working_on,
+        'job_type': "MAINTAIN",
         'repo_id': repo_id,
         'job_model': model
     }
