bda6784fd4d64ef4c6b0dd3a430147e3adb74413
Carter Landis <ccarterlandis@pm.me>
2020-02-02 12:20:27 -0600

    Refactor workers to use new read_config standard method
    Signed-off-by: Carter Landis <ccarterlandis@pm.me>

--- workers/linux_badge_worker/linux_badge_worker/runtime.py
+++ workers/linux_badge_worker/linux_badge_worker/runtime.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, Response
 import click, os, json, requests, logging
 from linux_badge_worker.worker import BadgeWorker
+from workers.standard_methods import read_config
 
 def create_server(app):
     """ Consists of AUGWOP endpoints for the broker to communicate to this worker
@@ -75,6 +76,7 @@ def main(augur_url, host, port):
         "broker_host": server['host'],
         "broker_port": server['port'],
         "host": credentials["host"],
+        "key": read_config("Database", "key", "AUGUR_GITHUB_API_KEY", "key"),
         "password": credentials["password"],
         "port": credentials["port"],
         "user": credentials["user"],
@@ -104,55 +106,3 @@ def main(augur_url, host, port):
 
     logging.info("Killing Flask App: " + str(os.getpid()))
     os.kill(os.getpid(), 9)
-
-def read_config(section, name=None, environment_variable=None, default=None, config_file='augur.config.json', no_config_file=0, use_main_config=0):
-    """
-    Read a variable in specified section of the config file, unless provided an environment variable
-
-    :param section: location of given variable
-    :param name: name of variable
-    """
-    __config_bad = False
-    if use_main_config == 0:
-        __config_file_path = os.path.abspath(os.getenv('AUGUR_CONFIG_FILE', config_file))
-    else:
-        __config_file_path = os.path.abspath(os.path.dirname(os.path.dirname(os.getcwd())) + '/augur.config.json')
-
-    __config_location = os.path.dirname(__config_file_path)
-    __export_env = os.getenv('AUGUR_ENV_EXPORT', '0') == '1'
-    __default_config = { 'Database': {"host": "nekocase.augurlabs.io"} }
-
-    if os.getenv('AUGUR_ENV_ONLY', '0') != '1' and no_config_file == 0:
-        try:
-            __config_file = open(__config_file_path, 'r+')
-        except:
-            if not os.path.exists(__config_location):
-                os.makedirs(__config_location)
-            __config_file = open(__config_file_path, 'w+')
-            __config_bad = True
-
-
-        # Options to export the loaded configuration as environment variables for Docker
-        if __export_env:
-            export_filename = os.getenv('AUGUR_ENV_EXPORT_FILE', 'augur.cfg.sh')
-            __export_file = open(export_filename, 'w+')
-            __export_file.write('#!/bin/bash\n')
-
-        # Load the config file and return [section][name]
-        try:
-            config_text = __config_file.read()
-            __config = json.loads(config_text)
-            if name is not None:
-                return(__config[section][name])
-            else:
-                return(__config[section])
-
-        except json.decoder.JSONDecodeError as e:
-            if not __config_bad:
-                __using_config_file = False
-
-            __config = __default_config
-            try:
-                return(__config[section][name])
-            except:
-                return(__config[section])
--- workers/repo_info_worker/repo_info_worker/runtime.py
+++ workers/repo_info_worker/repo_info_worker/runtime.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, Response
 import click, os, json, requests, logging
 from repo_info_worker.worker import GHRepoInfoWorker
+from workers.standard_methods import read_config
 
 def create_server(app, gw):
     """ Consists of AUGWOP endpoints for the broker to communicate to this worker
@@ -74,7 +75,7 @@ def main(augur_url, host, port):
             "location": "http://{}:{}".format(server['host'],worker_port),
             "zombie_id": 22,
             "host": credentials["host"],
-            "key": credentials["key"],
+            "key": read_config("Database", "key", "AUGUR_GITHUB_API_KEY", "key"),
             "password": credentials["password"],
             "port": credentials["port"],
             "user": credentials["user"],
@@ -106,58 +107,3 @@ def main(augur_url, host, port):
 
 
 
-def read_config(section, name=None, environment_variable=None, default=None, config_file='augur.config.json', no_config_file=0, use_main_config=0):
-    """
-    Read a variable in specified section of the config file, unless provided an environment variable
-
-    :param section: location of given variable
-    :param name: name of variable
-    """
-
-
-    __config_bad = False
-    if use_main_config == 0:
-        __config_file_path = os.path.abspath(os.getenv('AUGUR_CONFIG_FILE', config_file))
-    else:
-        __config_file_path = os.path.abspath(os.path.dirname(os.path.dirname(os.getcwd())) + '/augur.config.json')
-
-    __config_location = os.path.dirname(__config_file_path)
-    __export_env = os.getenv('AUGUR_ENV_EXPORT', '0') == '1'
-    __default_config = { 'Database': {"host": "nekocase.augurlabs.io"} }
-
-    if os.getenv('AUGUR_ENV_ONLY', '0') != '1' and no_config_file == 0:
-        try:
-            __config_file = open(__config_file_path, 'r+')
-        except:
-            # logger.info('Couldn\'t open {}, attempting to create. If you have a augur.cfg, you can convert it to a json file using "make to-json"'.format(config_file))
-            if not os.path.exists(__config_location):
-                os.makedirs(__config_location)
-            __config_file = open(__config_file_path, 'w+')
-            __config_bad = True
-
-
-        # Options to export the loaded configuration as environment variables for Docker
-
-        if __export_env:
-
-            export_filename = os.getenv('AUGUR_ENV_EXPORT_FILE', 'augur.cfg.sh')
-            __export_file = open(export_filename, 'w+')
-            # logger.info('Exporting {} to environment variable export statements in {}'.format(config_file, export_filename))
-            __export_file.write('#!/bin/bash\n')
-
-        # Load the config file and return [section][name]
-        try:
-            config_text = __config_file.read()
-            __config = json.loads(config_text)
-            if name is not None:
-                return(__config[section][name])
-            else:
-                return(__config[section])
-
-        except json.decoder.JSONDecodeError as e:
-            if not __config_bad:
-                __using_config_file = False
-                # logger.error('%s could not be parsed, using defaults. Fix that file, or delete it and run this again to regenerate it. Error: %s', __config_file_path, str(e))
-
-            __config = __default_config
-            return(__config[section][name])
