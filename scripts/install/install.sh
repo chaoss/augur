72fda4fb813d2e1b6dc432a327cc79bacb549bea
Carter Landis <c@carterlandis.com>
2020-02-29 13:17:44 -0600

    Refactor install.sh to use new targets feature
    Signed-off-by: Carter Landis <c@carterlandis.com>

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -19,6 +19,16 @@ fi
 
 target=${1-prod}
 
+if [[ $target == *"dev"* ]]; then
+  echo
+  echo "*****INSTALLING FOR PROD*****"
+  echo
+else
+  echo
+  echo "*****INSTALLING FOR DEVELOPMENT*****"
+  echo
+fi
+
 echo "Installing the backend and its dependencies..."
 scripts/install/backend.sh $target > logs/backend-installation.log 2>&1
 echo "Done!"
@@ -27,36 +37,39 @@ echo "Installing workers and their dependencies..."
 scripts/install/workers.sh $target > logs/workers-installation.log 2>&1
 echo "Done!"
 
-if [[ $target == *"dev"* ]]; then
-  echo "Generating documentation..."
-  scripts/install/api_docs.sh > logs/api-doc-generation.log 2>&1
-  echo "Done!"
-fi
-
 echo "Generating a config file..."
 scripts/install/config.sh > logs/config-generation.log 2>&1
 echo "Done!"
 
-echo
-echo "Would you like to install Augur's frontend dependencies?"
-echo
-select choice in "y" "n"
-do
-  case $choice in
-    "y" )
-      scripts/install/frontend.sh > logs/frontend-installation.log 2>&1
-      break
-      ;;
-    "n" )
-      echo "Skipping frontend dependencies..."
-      break
-      ;;
-   esac
-done
+
+if [[ $target == *"dev"* ]]; then
+  echo "Generating documentation..."
+  scripts/install/api_docs.sh > logs/api-doc-generation.log 2>&1
+  echo "Done!"
+
+  echo
+  echo "Would you like to install Augur's frontend dependencies?"
+  echo
+  select choice in "y" "n"
+  do
+    case $choice in
+      "y" )
+        scripts/install/frontend.sh > logs/frontend-installation.log 2>&1
+        break
+        ;;
+      "n" )
+        echo "Skipping frontend dependencies..."
+        break
+        ;;
+     esac
+  done
+else
+  echo "Installing frontend dependencies..."
+  scripts/install/frontend.sh > logs/frontend-installation.log 2>&1
+fi
+
 
 echo "**********************************"
 echo "*** INSTALLATION COMPLETE ***"
-echo
-echo "Documentation found here at:"
 echo "https://oss-augur.readthedocs.io/en/master/"
 echo "**********************************"
