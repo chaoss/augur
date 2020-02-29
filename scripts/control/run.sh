ce3d4280a1bc8df980c96d343b286f122c53e1b3
Carter Landis <c@carterlandis.com>
2020-02-29 17:36:33 -0600

    Clean up Makefile & misc scripts
    Signed-off-by: Carter Landis <c@carterlandis.com>

--- scripts/control/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-./scripts/control/augur.sh
-echo "Waiting for the server to start... (this will take a moment)"
-./scripts/control/collect.sh
-echo "Workers have been started. Happy collecting!"
--- scripts/control/status.sh
+++ scripts/control/status.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 set -euo pipefail
 
+monitor=${1-quick}
+
 echo "augur logs"
 echo "*****************************"
 
-tail logs/augur.log
+if [[ $monitor == "quick" ]]; then
+  tail -n 20 logs/augur.log
 
-echo
-echo "*****************************"
-echo "*****************************"
-echo
+  for WORKER in $(ls -d workers/*/)
+  do
+      if [[ $WORKER == *"_worker"* ]]; then
 
-for WORKER in $(ls -d workers/*/)
-do
-    if [[ $WORKER == *"_worker"* ]]; then
+        # make it pretty for formatting
+        FORMATTED_WORKER=${WORKER/#workers\//}
+        FORMATTED_WORKER=${FORMATTED_WORKER/%\//}
 
-      # make it pretty for formatting
-      FORMATTED_WORKER=${WORKER/#workers\//}
-      FORMATTED_WORKER=${FORMATTED_WORKER/%\//}
+        echo "$FORMATTED_WORKER"
+        echo "*****************************"
 
-      echo "$FORMATTED_WORKER"
-      echo "*****************************"
+        cd $WORKER
+        tail -n 20 *.log
+        echo
+        echo
+        cd ../..
 
-      cd $WORKER
-      tail *.log
-      echo
-      echo
-      cd ../..
-
-    fi
-done
+      fi
+  done
+else
+  less +F logs/augur.log workers/**/*.log
+fi
