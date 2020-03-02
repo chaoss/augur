4093c6ce1d5bee177fbdd090502b2a93961134b2
mrsaicharan1 <saicharan.reddy1@gmail.com>
2020-03-02 10:29:57 +0530

    Added API & metrics tests(PR closed no merge
    Signed-off-by: mrsaicharan1 <saicharan.reddy1@gmail.com>

--- test/api/test_pull_request_routes.py
+++ test/api/test_pull_request_routes.py
@@ -19,3 +19,9 @@ def test_pull_requests_merge_contributor_new_by_repo(metrics):
     assert len(data) >= 1
     assert data[0]["count"] > 0
 
+def test_pull_requests_closed_no_merge(metrics):
+    response = requests.get('http://localhost:5000/api/unstable/repo/20/pull-request-closed-no-merge')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) >= 1
+    assert data[0]["count"] > 0
