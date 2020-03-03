497ada2f78e2d83a13d3d294b5e341c212b0ed32
mrsaicharan1 <saicharan.reddy1@gmail.com>
2020-03-03 08:36:48 +0530

    Changed the metric test values to Travis deployment values
    Signed-off-by: mrsaicharan1 <saicharan.reddy1@gmail.com>

--- test/metrics/test_pull_request_metrics.py
+++ test/metrics/test_pull_request_metrics.py
@@ -28,7 +28,7 @@ def test_pull_request_acceptance_rate(metrics):
                                      end_date='2019-12-31 23:59:59',group_by='year').iloc[0]['rate'] > 0
 
 def test_pull_request_closed_no_merge(metrics):
-    assert metrics.pull_requests_closed_no_merge(10).iloc[0]['pr_count'] > 0
-    assert metrics.pull_requests_closed_no_merge(1, 25430, begin_date='2018-1-1 00:00:00',
+    assert metrics.pull_requests_closed_no_merge(24).iloc[0]['pr_count'] > 0
+    assert metrics.pull_requests_closed_no_merge(24, 21000, begin_date='2018-1-1 00:00:00',
                                      end_date='2019-12-31 23:59:59').iloc[0]['pr_count'] > 0
 
