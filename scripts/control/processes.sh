80a0a54ed3b60eb5c286671079822976a93de657
Carter Landis <c@carterlandis.com>
2020-03-08 16:41:03 -0500

    Increase verbosity of process control scripts
    Signed-off-by: Carter Landis <c@carterlandis.com>

--- scripts/control/processes.sh
+++ scripts/control/processes.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set -euo pipefail
 
 if [[ "$VIRTUAL_ENV" ]]; then
     echo "Listing augur processes..."
-     ps -ef | grep -ie $VIRTUAL_ENV/ | grep -v grep | awk '{print $2}'
+     ps -efx | grep -ie $VIRTUAL_ENV/ | grep -v grep | awk '{print "PID " $2 ": " $8}'
 
     echo "Listing worker processes..."
-     ps aux | grep -ie $VIRTUAL_ENV/ | grep -ie bin | grep worker | awk '{print $2}'
+     ps -efx | grep -ie $VIRTUAL_ENV/ | grep -v grep | grep worker | awk '{print "PID " $2 ": " $8}'
 else
     echo "We noticed you're not in a virtual environment. Please activate your augur virtual environment and run the command again."
 fi
