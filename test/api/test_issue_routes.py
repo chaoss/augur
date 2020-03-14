839c31d10432561ca0fb848b9e3100d8938f2145
Carter Landis <c@carterlandis.com>
2020-03-14 14:39:02 -0500

    Update metrics API testing IDs
    Signed-off-by: Carter Landis <c@carterlandis.com>

--- test/api/test_issue_routes.py
+++ test/api/test_issue_routes.py
@@ -13,7 +13,7 @@ def test_issues_new_by_group(metrics):
     assert data[0]['issues'] > 0
 
 def test_issues_new_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/issues-new')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/issues-new')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -27,7 +27,7 @@ def test_issues_active_by_group(metrics):
     assert data[0]['issues'] > 0
 
 def test_issues_active_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/issues-active')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/issues-active')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -41,7 +41,7 @@ def test_issues_closed_by_group(metrics):
     assert data[0]['issues'] > 0
 
 def test_issues_closed_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/issues-closed')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/issues-closed')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -54,7 +54,7 @@ def test_issue_duration_by_group(metrics):
     assert len(data) >= 1
 
 def test_issue_duration_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/issue-duration')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/issue-duration')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -67,7 +67,7 @@ def test_issue_participants_by_group(metrics):
     assert data[0]['participants'] > 0
 
 def test_issue_participants_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/issue-participants')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/issue-participants')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -81,7 +81,7 @@ def test_issue_throughput_by_group(metrics):
     assert data[0]['throughput'] >= 0
 
 def test_issue_throughput_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/issue-throughput')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/issue-throughput')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -95,7 +95,7 @@ def test_issue_backlog_by_group(metrics):
     assert data[0]['issue_backlog'] > 0
 
 def test_issue_backlog_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/issue-backlog')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/issue-backlog')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -109,7 +109,7 @@ def test_issues_first_time_opened_by_group(metrics):
     assert data[0]["count"] > 0
 
 def test_issues_first_time_opened_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/issues-first-time-opened')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/issues-first-time-opened')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -122,12 +122,12 @@ def test_issues_first_time_closed_by_group(metrics):
     assert len(data) >= 1
     assert data[0]["count"] > 0
 
-# def test_issues_first_time_closed_by_repo(metrics):
-#     response = requests.get('http://localhost:5000/api/unstable/repo-groups/20/repos/25436/issues-first-time-closed')
-#     data = response.json()
-#     assert response.status_code == 200
-#     assert len(data) >= 1
-#     assert data[0]["count"] > 0
+def test_issues_first_time_closed_by_repo(metrics):
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/20/repos/25430/issues-first-time-closed')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) >= 1
+    assert data[0]["count"] > 0
 
 def test_open_issues_count_by_group(metrics):
     response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/open-issues-count')
@@ -137,7 +137,7 @@ def test_open_issues_count_by_group(metrics):
     assert data[0]['open_count'] > 0
 
 def test_open_issues_count_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/open-issues-count')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/open-issues-count')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -151,7 +151,7 @@ def test_closed_issues_count_by_group(metrics):
     assert data[0]['closed_count'] > 0
 
 def test_closed_issues_count_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/closed-issues-count')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/closed-issues-count')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -165,7 +165,7 @@ def test_issues_open_age_by_group(metrics):
     assert data[0]["open_date"] > 1
 
 def test_issues_open_age_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/issues-open-age/')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/issues-open-age/')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -180,14 +180,14 @@ def test_issues_closed_resolution_duration_by_group(metrics):
 
 
 def test_issues_closed_resolution_duration_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/issues-closed-resolution-duration/')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/issues-closed-resolution-duration/')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
     assert data[0]["diffdate"] >= 0
 
 def test_issues_maintainer_response_duration_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/issues-maintainer-response-duration/')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/issues-maintainer-response-duration/')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -208,7 +208,7 @@ def test_average_issue_resolution_time_by_group(metrics):
     assert len(data) > 0
 
 def test_average_issue_resolution_time_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/average-issue-resolution-time')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/average-issue-resolution-time')
     data = response.json()
     assert response.status_code == 200
     assert len(data) > 0
@@ -220,7 +220,7 @@ def test_issue_comments_mean_by_group(metrics):
     assert len(data) >= 1
 
 def test_issue_comments_mean_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/issue-comments-mean')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/issue-comments-mean')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -232,7 +232,7 @@ def test_issue_comments_mean_std_by_group(metrics):
     assert len(data) >= 1
 
 def test_issue_comments_mean_std_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/issue-comments-mean-std')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/issue-comments-mean-std')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
--- test/api/test_repo_meta_routes.py
+++ test/api/test_repo_meta_routes.py
@@ -13,7 +13,7 @@ def test_code_changes_by_group(metrics):
     assert data[0]['commit_count'] > 0
 
 def test_code_changes_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/code-changes')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/code-changes')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -28,7 +28,7 @@ def test_code_changes_lines_by_group(metrics):
     assert data[0]['removed'] >= 0
 
 def test_code_changes_lines_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/code-changes-lines')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/code-changes-lines')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -43,14 +43,14 @@ def test_sub_projects_by_group(metrics):
     assert data[0]["sub_project_count"] > 0
 
 def test_sub_projects_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/sub-projects')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/sub-projects')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
     assert data[0]["sub_project_count"] > 0
 
 def test_cii_best_practices_badge_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/cii-best-practices-badge')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/cii-best-practices-badge')
     print(response)
     data = response.json()
     assert response.status_code == 200
@@ -71,28 +71,28 @@ def test_languages_by_repo(metrics):
 #     assert len(data) >= 1
 
 # def test_license_declared_by_repo(metrics):
-#     response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/license-declared')
+#     response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/license-declared')
 #     data = response.json()
 #     assert response.status_code == 200
 #     assert len(data) >= 1
 
-# def test_annual_lines_of_code_count_ranked_by_new_repo_in_repo_group_by_repo(metrics):
-#     response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/annual-lines-of-code-count-ranked-by-new-repo-in-repo-group')
-#     data = response.json()
-#     assert response.status_code == 200
-#     assert len(data) >= 1
-#     assert data[0]["net"] > 0
+def test_annual_lines_of_code_count_ranked_by_new_repo_in_repo_group_by_repo(metrics):
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/annual-lines-of-code-count-ranked-by-new-repo-in-repo-group')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) >= 1
+    assert data[0]["net"] > 0
 
-# def test_annual_lines_of_code_count_ranked_by_new_repo_in_repo_group_by_group(metrics):
-#     response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/annual-lines-of-code-count-ranked-by-new-repo-in-repo-group')
-#     data = response.json()
-#     assert response.status_code == 200
-#     assert len(data) >= 1
-#     assert data[0]["net"] > 0
+def test_annual_lines_of_code_count_ranked_by_new_repo_in_repo_group_by_group(metrics):
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/annual-lines-of-code-count-ranked-by-new-repo-in-repo-group')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) >= 1
+    assert data[0]["net"] > 0
 
 
 def test_annual_lines_of_code_count_ranked_by_repo_in_repo_group_by_repo(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/annual-lines-of-code-count-ranked-by-repo-in-repo-group')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/annual-lines-of-code-count-ranked-by-repo-in-repo-group')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
@@ -112,7 +112,7 @@ def test_annual_lines_of_code_count_ranked_by_repo_in_repo_group_by_group(metric
 #     assert len(data) >= 1
 
 # def test_license_coverage_by_repo(metrics):
-#     response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/license-coverage')
+#     response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/license-coverage')
 #     data = response.json()
 #     assert response.status_code == 200
 #     assert len(data) >= 1
@@ -124,7 +124,7 @@ def test_annual_lines_of_code_count_ranked_by_repo_in_repo_group_by_group(metric
 #     assert len(data) >= 1
 
 # def test_license_count_by_repo(metrics):
-#     response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25435/license-count')
+#     response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/license-count')
 #     data = response.json()
 #     assert response.status_code == 200
 #     assert len(data) >= 1
