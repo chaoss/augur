b4a34454a1b321689aa22833dea43a4bd715b121
Nodira <nodira.ibrogimova@gmail.com>
2020-04-13 22:45:41 +0500

    refactor: minor changes in BusFactor vue file
    Signed-off-by: Nodira <nodira.ibrogimova@gmail.com>

--- frontend/src/components/charts/BusFactor.vue
+++ frontend/src/components/charts/BusFactor.vue
@@ -17,7 +17,6 @@
   </div>
 </template>
 
-
 <script>
 export default {
   props: ['source', 'citeUrl', 'citeText', 'title'],
@@ -31,19 +30,16 @@ export default {
       return this.$store.state.baseRepo
     },
     chart() {
-      $(this.$el).find('.showme').addClass('invis')
-      $(this.$el).find('.textchart').addClass('loader')
+      $(this.$el).find('.showme').addClass('invis');
+      $(this.$el).find('.textchart').addClass('loader');
       if (this.repo) {
         window.AugurRepos[this.repo][this.source]().then((data) => {
-          $(this.$el).find('.showme, .hidefirst').removeClass('invis')
-          $(this.$el).find('.textchart').removeClass('loader')
-          this.values = data
-          console.log('Data:')
-          console.log(data)
+          $(this.$el).find('.showme, .hidefirst').removeClass('invis');
+          $(this.$el).find('.textchart').removeClass('loader');
+          this.values = data;
         })
       }
     }
   }
 }
-
 </script>
