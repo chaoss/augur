98bcc228229d0c7fb208995a23e45907706c61cd
Sean P. Goggins <s@goggins.com>
2020-04-20 12:23:17 -0500

    Fixing schema update.

--- schema/generate/14-schema_update_16.sql
+++ schema/generate/14-schema_update_16.sql
@@ -1,18 +1,31 @@
+
+-- ----------------------------
+-- Table structure for pull_request_files
+-- ----------------------------
+DROP TABLE IF EXISTS "augur_data"."pull_request_files";
 CREATE TABLE "augur_data"."pull_request_files" (
-  "pull_reques" int8,
+  "pull_request_id" int8,
   "tool_source" varchar(254) COLLATE "pg_catalog"."default",
-  "tool_versio" varchar(254) COLLATE "pg_catalog"."default",
+  "tool_version" varchar(254) COLLATE "pg_catalog"."default",
   "data_source" varchar(254) COLLATE "pg_catalog"."default",
-  "data_collec" date,
+  "data_collection_date" date,
   "pr_file_id" int8 NOT NULL,
-  "pr_file_add" int8,
-  "pr_file_del" int8,
-  "pr_file_pat" varchar(254) COLLATE "pg_catalog"."default",
-  CONSTRAINT "pull_request_files_pkey" PRIMARY KEY ("pr_file_id")
+  "pr_file_additions" int8,
+  "pr_file_deletions" int8,
+  "pr_file_path" varchar(254) COLLATE "pg_catalog"."default"
 )
 ;
-
-ALTER TABLE "augur_data"."pull_request_files" ADD CONSTRAINT "PR_ID-Files" FOREIGN KEY ("pull_reques") REFERENCES "augur_data"."pull_requests" ("pull_request_id") ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE;
-
 ALTER TABLE "augur_data"."pull_request_files" OWNER TO "augur";
 
+-- ----------------------------
+-- Primary Key structure for table pull_request_files
+-- ----------------------------
+ALTER TABLE "augur_data"."pull_request_files" ADD CONSTRAINT "pull_request_files_pkey" PRIMARY KEY ("pr_file_id");
+
+-- ----------------------------
+-- Foreign Keys structure for table pull_request_files
+-- ----------------------------
+ALTER TABLE "augur_data"."pull_request_files" ADD CONSTRAINT "PR_ID-Files" FOREIGN KEY ("pull_request_id") REFERENCES "augur_data"."pull_requests" ("pull_request_id") ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE;
+
+
+update "augur_operations"."augur_settings" set value = 16 where setting = 'augur_data_version'; 
