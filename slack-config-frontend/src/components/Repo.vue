8744fef62f35d36b0644393f7bd6aece4d40bb4b
michaelwoodruffdev <michaelwoodruffdev@gmail.com>
2020-05-14 12:43:24 -0500

    individual removal of tracked repos implementated

--- slack-config-frontend/src/components/Repo.vue
+++ slack-config-frontend/src/components/Repo.vue
@@ -3,6 +3,7 @@
     <p>{{ name }} ({{ group }})</p>
     <div class="righthand-options">
       <aug-checkbox v-if="checkable" ref="checkbox" @flipCheck="setIsChecked"/>
+      <i class="fas fa-times delete" v-if="deletable" @click="$emit('delete', id)" />
     </div>
   </div>
 </template>
@@ -51,5 +52,16 @@ export default {
 
 p {
   margin: 0;
+}
+
+.delete {
+  color: var(--red);
+  font-size: 1.5rem;
+  transition: text-shadow .3s ease;
+}
+
+.delete:hover {
+  text-shadow: 0 0 3px grey;
+  cursor: pointer;
 }
 </style>
