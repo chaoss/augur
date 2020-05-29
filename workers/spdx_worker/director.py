2f621a3d9f4f2d5226a5e6ffe8cf2c212645b8a2
Matt Snell <msnell@unomaha.edu>
2020-05-29 10:17:11 -0500

    Usability updates for spdx_worker
    Signed-off-by: Matt Snell <msnell@unomaha.edu>

--- workers/spdx_worker/director.py
+++ workers/spdx_worker/director.py
@@ -5,6 +5,7 @@ import json
 import re
 import os
 import requests
+from os.path import expanduser
 
 import sbom_populate as p
 import initial_scans as s
@@ -18,8 +19,12 @@ if __name__ == "__main__":
         host = config["Database"]["host"]
         port = config["Database"]["port"]
         dsfile = config["Workers"]["license_worker"]["tagfile"]
+        depth = config["Workers"]["license_worker"]["search_depth"]
         ipath = config["Workers"]["facade_worker"]["repo_directory"]
+        home = expanduser("~")
+
         print("IPATH EQUALS " + ipath)
+        print("HOME EQUALS " + home)
 
         configtools = 'postgresql://{}:{}@{}:{}/{}'.format(
             user, password, host, port, dbname
@@ -27,14 +32,21 @@ if __name__ == "__main__":
 
         with open("dosocs2-example.conf") as configfile:
             content = configfile.read()
-            content_new = re.sub('(connection_uri = .*)\n', "connection_uri = " + configtools + "\n", content)
+            content_new = re.sub('(connection_uri = .*)\n', "connection_uri = " + configtools + "?options=--search_path=spdx\n", content)
             with open("dosocs2.conf","w+") as outfile:
                 outfile.write(content_new)
+            with open(home + "/.config/dosocs2/dosocs2.conf","w+") as coreconfig:
+                coreconfig.write(content_new)
+
+        wd = os.getcwd()
 
         print("---------------------")
         print("INITIAL SCANS RUNNING")
         print("---------------------")
-        s.scan(dbname, user, password, host, port, dsfile, ipath)
+        s.scan(dbname, user, password, host, port, dsfile, ipath, depth)
+        #print(os.getcwd())
+        os.chdir(wd)
+        #print(os.getcwd())
         print("------------------")
         print("SBOM SCANS RUNNING")
         print("------------------")
--- workers/spdx_worker/initial_scans.py
+++ workers/spdx_worker/initial_scans.py
@@ -3,10 +3,33 @@ import psycopg2
 from subprocess import PIPE
 import json
 import re
+import time
 import os
 import requests
 
-def scan(dbname, user, password, host, port, dsfile, ipath):
+flevel = 0
+
+def depthwalk(ipath, depth, match):
+   k = 0
+   #print("IPATH " + ipath)
+   if depth > 0:
+     for dir in os.listdir(ipath):
+       if not ipath.endswith("/"):
+           usedir = ipath + "/" + dir
+       else:
+           usedir = ipath + dir
+       #print(str(usedir) + " " + str(depth))
+       if os.path.isdir(usedir):
+           #print("directory!")
+           if dir == match:
+              print("FOLDER FOUND: " + str(usedir))
+              pathtot.append(usedir)
+              break
+           depthwalk(usedir, depth - 1, match)
+       #else:
+           #print("not dir")
+
+def scan(dbname, user, password, host, port, dsfile, ipath, depth):
     connection = psycopg2.connect(
         user = user,
         password = password,
@@ -19,6 +42,8 @@ def scan(dbname, user, password, host, port, dsfile, ipath):
     r = cur.execute("set search_path to augur_data; select repo_path, repo_id, repo_group_id, repo_name from repo order by repo_group_id;")
     rec = cur.fetchall()
     for sector in rec:
+        global pathtot
+        pathtot = []
         print(sector)
         repo_id = sector[1]
         print("****************")
@@ -30,36 +55,48 @@ def scan(dbname, user, password, host, port, dsfile, ipath):
             cur.execute("select dosocs_pkg_id from spdx.augur_repo_map where repo_id = " + str(repo_id) + " LIMIT 1;")
             records = cur.fetchall()
             print("****************")
-            print("CREATING NEW RECORD")
             print(str(sector[0]))
-            path = ipath + "/" + str(sector[3])
-            print("llllllllllllllllllllllll")
-            print(path)
-            print("llllllllllllllllllllllll")
-            cur.execute("SELECT repo_path FROM spdx.augur_repo_map WHERE" + chr(39) + path + chr(39) + " " + chr(61) + " repo_path;")
-            check = bool(cur.rowcount)
-            if check == True:
-                print("Record Exists in Mapping Table")
+            if not ipath.endswith("/"):
+            	ipath = ipath + "/"
+            #path = ipath + str(sector[3])
+            os.chdir(ipath)
+            print("---------------")
+            #need to make this a config parameter
+            depthwalk(ipath, depth, sector[3])
+            time.sleep(0.2)
+            print("INSIDE: "  + str(pathtot))
+            print("---------------")
+            if pathtot != []:
+                path = pathtot[0]
+                print("PATH: " + str(path))
+                print("SELECT repo_path FROM spdx.augur_repo_map WHERE " + chr(39) + path + chr(39) + " " + chr(61) + " repo_path;")
+                cur.execute("SELECT repo_path FROM spdx.augur_repo_map WHERE " + chr(39) + path + chr(39) + " " + chr(61) + " repo_path;")
+                if str(len(cur.fetchall())) == "0":
+                    print("ALL CHECKS PASSED")
+                    #Create a new record in "packages" table.
+                    #dosocs will determine whether the entry has already been made
+                    print("Creating Record for " + str(sector[1]))
+                    #cur.execute("INSERT INTO spdx.augur_repo_map(repo_id, repo_path) VALUES (" + str(sector[1]) + "," + chr(39) + str(sector[0]) + str(sector[3]) + chr(39) + ");")
+                    cur.execute("INSERT INTO spdx.augur_repo_map(repo_id, repo_path) VALUES (" + str(sector[1]) + "," + chr(39) + path + chr(39) + ");")
+                    connection.commit()
+                    #Attempt to create new DoSOCS entry
+                    print("CREATING NEW DOSOCS DOCUMENT")
+                    print(path)
+                    p = subprocess.Popen(['dosocs2', 'scan', str(path)], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    print(str(p.communicate()))
+                    (output) = p
+                    print("####################")
+                    print(output)
+                    print("RECORD CREATED")
+                else:
+                    print("RECORD EXISTS IN MAP TABLE")
             else:
-                #Create a new record in "packages" table.
-                #dosocs will determine whether the entry has already been made
-                print("Creating Record for " + str(sector[1]))
-                cur.execute("INSERT INTO spdx.augur_repo_map(repo_id, repo_path) VALUES (" + str(sector[1]) + "," + chr(39) + str(sector[0]) + str(sector[3]) + chr(39) + ");")
-                connection.commit()
-                #Attempt to create new DoSOCS entry
-                print("CREATING NEW DOSOCS DOCUMENT")
-                print(path)
-                p = subprocess.call(['dosocs2', 'scan', str(path), '-f', 'dosocs2.conf'], shell=False, stdout=PIPE, stderr=PIPE)
-                (output) = p
-                print("####################")
-                print(output)
-                print("RECORD CREATED")
+                print("NO DIRECTORY, SKIPPING")
         else:
-            print("DUPLICATE RECORD FOUND. SKIPPING...")
-        cur.execute("update augur_repo_map a set dosocs_pkg_name = b.name from packages b where a.repo_path = b.download_location;")
-        print("FIRST QUERY RESULTS")
-        print(cur.fetchall)
-        cur.execute("update augur_repo_map a set dosocs_pkg_id = b.package_id from packages b where a.repo_path = b.download_location;")
-        print("SECOND QUERY RESUTS")
-        print(cur.fetchall)
+            print("DUPLICATE RECORD FOUND IN REPO_SBOM_SCANS. SKIPPING...")
+    cur.execute("update augur_repo_map a set dosocs_pkg_name = b.name from packages b where a.repo_path = b.download_location;")
+    cur.execute("update augur_repo_map a set dosocs_pkg_id = b.package_id from packages b where a.repo_path = b.download_location;")
+    connection.commit()
+    cur.close()
+    connection.close()
     return
