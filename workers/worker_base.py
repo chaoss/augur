ee58a76c89f1c090d8a35e11b47a777028574183
Carter Landis <c@carterlandis.com>
2020-06-03 15:47:50 -0500

    Update template_worker to use new worker_base
    Signed-off-by: Carter Landis <c@carterlandis.com>

--- workers/worker_base.py
+++ workers/worker_base.py
@@ -49,7 +49,7 @@ class Worker():
             worker_defaults = self.augur_config.get_default_config()["Workers"][self.config["worker_type"]]
             self.config.update(worker_defaults)
         except KeyError as e:
-            logger.warn("Could not get default configuration for ", self.config["worker_typed"])
+            logging.warn("Could not get default configuration for ", self.config["worker_type"])
 
         worker_info = self.augur_config.get_value("Workers", self.config["worker_type"])
         self.config.update(worker_info)
