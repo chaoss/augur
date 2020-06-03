753625adc826fe5be2c52df426480ad81092117f
Carter Landis <c@carterlandis.com>
2020-06-03 13:59:14 -0500

    Update CII Badge worker to use new worker_base
    Signed-off-by: Carter Landis <c@carterlandis.com>

--- workers/linux_badge_worker/linux_badge_worker.py
+++ workers/linux_badge_worker/linux_badge_worker.py
@@ -6,18 +6,20 @@ import json
 from urllib.parse import quote
 from multiprocessing import Process, Queue
 
-from linux_badge_worker import __data_source__, __tool_source__, __tool_version__
 import pandas as pd
 import sqlalchemy as s
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import MetaData
 from workers.worker_base import Worker
 
-class BadgeWorker(Worker):
+class LinuxBadgeWorker(Worker):
     """ Worker that collects repo badging data from CII
     config: database credentials, broker information, and ID
     """
-    def __init__(self, config, task=None):
+    def __init__(self, config={}):
+
+        worker_type = "linux_badge_worker"
+
         given = [['git_url']]
         models = ['badges']
 
@@ -25,31 +27,37 @@ class BadgeWorker(Worker):
         operations_tables = ['worker_history', 'worker_job']
 
         # Run the general worker initialization
-        super().__init__(config, given, models, data_tables, operations_tables)
+        super().__init__(worker_type, config, given, models, data_tables, operations_tables)
+
+        self.config.update({"endpoint": "https://bestpractices.coreinfrastructure.org/projects.json?pq="})
+        self.tool_source = 'Linux Badge Worker'
+        self.tool_version = '0.1.0'
+        self.data_source = 'CII Badging API'
+
 
     def badges_model(self, entry_info, repo_id):
         """ Data collection and storage method
         Query the CII API and store the result in the DB for the badges model
         """
         git_url = entry_info['given']['git_url']
-        logging.info("Collecting data for {}".format(git_url))
+        self.logger.info("Collecting data for {}".format(git_url))
         extension = quote(git_url[0:-4])
 
         url = self.config['endpoint'] + extension
-        logging.info("Hitting CII endpoint: " + url + " ...")
+        self.logger.info("Hitting CII endpoint: " + url + " ...")
         data = requests.get(url=url).json()
 
         if data != []:
-            logging.info("Inserting badging data for " + git_url)
+            self.logger.info("Inserting badging data for " + git_url)
             self.db.execute(self.repo_badging_table.insert()\
                             .values(repo_id=repo_id,
                                     data=data,
-                                    tool_source=__tool_source__,
-                                    tool_version=__tool_version__,
-                                    data_source=__data_source__))
+                                    tool_source=self.tool_source,
+                                    tool_version=self.tool_version,
+                                    data_source=self.data_source))
 
             self.results_counter += 1
         else:
-            logging.info("No CII data found for {}\n".format(git_url))
+            self.logger.info("No CII data found for {}\n".format(git_url))
 
         self.register_task_completion(entry_info, repo_id, "badges")
