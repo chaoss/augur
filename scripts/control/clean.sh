6a24bd819e7d2724708429c8c532eb5f1ea2e46f
Carter Landis <c@carterlandis.com>
2020-06-03 16:03:27 -0500

    Update clean.sh remove all logs
    Signed-off-by: Carter Landis <c@carterlandis.com>

--- scripts/control/clean.sh
+++ scripts/control/clean.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-rm -rf frontend/public .pytest_cache logs *.out env.txt pyenv.txt
-rm -rf workers/**/*.log workers/**/*.err workers/**/*.out
+rm -rf frontend/public .pytest_cache logs/**/*.log *.out env.txt pyenv.txt workers/**/*.log *.log
 find . -name \*.pyc -delete
 find . -type f -name "*.lock" -delete
