2e26c6ba0b7482feecde18ee818a2418a603572e
Carter Landis <c@carterlandis.com>
2020-06-04 11:10:44 -0500

    Remove duplicate schema changes to repo table
    Signed-off-by: Carter Landis <c@carterlandis.com>

--- schema/generate/16-schema_update_18.sql
+++ schema/generate/16-schema_update_18.sql
@@ -30,10 +30,6 @@ ALTER TABLE "augur_data"."releases" OWNER TO "augur";
 
 ALTER TABLE "augur_data"."repo" ALTER COLUMN "forked_from" TYPE varchar COLLATE "pg_catalog"."default" USING "forked_from"::varchar;
 
-ALTER TABLE "augur_data"."repo" ADD COLUMN "repo_archived" int4;
-
-ALTER TABLE "augur_data"."repo" ADD COLUMN "repo_archived_date_collected" timestamptz(0);
-
 SELECT setval('"augur_data"."releases_release_id_seq"', 1, false);
 
 ALTER SEQUENCE "augur_data"."releases_release_id_seq"
