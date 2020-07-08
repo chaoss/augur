d2246620ff2f79144aad0f3664617af0b0299f48
mrsaicharan1 <saicharan.reddy1@gmail.com>
2020-07-08 15:03:50 +0530

    Augur config tests added
    Signed-off-by: mrsaicharan1 <saicharan.reddy1@gmail.com>

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -4,6 +4,8 @@ import pandas as pd
 from queue import Queue
 from workers.util import read_config
 from workers.worker_base import Worker
+from augur.config import AugurConfig, default_config
+from augur import ROOT_AUGUR_DIRECTORY
 
 def test_dump_queues():
     sample_queue = Queue()
@@ -22,3 +24,21 @@ def test_read_config_no_exception():
 def test_read_config_exception():
     with pytest.raises(AttributeError):
         db_name = read_config('Server', 'username')
+
+def test_config_get_section_no_exception():
+    test_config = default_config
+    test_config['Database']['user'] = "test_user"
+    config_object = AugurConfig(ROOT_AUGUR_DIRECTORY, test_config)
+    assert type(config_object.get_section("Database")) == dict
+
+def test_config_get_section_exception():
+    test_config = default_config
+    test_config['Database']['user'] = "test_user"
+    config_object = AugurConfig(ROOT_AUGUR_DIRECTORY, test_config)
+    with pytest.raises(KeyError):
+        config_object.get_section("absent_section")
+
+
+
+
+
