b172d2fb24db17c5a1047a991ee88220a7a59f65
mrsaicharan1 <saicharan.reddy1@gmail.com>
2020-07-20 01:34:44 +0530

    Comparison of results working

--- tests/test_workers/test_gitlab_worker.py
+++ tests/test_workers/test_gitlab_worker.py
@@ -4,6 +4,8 @@ from time import sleep
 
 from workers.gitlab_issues_worker.gitlab_issues_worker import GitLabIssuesWorker
 from workers.worker_base import Worker
+import sqlalchemy as s
+
 @pytest.fixture
 def test_task():
     return {
@@ -27,14 +29,35 @@ def gitlab_issues_worker():
     return gitlab_issues_worker
 
 def test_gitlab_issues_worker(gitlab_issues_worker, test_task):
+
+    # connect to sqlalchemy models
+    worker_config = gitlab_issues_worker.config
+    database_connection_string = 'postgresql://{}:{}@{}:{}/{}'.format(
+            worker_config["user_database"], worker_config["password_database"],
+            worker_config["host_database"], worker_config["port_database"], worker_config["name_database"]
+        )
+
+    csearch_path_options = 'augur_data'
+
+    engine = s.create_engine(database_connection_string, poolclass=s.pool.NullPool,
+        connect_args={'options': f'-csearch_path={csearch_path_options}'}, pool_pre_ping=True)
+
+    conn = engine.connect()
+    result = engine.execute("select * from augur_data.issues")
+    for row in result:
+        print(row)
+
+
+
+
     gitlab_issues_worker._queue.put(test_task)
     gitlab_issues_worker.collect()
-
+    
     # data persistence test
     print('collection done')
     issues = requests.get('https://gitlab.com/api/v4/projects/6853087/issues', headers=gitlab_issues_worker.headers)
     issues_list = issues.json()
-    id_list = [issue['_id'] for issue in issues_list]
+    id_list = [issue['id'] for issue in issues_list]
 
     
 
