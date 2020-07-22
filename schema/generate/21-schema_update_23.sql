caf126194efd4f696dd67ab3bfee2164bca8741a
Sean P. Goggins <s@goggins.com>
2020-07-22 12:04:45 -0500

    error check on db update

--- schema/generate/21-schema_update_23.sql
+++ schema/generate/21-schema_update_23.sql
@@ -1,19 +1,19 @@
-CREATE INDEX CONCURRENTLY "contributor_delete_finder" ON "augur_data"."contributors" USING brin (
+CREATE INDEX if not exists CONCURRENTLY "contributor_delete_finder" ON "augur_data"."contributors" USING brin (
   "cntrb_id",
   "cntrb_email"
 );
 
-CREATE INDEX CONCURRENTLY "contributor_worker_finder" ON "augur_data"."contributors" USING brin (
+CREATE INDEX CONCURRENTLY if not exists "contributor_worker_finder" ON "augur_data"."contributors" USING brin (
   "cntrb_login",
   "cntrb_email", 
   "cntrb_id"
 );
 
-CREATE INDEX CONCURRENTLY "contributor_worker_fullname_finder" ON "augur_data"."contributors" USING brin (
+CREATE INDEX CONCURRENTLY if not exists "contributor_worker_fullname_finder" ON "augur_data"."contributors" USING brin (
   "cntrb_full_name"
 );
 
-CREATE INDEX CONCURRENTLY "contributor_worker_email_finder" ON "augur_data"."contributors" USING brin (
+CREATE INDEX CONCURRENTLY if not exists "contributor_worker_email_finder" ON "augur_data"."contributors" USING brin (
   "cntrb_email"
 );
 
