55632a6189a38d8efbb8cdd711b5ae752488055c
Sean P. Goggins <s@goggins.com>
2020-09-21 12:38:28 -0500

    update config.py to include housekeeper blocks for the gitlab issue and merge request workers.

--- augur/config.py
+++ augur/config.py
@@ -111,6 +111,40 @@ default_config = {
                     ],
                     "model": "releases",
                     "repo_group_id": 0
+                },
+                {
+                    "all_focused": 1,
+                    "delay": 150000,
+                    "given": [
+                        "git_url"
+                    ],
+                    "model": "gitlab_issues",
+                    "repo_group_id": 0
+                },
+                {
+                    "all_focused": 1,
+                    "delay": 150000,
+                    "given": [
+                        "gitlab_url"
+                    ],
+                    "model": "merge_requests",
+                    "repo_group_id": 0
+                },
+                {
+                    "delay": 150000,
+                    "given": [
+                        "gitlab_url"
+                    ],
+                    "model": "merge_request_files",
+                    "repo_group_id": 0
+                },
+                {
+                    "delay": 150000,
+                    "given": [
+                        "gitlab_url"
+                    ],
+                    "model": "merge_request_commits",
+                    "repo_group_id": 0
                 }
             ]
         },
