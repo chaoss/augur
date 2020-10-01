2cc228f55ac0497f75aacfaa190af9bf5d4d6bcc
Sean Goggins <s@goggins.com>
2020-10-01 12:58:44 -0500

    more changes

--- schema/generate/25-schema_update_27.sql
+++ schema/generate/25-schema_update_27.sql
@@ -18,4 +18,12 @@ ALTER TABLE "spdx"."packages" DROP CONSTRAINT "uc_sha256_ds2_dir_code_exactly_on
 
 ALTER TABLE "spdx"."packages" ADD CONSTRAINT "uc_sha256_ds2_dir_code_exactly_one" CHECK (((sha256 IS NOT NULL)::integer + (dosocs2_dir_code IS NOT NULL)::integer) = 1);
 
+ALTER TABLE "spdx"."identifiers" DROP CONSTRAINT "ck_identifier_exactly_one";
+
+ALTER TABLE "spdx"."identifiers" ADD CONSTRAINT "ck_identifier_exactly_one" CHECK ((((((document_id IS NOT NULL))::integer + ((package_id IS NOT NULL))::integer) + ((package_file_id IS NOT NULL))::integer) = 1));
+
+ALTER TABLE "spdx"."packages" DROP CONSTRAINT "uc_sha256_ds2_dir_code_exactly_one";
+
+ALTER TABLE "spdx"."packages" ADD CONSTRAINT "uc_sha256_ds2_dir_code_exactly_one" CHECK (((((sha256 IS NOT NULL))::integer + ((dosocs2_dir_code IS NOT NULL))::integer) = 1));
+
 update "augur_operations"."augur_settings" set value = 27 where setting = 'augur_data_version'; 
