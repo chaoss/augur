a5fa003846dc5dd3b7381fc2d875ba8f802c52bb
Gabe Heim <gabe@gabehe.im>
2020-10-25 16:08:50 -0500

    add length check in handle alias in case where a canonical contributor row is not found from email

--- workers/contributor_worker/contributor_worker.py
+++ workers/contributor_worker/contributor_worker.py
@@ -608,6 +608,9 @@ class ContributorWorker(Worker):
                 orient="records"))
             if len(canonical_id_result) > 1:
                 self.logger.info("MORE THAN ONE CANONICAL CONTRIBUTOR found for email: {}".format(cntrb_email))
+            elif len(canonical_id_result) == 0:
+                self.logger.info(f"Warning! Did not find a canonical contributor for supposed canonical email: {cntrb_email}")
+
             alias_tuple = {
                 'cntrb_id': canonical_id_result[0]['canonical_id'],
                 'cntrb_a_id': alias_id,
