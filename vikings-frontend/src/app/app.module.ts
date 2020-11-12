6770b07d2129fb21cf92aafeaa783ea4277de34b
Tiffany Young <tcy8v6@umsystem.edu>
2020-11-12 15:55:56 -0600

    create home component-- hit repogroup end points and pull in data to table

--- vikings-frontend/src/app/app.module.ts
+++ vikings-frontend/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ vikings-frontend/src/app/home/home.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  public repogroups:any;
+
+  constructor(public http: HttpClient) { }
+
+  ngOnInit(): void {
+
+    this.getRepoGroups();
+  }
+
+
+  getRepoGroups(){
+    const customheaders= new HttpHeaders()
+          .set('Content-Type', 'application/json');
+
+    this.http.get("http://zephyr.osshealth.io:5222/api/unstable/repo-groups", {headers: customheaders}).subscribe(
+      response=> {
+        console.log(response)
+        this.repogroups=response;
+
+
+
+
+      },
+      error => {
+        console.log(error)
+      }
+    )
+    
+	}
+
+
+
+
+}
