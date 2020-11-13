e15905dd4205b1ce296a75b4c06d5bea3c6ca8ee
Mike Weiss <mjwcbc@umsystem.edu>
2020-11-12 20:31:35 -0600

    Added dashboard component

--- packers-new-frontend/PackersNewFrontend/src/app/app-routing.module.ts
+++ packers-new-frontend/PackersNewFrontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: DashboardComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
