ffeaaef00ee50d4c9634ee3fb85c2a8a3a4c05b7
Carter Landis <c@carterlandis.com>
2020-11-18 18:18:27 -0600

    Add toss_review_duration metric
    Signed-off-by: Carter Landis <c@carterlandis.com>

--- augur/metrics/toss.py
+++ augur/metrics/toss.py
@@ -57,3 +57,34 @@ def toss_pull_request_acceptance_rate(self, repo_id, begin_date=None, end_date=N
     results = pd.read_sql(pr_acceptance_rate_sql, self.database, params={'repo_id': repo_id, 'group_by': group_by,
                                                     'begin_date': begin_date, 'end_date': end_date})
     return results
+
+
+@register_metric(type="toss")
+def toss_review_duration(self, repo_id, begin_date=None, end_date=None):
+    """
+    Timeseries of pull request acceptance rate (expressed as the ratio of pull requests merged on a date to the count of pull requests opened on a date)
+
+    :param repo_group_id: The repository's repo_group_id
+    :param repo_id: The repository's repo_id, defaults to None
+    :return: DataFrame with ratio/day
+    """
+    if not begin_date:
+        begin_date = '1970-1-1 00:00:01'
+    if not end_date:
+        end_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    pr_acceptance_rate_sql = s.sql.text("""
+        SELECT SUM
+            ( EXTRACT ( EPOCH FROM ( pr_merged_at - pr_created_at ) ) ) / COUNT ( * ) AS duration
+        FROM
+            pull_requests
+            JOIN repo ON pull_requests.repo_id = repo.repo_id
+        WHERE
+            pull_requests.repo_id = :repo_id
+            AND pr_merged_at IS NOT NULL
+            AND pr_created_at BETWEEN :begin_date
+            AND :end_date
+    """)
+    results = pd.read_sql(pr_acceptance_rate_sql, self.database, params={'repo_id': repo_id,
+                                                    'begin_date': begin_date, 'end_date': end_date})
+    return results
