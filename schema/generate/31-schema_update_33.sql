52eb15f5097fe589d2e3951df8cfe17fd075a0c8
Sean <outdoors@acm.org>
2020-11-23 15:12:24 +0100

    fixiging schema generate comment

--- schema/generate/31-schema_update_33.sql
+++ schema/generate/31-schema_update_33.sql
@@ -1,4 +1,4 @@
-# Pull request commit updates
+-- # Pull request commit updates
 ALTER TABLE "augur_data"."pull_request_commits" ADD COLUMN "pr_cmt_author_cntrb_id" int8;
 ALTER TABLE "augur_data"."pull_request_commits" ADD COLUMN "pr_cmt_timestamp" timestamp(0);
 ALTER TABLE "augur_data"."pull_request_commits" ADD COLUMN "pr_cmt_author_email" varchar COLLATE "pg_catalog"."default";
