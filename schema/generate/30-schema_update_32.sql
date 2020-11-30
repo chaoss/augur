c10d443be123a92c8ddd354b09a658c28eb1cc0c
Sean P. Goggins <s@goggins.com>
2020-11-30 13:51:33 -0600

    update to sql error

--- schema/generate/30-schema_update_32.sql
+++ schema/generate/30-schema_update_32.sql
@@ -19,8 +19,6 @@ COMMENT ON COLUMN "augur_data"."pull_request_analysis"."merge_probability" IS 'I
 
 COMMENT ON COLUMN "augur_data"."pull_request_analysis"."mechanism" IS 'the ML model used for prediction (It is XGBoost Classifier at present)';
 
-ALTER TABLE "augur_data"."discourse_insights" ADD CONSTRAINT "fk_discourse_insights_message_1" FOREIGN KEY ("msg_id") REFERENCES "augur_data"."message" ("msg_id");
-
 ALTER TABLE "augur_data"."pull_request_analysis" ADD CONSTRAINT "fk_pull_request_analysis_pull_requests_1" FOREIGN KEY ("pull_request_id") REFERENCES "augur_data"."pull_requests" ("pull_request_id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 CREATE  INDEX CONCURRENTLY "probability_idx"  ON "augur_data"."pull_request_analysis" USING btree (
