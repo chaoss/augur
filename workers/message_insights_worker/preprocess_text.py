3d7c463ab32547aff595e63f65a81fca4ccdaf95
Gabe Heim <gabe@gabehe.im>
2020-11-30 21:04:57 -0600

    import cluster / analysis workers

---
+++ workers/message_insights_worker/preprocess_text.py
@@ -0,0 +1,202 @@
+import re
+import string
+import unicodedata
+
+import nltk
+import pandas as pd
+from bs4 import BeautifulSoup
+from nltk.stem.snowball import SnowballStemmer
+from nltk.tokenize import word_tokenize
+
+CONTRACTION_MAP = {
+"ain't": "is not",
+"aren't": "are not",
+"can't": "cannot",
+"can't've": "cannot have",
+"'cause": "because",
+"could've": "could have",
+"couldn't": "could not",
+"couldn't've": "could not have",
+"didn't": "did not",
+"doesn't": "does not",
+"don't": "do not",
+"hadn't": "had not",
+"hadn't've": "had not have",
+"hasn't": "has not",
+"haven't": "have not",
+"he'd": "he would",
+"he'd've": "he would have",
+"he'll": "he will",
+"he'll've": "he he will have",
+"he's": "he is",
+"how'd": "how did",
+"how'd'y": "how do you",
+"how'll": "how will",
+"how's": "how is",
+"I'd": "I would",
+"I'd've": "I would have",
+"I'll": "I will",
+"I'll've": "I will have",
+"I'm": "I am",
+"I've": "I have",
+"i'd": "i would",
+"i'd've": "i would have",
+"i'll": "i will",
+"i'll've": "i will have",
+"i'm": "i am",
+"i've": "i have",
+"isn't": "is not",
+"it'd": "it would",
+"it'd've": "it would have",
+"it'll": "it will",
+"it'll've": "it will have",
+"it's": "it is",
+"let's": "let us",
+"ma'am": "madam",
+"mayn't": "may not",
+"might've": "might have",
+"mightn't": "might not",
+"mightn't've": "might not have",
+"must've": "must have",
+"mustn't": "must not",
+"mustn't've": "must not have",
+"needn't": "need not",
+"needn't've": "need not have",
+"o'clock": "of the clock",
+"oughtn't": "ought not",
+"oughtn't've": "ought not have",
+"shan't": "shall not",
+"sha'n't": "shall not",
+"shan't've": "shall not have",
+"she'd": "she would",
+"she'd've": "she would have",
+"she'll": "she will",
+"she'll've": "she will have",
+"she's": "she is",
+"should've": "should have",
+"shouldn't": "should not",
+"shouldn't've": "should not have",
+"so've": "so have",
+"so's": "so as",
+"that'd": "that would",
+"that'd've": "that would have",
+"that's": "that is",
+"there'd": "there would",
+"there'd've": "there would have",
+"there's": "there is",
+"they'd": "they would",
+"they'd've": "they would have",
+"they'll": "they will",
+"they'll've": "they will have",
+"they're": "they are",
+"they've": "they have",
+"to've": "to have",
+"wasn't": "was not",
+"we'd": "we would",
+"we'd've": "we would have",
+"we'll": "we will",
+"we'll've": "we will have",
+"we're": "we are",
+"we've": "we have",
+"weren't": "were not",
+"what'll": "what will",
+"what'll've": "what will have",
+"what're": "what are",
+"what's": "what is",
+"what've": "what have",
+"when's": "when is",
+"when've": "when have",
+"where'd": "where did",
+"where's": "where is",
+"where've": "where have",
+"who'll": "who will",
+"who'll've": "who will have",
+"who's": "who is",
+"who've": "who have",
+"why's": "why is",
+"why've": "why have",
+"will've": "will have",
+"won't": "will not",
+"won't've": "will not have",
+"would've": "would have",
+"wouldn't": "would not",
+"wouldn't've": "would not have",
+"y'all": "you all",
+"y'all'd": "you all would",
+"y'all'd've": "you all would have",
+"y'all're": "you all are",
+"y'all've": "you all have",
+"you'd": "you would",
+"you'd've": "you would have",
+"you'll": "you will",
+"you'll've": "you will have",
+"you're": "you are",
+"you've": "you have"
+}
+
+## Preprocessing text
+
+punc=list(string.punctuation)
+
+stopword = nltk.corpus.stopwords.words('english')
+snowBallStemmer = SnowballStemmer("english")
+
+# Expanding contractions
+def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
+    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())), 
+                                      flags=re.IGNORECASE|re.DOTALL)
+    def expand_match(contraction):
+        match = contraction.group(0)
+        first_char = match[0]
+        expanded_contraction = contraction_mapping.get(match)\
+                                if contraction_mapping.get(match)\
+                                else contraction_mapping.get(match.lower())                       
+        expanded_contraction = first_char+expanded_contraction[1:]
+        return expanded_contraction
+        
+    expanded_text = contractions_pattern.sub(expand_match, text)
+    expanded_text = re.sub("'", "", expanded_text)
+    return expanded_text
+
+# Removing stop words, punctuations, spaces, stemming...
+def clean_text(text):
+    soup = BeautifulSoup(text)
+    for tag in soup.find_all('strong'):
+        tag.replaceWith('')
+        text = soup.get_text()
+
+    # Removes whitespaces, code embeddings, blockquotes
+    text = re.sub('\n', ' ', text)
+    text = re.sub('\r', ' ', text)
+    text = re.sub('[()){}]', ' ', text)
+    text = re.sub('\<[^<>]*\>', '', text)
+    text = re.sub('\`[^``]*\`', '', text)
+    
+    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    # Removes personal mentions like @abc, and email addresses
+    s = re.sub(r'\w*@\w*', ' ', text)   
+
+    # Removes attached links
+    s=' '.join(word for word in s.split(' ') if not word.startswith('http'))   
+
+    # Separates joint words
+    # Removes punctuation
+    s = re.sub('[\.\-\_\\/&]', ' ', s)
+    s = "".join([word.lower() for word in s if word not in punc])
+    s = word_tokenize(s)
+
+    # Stemming
+    s = " ".join([snowBallStemmer.stem(word) for word in s if len(word)<=30 and word not in stopword])              
+
+    # Tokenization
+    s = re.sub('[0-9]+', '', s)
+    s = re.sub('lgtm', 'look good', s)
+    return s
+
+# Normalizes corpus
+def normalize_corpus(text, contraction_expansion=True, clean=True):
+    if contraction_expansion:
+        text = expand_contractions(text)
+    if clean:
+        text = clean_text(text)
+    return text
