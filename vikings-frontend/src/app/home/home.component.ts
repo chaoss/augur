95264311f520f136ff95dbe658b1bc3948f2003c
James Tompkins <jgt7cn@umsystem.edu>
2020-12-03 16:23:43 -0600

    got example graph showing on homepage

--- vikings-frontend/src/app/home/home.component.ts
+++ vikings-frontend/src/app/home/home.component.ts
@@ -18,6 +18,11 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
 
     this.getRepoGroups();
+    var TESTER = document.getElementById('tester');
+    Plotly.newPlot( TESTER, [{
+    x: [1, 2, 3, 4, 5],
+    y: [1, 2, 4, 8, 16] }], {
+    margin: { t: 0 } } );
   }
 
 
