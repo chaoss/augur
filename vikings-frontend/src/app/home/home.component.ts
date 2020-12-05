f4dc84f6b3905d67057d0740498de4f8384f3356
James Tompkins <jgt7cn@umsystem.edu>
2020-12-05 08:52:18 -0600

    added example bar chart on homepage

--- vikings-frontend/src/app/home/home.component.ts
+++ vikings-frontend/src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpHeaders } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
-//import * as Plotly from './../../assets/plotly-latest.min.js';
 
+//this works instead of an import statement, I don't know why
+declare var Plotly: any
 
 @Component({
   selector: 'app-home',
@@ -19,11 +20,45 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
 
     this.getRepoGroups();
-    //var TESTER = document.getElementById('tester');
-    //Plotly.newPlot( TESTER, [{
-    //x: [1, 2, 3, 4, 5],
-    //y: [1, 2, 4, 8, 16] }], {
-    //margin: { t: 0 } } );
+
+    //graph plotting
+    var TESTER = document.getElementById('tester');
+    var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May','Jun','Jul','Aug','Sept','Oct','Nov','dec'];
+
+    var base = {
+      x: month,
+      y: [0,0,0,0,0,0,0,0,0,0,0,0],
+      name: '',
+      type: 'bar',
+      showlegend: false
+    }
+
+    var trace1 = {
+      x: [month[0], month[1], month[2],month[5]],
+      y: [20, 14, 23,12],
+      name: 'Contributor1',
+      type: 'bar'
+    };
+    
+    var trace2 = {
+      x: [month[2], month[3], month[5],month[7]],
+      y: [12, 18, 29,30],
+      name: 'Contributor2',
+      type: 'bar'
+    };
+    
+    var trace3 = {
+      x: [month[8], month[9], month[10],month[11]],
+      y: [20, 40,10,15],
+      name: 'Contributor3',
+      type: 'bar'
+    };
+    
+    var data = [base, trace1, trace2, trace3];
+    
+    var layout = {barmode: 'stack'};
+    
+    Plotly.newPlot(TESTER, data, layout);
   }
 
 
