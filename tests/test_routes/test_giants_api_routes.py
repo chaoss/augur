f67fbed5375b9549d1f740d658f5989343fa5d4d
Zachary S <zasample18+github@gmail.com>
2020-12-06 19:08:19 -0600

    Moved response code asserts to before usage of response

--- tests/test_routes/test_giants_api_routes.py
+++ tests/test_routes/test_giants_api_routes.py
@@ -4,39 +4,39 @@ import pytest
 
 def test_giants_api_repos(metrics):
     response = requests.get('http://localhost:5112/api/unstable/giants-project/repos')
-    data = response.json()
     assert response.status_code == 200
+    data = response.json()
     assert len(data) >= 1
     assert data[0]['commit_count'] > 0
 
 def test_giants_api_status(metrics):
     response = requests.get('http://localhost:5112/api/unstable/giants-project/repos')
-    data = response.json()
     assert response.status_code == 200
+    data = response.json()
     assert len(data) >= 1
     assert data[0]
     repo_id = data[0]
     response = requests.get('http://localhost:5112/api/unstable/giants-project/status/' + repo_id)
-    data = response.json()
     assert response.status_code == 200
+    data = response.json()
     assert len(data) >= 1
     assert data[0]
 
 def test_giants_api_tracked(metrics):
     response = requests.get('http://localhost:5112/api/unstable/giants-project/new-key')
-    data = response.json()
     assert response.status_code == 200
+    data = response.json()
     assert len(data) >= 1
     key = data[0]
     response = requests.get('http://localhost:5112/api/unstable/giants-project/tracked/' + key + '/api/unstable/giants-project/repos')
-    data = response.json()
     assert response.status_code == 200
+    data = response.json()
     assert len(data) >= 1
     assert data[0]['added'] >= 0
     assert data[0]['removed'] >= 0
     response = requests.get('http://localhost:5112/api/unstable/giants-project/history/' + key)
-    data = response.json()
     assert response.status_code == 200
+    data = response.json()
     assert len(data) >= 1
     assert data[0] == "api/unstable/giants-project/repos"
 
