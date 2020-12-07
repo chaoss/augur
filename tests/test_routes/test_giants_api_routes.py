a081233552eecb7d2fefbe7808c19281d584b40c
Zachary S <zasample18+github@gmail.com>
2020-12-06 19:32:15 -0600

    Changed port in tests to correct for our group

--- tests/test_routes/test_giants_api_routes.py
+++ tests/test_routes/test_giants_api_routes.py
@@ -3,38 +3,38 @@ import requests
 import pytest
 
 def test_giants_api_repos(metrics):
-    response = requests.get('http://localhost:5112/api/unstable/giants-project/repos')
+    response = requests.get('http://localhost:5113/api/unstable/giants-project/repos')
     assert response.status_code == 200
     data = response.json()
     assert len(data) >= 1
     assert data[0]['commit_count'] > 0
 
 def test_giants_api_status(metrics):
-    response = requests.get('http://localhost:5112/api/unstable/giants-project/repos')
+    response = requests.get('http://localhost:5113/api/unstable/giants-project/repos')
     assert response.status_code == 200
     data = response.json()
     assert len(data) >= 1
     assert data[0]
     repo_id = data[0]
-    response = requests.get('http://localhost:5112/api/unstable/giants-project/status/' + repo_id)
+    response = requests.get('http://localhost:5113/api/unstable/giants-project/status/' + repo_id)
     assert response.status_code == 200
     data = response.json()
     assert len(data) >= 1
     assert data[0]
 
 def test_giants_api_tracked(metrics):
-    response = requests.get('http://localhost:5112/api/unstable/giants-project/new-key')
+    response = requests.get('http://localhost:5113/api/unstable/giants-project/new-key')
     assert response.status_code == 200
     data = response.json()
     assert len(data) >= 1
     key = data[0]
-    response = requests.get('http://localhost:5112/api/unstable/giants-project/tracked/' + key + '/api/unstable/giants-project/repos')
+    response = requests.get('http://localhost:5113/api/unstable/giants-project/tracked/' + key + '/api/unstable/giants-project/repos')
     assert response.status_code == 200
     data = response.json()
     assert len(data) >= 1
     assert data[0]['added'] >= 0
     assert data[0]['removed'] >= 0
-    response = requests.get('http://localhost:5112/api/unstable/giants-project/history/' + key)
+    response = requests.get('http://localhost:5113/api/unstable/giants-project/history/' + key)
     assert response.status_code == 200
     data = response.json()
     assert len(data) >= 1
