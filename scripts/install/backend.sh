a2a9e49234fc2047ec718cbd8f9f86098b7398e2
Sean P. Goggins <outdoors@acm.org>
2020-12-15 19:43:27 +0100

    adding wordlists for NLTK

--- scripts/install/backend.sh
+++ scripts/install/backend.sh
@@ -12,3 +12,20 @@ if [[ $target == *"prod"* ]]; then
 else
     pip install -e .[dev]
 fi
+
+
+
+read -r -p "Would you like to install required NLTK word lists for machine learning workers? [Y/n] " response
+case "$response" in
+  [yY][eE][sS]|[yY])
+    echo "Installing..."
+    python -m nltk.downloader stopwords > logs/stopwords.log
+    python -m nltk.downloader punkt >logs/punkt.log
+    python -m nltk.downloader popular >logs/popular.log
+    python -m nltk.downloader universal_tagset >logs/universal_tagset.log
+    echo "Done!"
+    ;;
+  *)
+    echo "Skipping frontend dependencies..."
+    ;;
+esac
--- workers/pull_request_analysis_worker/setup.py
+++ workers/pull_request_analysis_worker/setup.py
@@ -46,4 +46,5 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
     ]
+    
 )
