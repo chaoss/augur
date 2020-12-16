56dea4d6788da90191dbb275bd8fb28207a7f2bd
Sean P. Goggins <s@goggins.com>
2020-12-16 06:46:41 -0600

    Think I fixed @CCARTERLANDIS concerns regarding the unruly nature of installing NLTK wordfiles on last review

--- scripts/install/nltk_dictionaries.sh
+++ scripts/install/nltk_dictionaries.sh
@@ -7,17 +7,23 @@ read -r -p "Would you like to install required NLTK word lists for machine learn
 case "$response" in
   [yY][eE][sS]|[yY])
     echo "Installing..."
-    mkdir ~/nltk_wordlist_installations
+    mkdir ~/nltk_wordlist_installations_
+    mkdir /usr/local/share/nltk-wordlists
     python -m nltk.downloader stopwords -d /usr/local/share >> ~/nltk_wordlist_installations/stopwords.log
-    python -m nltk.downloader punkt -d /usr/local/share  >> ~/nltk_wordlist_installations/punkt.log 
-    python -m nltk.downloader popular -d /usr/local/share >> ~/nltk_wordlist_installations//popular.log
+    cp /usr/local/share/stopwords /usr/local/share/nltk-wordlists
+    python -m nltk.downloader punkt -d /usr/local/share  >> ~/nltk_wordlist_installations/punkt.log               
+    cp /usr/local/share/punkt /usr/local/share/nltk-wordlists
+    python -m nltk.downloader popular -d /usr/local/share >> ~/nltk_wordlist_installations/popular.log
+    cp /usr/local/share/popular /usr/local/share/nltk-wordlists
     python -m nltk.downloader universal_tagset -d /usr/local/share >> ~/nltk_wordlist_installations/universal_tagset.log
+    cp /usr/local/share/universal_tagset /usr/local/share/nltk-wordlists
     echo "Done!"
-    echo "Logs are stored in your home directory under 'nltk_wordlist_installations'." 
+    echo "Logs are stored in your home directory under '/usr/local/share, /usr/local/share/nltk-worklists, and in some instances based on operating system, your home directroy'."
     ;;
   *)
     echo "Skipping NLTK Word Vector dependencies. Please be advised that the machine learning workers, including the clustering worker, discourse worker, messsage_analysis_worker, pull_request_analysis_worker, and the LSTM model of the insight_worker will not function fully without these lists."
     ;;
+
 esac
 
 
