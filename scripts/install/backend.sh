05f49627b51f45601745336e709fb7a0931833dc
Sean P. Goggins <s@goggins.com>
2020-12-16 04:07:09 -0600

    Attempting to fix NLTK dictionary installations so they do no violate our established coding protocols

--- scripts/install/backend.sh
+++ scripts/install/backend.sh
@@ -13,19 +13,3 @@ else
     pip install -e .[dev]
 fi
 
-
-
-read -r -p "Would you like to install required NLTK word lists for machine learning workers? [Y/n] " response
-case "$response" in
-  [yY][eE][sS]|[yY])
-    echo "Installing..."
-    python -m nltk.downloader stopwords > logs/stopwords.log
-    python -m nltk.downloader punkt >logs/punkt.log
-    python -m nltk.downloader popular >logs/popular.log
-    python -m nltk.downloader universal_tagset >logs/universal_tagset.log
-    echo "Done!"
-    ;;
-  *)
-    echo "Skipping frontend dependencies..."
-    ;;
-esac
