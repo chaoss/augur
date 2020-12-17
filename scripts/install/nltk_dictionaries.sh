1c2bc7d5f747ea1e8f3abaa1ef8fe305720b0ffc
Sean Goggins <s@goggins.com>
2020-12-17 21:14:33 +0100

    fixing nltk install

--- scripts/install/nltk_dictionaries.sh
+++ scripts/install/nltk_dictionaries.sh
@@ -7,16 +7,10 @@ read -r -p "Would you like to install required NLTK word lists for machine learn
 case "$response" in
   [yY][eE][sS]|[yY])
     echo "Installing..."
-    mkdir ~/nltk_wordlist_installations_
-    mkdir /usr/local/share/nltk-wordlists
-    python -m nltk.downloader stopwords -d /usr/local/share >> ~/nltk_wordlist_installations/stopwords.log
-    cp /usr/local/share/stopwords /usr/local/share/nltk-wordlists
-    python -m nltk.downloader punkt -d /usr/local/share  >> ~/nltk_wordlist_installations/punkt.log               
-    cp /usr/local/share/punkt /usr/local/share/nltk-wordlists
-    python -m nltk.downloader popular -d /usr/local/share >> ~/nltk_wordlist_installations/popular.log
-    cp /usr/local/share/popular /usr/local/share/nltk-wordlists
-    python -m nltk.downloader universal_tagset -d /usr/local/share >> ~/nltk_wordlist_installations/universal_tagset.log
-    cp /usr/local/share/universal_tagset /usr/local/share/nltk-wordlists
+    python -m nltk.downloader stopwords 
+    python -m nltk.downloader punkt                
+    python -m nltk.downloader popular 
+    python -m nltk.downloader universal_tagset 
     echo "Done!"
     echo "Logs are stored in your home directory under '/usr/local/share, /usr/local/share/nltk-worklists, and in some instances based on operating system, your home directroy'."
     ;;
