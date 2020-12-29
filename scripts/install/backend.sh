5a434d52195e49f0ec732e66eece3406b465b293
Sean P. Goggins <s@goggins.com>
2020-12-29 14:58:30 -0600

    install script fixing for machine learning

--- scripts/install/backend.sh
+++ scripts/install/backend.sh
@@ -5,17 +5,12 @@ echo "Installing backend dependencies..."
 echo "**********************************"
 echo
 
+scripts/install/nltk_dictionaries.sh
+
 target=$1
 
 if [[ $target == *"prod"* ]]; then
     pip install .
 else
     pip install -e .[dev]
-fi
-
-
-if [[ ! -e ~/nltk_wordlist_installations/stopwords.log ]]; then
-
-    echo "You chose not to install NLTK dictionaries when you installed Augur, or installed augur after these workers were instroduced. They will be located in the \/usr/local/share directory, and can be removed following your rebuild."
-    scripts/install/nltk_dictionaries.sh
 fi
