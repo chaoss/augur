8da9694abd2ec6e72e41c842896b0a4709e59594
Sean P. Goggins <s@goggins.com>
2021-01-26 12:42:04 -0600

    query optimization.

--- schema/20201025-Release-0.14.0.sql
+++ schema/20201025-Release-0.14.0.sql
@@ -3874,3 +3874,13 @@ ALTER TABLE "augur_data"."pull_requests" ADD CONSTRAINT  "fk_pull_requests_repo_
 
 update "augur_operations"."augur_settings" set value = 40 where setting = 'augur_data_version'; 
 
+
+
+CREATE INDEX pull_requests_idx_repo_id_data_datex ON pull_requests (repo_id,data_collection_date);
+CREATE INDEX repo_idx_repo_id_repo_namex ON repo (repo_id,repo_name);
+CREATE INDEX repo_info_idx_repo_id_data_datex ON repo_info (repo_id,data_collection_date);
+CREATE INDEX repo_info_idx_repo_id_data_date_1x ON repo_info (repo_id,data_collection_date);
+
+update "augur_operations"."augur_settings" set value = 41 where setting = 'augur_data_version'; 
+
+
