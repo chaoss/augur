85ace6fa6bfb7b0664b0a063f7becfaea76a0b78
Sean P. Goggins <outdoors@acm.org>
2021-03-18 14:35:36 +0100

    debuggign

--- workers/clustering_worker/clustering_worker.py
+++ workers/clustering_worker/clustering_worker.py
@@ -268,9 +268,9 @@ class ClusteringWorker(Worker):
 
 			for i in topic.argsort()[:-self.num_words_per_topic-1:-1]:
 				twid = self.db.execute(key_sequence_words_sql)
-
+                                logging.info("twid variable is: {}".format(twid))
 				record = {
-				  'topic_words_id': int(twid),
+				  'topic_words_id': twid,
 				  'topic_id': int(topic_id),
 				  'word': feature_names[i]
 				  }
