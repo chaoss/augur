346a6afba4b0a3f202dff5481356a92f6a980275
Sean Goggins <s@goggins.com>
2021-03-20 04:54:17 -0500

    Schema update to fix topic_words table population.

--- schema/20201025-Release-0.14.0.sql
+++ schema/20201025-Release-0.14.0.sql
@@ -4397,3 +4397,16 @@ update "augur_operations"."augur_settings" set value = 47 where setting = 'augur
 
 COMMIT; 
 
+
+BEGIN;
+
+ALTER TABLE "augur_data"."topic_words" 
+  DROP CONSTRAINT IF EXISTS "fk_topic_words_repo_topic_1";
+
+update "augur_operations"."augur_settings" set value = 48 where setting = 'augur_data_version';
+
+
+COMMIT; 
+
+
+
