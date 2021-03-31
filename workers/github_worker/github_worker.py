26feae4b52012780a0d28890e19e243b7d00b690
Sean P. Goggins <s@goggins.com>
2021-03-31 10:15:22 -0500

    fixing split issue in github issue worker.

--- workers/github_worker/github_worker.py
+++ workers/github_worker/github_worker.py
@@ -81,8 +81,8 @@ class GitHubWorker(Worker):
             {
                 'repo_id': repo_id,
                 'reporter_id': self.find_id_from_login(issue['user']['login']),
-                'pull_request': issue['pull_request']['url'].split('/', -1) if 'pull_request' in issue else None,
-                'pull_request_id': issue['pull_request']['url'].split('/', -1) if 'pull_request' in issue else None,
+                'pull_request': issue['pull_request']['url'].split('/') if 'pull_request' in issue else None,
+                'pull_request_id': issue['pull_request']['url'].split('/') if 'pull_request' in issue else None,
                 'created_at': issue['created_at'],
                 'issue_title': issue['title'],
                 'issue_body': issue['body'].replace('0x00', '____') if issue['body'] else None,
