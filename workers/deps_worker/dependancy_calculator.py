aceb9ed733d61e44b74e46f5def20761f909f7c1
Stuart Aldrich <stuart.aldrich.1@gmail.com>
2021-05-17 23:41:46 -0500

    deps calculation working for multiple languages

--- workers/deps_worker/dependancy_calculator.py
+++ workers/deps_worker/dependancy_calculator.py
@@ -1,4 +1,12 @@
 import python_deps
+import ruby_deps
+import php_deps
+import javascript_deps
+import vb_deps
+import csharp_deps
+import java_deps
+import cpp_deps
+import c_deps
 
 class Dep:
 	def __init__(self, name, language, count):
@@ -10,10 +18,18 @@ class Dep:
 
 def get_deps(path):
 	deps = []
-	deps.extend(get_language_deps(path, python_deps))
+	deps.extend(get_language_deps(path, python_deps, 'python'))
+	deps.extend(get_language_deps(path, ruby_deps, 'ruby'))
+	deps.extend(get_language_deps(path, php_deps, 'php'))
+	deps.extend(get_language_deps(path, javascript_deps, 'javascript'))
+	deps.extend(get_language_deps(path, vb_deps, 'visual basic'))
+	deps.extend(get_language_deps(path, csharp_deps, 'C#'))
+	deps.extend(get_language_deps(path, java_deps, 'java'))
+	deps.extend(get_language_deps(path, cpp_deps, 'C++'))
+	deps.extend(get_language_deps(path, c_deps, 'C'))
 	return deps
 	
-def get_language_deps(path, language):
+def get_language_deps(path, language, name):
 	files = language.get_files(path)
 	deps_map = {}
 	for f in files:
@@ -24,5 +40,5 @@ def get_language_deps(path, language):
 			if dep in deps_map:
 				deps_map[dep].count += 1
 			else:
-				deps_map[dep] = Dep(dep, 'python', 1)
+				deps_map[dep] = Dep(dep, name, 1)
 	return list(deps_map.values())
