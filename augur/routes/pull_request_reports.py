95c7a9d69eef71f06069800ceba601028d84ae43
Sean P. Goggins <s@goggins.com>
2021-05-19 14:11:00 -0500

    image api fun

--- augur/routes/pull_request_reports.py
+++ augur/routes/pull_request_reports.py
@@ -1652,6 +1652,8 @@ def create_routes(server):
         opts = FirefoxOptions()
         opts.add_argument("--headless")
         driver = webdriver.Firefox(firefox_options=opts)
+        newt = get_screenshot_as_png(grid, timeout=180, webdriver=selenium.webdriver.firefox.webdriver)
         filename = export_png(grid, timeout=180, webdriver=selenium.webdriver.firefox.webdriver)
         
-        return send_file(filename)
+        return newt 
+        #return send_file(filename)
