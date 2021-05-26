a0c8cf0c0b2fe214b5052aaddb9239a06757eb9f
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2021-05-26 05:09:37 -0500

    Fixes to setup.py file for deps_worker

--- workers/deps_worker/setup.py
+++ workers/deps_worker/setup.py
@@ -6,6 +6,12 @@ import re
 from setuptools import find_packages
 from setuptools import setup
 
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
 setup(
     name="deps_worker",
     version="1.0.0",
@@ -18,7 +24,7 @@ setup(
     install_requires=[
         'flask',
         'requests',
-        'psycopg2-binary',
+        'psycopg2-binary'
     ],
     entry_points={
         'console_scripts': [
@@ -32,5 +38,5 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
-    ],
+    ]
 )
