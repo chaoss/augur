9198652c1bff4726eb348c30709db61df779182f
Sean Goggins <outdoors@acm.org>
2021-05-31 10:41:23 -0500

    freezing worker libversions for release
    Signed-off-by: isaacmilarky <imilarksy@gmail.com>

--- workers/facade_worker/setup.py
+++ workers/facade_worker/setup.py
@@ -22,12 +22,14 @@ setup(
     description="Augur Worker that parses and collects git log data",
     packages=find_packages(exclude=('tests',)),
     install_requires=[
-        'flask', 
-        'requests', 
-        'xlsxwriter', 
-        'psycopg2-binary', 
-        'click',
-        'pymysql'
+        'Flask==1.1.4',
+        'Flask-Cors==3.0.10',
+        'Flask-Login==0.5.0',
+        'Flask-WTF==0.14.3',
+        'requests==2.22.0',
+        'psycopg2-binary==2.8.6',
+        'click==7.1.2', 
+        'XlsxWriter==1.3.7'
     ],
     entry_points={
         'console_scripts': [
