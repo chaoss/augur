ea0523e6d2fae57b8866b242851f6f164afaee61
IsaacMilarky <imilarsky@gmail.com>
2021-05-31 16:45:36 -0500

    Trying to make docker containers functional
    Signed-off-by: isaacmilarky <imilarksy@gmail.com>

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 
 #automate the small things for setting up docker containers
+rm docker_env.txt
 touch docker_env.txt
 #Prompt for user info
+#TODO: Make container work with gitlab key
 echo "Please input Github API key: "
 read githubAPIKey
 
 echo "AUGUR_GITHUB_API_KEY=$githubAPIKey" >> docker_env.txt
 
 echo "Please choose which database hostname to use."
-echo "Note: localhost will attempt to connect to a local database on your machine"
-echo "0.0.0.0 will attempt to connect to a local database in a docker image on your machine"
-echo "Any other hostname will work normally."
 echo "Plase input database hostname: "
 read dbHostname
 
@@ -19,6 +22,6 @@ echo "AUGUR_DB_HOST=$dbHostname" >> docker_env.txt
 echo "AUGUR_DB_NAME=augur" >> docker_env.txt
 echo "AUGUR_DB_PORT=5432" >> docker_env.txt
 echo "AUGUR_DB_USER=augur" >> docker_env.txt
-echo "AUGUR_DB_PASSWORD=augur" >> docker_env.txtrm 
+echo "AUGUR_DB_PASSWORD=password" >> docker_env.txt
 
 docker-compose -f docker-compose.yml up
