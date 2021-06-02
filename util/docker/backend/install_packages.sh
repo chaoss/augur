5bb7effb1a18086f27c58feca1841720e439de86
isaacmilarky <imilarksy@gmail.com>
2021-06-02 12:03:26 -0500

    Changed python 3.7 to 3.8. Its bit clunky as the debian image used here requires python3.8 to be built from source. Might want to make it cleaner in the future to cut down on image size
    Signed-off-by: isaacmilarky <imilarksy@gmail.com>
    Signed-off-by: isaacmilarky <imilarsky@gmail.com>

--- util/docker/backend/install_packages.sh
+++ util/docker/backend/install_packages.sh
@@ -9,13 +9,24 @@ apt-get update
 apt-get -y upgrade
 apt-get -y install --no-install-recommends procps
 
+#Debian requires python3.8 to be built from source so these are needed
+apt-get -y install --no-install-recommends build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+curl -O https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tar.xz
+tar -xf Python-3.8.2.tar.xz
+cd Python-3.8.2
+./configure --enable-optimizations
+make -j 4
+#This overwrites any other python binaries it finds so keep that in mind.
+make install
+cd ..
+
 #facade
 apt-get -y install --no-install-recommends git
 
 # https://github.com/giampaolo/psutil/issues/1714
 apt-get -y install --no-install-recommends gcc
 uname -a
-apt-get -y install --no-install-recommends python3.8 python3-pip
+apt-get -y install --no-install-recommends python3-pip
 #maybe this will fix it? 
 #apt-get -y install --no-install-recommends python3.7 python3-pip
 # python3.8-dev
