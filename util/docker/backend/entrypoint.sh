5d8544d39c8286f7ce423d5c211d42e93e09f289
Maximilian Huber <maximilian.huber@tngtech.com>
2021-07-07 21:29:32 +0200

    add repos and repo_groups initialization functionallity to docker image
    Signed-off-by: Maximilian Huber <maximilian.huber@tngtech.com>

--- util/docker/backend/entrypoint.sh
+++ util/docker/backend/entrypoint.sh
@@ -13,4 +13,12 @@ augur config init \
     --db_password "$AUGUR_DB_PASSWORD" \
     --github_api_key "$AUGUR_GITHUB_API_KEY"
 
+if [[ -f /repo_groups.csv ]]; then
+    augur db add-repo-groups /repo_groups.csv
+fi
+
+if [[ -f /repos.csv ]]; then
+   augur db add-repos /repos.csv
+fi
+
 exec augur backend start
