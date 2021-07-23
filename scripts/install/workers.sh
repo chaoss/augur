65e99d8b15e5d1d3ce8d566a37d746512a1a2312
Isaac Milarsky <imilarsky@gmail.com>
2021-07-23 14:19:56 -0500

    Isolating go as the build problem
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- scripts/install/workers.sh
+++ scripts/install/workers.sh
@@ -43,16 +43,3 @@ do
 
 done
 
-
-
-if [ -d "$HOME/scorecard" ]; then
-  echo " Scorecard already exists, skipping cloning ..."
-else
-  echo "Cloning OSSF Scorecard to generate scorecard data ..."
-  git clone https://github.com/ossf/scorecard $HOME/scorecard
-  CURRENT_DIR=$PWD;
-  cd $HOME/scorecard;
-  go build;
-  echo "scorecard build done"
-  cd $CURRENT_DIR
-fi
