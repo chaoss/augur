926c6252990dc188a3f91d933330b43cc651f834
Isaac Milarsky <imilarsky@gmail.com>
2021-07-23 15:34:58 -0500

    Still trying to get all go modules built
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- scripts/install/workers.sh
+++ scripts/install/workers.sh
@@ -43,3 +43,16 @@ do
 
 done
 
+go env -w GO111MODULE=auto
+
+if [ -d "$HOME/scorecard" ]; then
+  echo " Scorecard already exists, skipping cloning ..."
+else
+  echo "Cloning OSSF Scorecard to generate scorecard data ..."
+  git clone https://github.com/ossf/scorecard $HOME/scorecard
+  CURRENT_DIR=$PWD;
+  cd $HOME/scorecard;
+  go build;
+  echo "scorecard build done"
+  cd $CURRENT_DIR
+fi
