5548daa1a8a3ed56ecd668453cf9f91ca42fe1b9
Isaac Milarsky <imilarsky@gmail.com>
2021-07-29 11:30:26 -0500

    debug
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- workers/worker_git_integration.py
+++ workers/worker_git_integration.py
@@ -276,7 +276,7 @@ class WorkerGitInterfaceable(Worker):
             action_map=cntrb_action_map
         )
 
-        self.logger.info(f"source_cntrb_insert: {source_cntrb_insert[0]}")
+        self.logger.info(f"source_cntrb_insert: {source_cntrb_insert}")
 
         cntrb_insert = [
             {
