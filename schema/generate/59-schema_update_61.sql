e5c5d59284124409f97c902fa235ef288572b1ba
Sean P. Goggins <s@goggins.com>
2021-08-06 13:33:56 -0500

    schema change

---
+++ schema/generate/59-schema_update_61.sql
@@ -0,0 +1,9 @@
+BEGIN; 
+
+INSERT INTO "augur_operations"."worker_history" ("history_id", "repo_id", "worker", "job_model", "oauth_id", "timestamp", "status", "total_results") VALUES (1, 1, 'seed.worker.record', 'seed', NULL, '2021-07-27 09:50:56', 'Success', 0);
+
+update "augur_operations"."augur_settings" set value = 60 where setting = 'augur_data_version';
+
+
+COMMIT; 
+
