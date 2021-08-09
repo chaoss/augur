41f13b1e39ce8667721388579df8dad8ce8c432d
Sean P. Goggins <s@goggins.com>
2021-08-09 16:14:27 -0500

    repo archive seed data.

--- schema/generate/05-seed_data.sql
+++ schema/generate/05-seed_data.sql
@@ -13,8 +13,7 @@ COMMIT;
 
 BEGIN; 
 
-INSERT INTO "augur_data"."repo" ("repo_id", "repo_group_id", "repo_git", "repo_path", "repo_name", "repo_added", "repo_status", "repo_type", "url", "owner_id", "description", "primary_language", "created_at", "forked_from", "updated_at", "tool_source", "tool_version", "data_source", "data_collection_date", "repo_archived", "repo_archived_date_collected") VALUES (1, 1, 'https://github.com/chaoss/augur', NULL, NULL, '2021-08-10 14:28:44', 'New', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'data load', 'one', 'git', '2021-06-05 18:41:14', NULL, NULL);
-INSERT INTO "augur_data"."repo" ("repo_id", "repo_group_id", "repo_git", "repo_path", "repo_name", "repo_added", "repo_status", "repo_type", "url", "owner_id", "description", "primary_language", "created_at", "forked_from", "updated_at", "tool_source", "tool_version", "data_source", "data_collection_date", "repo_archived", "repo_archived_date_collected") VALUES (2, 1, 'https://github.com/saltstack/salt', NULL, NULL, '2021-08-10 17:58:43', 'New', '', NULL, NULL, NULL, NULL, NULL, 'Parent not available', NULL, NULL, NULL, NULL, NULL, 0, NULL);
+
 
 COMMIT; 
 
