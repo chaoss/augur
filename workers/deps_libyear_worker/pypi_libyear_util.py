3d40e9118d657239d3e7db37d3ec81158a058030
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-08-12 01:51:45 +0530

    sorting and handling upper limit dependencies and getting latest version
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- workers/deps_libyear_worker/pypi_libyear_util.py
+++ workers/deps_libyear_worker/pypi_libyear_util.py
@@ -5,6 +5,7 @@ import dateutil.parser
 from distutils.version import LooseVersion
 import re 
 
+
 def get_pypi_data(name, version=None):
     """return a dictionary with pypi project data"""
     url = "https://pypi.org/pypi/%s/json" % name
@@ -44,3 +45,53 @@ def get_version(pypi_data, version, lt=False):
         if idx < len(releases) - 1:
             return releases[idx + 1]
     return version
+
+
+def handle_upper_limit_dependency(dependency, data):
+    versions = dependency['requirement'].split(',')
+    upper_limit = clean_version(versions[0])
+    lower_limit = clean_version(versions[1])
+    release_list = list(data['releases'])
+
+    if upper_limit not in release_list:
+        upper_limit += '.0'
+    # print('the upper limit and lower limit is %s and %s' %(upper_limit,lower_limit))
+    upper_index = release_list.index(upper_limit)
+    return release_list[upper_index -1]
+    # return get_version(data, upper_limit, lt= True)
+
+
+def get_latest_version(data):
+    # dict_list = list(data['releases'])
+    # return dict_list[-1]
+    return data['info']['version']
+
+
+def get_release_date(data, version):
+    if not data:
+        print('invalid data')
+        return None
+    releases = data['releases']
+    name = data['info']['name']
+    try:
+        version_date = releases[version][-1]['upload_time_iso_8601']
+    except IndexError:
+        print(f'Used release of {name}=={version} has no upload time.')
+        return None    
+    # version_date = dateutil.parser.parse(version_date)
+    return version_date
+
+
+def sort_dependency_requirement(dependency,data):
+    if dependency['requirement'] == '' or dependency['requirement'] is None or dependency['requirement'] == '*':
+        return None
+
+    elif re.search(r'<', dependency['requirement']):
+        return handle_upper_limit_dependency(dependency, data)
+
+    elif re.search(r'>=', dependency['requirement']):
+        return None
+
+    else:
+        # return get_version(data, clean_version(dependency['requirement']))
+        return clean_version(dependency['requirement'])
