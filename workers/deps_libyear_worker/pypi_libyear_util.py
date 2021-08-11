a7f95a05ea843b0141cf207cabc6ab73c9b03daf
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-08-12 01:52:50 +0530

    libyear calculation function added
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- workers/deps_libyear_worker/pypi_libyear_util.py
+++ workers/deps_libyear_worker/pypi_libyear_util.py
@@ -95,3 +95,23 @@ def sort_dependency_requirement(dependency,data):
     else:
         # return get_version(data, clean_version(dependency['requirement']))
         return clean_version(dependency['requirement'])
+
+
+def get_libyear(current_version, current_release_date, latest_version, latest_release_date):
+
+    if not latest_version:
+        return -1
+    
+    if not latest_release_date:
+        return -1
+
+    if not current_version:
+        return 0
+
+    current_release_date= dateutil.parser.parse(current_release_date)
+    latest_release_date = dateutil.parser.parse(latest_release_date)    
+
+    libdays = (latest_release_date - current_release_date).days
+    print(libdays)
+    libyear = libdays/365
+    return libyear
