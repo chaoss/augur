2764b1674de6eadc235ff080932a254659fbbc16
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-08-12 01:59:23 +0530

    added regular expressions to parse file
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- workers/deps_libyear_worker/pypi_parser.py
+++ workers/deps_libyear_worker/pypi_parser.py
@@ -5,3 +5,13 @@ import toml
 import dateutil.parser
 from pypi_libyear_util import sort_dependency_requirement,get_pypi_data,get_latest_version,get_release_date
 from pypi_libyear_util import get_libyear
+
+
+INSTALL_REGEXP = r'install_requires\s*=\s*\[([\s\S]*?)\]'
+REQUIRE_REGEXP = r'([a-zA-Z0-9]+[a-zA-Z0-9\-_\.]+)([><=\w\.,]+)?'
+REQUIREMENTS_REGEXP = '^#{REQUIRE_REGEXP}'
+MANIFEST_REGEXP = r'.*require[^\/]*(\/)?[^\/]*\.(txt|pip)$'
+
+install_regrex = re.compile(INSTALL_REGEXP)
+require_regrex = re.compile(REQUIRE_REGEXP)
+requirement_regrex = re.compile(REQUIREMENTS_REGEXP)
