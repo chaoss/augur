38dd9bcb227e35aa91ae9af0ea9f6b0cf974f5ff
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-08-12 01:49:20 +0530

    getting and cleaning versions
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- workers/deps_libyear_worker/pypi_libyear_util.py
+++ workers/deps_libyear_worker/pypi_libyear_util.py
@@ -14,3 +14,33 @@ def get_pypi_data(name, version=None):
     if r.status_code < 400:
         return r.json()
     return {}
+
+
+def clean_version(version):
+    version = [v for v in version if v.isdigit() or v == '.']
+    return ''.join(version)
+
+
+def get_version(pypi_data, version, lt=False):
+    if not version:
+        return None
+
+    orig_ver = version
+    releases = pypi_data['releases']
+    if version not in releases:
+        version_data = get_pypi_data(pypi_data['info']['name'], version=version)
+        version = version_data.get('info', {}).get('version')
+    if lt:
+        releases = [(r, rd[-1]['upload_time_iso_8601']) for r, rd in releases.items() if rd]
+        releases = sorted(releases, key=lambda x: x[1], reverse=True)
+        releases = [r for r, rd in releases]
+        if version is None:
+            curr_ver = LooseVersion(clean_version(orig_ver))
+            releases_float = [clean_version(r) for r in releases]
+            releases_float = [r for r in releases_float if LooseVersion(r) >= curr_ver]
+            return releases[len(releases_float)]
+
+        idx = releases.index(version)
+        if idx < len(releases) - 1:
+            return releases[idx + 1]
+    return version
