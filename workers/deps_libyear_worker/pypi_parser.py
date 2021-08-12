dd6f73f065bbf4336b5467f0c910dc0312eab32f
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-08-12 16:23:17 +0530

    Added major code to classify files and get them to right parser and use funtions to get all the data
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- workers/deps_libyear_worker/pypi_parser.py
+++ workers/deps_libyear_worker/pypi_parser.py
@@ -7,6 +7,17 @@ from pypi_libyear_util import sort_dependency_requirement,get_pypi_data,get_late
 from pypi_libyear_util import get_libyear
 
 
+#Files that would be parsed should be added here.
+file_list = [
+    'Requirement.txt',
+    'setup.py',
+    'Pipfile',
+    'Pipfile.lock',
+    'pyproject.toml',
+    'poetry.lock'
+]
+
+
 def find(name, path):
     for root, dirs, files in os.walk(path):
         if name in files:
@@ -23,7 +34,9 @@ require_regrex = re.compile(REQUIRE_REGEXP)
 requirement_regrex = re.compile(REQUIREMENTS_REGEXP)
 
 
-def parse_requirement_txt(manifest):
+def parse_requirement_txt(file_handle):
+
+    manifest= file_handle.read()
     deps=list()
     for line in manifest.split('\n'):
         matches = require_regrex.search(line.replace("'",""))
@@ -105,6 +118,74 @@ def parse_setup_py(file_handle):
 
 def parse_poetry(file_handle):
     manifest = toml.load(file_handle)
-    print(manifest)
-    # manifest = toml.load(file_handle)['tool']['poetry']
-    # return map_dependencies_pipfile(manifest['dependencies'], 'runtime') + map_dependencies_pipfile(manifest['dev-dependencies'], 'develop')
+    # print(manifest)
+    manifest = toml.load(file_handle)['tool']['poetry']
+    return map_dependencies_pipfile(manifest['dependencies'], 'runtime') + map_dependencies_pipfile(manifest['dev-dependencies'], 'develop')
+
+
+def get_parsed_deps(path):
+
+    deps_file = None
+    dependency_list = list()
+
+    for f in file_list:
+        deps_file = find(f, path)
+        if not deps_file:
+            continue
+        file_handle= open(deps_file)
+
+        if f == 'Requirement.txt':
+            dependency_list = parse_requirement_txt(file_handle)
+
+        elif f == 'setup.py':
+            dependency_list = parse_setup_py(file_handle)
+
+        elif f == 'Pipfile':
+            dependency_list = parse_pipfile(file_handle)
+
+        elif f == 'Pipfile.lock':
+            dependency_list = parse_pipfile_lock(file_handle)
+
+        # elif f == 'pyproject.toml':
+        #     dependency_list = parse_poetry(file_handle)
+
+        # elif f == 'poetry.lock':
+        #     pass
+        return dependency_list
+
+
+def get_deps_libyear_data(path):
+    current_release_date = None
+    libyear = None
+
+    dependencies = get_parsed_deps(path)
+    if dependencies:
+        for dependency in dependencies:
+            data = get_pypi_data(dependency['name'])
+            current_version = sort_dependency_requirement(dependency,data)
+            latest_version = get_latest_version(data)
+            latest_release_date = get_release_date(data, latest_version)
+            if current_version:
+                current_release_date = get_release_date(data, current_version)
+            libyear = get_libyear(current_version, current_release_date, latest_version, latest_release_date)
+            if not latest_release_date:
+                latest_release_date = dateutil.parser.parse('1970-01-01 00:00:00')
+                libyear = -1
+
+            if not latest_version:
+                latest_version = 'unspecified'     
+
+            if not current_version:
+                current_version = latest_version
+                current_release_date = latest_release_date
+
+            if not dependency['requirement']:
+                dependency['requirement'] = 'unspecified'    
+
+            dependency['current_version'] = current_version    
+            dependency['latest_version'] = latest_version
+            dependency['current_release_date'] = current_release_date
+            dependency['latest_release_date'] = latest_release_date
+            dependency['libyear'] = libyear
+
+        return dependencies    
