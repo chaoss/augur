039047bbdb1a38814fa2a12c020a4ab94a2af69a
Isaac Milarsky <imilarsky@gmail.com>
2021-08-12 16:19:56 -0500

    shell workaround
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- scripts/install/workers.sh
+++ scripts/install/workers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #python venv
-activate /virtualenv/augur_env/bin/activate
+source /virtualenv/augur_env/bin/activate
 set -eo pipefail
 
 echo "Installing workers and their dependencies..."
