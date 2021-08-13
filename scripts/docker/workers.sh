dfa4575e41f7fd8f78567cce8d5f6d1b1cca3cfd
Isaac Milarsky <imilarsky@gmail.com>
2021-08-13 13:22:40 -0500

    add file
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

---
+++ scripts/docker/workers.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#python venv
+source /virtualenv/augur_env/bin/activate
+set -eo pipefail
+
+echo "Installing workers and their dependencies..."
+echo "**********************************"
+echo
+
+target=$1
+
+for WORKER in $(ls -d workers/*/)
+do
+    if [[ $WORKER == *"_worker"* ]]; then
+
+      if [[ \
+        $WORKER != *"spdx_worker"* \
+     && $WORKER != *"template_worker"* \
+     && $WORKER != *"metric_status_worker"* \
+     ]]; then
+
+        # make it pretty for formatting
+        FORMATTED_WORKER=${WORKER/#workers\//}
+        FORMATTED_WORKER=${FORMATTED_WORKER/%\//}
+
+        echo
+        echo "**********************************"
+        echo "Installing $FORMATTED_WORKER..."
+        echo "**********************************"
+        echo
+
+        cd $WORKER
+        rm -rf build/*;
+        rm -rf dist/*;
+        if [[ $target == *"prod"* ]]; then
+            pip install .
+        else
+            pip install -e .[dev]
+        fi
+        cd ../..
+      fi
+
+    fi
+
+
+done
+
+export PATH=$PATH:/usr/local/go/bin
+
+if [ -d "$HOME/scorecard" ]; then
+  echo " Scorecard already exists, skipping cloning ..."
+else
+  echo "Cloning OSSF Scorecard to generate scorecard data ..."
+  git clone https://github.com/ossf/scorecard $HOME/scorecard
+  CURRENT_DIR=$PWD;
+  cd $HOME/scorecard;
+  go build;
+  echo "scorecard build done"
+  cd $CURRENT_DIR
+fi
