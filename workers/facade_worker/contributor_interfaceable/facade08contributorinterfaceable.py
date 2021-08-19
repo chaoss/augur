b7f85ef606ad9b6d243cf5900d462ecd67e20b0f
Isaac Milarsky <imilarsky@gmail.com>
2021-08-19 12:25:04 -0500

    Making progress and commenting what I have. Trying to make it just get most of its config from facade. Need to figure out when facade should create/call the object
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- workers/facade_worker/contributor_interfaceable/facade08contributorinterfaceable.py
+++ workers/facade_worker/contributor_interfaceable/facade08contributorinterfaceable.py
@@ -3,27 +3,32 @@ from workers.worker_base import *
 
 from workers.worker_git_integration import WorkerGitInterfaceable
 
+"""
+This class serves as an extension for the facade worker to allow it to make api calls and interface with GitHub.
+The motivation for doing it this way is because the functionality needed to interface with Github and/or GitLab 
+is contained within WorkerGitInterfaceable. This is a problem because facade was migrated into augur from its own 
+project and just making it inherit from a new class could have unintended consequences and moreover, the facade worker
+doesn't even really need the WorkerGitInterfaceable for too much. This is mainly just to have better parity with the contributor
+worker and table. 
+"""
+
 #TODO : Make this borrow everything that it can from the facade worker.
 #i.e. port, logging, etc
 class ContributorInterfaceable(WorkerGitInterfaceable):
     def __init__(self, config={}):
         self.db_schema = None
-        # Get configured collection logger
-        self.config.update(config)
+        # Get config passed from the facade worker.
+        self.config = config
 
         self.data_tables = ['contributors', 'contributors_aliases', 'contributor_affiliations',
-                            'issue_events', 'pull_request_events', 'issues', 'message', 'issue_assignees',
+                            'issue_events', 'pull_request_events', 'issues', 'message', 'issue_assignees', 'commits',
                             'pull_request_assignees', 'pull_request_reviewers', 'pull_request_meta', 'pull_request_repo']
 
         self._root_augur_dir = Persistant.ROOT_AUGUR_DIR
+        
+        self.worker_type = self.config['worker_type'] 
 
-        self.config = {
-            'worker_type': "contributor_interface",
-            'host': self.augur_config.get_value('Server', 'host'),
-            'gh_api_key': self.augur_config.get_value('Database', 'key'),
-            'gitlab_api_key': self.augur_config.get_value('Database', 'gitlab_api_key')
-        }
-
+        #This is the same as the facade port, Needs to be incremented like done below.
         worker_port = self.config['port']
         while True:
             try:
@@ -38,7 +43,7 @@ class ContributorInterfaceable(WorkerGitInterfaceable):
         # add credentials to db config. Goes to databaseable
         self.config.update({
             'port': worker_port,
-            'id': "workers.{}.{}".format("contributor_interface", worker_port),
+            #'id': "workers.{}.{}".format("contributor_interface", worker_port), This should be the same as the facade worker
             'capture_output': False,
             'location': 'http://{}:{}'.format(self.config['host'], worker_port),
             'port_broker': self.augur_config.get_value('Server', 'port'),
@@ -52,14 +57,11 @@ class ContributorInterfaceable(WorkerGitInterfaceable):
 
         self.initialize_logging()
 
-        # Clear log contents from previous runs
-        open(self.config["server_logfile"], "w").close()
-        open(self.config["collection_logfile"], "w").close()
 
         self.logger = logging.getLogger(self.config["id"])
 
+        #This method also calls oauth_init
         self.initialize_database_connections()
-        self.init_oauths()
         self.logger.info(
             'Worker (PID: {}) initializing...'.format(str(os.getpid())))
 
@@ -67,6 +69,7 @@ class ContributorInterfaceable(WorkerGitInterfaceable):
         self.tool_version = "0.1"
         self.data_source = "contributor_interface"
 
+    #Try to construct the best url to ping GitHub's API for a username given a full name and a email.
     def resolve_user_url_from_email(self,contributor):
       try:
         cmt_cntrb = {
@@ -93,6 +96,8 @@ class ContributorInterfaceable(WorkerGitInterfaceable):
       
       return url
 
+    #Hit the endpoint specified by the url and return the json that it returns if it returns a dict.
+    #Returns None on failure.
     def request_dict_from_endpoint(self,url):
       self.logger.info(f"Hitting endpoint: {url}")
 
@@ -100,6 +105,7 @@ class ContributorInterfaceable(WorkerGitInterfaceable):
       response_data = None
       success = False
 
+      #This borrow's the logic to safely hit an endpoint from paginate_endpoint.
       while attempts < 10:
         try:
           response = requests.get(url=url, headers=self.headers)
@@ -107,7 +113,8 @@ class ContributorInterfaceable(WorkerGitInterfaceable):
           self.logger.info(f"User data request for enriching contributor data failed with {attempts} attempts! Trying again...")
           time.sleep(10)
           continue
-
+        
+        #Make sure we know how many requests our api tokens have.
         self.update_rate_limit(response,platform="github")
 
         try:
@@ -131,6 +138,7 @@ class ContributorInterfaceable(WorkerGitInterfaceable):
             self.logger.warning("Empty string, trying again...\n")
           else:
             try:
+              #Sometimes raw text can be converted to a dict
               response_data = json.loads(response_data)
               success = True
               break
@@ -144,52 +152,12 @@ class ContributorInterfaceable(WorkerGitInterfaceable):
 
 
 
-      
+    #Update the contributors table from the data facade has gathered.
     def insert_facade_contributors(self, entry_info, repo_id):
         self.logger.info(
             "Beginning process to insert contributors from facade commits for repo w entry info: {}\n".format(entry_info))
 
-        # sql query used 
-        resolve_email_to_cntrb_id_sql = s.sql.text("""
-          select distinct cntrb_id, contributors.cntrb_email, commits.cmt_author_raw_email
-          from 
-              contributors, commits
-          where 
-              contributors.cntrb_email = commits.cmt_author_raw_email
-              AND commits.repo_id =:repo_id
-          union 
-          select distinct cntrb_id, contributors.cntrb_email, commits.cmt_committer_raw_email
-          from 
-              contributors, commits
-          where 
-              contributors.cntrb_email = commits.cmt_committer_raw_email
-              AND commits.repo_id =:repo_id
-        """)
-
-        existing_contrib_sql = s.sql.text("""
-          select distinct contributors.cntrb_email, commits.cmt_author_raw_email
-          from 
-              contributors, commits
-          where 
-              contributors.cntrb_email = commits.cmt_author_raw_email
-              AND commits.repo_id =:repo_id
-          union 
-          select distinct contributors.cntrb_email, commits.cmt_committer_raw_email
-          from 
-              contributors, commits
-          where 
-              contributors.cntrb_email = commits.cmt_committer_raw_email
-              AND commits.repo_id =:repo_id
-        """)
-
-        existing_cntrb_emails = json.loads(pd.read_sql(existing_contrib_sql, self.db, params={
-                                           'repo_id': repo_id}).to_json(orient="records"))
-
-        for cntrb_email in existing_cntrb_emails:
-            pass
-            # query database with 'cntrb_email' and get the 'cntb_id'
-            # find all commits in commits table with 'cntrb_email'
-            # update 'cntrb_id' to each commit
+        
 
         new_contrib_sql = s.sql.text("""
           SELECT distinct 
@@ -263,7 +231,7 @@ class ContributorInterfaceable(WorkerGitInterfaceable):
               commits.cmt_committer_email,
               commits.cmt_committer_date,
               commits.cmt_committer_name
-    """)
+        """)
 
         new_contribs = json.loads(pd.read_sql(new_contrib_sql, self.db, params={
                                   'repo_id': repo_id}).to_json(orient="records"))
@@ -280,8 +248,13 @@ class ContributorInterfaceable(WorkerGitInterfaceable):
             if login_json['total_count'] == 0:
                 self.logger.info(
                     "Search query did not return any results, adding 1's to commits table...\n")
-                # find all commits in commits table with 'cntrb_email'
-                # update 'cntrb_id' to each commit
+
+                #At this point we know we can't do anything with the email so we make the cntrb_id foreign key 1
+                self.db.execute(self.commits_table.update().where(
+                  self.commits_table.c.cmt_committer_email==contributor['commit_email']
+                ).values({
+                  'cmt_ght_author_id' : 1
+                }))
                 continue
 
             self.logger.info("When searching for a contributor with info {}, we found the following users: {}\n".format(
@@ -295,14 +268,100 @@ class ContributorInterfaceable(WorkerGitInterfaceable):
 
             url = ("https://api.github.com/users/" + match['login'])
 
-            
+            user_data = self.request_dict_from_endpoint(url)
+
+            if user_data == None:
+              self.logger.warning(f"user_data was unable to be reached. Skipping...")
+              continue
+
             # try to add contributor to database
+            cntrb = {
+              "cntrb_login": user_data['login'],
+              "cntrb_created_at": user_data['created_at'],
+              "cntrb_email": user_data['email'] if 'email' in user_data else None,
+              "cntrb_company": user_data['company'] if 'company' in user_data else None,
+              "cntrb_location": user_data['location'] if 'location' in user_data else None,
+              # "cntrb_type": , dont have a use for this as of now ... let it default to null
+              "cntrb_canonical": user_data['email'] if 'email' in user_data else None,
+              "gh_user_id": user_data['id'],
+              "gh_login": user_data['login'],
+              "gh_url": user_data['url'],
+              "gh_html_url": user_data['html_url'],
+              "gh_node_id": user_data['node_id'],
+              "gh_avatar_url": user_data['avatar_url'],
+              "gh_gravatar_id": user_data['gravatar_id'],
+              "gh_followers_url": user_data['followers_url'],
+              "gh_following_url": user_data['following_url'],
+              "gh_gists_url": user_data['gists_url'],
+              "gh_starred_url": user_data['starred_url'],
+              "gh_subscriptions_url": user_data['subscriptions_url'],
+              "gh_organizations_url": user_data['organizations_url'],
+              "gh_repos_url": user_data['repos_url'],
+              "gh_events_url": user_data['events_url'],
+              "gh_received_events_url": user_data['received_events_url'],
+              "gh_type": user_data['type'],
+              "gh_site_admin": user_data['site_admin'],
+              "cntrb_last_used" : None if 'updated_at' not in user_data else user_data['updated_at'],
+              "cntrb_full_name" : None if 'name' not in user_data else user_data['name'],
+              "tool_source": self.tool_source,
+              "tool_version": self.tool_version,
+              "data_source": self.data_source
+            }
             # expcpetion: log that the user was already THERE
 
-            # get the cntrb_id based on the logging
-            # find all commits in commits table with 'cntrb_email'
-            # update 'cntrb_id' to each commit
+            try:
+              self.db.execute(self.contributors_table.insert().values(cntrb))
+            except Exception as e:
+              self.logger.info("Ran into likely database collision. Assuming contributor exists in database. Error: {e}")
+        
 
+        # sql query used to find corresponding cntrb_id's of emails found in the contributor's table
+        resolve_email_to_cntrb_id_sql = s.sql.text("""
+          select distinct cntrb_id, contributors.cntrb_email, commits.cmt_author_raw_email
+          from 
+              contributors, commits
+          where 
+              contributors.cntrb_email = commits.cmt_author_raw_email
+              AND commits.repo_id =:repo_id
+          union 
+          select distinct cntrb_id, contributors.cntrb_email, commits.cmt_committer_raw_email
+          from 
+              contributors, commits
+          where 
+              contributors.cntrb_email = commits.cmt_committer_raw_email
+              AND commits.repo_id =:repo_id
+        """)
+
+        #sql query to get emails from current repo that already exist in the contributor's table.
+        
+        #existing_contrib_sql = s.sql.text("""
+        #  select distinct contributors.cntrb_email, commits.cmt_author_raw_email
+        #  from 
+        #      contributors, commits
+        #  where 
+        #      contributors.cntrb_email = commits.cmt_author_raw_email
+        #      AND commits.repo_id =:repo_id
+        #  union 
+        #  select distinct contributors.cntrb_email, commits.cmt_committer_raw_email
+        #  from 
+        #      contributors, commits
+        #  where 
+        #      contributors.cntrb_email = commits.cmt_committer_raw_email
+        #      AND commits.repo_id =:repo_id
+        #""")
+        
+        #Get a list of dicts that contain the emails and cntrb_id's of commits that appear in the contributor's table.
+        existing_cntrb_emails = json.loads(pd.read_sql(resolve_email_to_cntrb_id_sql, self.db, params={
+                                           'repo_id': repo_id}).to_json(orient="records"))
+
+        #iterate through all the commits with emails that appear in contributors and give them the relevant cntrb_id.
+        for cntrb_email in existing_cntrb_emails:
+            self.logger.info(f"These are the emails and cntrb_id's  returned: {cntrb_email}")
+            self.db.execute(self.commits_table.update().where(
+              self.commits_table.c.cmt_committer_email==cntrb_email['cntrb_email']
+            ).values({
+              'cmt_ght_author_id' : cntrb_email['cntrb_id']
+            }))
         return
         # old method
         """
