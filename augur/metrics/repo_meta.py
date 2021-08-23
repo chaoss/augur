795e84d4d488b2fb278c7471988626cd9d8a3ed7
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2021-08-23 11:47:04 -0500

    Fix errors in cii-best-practices-badge endpoint
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur/metrics/repo_meta.py
+++ augur/metrics/repo_meta.py
@@ -210,6 +210,9 @@ def cii_best_practices_badge(self, repo_group_id, repo_id=None):
 
     raw_df = pd.read_sql(cii_best_practices_badge_SQL, self.database, params={'repo_id': repo_id})
 
+    if len(raw_df) == 0:
+        return []
+
     badging_data = raw_df.iloc[0,0][0]
 
     result = {
@@ -903,10 +906,10 @@ def average_weekly_commits(self, repo_group_id=None, repo_id=None, calendar_year
 
     extra_and = "AND repo.repo_group_id = :repo_group_id" if repo_group_id and not repo_id else "AND repo.repo_id = :repo_id" if repo_group_id and repo_id else ""
     average_weekly_commits_sql = s.sql.text("""
-        SELECT repo.repo_id, repo.repo_name, year, sum(patches)/52 AS average_weekly_commits 
+        SELECT repo.repo_id, repo.repo_name, year, sum(patches)/52 AS average_weekly_commits
         FROM dm_repo_annual, repo
         WHERE YEAR = :calendar_year -- or other year
-        AND dm_repo_annual.repo_id = repo.repo_id 
+        AND dm_repo_annual.repo_id = repo.repo_id
         {}
         GROUP BY repo.repo_id, repo.repo_name, YEAR
         ORDER BY repo_name
