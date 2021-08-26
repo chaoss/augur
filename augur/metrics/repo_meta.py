f1249f476908fcd3687afbc60e4ed41c59c584bb
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2021-08-26 13:13:08 -0500

    Add rg-id functionality to cii-best-practices-badge endpoint
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur/metrics/repo_meta.py
+++ augur/metrics/repo_meta.py
@@ -200,13 +200,22 @@ def cii_best_practices_badge(self, repo_group_id, repo_id=None):
     :param repo_id: The repository's repo_id, defaults to None
     :return: CII best parctices badge level
     """
-    cii_best_practices_badge_SQL = s.sql.text("""
-        SELECT data
-        FROM augur_data.repo_badging
-        WHERE repo_id = :repo_id
-        ORDER BY created_at DESC
-        LIMIT 1
-    """)
+    if not repo_id:
+        cii_best_practices_badge_SQL = s.sql.text("""
+            SELECT data
+            FROM augur_data.repo_badging
+            WHERE repo_id IN (SELECT repo_id FROM repo WHERE repo_group_id = :repo_group_id)
+            ORDER BY created_at DESC
+            LIMIT 1
+        """)
+    else:
+        cii_best_practices_badge_SQL = s.sql.text("""
+            SELECT data
+            FROM augur_data.repo_badging
+            WHERE repo_id = :repo_id
+            ORDER BY created_at DESC
+            LIMIT 1
+        """)
 
     raw_df = pd.read_sql(cii_best_practices_badge_SQL, self.database, params={'repo_id': repo_id})
 
