b1a5e8beb90ce917ac74b2e448f37706078b1c5c
Sean Goggins <s@goggins.com>
2021-09-01 07:59:39 -0500

    checking default branch before pull

--- workers/facade_worker/facade_worker/facade05repofetch.py
+++ workers/facade_worker/facade_worker/facade05repofetch.py
@@ -300,6 +300,58 @@ def git_repo_updates(cfg):
 
         while attempt < 3:
 
+            try:
+
+                cmd_default_branch_change = ("git -C %s%s/%s%s remote show origin | sed -n '/HEAD branch/s/.*: //p'"
+                    % (cfg.repo_base_directory,row[1],row[4],row[3]))
+
+                cmd_default_branch_change = ("git -C %s%s/%s%s remote show origin | sed -n '/HEAD branch/s/.*: //p'"
+                    % (cfg.repo_base_directory,row[1],row[4],row[3]))
+
+                cfg.log_activity('Verbose','finding default branch '
+                    ' for %s' % row[2])
+                
+                return_code_default_change = subprocess.Popen([cmd_default_branch_change],stdout=subprocess.PIPE,shell=True).wait()
+
+                branch_test = subprocess.Popen([cmd_default_branch_change],stdout=subprocess.PIPE,shell=True).communicate()[0]
+
+                cfg.log_activity('Verbose', f'default branch encoded return is {return_code_default_change} '
+                    ' for %s' % row[2])                    
+
+                cfg.log_activity('Verbose', f'default branch communicate return is {branch_test} '
+                    ' for %s' % row[2])   
+
+                default_branch = branch_test.decode()
+
+                #default_branch = cmd_default_branch_change.communicate()[0]
+
+                cfg.log_activity('Verbose', f'default branch is {default_branch} '
+                    ' for %s' % row[2])                    
+
+                #default_branch = cmd_default_branch_change.communicate()[0]
+
+                #default_branch = return_code_default_change
+
+                cfg.log_activity('Verbose', f'default branch is {default_branch} '
+                    ' for %s' % row[2])
+
+                cmd_checkout_default = (f"git -C %s%s/%s%s checkout {default_branch}" 
+                    % (cfg.repo_base_directory,row[1],row[4],row[3]))
+
+                cfg.log_activity('Verbose', f'checkout command is {cmd_checkout_default}.')
+
+                cfg.log_activity('Verbose',f'attempting to checkout default branch {default_branch} '
+                    ' for %s' % row[2])                
+
+                return_code_default_change = subprocess.Popen([cmd_checkout_default],shell=True).wait()
+
+                cfg.log_activity('Verbose',f'return code  from default branch change is: {return_code_default_change} '
+                    ' for %s' % row[2])   
+
+            except Exception as e: 
+                cfg.log_activity('Verbose', f'Error code on branch change is {e}.')
+
+
             cmd = ("git -C %s%s/%s%s pull"
                 % (cfg.repo_base_directory,row[1],row[4],row[3]))#['projects_id'],row['path'],row['name']))
 
