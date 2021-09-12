52d124918d8199e66e536a46410fb11ee3e10244
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-09-12 23:29:06 +0530

    getting NPM data from registry
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- workers/deps_libyear_worker/npm_libyear_utils.py
+++ workers/deps_libyear_worker/npm_libyear_utils.py
@@ -1,3 +1,12 @@
 import json
 import os, re
 import requests
+
+def get_NPM_data(package):
+    url = "https://registry.npmjs.org/%s" % package
+    r = requests.get(url)
+    if r.status_code < 400:
+        return r.json()
+    return {}
+
+
