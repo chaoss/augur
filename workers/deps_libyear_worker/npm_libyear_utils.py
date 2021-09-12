1648c8273dc4cacf29b9cf1a0f9efcb0b8d9a22f
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-09-12 23:29:57 +0530

    getting the latest patch
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- workers/deps_libyear_worker/npm_libyear_utils.py
+++ workers/deps_libyear_worker/npm_libyear_utils.py
@@ -10,3 +10,18 @@ def get_NPM_data(package):
     return {}
 
 
+def get_latest_patch(version, data):
+    versions = data['versions']
+    try:
+        index = list(versions.keys()).index(version)
+    except:
+        #NOTE: Add error logging here
+        pass
+    major,minor,patch = version.split('.')
+    consider_version = version
+    for v in list(versions.keys())[index:]:
+        if v.split('.')[0]==major:
+            if v.split('.')[1]== minor:
+                if v.split('.')[2]>patch:
+                    consider_version = v
+    return consider_version
