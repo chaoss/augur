f3172e000de2fe4a3a393a56677004273ba8118d
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-09-12 23:30:48 +0530

    getting the latest minor and clean version
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- workers/deps_libyear_worker/npm_libyear_utils.py
+++ workers/deps_libyear_worker/npm_libyear_utils.py
@@ -10,6 +10,11 @@ def get_NPM_data(package):
     return {}
 
 
+def clean_version(version):
+    version = [v for v in version if v.isdigit() or v == '.']
+    return ''.join(version)
+
+
 def get_latest_patch(version, data):
     versions = data['versions']
     try:
@@ -25,3 +30,19 @@ def get_latest_patch(version, data):
                 if v.split('.')[2]>patch:
                     consider_version = v
     return consider_version
+
+
+def get_lastest_minor(version, data):
+    versions = data['versions']
+    try:
+        index = list(versions.keys()).index(version)
+    except:
+        #NOTE: Add error logging here
+        pass
+    major,minor,patch = version.split('.')
+    consider_version = version
+    for v in list(versions.keys())[index:]:
+        if v.split('.')[0]==major:
+            if v.split('.')[1]>minor:
+                    consider_version = v
+    return consider_version 
