946c21a46ebf5fe279795bee4ad5d171bdbda327
Sean Goggins <s@goggins.com>
2021-09-16 17:30:08 -0500

    sean update

--- workers/worker_persistance.py
+++ workers/worker_persistance.py
@@ -818,7 +818,7 @@ class Persistant():
                     # s_buf_encoded = s_buf.read().encode("UTF-8")
                     #self.logger.info(f"this is the sbuf_encdoded {s_buf_encoded}")
                     try:
-                        Session=sessy.sessionmaker(bind=dbapi_conn)
+                        sessy.Session=sessy.sessionmaker(bind=dbapi_conn)
                         session=Session()
                         # session.copy_expert(sql=sql, file=s_buf)
                         #copy_expert(sql=sql, file=s_buf)
