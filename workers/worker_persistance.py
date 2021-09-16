6fd40ceb93af8d73271ecb1a5f01205ad03a268c
Sean P. Goggins <s@goggins.com>
2021-09-16 15:53:50 -0500

    trying different bulk insert strategy.

--- workers/worker_persistance.py
+++ workers/worker_persistance.py
@@ -818,29 +818,32 @@ class Persistant():
                     try:
                         curs.copy_expert(sql=sql, file=s_buf)
                     except Exception as e:
-                        self.logger.info(f"Bulk insert error: {e}. exception registerred")
+                        self.logger.info(f"Bulk insert error: {e}. exception registered")
                         stacker = traceback.format_exc()
                         self.logger.debug(f"{stacker}")
 
+            try: 
+                df = pd.DataFrame(insert)
+                if convert_float_int:
+                    df = self._convert_float_nan_to_int(df)
+                df.to_sql(
+                    schema = self.db_schema,
+                    name=table.name,
+                    con=self.db,
+                    if_exists="append",
+                    index=False,
+                    method=None
+                    #method=psql_insert_copy
+                )
+                if increment_counter:
+                    self.insert_counter += len(insert)
 
-            df = pd.DataFrame(insert)
-            if convert_float_int:
-                df = self._convert_float_nan_to_int(df)
-            df.to_sql(
-                schema = self.db_schema,
-                name=table.name,
-                con=self.db,
-                if_exists="append",
-                index=False,
-                method=psql_insert_copy
-            )
-            if increment_counter:
-                self.insert_counter += len(insert)
-
-            self.logger.info(
-                f"Inserted {len(insert)} rows in {time.time() - insert_start_time} seconds "
-                "thanks to postgresql's COPY FROM CSV! :)"
-            )
+                self.logger.info(
+                    f"Inserted {len(insert)} rows in {time.time() - insert_start_time} seconds "
+                    "thanks to postgresql's COPY FROM CSV! :)"
+                )
+            except Exception as e: 
+                self.logger.info(f"Bulk insert error 2: {e}. exception registered.")
 
         return insert_result, update_result
 
