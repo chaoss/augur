1c42643d2aa90cbb8eaeacd83d649c6939bdd177
Sean P. Goggins <s@goggins.com>
2021-09-17 15:52:10 -0500

    adding unresolved committer cache table.

---
+++ schema/generate/67-schema_update_69.sql
@@ -0,0 +1,18 @@
+BEGIN; 
+
+CREATE TABLE "augur_data"."unresolved_commit_emails" (
+  "email_unresolved_id" serial8,
+  "email" varchar COLLATE "pg_catalog"."default" NOT NULL,
+  "tool_source" varchar(255) COLLATE "pg_catalog"."default",
+  "tool_version" varchar(255) COLLATE "pg_catalog"."default",
+  "data_source" varchar(255) COLLATE "pg_catalog"."default",
+  "data_collection_date" timestamp(0) DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY ("email_unresolved_id"),
+  UNIQUE ("email")
+)
+;
+
+
+update "augur_operations"."augur_settings" set value = 69 where setting = 'augur_data_version'; 
+
+COMMIT; 
