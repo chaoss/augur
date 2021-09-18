a1a07103272f581e222da309a79bf20975e2beee
Sean Goggins <s@goggins.com>
2021-09-18 14:02:03 -0500

    created_at hell

--- workers/github_worker/github_worker.py
+++ workers/github_worker/github_worker.py
@@ -555,11 +555,12 @@ class GitHubWorker(WorkerGitInterfaceable):
                 '''
 
                 ## Cast the numerics as ints, as prior update on 9/17 did not eliminate the error noted above. SPG, 9/18/2021
+                self.logger.info(f"issue close_at is: {issue['closed_at']}")
                 closed_issue_updates.append({
                     'b_issue_id': int(issue['issue_id']),
                     'cntrb_id': int(closed_event['cntrb_id']),
                     'issue_state': issue['state'],
-                    'closed_at': s.to_datetime(issue['closed_at'])
+                    'closed_at': issue['closed_at']
                 })
 
                 self.logger.info(f"Current closed issue count is {len(closed_issue_updates)}.")
