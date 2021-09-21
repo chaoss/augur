18b4905f4626cec7e7ece6c9747b76b4715d5174
Sean P. Goggins <s@goggins.com>
2021-09-21 12:51:14 -0500

    version update on schema missed. updated.

--- schema/generate/71-schema_update_73.sql
+++ schema/generate/71-schema_update_73.sql
@@ -5,7 +5,7 @@ ALTER TABLE "augur_data"."pull_request_reviews"
   ADD CONSTRAINT "fk-review-platform" FOREIGN KEY ("platform_id") REFERENCES "augur_data"."platform" ("pltfrm_id") ON DELETE RESTRICT ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
 
 
-update "augur_operations"."augur_settings" set value = 72 where setting = 'augur_data_version'; 
+update "augur_operations"."augur_settings" set value = 73 where setting = 'augur_data_version'; 
 
 
 COMMIT; 
