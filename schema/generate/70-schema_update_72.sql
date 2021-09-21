1038cf0c991a848825d87409c13b890ec7299281
Sean P. Goggins <s@goggins.com>
2021-09-21 10:12:03 -0500

    duplicate

--- schema/generate/70-schema_update_72.sql
+++ schema/generate/70-schema_update_72.sql
@@ -8,14 +8,6 @@ ALTER TABLE "augur_data"."pull_request_events"
   ADD CONSTRAINT "fkprevent_repo_id" FOREIGN KEY ("repo_id") REFERENCES "augur_data"."repo" ("repo_id") ON DELETE RESTRICT ON UPDATE RESTRICT DEFERRABLE INITIALLY DEFERRED,
   ADD CONSTRAINT "unique-pr-event-id" UNIQUE ("pr_platform_event_id", "platform_id");
 
-ALTER TABLE "augur_data"."pull_request_events" 
-  ADD COLUMN "platform_id" varchar DEFAULT 25150,
-  ADD COLUMN "pr_platform_event_id" int8,
-  ADD COLUMN "repo_id" int8,
-  ADD CONSTRAINT "fkpr_platform" FOREIGN KEY ("platform_id") REFERENCES "augur_data"."platform" ("pltfrm_id") ON DELETE RESTRICT ON UPDATE RESTRICT DEFERRABLE INITIALLY DEFERRED,
-  ADD CONSTRAINT "fkprevent_repo_id" FOREIGN KEY ("repo_id") REFERENCES "augur_data"."repo" ("repo_id") ON DELETE RESTRICT ON UPDATE RESTRICT DEFERRABLE INITIALLY DEFERRED,
-  ADD CONSTRAINT "unique-pr-event-id" UNIQUE ("pr_platform_event_id", "platform_id");
-
 ALTER TABLE "augur_data"."pull_request_labels" 
   ADD CONSTRAINT "unique-pr-src-label-id" UNIQUE ("pr_src_id");
 
