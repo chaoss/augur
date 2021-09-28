2987d2c3c5674b398b3952cf16e53801ce89ca44
Isaac <imilarsky@gmail.com>
2021-09-28 14:37:37 -0500

    pdb
    Signed-off-by: Isaac <imilarsky@gmail.com>

--- workers/facade_worker/facade_worker/facade00mainprogram.py
+++ workers/facade_worker/facade_worker/facade00mainprogram.py
@@ -43,6 +43,8 @@ from contributor_interfaceable.contributor_interface import ContributorInterface
 from workers.util import read_config
 from workers.worker_base import Worker
 
+import pdb
+
 html = html.parser.HTMLParser()
 
 class FacadeWorker(Worker):
@@ -334,7 +336,7 @@ class FacadeWorker(Worker):
 
         all_repos = list(self.cfg.cursor)
 
-        breakpoint()
+        pdb.set_trace()
         for repo in all_repos:
           self.logger.info(f"Processing repo {repo}")
           self.github_interface.insert_facade_contributors(repo[0])
