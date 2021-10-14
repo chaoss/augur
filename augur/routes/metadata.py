27344cda2af8ea6126ecd312f6892094a342b613
Sean P. Goggins <s@goggins.com>
2021-10-14 09:29:12 -0500

    debug 2

--- augur/routes/metadata.py
+++ augur/routes/metadata.py
@@ -17,34 +17,33 @@ def create_routes(server):
 	@server.app.route(f"/{server.api_version}/metadata/repo_info")
 	def get_repo_info():
         repo_info_sql = s.sql.text("""
-						SELECT
-							repo.repo_git,
-							repo.repo_name,
-							repo.repo_id,
-							repo_info.default_branch,
-							repo_info.license,
-							repo_info.fork_count,
-							repo_info.watchers_count,
-							repo_info.stars_count,
-							repo_info.committers_count,
-							repo_info.open_issues,
-							repo_info.issues_count,
-							repo_info.issues_closed,
-							repo_info.pull_request_count,
-							repo_info.pull_requests_open,
-							repo_info.pull_requests_closed,
-							repo_info.pull_requests_merged 
-						FROM
-							repo_info,
-							repo,
-							( SELECT repo_id, MAX ( data_collection_date ) AS last_collected FROM augur_data.repo_info GROUP BY repo_id ORDER BY repo_id ) e 
-						WHERE
-							repo_info.repo_id = repo.repo_id 
-							AND e.repo_id = repo_info.repo_id 
-							AND e.last_collected = repo_info.data_collection_date 
-						ORDER BY
-							repo.repo_name
-
+			SELECT
+				repo.repo_git,
+				repo.repo_name,
+				repo.repo_id,
+				repo_info.default_branch,
+				repo_info.license,
+				repo_info.fork_count,
+				repo_info.watchers_count,
+				repo_info.stars_count,
+				repo_info.committers_count,
+				repo_info.open_issues,
+				repo_info.issues_count,
+				repo_info.issues_closed,
+				repo_info.pull_request_count,
+				repo_info.pull_requests_open,
+				repo_info.pull_requests_closed,
+				repo_info.pull_requests_merged 
+			FROM
+				repo_info,
+				repo,
+				( SELECT repo_id, MAX ( data_collection_date ) AS last_collected FROM augur_data.repo_info GROUP BY repo_id ORDER BY repo_id ) e 
+			WHERE
+				repo_info.repo_id = repo.repo_id 
+				AND e.repo_id = repo_info.repo_id 
+				AND e.last_collected = repo_info.data_collection_date 
+			ORDER BY
+				repo.repo_name
         """)
         results = pd.read_sql(repo_info_sql, server.augur_app.database)
         data = results.to_json(orient="columns")
