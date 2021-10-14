bc1a764a8d99ee188953be118117f73de5bc7506
Sean P. Goggins <s@goggins.com>
2021-10-14 08:59:59 -0500

    api routes for data validation: Updates.

--- augur/routes/collection_status.py
+++ augur/routes/collection_status.py
@@ -92,8 +92,9 @@ def create_routes(server):
             WHERE d.issues_enabled = 'true';
         """)
         results = pd.read_sql(issue_collection_sql, server.augur_app.database)
-        data = results.to_json(orient="records", date_format='iso', date_unit='ms')
-        return Response(response=data,
+        data = results.to_json(orient="columns", date_format='iso', date_unit='ms')
+        parsed_data = json.loads(data)
+        return Response(response=parsed_data,
                         status=200,
                         mimetype="application/json")
 
@@ -160,7 +161,8 @@ def create_routes(server):
                 ratio_abs;
         """)
         results = pd.read_sql(pull_request_collection_sql, server.augur_app.database)
-        data = results.to_json(orient="records", date_format='iso', date_unit='ms')
-        return Response(response=data,
+        data = results.to_json(orient="columns", date_format='iso', date_unit='ms')
+        parsed_data = json.loads(data)
+        return Response(response=parsed_data,
                         status=200,
                         mimetype="application/json")
