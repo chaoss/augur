2dcb3192af483409375a5c16c93af466cab3f69a
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2021-10-15 12:06:16 -0500

    Set up basic structure for worker_persistance tests
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

---
+++ tests/test_workers/worker_persistance/test_enrich_data_primary_keys.py
@@ -0,0 +1,19 @@
+#SPDX-License-Identifier: MIT
+import pytest
+from workers.worker_peristance import *
+from tests.test_workers.test_data import *
+from tests.test_workers.test_set_up_fixtures import *
+
+
+def test_enrich_data_primary_keys(set_up_and_tear_down_database):
+
+    #instantiate Worker Persistance
+    # assign db connection to self.db
+    # insert test data into db
+    # define data passed to method
+
+    # call method
+
+    # assert result
+
+    # set_up_and_tear_down_database fixture will clean up docker container
