0f58631378c16548c651cd05698031e529a3d7cb
Sean P. Goggins <s@goggins.com>
2021-10-20 12:53:20 -0500

    release clean

--- metadata.py
+++ metadata.py
@@ -5,8 +5,8 @@ __url__ = "https://github.com/chaoss/augur"
 
 __short_description__ = "Python 3 package for free/libre and open-source software community metrics & data collection"
 
-__version__ = "0.21.4"
-__release__ = "v0.21.4"
+__version__ = "0.21.5"
+__release__ = "v0.21.5"
 
 __license__ = "MIT"
 __copyright__ = "CHAOSS & Augurlabs 2021"
