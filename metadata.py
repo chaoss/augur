974579beca357576098f17bdf3af09d70a9381a6
Sean P. Goggins <s@goggins.com>
2021-10-20 12:31:20 -0500

    updated release version. Removed two not ready workers.

--- metadata.py
+++ metadata.py
@@ -5,8 +5,8 @@ __url__ = "https://github.com/chaoss/augur"
 
 __short_description__ = "Python 3 package for free/libre and open-source software community metrics & data collection"
 
-__version__ = "0.21.3"
-__release__ = "v0.21.3"
+__version__ = "0.21.4"
+__release__ = "v0.21.4"
 
 __license__ = "MIT"
 __copyright__ = "CHAOSS & Augurlabs 2021"
