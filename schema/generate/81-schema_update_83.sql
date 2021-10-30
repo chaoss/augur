103d42ed60ef52edc60219dff72d178ec61308df
Sean Goggins <s@goggins.com>
2021-10-30 09:51:02 -0500

    schema name omitted

--- schema/generate/81-schema_update_83.sql
+++ schema/generate/81-schema_update_83.sql
@@ -22,7 +22,7 @@ COMMIT;
 
 BEGIN; 
 
-DELETE FROM pull_request_files a
+DELETE FROM augur_data.pull_request_files a
 USING pull_request_files b
 WHERE a.pr_file_id < b.pr_file_id
 AND a.pull_request_id = b.pull_request_id
