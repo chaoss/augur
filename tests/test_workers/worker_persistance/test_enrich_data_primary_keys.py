58a2df77207fc9821db9ff25d88632a79d4d89cd
Isaac M <krabs@tilde.team>
2021-11-19 14:28:48 -0600

    Test github interfaceable worker dummy class written
    Signed-off-by: Isaac M <krabs@tilde.team>

--- tests/test_workers/worker_persistance/test_enrich_data_primary_keys.py
+++ tests/test_workers/worker_persistance/test_enrich_data_primary_keys.py
@@ -1,40 +1,6 @@
 #SPDX-License-Identifier: MIT
-import pytest
 
-#from tests.test_workers.test_data import *
-from tests.test_workers.test_set_up_fixtures import *
-
-
-
-#Sample source data generation that pulls json data that has contributions listed
-@pytest.fixture
-def sample_source_data_enriched():
-    jsonFile = open("tests/test_workers/worker_persistance/contributors.json")
-
-    source_data = json.load(jsonFile)
-
-    jsonFile.close()
-    return source_data
-
-#Sample source data generation that opens json data that doesn't have contributions listed
-@pytest.fixture
-def sample_source_data_unenriched():
-    jsonFile = open("tests/test_workers/worker_persistance/contributors_un_enriched.json")
-
-    source_data = json.load(jsonFile)
-
-    jsonFile.close()
-    return source_data
-
-#Bad data that an api might return
-@pytest.fixture
-def sample_source_data_bad_api_return():
-    jsonFile = open("tests/test_workers/worker_persistance/bad_Data.json")
-
-    source_data = json.load(jsonFile)
-
-    jsonFile.close()
-    return source_data
+from tests.test_workers.worker_persistance.util_persistance import *
 
 
 
