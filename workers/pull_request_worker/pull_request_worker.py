132bdf506fe42ef3c8a3dd07e3c60394aa13c5e3
Sean P. Goggins <s@goggins.com>
2021-12-03 06:18:36 -0600

    Continuing to debug errors in last 2 commits.

--- workers/pull_request_worker/pull_request_worker.py
+++ workers/pull_request_worker/pull_request_worker.py
@@ -440,59 +440,64 @@ class GitHubPullRequestWorker(WorkerGitInterfaceable):
                 self.logger.debug(f"{stacker}")
 
 
-
-            prs_insert = [
-            {
-                'repo_id': self.repo_id,
-                'pr_url': pr['url'],
-                'pr_src_id': pr['id'],
-                'pr_src_node_id': pr['node_id'],  ## 9/20/2021 - This was null. No idea why.
-                'pr_html_url': pr['html_url'],
-                'pr_diff_url': pr['diff_url'],
-                'pr_patch_url': pr['patch_url'],
-                'pr_issue_url': pr['issue_url'],
-                'pr_augur_issue_id': None,
-                'pr_src_number': pr['number'],
-                'pr_src_state': pr['state'],
-                'pr_src_locked': pr['locked'],
-                'pr_src_title': str(pr['title']),
-                'pr_augur_contributor_id': pr['cntrb_id'],
-                'pr_body': str(pr['body']).encode(encoding='UTF-8',errors='backslashreplace').decode(encoding='UTF-8',errors='ignore') if (
-                    pr['body']
-                ) else None,
-                'pr_created_at': pr['created_at'],
-                'pr_updated_at': pr['updated_at'],
-                'pr_closed_at': sqlalchemy.sql.expression.null() if not (  # This had to be changed because "None" is JSON. SQL requires NULL SPG 11/28/2021
-                    pr['closed_at']
-                ) else pr['closed_at'],
-                'pr_merged_at': sqlalchemy.sql.expression.null() if not (  # This had to be changed because "None" is JSON. SQL requires NULL
-                    pr['merged_at']
-                ) else pr['merged_at'],
-                'pr_merge_commit_sha': pr['merge_commit_sha'],
-                'pr_teams': None,
-                'pr_milestone': None,
-                'pr_commits_url': pr['commits_url'],
-                'pr_review_comments_url': pr['review_comments_url'],
-                'pr_review_comment_url': pr['review_comment_url'],
-                'pr_comments_url': pr['comments_url'],
-                'pr_statuses_url': pr['statuses_url'],
-                'pr_meta_head_id': None if not (
-                    pr['head']
-                ) else pr['head']['label'],
-                'pr_meta_base_id': None if not (
-                    pr['base']
-                ) else pr['base']['label'],
-                'pr_src_issue_url': pr['issue_url'],
-                'pr_src_comments_url': pr['comments_url'],
-                'pr_src_review_comments_url': pr['review_comments_url'],
-                'pr_src_commits_url': pr['commits_url'], 
-                'pr_src_statuses_url': pr['statuses_url'],
-                'pr_src_author_association': pr['author_association'],
-                'tool_source': self.tool_source,
-                'tool_version': self.tool_version,
-                'data_source': 'GitHub API'
-            } for pr in inc_source_prs['insert']
-            ]
+            try: 
+                prs_insert = [
+                {
+                    'repo_id': self.repo_id,
+                    'pr_url': pr['url'],
+                    'pr_src_id': pr['id'],
+                    'pr_src_node_id': pr['node_id'],  ## 9/20/2021 - This was null. No idea why.
+                    'pr_html_url': pr['html_url'],
+                    'pr_diff_url': pr['diff_url'],
+                    'pr_patch_url': pr['patch_url'],
+                    'pr_issue_url': pr['issue_url'],
+                    'pr_augur_issue_id': None,
+                    'pr_src_number': pr['number'],
+                    'pr_src_state': pr['state'],
+                    'pr_src_locked': pr['locked'],
+                    'pr_src_title': str(pr['title']),
+                    'pr_augur_contributor_id': int(pr['cntrb_id']), ### Changed to int cast based on error 12/3/2021 SPG
+                    'pr_body': str(pr['body']).encode(encoding='UTF-8',errors='backslashreplace').decode(encoding='UTF-8',errors='ignore') if (
+                        pr['body']
+                    ) else None,
+                    'pr_created_at': pr['created_at'],
+                    'pr_updated_at': pr['updated_at'],
+                    'pr_closed_at': sqlalchemy.sql.expression.null() if not (  # This had to be changed because "None" is JSON. SQL requires NULL SPG 11/28/2021
+                        pr['closed_at']
+                    ) else pr['closed_at'],
+                    'pr_merged_at': sqlalchemy.sql.expression.null() if not (  # This had to be changed because "None" is JSON. SQL requires NULL
+                        pr['merged_at']
+                    ) else pr['merged_at'],
+                    'pr_merge_commit_sha': pr['merge_commit_sha'],
+                    'pr_teams': None,
+                    'pr_milestone': None,
+                    'pr_commits_url': pr['commits_url'],
+                    'pr_review_comments_url': pr['review_comments_url'],
+                    'pr_review_comment_url': pr['review_comment_url'],
+                    'pr_comments_url': pr['comments_url'],
+                    'pr_statuses_url': pr['statuses_url'],
+                    'pr_meta_head_id': None if not (
+                        pr['head']
+                    ) else pr['head']['label'],
+                    'pr_meta_base_id': None if not (
+                        pr['base']
+                    ) else pr['base']['label'],
+                    'pr_src_issue_url': pr['issue_url'],
+                    'pr_src_comments_url': pr['comments_url'],
+                    'pr_src_review_comments_url': pr['review_comments_url'],
+                    'pr_src_commits_url': pr['commits_url'], 
+                    'pr_src_statuses_url': pr['statuses_url'],
+                    'pr_src_author_association': pr['author_association'],
+                    'tool_source': self.tool_source,
+                    'tool_version': self.tool_version,
+                    'data_source': 'GitHub API'
+                } for pr in inc_source_prs['insert']
+                ]
+            except Exception as e: 
+                self.logger.debug(f"Pull Requests model failed with {e}.")
+                stacker = traceback.format_exc()
+                self.logger.debug(f"{stacker}")
+                pass 
 
             # Removed due to error 11/18/2021 : and 'title' in pr['milestone']
 
