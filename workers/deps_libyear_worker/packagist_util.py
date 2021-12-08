047c4af7a86d860e45393137b80dfa14dcd5d364
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-12-08 12:23:45 +0530

    correction in function
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- workers/deps_libyear_worker/packagist_util.py
+++ workers/deps_libyear_worker/packagist_util.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_NPM_data(package):
+def get_packagist_data(package):
     url = "https://repo.packagist.org/packages/%s.json" % package
     r = requests.get(url)
     if r.status_code < 400:
