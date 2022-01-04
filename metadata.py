ccc365a38c8fac41533c460d565f82f246121b5a
Sean Goggins <outdoors@acm.org>
2022-01-03 18:36:50 -0600

    version update

--- metadata.py
+++ metadata.py
@@ -5,8 +5,8 @@ __url__ = "https://github.com/chaoss/augur"
 
 __short_description__ = "Python 3 package for free/libre and open-source software community metrics & data collection"
 
-__version__ = "0.23.0"
-__release__ = "v0.23.0"
+__version__ = "0.23.1"
+__release__ = "v0.23.1"
 
 __license__ = "MIT"
-__copyright__ = "CHAOSS & Augurlabs 2021"
+__copyright__ = "University of Missouri, University of Nebraska-Omaha, CHAOSS & Augurlabs 2022"
