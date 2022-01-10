41d1f5a68bf1e7f3b95b30aab2c984279b27cb9c
Isaac Milarsky <krabs@tilde.team>
2022-01-10 15:56:37 -0600

    basic test
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

--- tests/test_facade/test_facade_contributor_interface/test_endpoints.py
+++ tests/test_facade/test_facade_contributor_interface/test_endpoints.py
@@ -5,9 +5,9 @@ from tests.test_workers.worker_persistance.util_persistance import *
 
 
 #Function to add repo_groups without starting an augur app instance
-def set_up_repo_groups(database_connection):
+def set_up_repo_groups(db):
 
-    df = pd.read_sql(s.sql.text("SELECT repo_group_id FROM augur_data.repo_groups"), database_connection)
+    df = pd.read_sql(s.sql.text("SELECT repo_group_id FROM augur_data.repo_groups"), db)
     repo_group_IDs = df['repo_group_id'].values.tolist()
 
     insert_repo_group_sql = s.sql.text("""
@@ -20,13 +20,13 @@ def set_up_repo_groups(database_connection):
             logger.info(f"Inserting repo group with name {row[1]} and ID {row[0]}...")
             if int(row[0]) not in repo_group_IDs:
                 repo_group_IDs.append(int(row[0]))
-                database_connection.execute(insert_repo_group_sql, repo_group_id=int(row[0]), repo_group_name=row[1])
+                db.execute(insert_repo_group_sql, repo_group_id=int(row[0]), repo_group_name=row[1])
             else:
                 logger.info(f"Repo group with ID {row[1]} for repo group {row[1]} already exists, skipping...")
 
 
 
-    df = database_connection.execute(s.sql.text("SELECT repo_group_id FROM augur_data.repo_groups"))
+    df = db.execute(s.sql.text("SELECT repo_group_id FROM augur_data.repo_groups"))
 
     repo_group_IDs = [group[0] for group in df.fetchall()]
 
@@ -41,7 +41,7 @@ def set_up_repo_groups(database_connection):
         for row in data:
             logger.info(f"Inserting repo with Git URL `{row[1]}` into repo group {row[0]}")
             if int(row[0]) in repo_group_IDs:
-                result = database_connection.execute(insertSQL, repo_group_id=int(row[0]), repo_git=row[1])
+                result = db.execute(insertSQL, repo_group_id=int(row[0]), repo_git=row[1])
             else:
                 logger.warning(f"Invalid repo group id specified for {row[1]}, skipping.")
 
@@ -50,3 +50,16 @@ def set_up_repo_groups(database_connection):
 
 def test_create_sha_endpoint_default(database_connection):
     set_up_repo_groups(database_connection)
+
+    #Dummy class for testing
+    dummy = DummyFullWorker(database_connection)
+
+    url = dummy.create_endpoint_from_commit_sha("e362752b3c475903e185d039bca4bb892dd5e432", "10")
+
+    try:
+        response = requests.get(url=url, headers=dummy.headers)
+    except:
+        raise AssertionError
+
+
+
