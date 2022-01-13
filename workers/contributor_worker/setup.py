513903b531e6093228016b7beff78a824672f92a
Sean Goggins <s@goggins.com>
2022-01-13 17:26:52 -0600

    dependencies update: python

--- workers/contributor_worker/setup.py
+++ workers/contributor_worker/setup.py
@@ -29,7 +29,7 @@ setup(
         'requests==2.22.0',
         'psycopg2-binary==2.8.6',
         'click==8.0.3', 
-        'scipy==1.6.3',
+        'scipy==1.7.3',
         'sklearn==0.0'
     ],
     entry_points={
