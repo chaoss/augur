a2bc8c1cd967bcadf1cdc9443cfa1cfe349a6b0a
Isaac Milarsky <krabs@tilde.team>
2022-02-01 14:55:10 -0600

    Interface using query_github_contributors
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

--- workers/worker_persistance.py
+++ workers/worker_persistance.py
@@ -49,7 +49,7 @@ class Persistant():
         # count of tuples inserted in the database ( to store stats for each task in op tables)
         self.update_counter = 0
         self.insert_counter = 0
-        self._results_counter = 0
+         = 0
 
         # Update config with options that are general and not specific to any worker
         self.augur_config = AugurConfig(self._root_augur_dir)
