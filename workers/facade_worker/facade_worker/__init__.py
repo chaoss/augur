b2c4abd4abfd480ed0ebedaa86cb2bce5b499285
Sean P. Goggins <outdoors@acm.org>
2022-02-24 15:40:36 -0600

    Update __init__.py

--- workers/facade_worker/facade_worker/__init__.py
+++ workers/facade_worker/facade_worker/__init__.py
@@ -1,6 +1,6 @@
 #SPDX-License-Identifier: MIT
 """augur_worker_github - Augur Worker that collects GitHub data"""
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Augur Team <s@goggins.com>'
 __all__ = []
