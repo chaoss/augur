282452e5b543fb91379bb080ad41796211598f1b
Sean Goggins <s@goggins.com>
2022-03-21 16:05:19 -0500

    ORM Unique Update
    Signed-off-by: Sean Goggins <s@goggins.com>

--- schema/create_schema.sql
+++ schema/create_schema.sql
@@ -19,6 +19,7 @@
 \i schema/generate/92-schema_update_94.sql
 \i schema/generate/93-schema_update_95.sql
 \i schema/generate/94-schema_update_96.sql
+\i schema/generate/95-schema_update_97.sql
 
 -- prior update scripts incorporated into 
 -- augur.sql file for release v0.21.1
