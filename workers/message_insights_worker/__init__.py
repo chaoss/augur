25ee4397d704c5a82322b9f9d4d69eaf39b6bf0f
Sean P. Goggins <s@goggins.com>
2022-04-11 13:08:30 -0500

    License Identifiers to Message Insights Worker.
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- workers/message_insights_worker/__init__.py
+++ workers/message_insights_worker/__init__.py
@@ -1,5 +1,7 @@
+#SPDX-License-Identifier: MIT
+
 """message_insights_worker - Augur Worker that analyzes PR and issue messages"""
 
-__version__ = '0.0.0'
+__version__ = '0.0.2'
 __author__ = 'Augur Team <s@goggins.com>'
 __all__ = []
