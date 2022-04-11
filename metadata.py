0ae323c525508dcd286624cb2bc53b723c1f1360
Sean P. Goggins <s@goggins.com>
2022-04-11 12:21:50 -0500

    Slug fix.
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- metadata.py
+++ metadata.py
@@ -1,6 +1,6 @@
 #SPDX-License-Identifier: MIT
 __name__ = "Augur"
-__slug__ = "augur"
+__slug__ = "Augur: Growing Up"
 __url__ = "https://github.com/chaoss/augur"
 
 __short_description__ = "Python 3 package for free/libre and open-source software community metrics, models & data collection"
