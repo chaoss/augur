1d15508e1d4cc6e008cd48ee95e21f9e9fa3d697
Sean P. Goggins <s@goggins.com>
2022-04-11 16:15:46 -0500

    message insight worker update.
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- workers/message_insights_worker/__init__.py
+++ workers/message_insights_worker/__init__.py
@@ -2,6 +2,6 @@
 
 """message_insights_worker - Augur Worker that analyzes PR and issue messages"""
 
-__version__ = '0.0.2'
+__version__ = '0.3.1'
 __author__ = 'Augur Team <s@goggins.com>'
 __all__ = []
