ef1adbe4691df326523b420e3dac5f8bd917fb97
Isaac Milarsky <imilarsky@gmail.com>
2022-04-12 17:14:31 -0500

    init facade new
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- workers/facade_worker/tasks.py
+++ workers/facade_worker/tasks.py
@@ -1,4 +1,6 @@
+from celery.utils.log import get_task_logger
 from .celery import facadeApp
+
 
 #!/usr/bin/env python3
 
@@ -48,7 +50,128 @@ from workers.worker_base import Worker
 
 html = html.parser.HTMLParser()
 
+logger = get_task_logger(__name__)
 
+#TODO: talk about weird command line arguments.
+
+#OPTIONS are completely disregarded from facade instead provided by config.
 @facadeApp.task
-def commits_model(message)
+def commits_model(config,message):
+    # Figure out what we need to do
+    limited_run = config["limited_run"]
+    delete_marked_repos = config["delete_marked_repos"]
+    pull_repos = config["pull_repos"]
+    clone_repos = config["clone_repos"]
+    check_updates = config["check_updates"]
+    force_updates = config["force_updates"]
+    run_analysis = config["run_analysis"]
+    force_analysis = config["force_analysis"]
+    nuke_stored_affiliations = config["nuke_stored_affiliations"]
+    fix_affiliations = config["fix_affiliations"]
+    force_invalidate_caches = config["force_invalidate_caches"]
+    rebuild_caches = config["rebuild_caches"] #if abs((datetime.datetime.strptime(self.cfg.get_setting('aliases_processed')[:-3], 
+        # '%Y-%m-%d %I:%M:%S.%f') - datetime.datetime.now()).total_seconds()) // 3600 > int(self.cfg.get_setting(
+        #   'update_frequency')) else 0
+    force_invalidate_caches = config["force_invalidate_caches"]
+    create_xlsx_summary_files = config["create_xlsx_summary_files"]
+    multithreaded = config["multithreaded"]
+
+    # Get the location of the directory where git repos are stored
+    repo_base_directory = config['repo_base_directory']
+
+    #Determine if it's safe to start the script
+    current_status = config['utility_status']
+
+    if current_status != 'Idle':
+        logger.error('Something is already running, aborting maintenance and analysis.\nIt is unsafe to continue.')
+        # sys.exit(1)
+    if len(repo_base_directory) == 0:
+        logger.error('No base directory. It is unsafe to continue.')
+        logger.error('Failed: No base directory')
+        sys.exit(1)
+
+    # Begin working
+
+    start_time = time.time()
+    logger.info('Running facade-worker')
+
+    cfg = Config(logger)
+
+    if not limited_run or (limited_run and delete_marked_repos):
+        git_repo_cleanup(cfg)
+
+    if not limited_run or (limited_run and clone_repos):
+        git_repo_initialize(cfg)
+
+    if not limited_run or (limited_run and check_updates):
+        check_for_repo_updates(cfg)
+
+    if force_updates:
+        force_repo_updates(cfg)
+
+    if not limited_run or (limited_run and pull_repos):
+        git_repo_updates(cfg)
+
+    if force_analysis:
+        force_repo_analysis(cfg)
+
+    #TODO: rewrite the contributer interface.
+    #Doesn't really need to be a class at all.
+    github_interface = ContribInterface(config=config, logger=logger) 
+    #Give analysis the github interface so that it can make API calls
+    if not limited_run or (limited_run and run_analysis):
+        analysis(cfg, multithreaded, interface=github_interface)
+    
+
+    ### moved up by spg on 12/1/2021
+    #Interface with the contributor worker and inserts relevant data by repo
+    cfg.update_status('Updating Contributors')
+    cfg.log_activity('Info', 'Updating Contributors with commits')
+    query = ("SELECT repo_id FROM repo")
+
+    cfg.cursor.execute(query)
+
+    all_repos = list(cfg.cursor)
+
+    for repo in all_repos:
+        logger.info(f"Processing repo {repo}")
+        github_interface.insert_facade_contributors(repo[0],multithreaded=multithreaded)
+        logger.info(f"Processing repo contributors for repo: {repo}")
+    
+
+    if nuke_stored_affiliations:
+        nuke_affiliations(cfg)
+
+    if not limited_run or (limited_run and fix_affiliations):
+        fill_empty_affiliations(cfg)
+    
+
+    if force_invalidate_caches:
+        invalidate_caches(cfg)
+
+    if not limited_run or (limited_run and rebuild_caches):
+        rebuild_unknown_affiliation_and_web_caches(cfg)
+
+    if not limited_run or (limited_run and create_xlsx_summary_files):
+
+        cfg.log_activity('Info','Creating summary Excel files')
+
+        # from excel_generators import *
+
+        cfg.log_activity('Info','Creating summary Excel files (complete)')
+    
+    # All done
+    cfg.update_status('Idle')
+    cfg.log_activity('Quiet','facade-worker.py completed')
+
+    elapsed_time = time.time() - start_time
+
+    print('\nCompleted in %s\n' % datetime.timedelta(seconds=int(elapsed_time)))
+
+    cfg.cursor.close()
+    cfg.cursor_people.close()
+    cfg.db.close()
+    cfg.db_people.close()
+
+    
 
