fb19e4b72b61395deaf40003ea679a3b62bee2a2
Sean P. Goggins <s@goggins.com>
2022-04-15 11:03:44 -0500

    version bump

--- metadata.py
+++ metadata.py
@@ -1,12 +1,12 @@
 #SPDX-License-Identifier: MIT
 __name__ = "Augur"
-__slug__ = "Augur: Monica"
+__slug__ = "Augur: Muskellunge"
 __url__ = "https://github.com/chaoss/augur"
 
 __short_description__ = "Python 3 package for free/libre and open-source software community metrics, models & data collection"
 
-__version__ = "0.25.19"
-__release__ = "v0.25.19"
+__version__ = "0.26.0"
+__release__ = "v0.26.0"
 
 __license__ = "MIT"
 __copyright__ = "University of Missouri, University of Nebraska-Omaha, CHAOSS, & Augurlabs 2022"
