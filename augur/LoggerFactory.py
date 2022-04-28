39332c9b1a8865a1929df1727ab34c0cbd3614a6
isaacschroeder <82746033+isaacschroeder@users.noreply.github.com>
2022-04-28 14:56:02 -0500

    Modified LogFactory, still needs help

--- augur/LoggerFactory.py
+++ augur/LoggerFactory.py
@@ -5,7 +5,7 @@ import AugurLogger
 
 class LoggerFactory:
     @staticmethod
-    def create_logger(handler_tags: str, logfile_dir: str, author: (str, str), augur_config) -> AugurLogger:
+    def create_logger(handler_tags: str, logfile_dir: str, worker_type: str, verbose: bool=False, debug: bool=False) -> AugurLogger:
         #Get the log directory folder name and append a slash
         logfile_directory = AugurLogConfigurer.get_log_directories(augur_config, reset_logfiles=False) + "/"
         #create new AugurLogger
@@ -15,5 +15,6 @@ class LoggerFactory:
         #attach handlers
         logger.addHandler(logHandlerFactory.create_handler(handler_tags, logging.INFO))
         logger.addHandler(logHandlerFactory.create_handler(handler_tags, logging.ERROR))
-        logger.addHandler(logHandlerFactory.create_handler(handler_tags, logging.DEBUG)) # how to determine whether to add debug or not?
+        if ()
+            logger.addHandler(logHandlerFactory.create_handler(handler_tags, logging.DEBUG))
         return logger
