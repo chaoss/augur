c8a7fccdf6778e5f54bdc0c2a0d0166f574e706b
Ashwin <ashwinprayaga@gmail.com>
2022-04-28 15:30:46 -0500

    fixed import again

--- augur/routes/pull_request_reports.py
+++ augur/routes/pull_request_reports.py
@@ -7,7 +7,7 @@ import datetime
 import json
 # from scipy import stats
 from flask import request, send_file, Response, Flask
-import requests.request as api_request
+import requests as api_req
 import math
 
 import plotly.graph_objects as go
@@ -1922,7 +1922,7 @@ def create_routes(server):
     @server.app.route('/{}/pull_request_reports/closed-issues-per-week/'.format(server.api_version), methods=["GET"])
     def issues_closed_per_week():
         try:
-            r = api_request(url = 'http://augur.chaoss.io/api/unstable/repos/25205/closed-issues-count', method = 'get')
+            r = api_req.request(url = 'http://augur.chaoss.io/api/unstable/repos/25205/closed-issues-count', method = 'get')
             e = r.json()
             df = pd.DataFrame(e)
 
