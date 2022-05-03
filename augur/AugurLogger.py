29068a17754eb6cb0294fe8a862694945c6f37c9
winningham <54338413+winningham@users.noreply.github.com>
2022-05-03 14:18:53 -0500

    bug fix
    fixes errors

--- augur/AugurLogger.py
+++ augur/AugurLogger.py
@@ -1,16 +1,16 @@
+from logging import Logger 
 import logging
-
 #add additional tags to log messages
-class AugurLogger(logging.Logger):
+class AugurLogger(Logger):
 
-     def info_with_tags (self, additional_tags, message, *args, **kwargs): 
-        Logger.debug(self, message, args, kwargs + {"ExtraTags": additional_tags})
+     def info_with_tags (self, additional_tags, message, *args, **kwargs):
+        self.info(message, *args, **(kwargs | {"extra": {"ExtraTags": additional_tags}}))
 
      def error_with_tags (self, additional_tags, message, *args, **kwargs): 
-        Logger.debug(self, message, args, kwargs + {"ExtraTags": additional_tags})
+        self.error(message, *args, **(kwargs | {"extra": {"ExtraTags": additional_tags}}))
         
      def debug_with_tags (self, additional_tags, message, *args, **kwargs): 
-        Logger.debug(self, message, args, kwargs + {"ExtraTags": additional_tags})
+        self.debug(message, *args, **(kwargs | {"extra": {"ExtraTags": additional_tags}}))
         
 
 
