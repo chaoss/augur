bf322bcfa9dfe1eccfb655adb2c02e859d7fe31b
ijsfyp <isaacschrdr@gmail.com>
2022-05-08 00:21:02 -0500

    fixed import?

--- augur/LoggerFactory.py
+++ augur/LoggerFactory.py
@@ -1,19 +1,19 @@
 import logging
 from collections import namedtuple
-import LogHandlerFactory
 import AugurLogger
+from augur.LogHandlerFactory import LogHandlerFactory, Author
 
 HandlerTags = namedtuple("HandlerTags", "info error debug")
 
 
 class LoggerFactory:
     @staticmethod
-    def create_logger(name: str, general_tags: str, handler_tags: HandlerTags, author: LogHandlerFactory.Author,
+    def create_logger(name: str, general_tags: str, handler_tags: HandlerTags, author: Author,
                       logfile_dir, verbose: bool = False, debug: bool = False) -> AugurLogger:
         # create new AugurLogger
         logger = AugurLogger.AugurLogger(name)
         # instantiate a log handler Factory
-        log_handler_factory = LogHandlerFactory.LogHandlerFactory(general_tags, logfile_dir, author, verbose)
+        log_handler_factory = LogHandlerFactory(general_tags, logfile_dir, author, verbose)
         # attach handlers
         logger.addHandler(log_handler_factory.create_handler(handler_tags.info, logging.INFO))
         logger.addHandler(log_handler_factory.create_handler(handler_tags.error, logging.ERROR))
