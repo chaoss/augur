d1ef706eab3d10e142825e55d17afd2167bbca27
Kevin Zemon <kevin@zemon.name>
2022-05-08 16:05:54 -0500

    Fix using | to add dictionaries together in AugurLogger
    Signed-off-by: Kevin Zemon <kevin@zemon.name>

--- augur/AugurLogger.py
+++ augur/AugurLogger.py
@@ -1,25 +1,24 @@
-from logging import Logger 
-import logging
-#add additional tags to log messages
+from logging import Logger
+
+
+# add additional tags to log messages
 class AugurLogger(Logger):
+    def info_with_tags(self, additional_tags, message, *args, **kwargs):
+        kwargs.update({"extra": {"ExtraTags": additional_tags}})
+        self.info(message, *args, **kwargs)
 
-     def info_with_tags (self, additional_tags, message, *args, **kwargs):
-        self.info(message, *args, **(kwargs | {"extra": {"ExtraTags": additional_tags}}))
+    def error_with_tags(self, additional_tags, message, *args, **kwargs):
+        kwargs.update({"extra": {"ExtraTags": additional_tags}})
+        self.error(message, *args, **kwargs)
 
-     def error_with_tags (self, additional_tags, message, *args, **kwargs): 
-        self.error(message, *args, **(kwargs | {"extra": {"ExtraTags": additional_tags}}))
-        
-     def debug_with_tags (self, additional_tags, message, *args, **kwargs): 
-        self.debug(message, *args, **(kwargs | {"extra": {"ExtraTags": additional_tags}}))
+    def debug_with_tags(self, additional_tags, message, *args, **kwargs):
+        kwargs.update({"extra": {"ExtraTags": additional_tags}})
+        self.debug(message, *args, **kwargs)
 
-     def warning_with_tags (self, additional_tags, message, *args, **kwargs): 
-        self.warning(message, *args, **(kwargs | {"extra": {"ExtraTags": additional_tags}}))
+    def warning_with_tags(self, additional_tags, message, *args, **kwargs):
+        kwargs.update({"extra": {"ExtraTags": additional_tags}})
+        self.warning(message, *args, **kwargs)
 
-     def critical_with_tags (self, additional_tags, message, *args, **kwargs): 
-        self.critical(message, *args, **(kwargs | {"extra": {"ExtraTags": additional_tags}}))
-        
-
-
-
-    
-    
+    def critical_with_tags(self, additional_tags, message, *args, **kwargs):
+        kwargs.update({"extra": {"ExtraTags": additional_tags}})
+        self.critical(message, *args, **kwargs)
