09005ddaa0d0c135fd1b6e1ede173618a1912a50
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-06-03 11:07:39 -0500

    Add class structure for github paginator
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

---
+++ workers/github_paginator.py
@@ -0,0 +1,18 @@
+import collections
+
+
+class GithubPaginator(collections.abc.Sequence):
+    def __init__(self, url, from_datetime=None, to_datetime=None):
+        pass
+
+    def __getitem__(self, index):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __aiter__(self):
+        pass
