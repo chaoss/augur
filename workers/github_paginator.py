63c4fb004078941e7e0ae600e6c5d4d711883eae
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-06-05 14:53:09 -0500

    Make __iter__ more robust
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- workers/github_paginator.py
+++ workers/github_paginator.py
@@ -1,5 +1,7 @@
 import collections
 import httpx
+import time
+import json
 
 from urllib.parse import *
 from urllib.parse import parse_qs
@@ -17,6 +19,7 @@ class GithubPaginator(collections.abc.Sequence):
         self.url = url
         self.from_datetime = from_datetime
         self.to_datetime = to_datetime
+        self.rate_limit = None
 
         self.headers = {'Authorization': f'token {access_token}'}
 
@@ -74,22 +77,23 @@ class GithubPaginator(collections.abc.Sequence):
 
     def __iter__(self):
 
-        r = httpx.get(url=self.url, headers=self.headers)
+        data_list, response = hit_api(self.url, self.headers)
 
-        print(self.headers)
+        if data_list is None:
+            yield None
+            return 
 
         print("Page 1")
-        for data in r.json():
+        for data in data_list:
             yield data
 
-        # add connection timeout checks
 
-        while 'next' in r.links.keys():
-            next_page = get_url_from_header(r, 'next')
+        while 'next' in response.links.keys():
+            next_page = get_url_from_header(response, 'next')
             print(f"Hitting url: {next_page}")
-            r = httpx.get(next_page, headers=self.headers)
+            data_list, response = hit_api(next_page, self.headers)
 
-            for data in r.json():
+            for data in data_list:
                 yield data
 
     def __aiter__(self):
@@ -122,6 +126,90 @@ def add_query_params(url: str, additional_params: dict) -> str:
 def get_url_from_header(request, type):
 
     return request.links[type]['url']
+
+def hit_api(url, headers):
+
+    num_attempts = 0
+    success = False
+    while num_attempts < 10:
+        print(f"Hitting endpoint: {url}...\n")
+
+        try:
+            response = httpx.get(url=url, headers=headers)
+        except TimeoutError:
+            print("Request timed out. Sleeping 10 seconds and trying again...\n")
+            time.sleep(10)
+            continue
+
+        # update rate limit here
+
+        try:
+            page_data = response.json()
+        except:
+           page_data = json.loads(json.dumps(response.text))
+
+        if type(page_data) == list:
+            return page_data, response
+
+        elif type(page_data) == dict:
+            result = process_dict_response(response, page_data)
+
+            if result == "break":
+                break
+            elif result == "decrease_attempts":
+                num_attempts -= 1
+
+        elif type(page_data) == str:
+            result, data_loaded = process_str_response(response, page_data)
+
+            if data_loaded:
+                return result, response
+
+        num_attempts += 1
+
+    return None, None
+
+
+def process_dict_response(response, page_data):
+    
+    print("Request returned a dict: {}\n".format(page_data))
+    if page_data['message'] == "Not Found":
+        print(
+            "Github repo was not found or does not exist for endpoint: "
+            f"{url.format(page_number)}\n"
+        )
+        return "break"
+
+    if "You have exceeded a secondary rate limit. Please wait a few minutes before you try again" in page_data['message']:
+        print('\n\n\n\nSleeping for 100 seconds due to secondary rate limit issue.\n\n\n\n')
+        time.sleep(100)
+
+        return "decrease_attempts"
+
+    if "You have triggered an abuse detection mechanism." in page_data['message']:
+        #self.update_rate_limit(response, temporarily_disable=True,platform=platform)
+
+        return "decrease_attempts"
+
+    if page_data['message'] == "Bad credentials":
+        print("\n\n\n\n\n\n\n POSSIBLY BAD TOKEN \n\n\n\n\n\n\n")
+        #self.update_rate_limit(response, bad_credentials=True, platform=platform)
+        return "bad_credentials"
+
+def process_str_response(response, page_data):
+        print(f"Warning! page_data was string: {page_data}\n")
+        if "<!DOCTYPE html>" in page_data:
+            print("HTML was returned, trying again...\n")
+            return "html_response", False
+        elif len(page_data) == 0:
+            print("Empty string, trying again...\n")
+            return "empty_string", False
+        else:
+            try:
+                page_data = json.loads(page_data)
+                return page_data, True
+            except:
+                pass
 
 
 url = "https://api.github.com/repos/chaoss/augur/issues/events?per_page=50&page=5"
