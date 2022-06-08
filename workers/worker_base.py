428073e9610dfa7c5ee4c432e2a773df6323a7be
Isaac Milarsky <imilarsky@gmail.com>
2022-06-08 11:44:02 -0500

    Easier to test seperate method
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- workers/worker_base.py
+++ workers/worker_base.py
@@ -19,6 +19,23 @@ class TaskSession(sqlalchemy.orm.Session):
         self.logger = logger
         
         self.root_augur_dir = ROOT_AUGUR_DIR
+        self.__init_config(self.root_augur_dir)
+        
+        DB_STR = f'postgresql://{self.config["user_database"]}:{self.config["password_database"]}@{self.config["host_database"]}:{self.config["port_database"]}/{self.config["name_database"]}'
+
+        self.config.update(config)
+        self.platform = platform
+        
+        #print(f"path = {str(ROOT_AUGUR_DIR) + "augur.config.json"}")
+        
+
+        self._engine = create_engine(DB_STR)
+
+        self._oauths = OauthKeyManager(self.config,db_str=DB_STR)
+
+        super.__init__(self._engine)
+
+    def __init_config(self, root_augur_dir):
         #Load config.
         self.augur_config = AugurConfig(self.root_augur_dir,config)
         self.config = {
@@ -36,19 +53,8 @@ class TaskSession(sqlalchemy.orm.Session):
             'key_database' : self.augur_config.get_value('Database', 'key')
         })
 
-        DB_STR = f'postgresql://{self.config["user_database"]}:{self.config["password_database"]}@{self.config["host_database"]}:{self.config["port_database"]}/{self.config["name_database"]}'
+        print(self.config)
 
-        self.config.update(config)
-        self.platform = platform
-        
-        #print(f"path = {str(ROOT_AUGUR_DIR) + "augur.config.json"}")
-        
-
-        self._engine = create_engine(DB_STR)
-
-        self._oauths = OauthKeyManager(self.config,db_str=DB_STR)
-
-        super.__init__(self._engine)
     
     @property
     def access_token(self):
