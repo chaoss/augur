11917c4f4cb4b5562ec65d18f1eb18cbe5b39ff4
Isaac Milarsky <imilarsky@gmail.com>
2022-06-13 16:32:09 -0500

    Progress
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- augur_new/worker_base.py
+++ augur_new/worker_base.py
@@ -38,6 +38,16 @@ class TaskSession(s.orm.Session):
 
         self.engine = s.create_engine(DB_STR)
 
+        #Derek 
+        @s.event.listens_for(self.engine, "connect", insert=True)
+        def set_search_path(dbapi_connection, connection_record):
+            existing_autocommit = dbapi_connection.autocommit
+            dbapi_connection.autocommit = True
+            cursor = dbapi_connection.cursor()
+            cursor.execute("SET SESSION search_path=public,augur_data,augur_operations,spdx")
+            cursor.close()
+            dbapi_connection.autocommit = existing_autocommit
+
         self.__oauths = OauthKeyManager(self.config,self.engine,self.logger)
 
         super().__init__(self.engine)
@@ -128,12 +138,3 @@ class TaskSession(s.orm.Session):
         self.execute(stmnt)
 
 
-#Derek 
-@s.event.listens_for(TaskSession, "connect", insert=True)
-def set_search_path(dbapi_connection, connection_record):
-    existing_autocommit = dbapi_connection.autocommit
-    dbapi_connection.autocommit = True
-    cursor = dbapi_connection.cursor()
-    cursor.execute("SET SESSION search_path=public,augur_data,augur_operations,spdx")
-    cursor.close()
-    dbapi_connection.autocommit = existing_autocommit
