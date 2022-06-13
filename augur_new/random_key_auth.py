de60d35c6ad99e380318b56a260ef942fa7a60bf
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-06-13 13:02:27 -0500

    Fix bugs in key handling
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur_new/random_key_auth.py
+++ augur_new/random_key_auth.py
@@ -10,10 +10,13 @@ class RandomKeyAuth(Auth):
 
         # the choice function is from the random library, and gets a random value from a list
         # this gets a random key from the list
-        key = choice(self.list_of_keys)
+        key_value = choice(self.list_of_keys)
+
+        # formats the key string into a format GitHub will accept
+        key_string = f"token {key_value}"
 
         # set the headers of the request with the new key
-        request.headers['Authorization'] = key
+        request.headers['Authorization'] = key_string
 
         # sends the request back with modified headers
         # basically it saves our changes to the request object
