86a31cf04655740ae1477dbdca9ec129bbbba69e
Sean Goggins <s@goggins.com>
2022-06-14 14:54:25 -0500

    troubleshooting

--- workers/worker_git_integration.py
+++ workers/worker_git_integration.py
@@ -256,51 +256,51 @@ class WorkerGitInterfaceable(Worker):
                 self.headers = {'Authorization': 'Bearer %s' % oauth['access_token']}
 
             number_of_attempts=0
-            #while number_of_attempts < 10: 
-            try: 
-                response = requests.get(url=url, headers=self.headers, timeout=10)
-                self.logger.debug('response obtained.')
-            except TimeoutError: 
-                self.logger.debug(f'Tried to get key headers. Timed out. Napping 10 seconds.')
-                time.sleep(10)
-                number_of_attempts += number_of_attempts+1 
-                continue 
+            while number_of_attempts < 10: 
+                try: 
+                    response = requests.get(url=url, headers=self.headers, timeout=10)
+                    self.logger.debug('response obtained.')
+                except TimeoutError: 
+                    self.logger.debug(f'Tried to get key headers. Timed out. Napping 10 seconds.')
+                    time.sleep(10)
+                    number_of_attempts += number_of_attempts+1 
+                    continue 
 
-            if platform == 'github':
+                if platform == 'github':
 
-                self.logger.debug('checking rate limit status.')
+                    self.logger.debug('checking rate limit status.')
 
-                rate_limit_data = data["resources"]["core"]
+                    rate_limit_data = data["resources"]["core"]
 
-                seconds_to_reset = (
-                    datetime.datetime.fromtimestamp(
-                        int(
-                            rate_limit_data["reset"])
-                    ) - datetime.datetime.now()
-                ).total_seconds()
+                    seconds_to_reset = (
+                        datetime.datetime.fromtimestamp(
+                            int(
+                                rate_limit_data["reset"])
+                        ) - datetime.datetime.now()
+                    ).total_seconds()
 
-                self.oauths.append({
-                        'oauth_id': oauth['oauth_id'],
-                        'access_token': oauth['access_token'],
-                        'rate_limit': int(rate_limit_data["remaining"]),
-                        'seconds_to_reset': seconds_to_reset
-                })
-
-
-            elif platform == 'gitlab'
-
-                self.oauths.append({
-                        'oauth_id': oauth['oauth_id'],
-                        'access_token': oauth['access_token'],
-                        'rate_limit': int(response.headers[rate_limit_header_key]),
-                        'seconds_to_reset': (
-                            datetime.datetime.fromtimestamp(
-                                int(response.headers[rate_limit_reset_header_key])
-                            ) - datetime.datetime.now()
-                        ).total_seconds()
+                    self.oauths.append({
+                            'oauth_id': oauth['oauth_id'],
+                            'access_token': oauth['access_token'],
+                            'rate_limit': int(rate_limit_data["remaining"]),
+                            'seconds_to_reset': seconds_to_reset
                     })
-            self.logger.debug("Found OAuth available for use: {}".format(self.oauths[-1]))
-#            break 
+
+
+                elif platform == 'gitlab'
+
+                    self.oauths.append({
+                            'oauth_id': oauth['oauth_id'],
+                            'access_token': oauth['access_token'],
+                            'rate_limit': int(response.headers[rate_limit_header_key]),
+                            'seconds_to_reset': (
+                                datetime.datetime.fromtimestamp(
+                                    int(response.headers[rate_limit_reset_header_key])
+                                ) - datetime.datetime.now()
+                            ).total_seconds()
+                        })
+                self.logger.debug("Found OAuth available for use: {}".format(self.oauths[-1]))
+                break 
 
         if len(self.oauths) == 0:
             self.logger.info(
