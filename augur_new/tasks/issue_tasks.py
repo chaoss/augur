13795de6a6fc62df106588c3a5fc64bdf68e1b48
Isaac Milarsky <krabs@tilde.team>
2022-06-18 15:30:00 -0500

    issue_tasks.py
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

--- augur_new/tasks/issue_tasks.py
+++ augur_new/tasks/issue_tasks.py
@@ -6,37 +6,6 @@ from augur_new.tasks import facade_tasks
 
 
 @app.task
-def start(owner: str, repo):
-    
-    logger = get_task_logger(start.name)
-    session = TaskSession(logger, config)
-
-    logger.info(f"Collecting data for {owner}/{repo}")
-    logger.info("Prepping to start pr comments, pr events, and pr reviews")
-
-    start_task_list = []
-    start_task_list.append(pull_requests.s(owner, repo))
-    # start_task_list.append(issues.s(owner, repo))
-
-    start_tasks_group = group(start_task_list)
-
-
-    secondary_task_list = []
-    # task_list.append(pull_request_reviews.s(owner, repo))
-    # secondary_task_list.append(github_events.s(owner, repo))
-    # task_list.append(github_messages.s(owner, repo))
-    
-    secondary_task_group = group(secondary_task_list)
-
-    job = chain(
-        start_tasks_group,
-        secondary_task_group,
-    )
-
-    job.apply_async()
-
-
-@app.task
 def pull_requests(owner: str, repo: str) -> None:
 
     logger = get_task_logger(pull_requests.name)
@@ -360,7 +329,7 @@ def pull_request_reviews(owner: str, repo: str, pr_number_list: [int]) -> None:
 
 
 
-start("chaoss", "augur")
+
 # start("grafana", "oncall")
 
 
