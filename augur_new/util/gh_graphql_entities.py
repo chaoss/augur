ef7afe37ab1eb82541da35db79c3f8d2b86d07e7
Isaac Milarsky <imilarsky@gmail.com>
2022-06-21 13:16:08 -0500

    Think about how to implement async
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- augur_new/util/gh_graphql_entities.py
+++ augur_new/util/gh_graphql_entities.py
@@ -2,6 +2,7 @@ from augur_new.worker_base import *
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 from random import choice
+import collections
 
 """
     Should be designed on a per entity basis that has attributes that call 
@@ -14,12 +15,36 @@ from random import choice
 """
 
 
+#Should take the query and two page variables for pagination
+class GraphQlPageCollection(collections.abc.Sequence):
+    def __init__(self,query,client,numPerPage=100):
+        self.per_page = numPerPage
+        self.query = query
+        self.client = client
+
+        self.url = "https://api.github.com/graphql"
+
+    def __getitem__(self, index: int) -> dict:
+        #borrowed from the default github paginator
+        items_page = (index // self.per_page) + 1
+
+        params = {
+            "numRecords" : self.per_page,
+            "cursor"    : None
+        }
+        pass
+
+
 class GitHubRepo():
-    def __init__(self, session: TaskSession):
+    def __init__(self, session: TaskSession, owner: str, repo: str):
 
         self.list_of_keys = get_list_of_oauth_keys(session.engine, session.config["Database"]["key"])
     
         self.url = "https://api.github.com/graphql"
+
+
+        self.owner = owner
+        self.repo = repo
     
     @property
     def headers(self):
@@ -32,7 +57,7 @@ class GitHubRepo():
     def gqlClient(self):
         #Create objects to execute graphql queries on the endpoint
         #Use random key defined earlier.
-        
+
         transport = AIOHTTPTransport(url=self.url,headers=self.headers)
         client = Client(transport=transport)
         return client
