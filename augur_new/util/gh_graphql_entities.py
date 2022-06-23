f78c9a87a7333dc2a4fab3c43fd8326d41c353fa
Isaac Milarsky <imilarsky@gmail.com>
2022-06-23 11:44:39 -0500

    syntax
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- augur_new/util/gh_graphql_entities.py
+++ augur_new/util/gh_graphql_entities.py
@@ -54,7 +54,7 @@ class GraphQlPageCollection(collections.abc.Sequence):
                     json_dict['variables'] = json.dumps(json_dict['variables'],indent=4)
                     #print(json_dict['variables'])
                 
-                print(json_dict)
+                print(json.dumps(json_dict))
                 response = client.post(
                     url=self.url,auth=self.keyAuth,json=json_dict
                     )
