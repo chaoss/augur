64f700bedf9643fbf87d4238b426c2191485110f
Isaac Milarsky <imilarsky@gmail.com>
2022-06-24 14:38:31 -0500

    why is self.cursor_people None?
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- augur_new/facade_worker/facade_worker/facade01config.py
+++ augur_new/facade_worker/facade_worker/facade01config.py
@@ -167,12 +167,15 @@ class Config:
 
 ## TODO: Does this need a block for if the database connection IS multithreaded? I think so, @sgoggins
 
+        self.cursor_people = cursor
+        self.db_people = db
+        """
         if people and not multi_threaded_connection:
             self.cursor_people = cursor
             self.db_people = db
         elif not multi_threaded_connection:
             self.cursor = cursor
-            self.db = db
+            self.db = db"""
 
         # Figure out how much we're going to log
         #self.log_level = self.get_setting('log_level')
