d9cf536bb55f95ecf07c8b1f486f1a0eef96934b
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-06-29 13:29:04 -0500

    Syntax fixes
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur_new/tasks/start_tasks.py
+++ augur_new/tasks/start_tasks.py
@@ -26,7 +26,7 @@ def start_task(owner: str, repo):
 
 
     secondary_task_list = []
-    secondary_task_list.append(pull_request_review_comments.s(owner, repo))
+    secondary_task_list.append(pull_request_reviews.s(owner, repo))
     # secondary_task_list.append(github_events.s(owner, repo))
     # secondary_task_list.append(github_comments.s(owner, repo))
     
