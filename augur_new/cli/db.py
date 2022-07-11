e0da0f17c168fdabcea0b7c1d7fba13d46966e0e
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-07-11 15:59:26 -0500

    Fix db commands to work with new engine
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur_new/cli/db.py
+++ augur_new/cli/db.py
@@ -16,6 +16,7 @@ import requests
 from sqlalchemy import exc
 
 from augur.cli import pass_config, pass_application
+from db.engine import engine
 
 logger = logging.getLogger(__name__)
 
@@ -27,52 +28,53 @@ def cli():
 
 @cli.command("add-repos")
 @click.argument("filename", type=click.Path(exists=True))
-@pass_application
-def add_repos(augur_app, filename):
+def add_repos(filename):
     """
     Add repositories to Augur's database
     """
-    df = augur_app.database.execute(
-        s.sql.text("SELECT repo_group_id FROM augur_data.repo_groups")
-    )
-    repo_group_IDs = [group[0] for group in df.fetchall()]
+    with engine.connect() as connection:
+        df = connection.execute(
+            s.sql.text("SELECT repo_group_id FROM augur_data.repo_groups")
+        )
+        repo_group_IDs = [group[0] for group in df.fetchall()]
 
-    insertSQL = s.sql.text(
+        insertSQL = s.sql.text(
+            """
+            INSERT INTO augur_data.repo(repo_group_id, repo_git, repo_status,
+            tool_source, tool_version, data_source, data_collection_date)
+            VALUES (:repo_group_id, :repo_git, 'New', 'CLI', 1.0, 'Git', CURRENT_TIMESTAMP)
         """
-        INSERT INTO augur_data.repo(repo_group_id, repo_git, repo_status,
-        tool_source, tool_version, data_source, data_collection_date)
-        VALUES (:repo_group_id, :repo_git, 'New', 'CLI', 1.0, 'Git', CURRENT_TIMESTAMP)
-    """
-    )
+        )
 
-    with open(filename) as upload_repos_file:
-        data = csv.reader(upload_repos_file, delimiter=",")
-        for row in data:
-            logger.info(
-                f"Inserting repo with Git URL `{row[1]}` into repo group {row[0]}"
-            )
-            if int(row[0]) in repo_group_IDs:
-                result = augur_app.database.execute(
-                    insertSQL, repo_group_id=int(row[0]), repo_git=row[1]
+        with open(filename) as upload_repos_file:
+            data = csv.reader(upload_repos_file, delimiter=",")
+            for row in data:
+                logger.info(
+                    f"Inserting repo with Git URL `{row[1]}` into repo group {row[0]}"
                 )
-            else:
-                logger.warning(
-                    f"Invalid repo group id specified for {row[1]}, skipping."
-                )
+                if int(row[0]) in repo_group_IDs:
+                    result = connection.execute(
+                        insertSQL, repo_group_id=int(row[0]), repo_git=row[1]
+                    )
+                else:
+                    logger.warning(
+                        f"Invalid repo group id specified for {row[1]}, skipping."
+                    )
 
 
 @cli.command("get-repo-groups")
-@pass_application
-def get_repo_groups(augur_app):
+def get_repo_groups():
     """
     List all repo groups and their associated IDs
     """
-    df = pd.read_sql(
-        s.sql.text(
-            "SELECT repo_group_id, rg_name, rg_description FROM augur_data.repo_groups"
-        ),
-        augur_app.database,
-    )
+
+    with engine.connect() as connection:
+        df = pd.read_sql(
+            s.sql.text(
+                "SELECT repo_group_id, rg_name, rg_description FROM augur_data.repo_groups"
+            ),
+            connection,
+        )
     print(df)
 
     return df
@@ -80,50 +82,50 @@ def get_repo_groups(augur_app):
 
 @cli.command("add-repo-groups")
 @click.argument("filename", type=click.Path(exists=True))
-@pass_application
-def add_repo_groups(augur_app, filename):
+def add_repo_groups(filename):
     """
     Create new repo groups in Augur's database
     """
-    df = pd.read_sql(
-        s.sql.text("SELECT repo_group_id FROM augur_data.repo_groups"),
-        augur_app.database,
-    )
-    repo_group_IDs = df["repo_group_id"].values.tolist()
+    with engine.connect() as connection:
 
-    insert_repo_group_sql = s.sql.text(
+        df = pd.read_sql(
+            s.sql.text("SELECT repo_group_id FROM augur_data.repo_groups"),
+            connection,
+        )
+        repo_group_IDs = df["repo_group_id"].values.tolist()
+
+        insert_repo_group_sql = s.sql.text(
+            """
+        INSERT INTO "augur_data"."repo_groups"("repo_group_id", "rg_name", "rg_description", "rg_website", "rg_recache", "rg_last_modified", "rg_type", "tool_source", "tool_version", "data_source", "data_collection_date") VALUES (:repo_group_id, :repo_group_name, '', '', 0, CURRENT_TIMESTAMP, 'Unknown', 'Loaded by user', '1.0', 'Git', CURRENT_TIMESTAMP);
         """
-    INSERT INTO "augur_data"."repo_groups"("repo_group_id", "rg_name", "rg_description", "rg_website", "rg_recache", "rg_last_modified", "rg_type", "tool_source", "tool_version", "data_source", "data_collection_date") VALUES (:repo_group_id, :repo_group_name, '', '', 0, CURRENT_TIMESTAMP, 'Unknown', 'Loaded by user', '1.0', 'Git', CURRENT_TIMESTAMP);
-    """
-    )
+        )
 
-    with open(filename) as create_repo_groups_file:
-        data = csv.reader(create_repo_groups_file, delimiter=",")
-        for row in data:
+        with open(filename) as create_repo_groups_file:
+            data = csv.reader(create_repo_groups_file, delimiter=",")
+            for row in data:
 
-            # Handle case where there's a hanging empty row.
-            if not row:
-                logger.info("Skipping empty data...")
-                continue
+                # Handle case where there's a hanging empty row.
+                if not row:
+                    logger.info("Skipping empty data...")
+                    continue
 
-            logger.info(f"Inserting repo group with values {row}...")
-            if int(row[0]) not in repo_group_IDs:
-                repo_group_IDs.append(int(row[0]))
-                augur_app.database.execute(
-                    insert_repo_group_sql,
-                    repo_group_id=int(row[0]),
-                    repo_group_name=row[1],
-                )
-            else:
-                logger.info(
-                    f"Repo group with ID {row[1]} for repo group {row[1]} already exists, skipping..."
-                )
+                logger.info(f"Inserting repo group with values {row}...")
+                if int(row[0]) not in repo_group_IDs:
+                    repo_group_IDs.append(int(row[0]))
+                    connection.execute(
+                        insert_repo_group_sql,
+                        repo_group_id=int(row[0]),
+                        repo_group_name=row[1],
+                    )
+                else:
+                    logger.info(
+                        f"Repo group with ID {row[1]} for repo group {row[1]} already exists, skipping..."
+                    )
 
 
 @cli.command("add-github-org")
 @click.argument("organization_name")
-@pass_application
-def add_github_org(augur_app, organization_name):
+def add_github_org(organization_name):
     """
     Create new repo groups in Augur's database
     """
@@ -156,35 +158,41 @@ def add_github_org(augur_app, organization_name):
     INSERT INTO "augur_data"."repo_groups"("rg_name", "rg_description", "rg_website", "rg_recache", "rg_last_modified", "rg_type", "tool_source", "tool_version", "data_source", "data_collection_date") VALUES (:repo_group_name, '', '', 0, CURRENT_TIMESTAMP, 'Unknown', 'Loaded by user', '1.0', 'Git', CURRENT_TIMESTAMP) RETURNING repo_group_id;
     """
     )
-    new_repo_group_id = augur_app.database.execute(
-        insert_repo_group_sql, repo_group_name=organization_name
-    ).fetchone()[0]
 
-    insert_repo_sql = s.sql.text(
+    with engine.connect() as connection:
+
+        new_repo_group_id = connection.execute(
+            insert_repo_group_sql, repo_group_name=organization_name
+        ).fetchone()[0]
+
+        insert_repo_sql = s.sql.text(
+            """
+            INSERT INTO augur_data.repo(repo_group_id, repo_git, repo_status,
+            tool_source, tool_version, data_source, data_collection_date)
+            VALUES (:repo_group_id, :repo_git, 'New', 'CLI', 1.0, 'Git', CURRENT_TIMESTAMP)
         """
-        INSERT INTO augur_data.repo(repo_group_id, repo_git, repo_status,
-        tool_source, tool_version, data_source, data_collection_date)
-        VALUES (:repo_group_id, :repo_git, 'New', 'CLI', 1.0, 'Git', CURRENT_TIMESTAMP)
-    """
-    )
-    logger.info(f"{organization_name} repo group created")
-
-    for repo in all_repos:
-        logger.info(f"Adding {organization_name}/{repo['name']} ({repo['clone_url']})")
-        result = augur_app.database.execute(
-            insert_repo_sql, repo_group_id=new_repo_group_id, repo_git=repo["clone_url"]
         )
+        logger.info(f"{organization_name} repo group created")
+
+        for repo in all_repos:
+            logger.info(f"Adding {organization_name}/{repo['name']} ({repo['clone_url']})")
+            result = connection.execute(
+                insert_repo_sql, repo_group_id=new_repo_group_id, repo_git=repo["clone_url"]
+            )
 
 
 # get_db_version is a helper function to print_db_version and upgrade_db_version
-def get_db_version(augur_app):
+def get_db_version():
+
     db_version_sql = s.sql.text(
         """
         SELECT * FROM augur_operations.augur_settings WHERE setting = 'augur_data_version'
         """
     )
 
-    return int(augur_app.database.execute(db_version_sql).fetchone()[2])
+    with engine.connect() as connection:
+
+        return int(connection.execute(db_version_sql).fetchone()[2])
 
 
 @cli.command("print-db-version")
@@ -242,8 +250,7 @@ def generate_api_key(ctx):
 
 @cli.command("update-api-key")
 @click.argument("api_key")
-@pass_application
-def update_api_key(augur_app, api_key):
+def update_api_key(api_key):
     """
     Update the API key in the database to the given key
     """
@@ -257,13 +264,14 @@ def update_api_key(augur_app, api_key):
     """
     )
 
-    augur_app.database.execute(update_api_key_sql, api_key=api_key)
-    logger.info(f"Updated Augur API key to: {api_key}")
+    with engine.connect() as connection:
+
+        augur_app.database.execute(update_api_key_sql, api_key=api_key)
+        logger.info(f"Updated Augur API key to: {api_key}")
 
 
 @cli.command("get-api-key")
-@pass_application
-def get_api_key(augur_app):
+def get_api_key():
     get_api_key_sql = s.sql.text(
         """
         SELECT value FROM augur_operations.augur_settings WHERE setting='augur_api_key';
@@ -271,7 +279,8 @@ def get_api_key(augur_app):
     )
 
     try:
-        print(augur_app.database.execute(get_api_key_sql).fetchone()[0])
+        with engine.connect() as connection:
+            print(connection.execute(get_api_key_sql).fetchone()[0])
     except TypeError:
         logger.error("No Augur API key found.")
 
