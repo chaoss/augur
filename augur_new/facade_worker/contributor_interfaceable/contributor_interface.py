5525091af712956b76d93f3032dfa78e68eaecb9
Isaac Milarsky <imilarsky@gmail.com>
2022-07-12 14:28:02 -0500

    debug
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- augur_new/facade_worker/contributor_interfaceable/contributor_interface.py
+++ augur_new/facade_worker/contributor_interfaceable/contributor_interface.py
@@ -112,7 +112,7 @@ def insert_alias(session, contributor, email):
 
     # Insert new alias
     
-    session.insert_data(alias, ContributorsAliases, ['alias_email'])
+    session.insert_data(alias, ContributorsAliases, ['cntrb_alias_id'])
     
 
     return
