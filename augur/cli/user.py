2f21f567305bdf56dd6f8524ab587114139a5756
Priya Srivastava <shivikapriya730@gmail.com>
2022-07-13 00:12:40 +0530

    Add user with email
    Add user with unique email and username command
    
    Signed-off-by: Priya Srivastava <shivikapriya730@gmail.com>

--- augur/cli/user.py
+++ augur/cli/user.py
@@ -16,11 +16,10 @@ app = Flask(__name__)
 # SQLAlchemy Settings
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  
-# SQLAlchemy interface                                                               
+# Construct the DB Object (SQLAlchemy interface)                                                                  
 db = SQLAlchemy (app)
 db.app = app 
 db.init_app(app)
-db.create_all(app=app)
 
 #User Class
 class User(db.Model):
@@ -30,6 +29,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     #password_hash = db.Column(db.String(100), nullable=False)
     admin = db.Column(db.Boolean, default=False)
     public_id = db.Column(db.String(36), unique=True, default=lambda: str(uuid4()))
@@ -39,25 +39,36 @@ class User(db.Model):
 
     def __repr__(self):
         return (
-            f"<User username={self.username}, public_id={self.public_id}, admin={self.admin}>"
+            f"<User username={self.username}, email={self.email}, public_id={self.public_id}, admin={self.admin}>"
         )
 
-@click.group('user', short_help='Add user utilities')
+#create user table
+db.create_all()
+
+#user subcommand
+@click.group('user', short_help='Support for adding regular users or administrative users')
 def cli():
     pass
 @cli.command('add', short_help="Add a new user")
 @click.argument("username")
+@click.argument("email")
 @click.option(
     "--admin", is_flag=True, default=False, help="New user has administrator role"
 )
 @click.password_option(help="Set password")
-def add_user(username, admin, password):
-    """Add a new user to the database with username."""
-    new_user = User(username=username, admin=admin)
-    db.session.add(new_user)
-    db.session.commit()
-    user_type = "admin user" if admin else "user"
-    message = f"Successfully added new {user_type}:\n {new_user}"
-    click.secho(message, fg="blue", bold=True)
-    return 0
-'''Next step: password hashing and login using email'''
+def add_user(username, email, admin, password):
+    """Add a new user to the database with email address = EMAIL."""
+    if User.query.filter(User.username == username).first() is not None:
+        return click.echo("username already taken")
+    if User.query.filter(User.email == email).first() is not None:
+        return click.echo("email already signed-up")
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        new_user = User(username=username, email=email, admin=admin)
+        db.session.add(new_user)
+        db.session.commit()
+        user_type = "admin user" if admin else "user"
+        message = f"Successfully added new {user_type}:\n {new_user}"
+        click.secho(message, fg="blue", bold=True)
+        return 0
+'''Next step: password hashing'''
