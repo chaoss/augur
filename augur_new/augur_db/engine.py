1523bb2fb1a6fe93fff457e5c6f4bfa090360b11
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-07-15 14:54:48 -0500

    Add exception when AUGUR_DB env var is not set
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur_new/augur_db/engine.py
+++ augur_new/augur_db/engine.py
@@ -1,14 +1,20 @@
 import os
 from sqlalchemy import create_engine, event
 
-engine = create_engine(os.getenv("AUGUR_DB"))
+augur_db_environment_var = os.getenv("AUGUR_DB")
 
+if augur_db_environment_var:
 
-@event.listens_for(engine, "connect", insert=True)
-def set_search_path(dbapi_connection, connection_record):
-    existing_autocommit = dbapi_connection.autocommit
-    dbapi_connection.autocommit = True
-    cursor = dbapi_connection.cursor()
-    cursor.execute("SET SESSION search_path=public,augur_data,augur_operations,spdx")
-    cursor.close()
-    dbapi_connection.autocommit = existing_autocommit
+    engine = create_engine(augur_db_environment_var)
+
+    @event.listens_for(engine, "connect", insert=True)
+    def set_search_path(dbapi_connection, connection_record):
+        existing_autocommit = dbapi_connection.autocommit
+        dbapi_connection.autocommit = True
+        cursor = dbapi_connection.cursor()
+        cursor.execute("SET SESSION search_path=public,augur_data,augur_operations,spdx")
+        cursor.close()
+        dbapi_connection.autocommit = existing_autocommit
+
+else:
+    raise Exception("AUGUR_DB environment variable is not set, please set with command below\n\t  export AUGUR_DB=postgresql+psycopg2://<user>:<password>@<host>:<port>/<db_name>")
