1e57f9a073ebcf651eb6b92fe0353d6053bf185f
Priya Srivastava <shivikapriya730@gmail.com>
2022-07-16 11:37:48 +0530

    Password hashing added
    Signed-off-by: Priya Srivastava <shivikapriya730@gmail.com>

--- augur/cli/user.py
+++ augur/cli/user.py
@@ -8,7 +8,7 @@ import os
 from flask import Flask
 import click
 from flask_sqlalchemy import SQLAlchemy 
-#from flask_bcrypt import Bcrypt
+from werkzeug.security import generate_password_hash
 from uuid import uuid4
 
 app = Flask(__name__)
@@ -30,17 +30,20 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    #password_hash = db.Column(db.String(100), nullable=False)
+    password = db.Column('password' , db.String(250))
     admin = db.Column(db.Boolean, default=False)
     public_id = db.Column(db.String(36), unique=True, default=lambda: str(uuid4()))
 
-    def __init__(self, **kwargs):
-        super(User, self).__init__(**kwargs)
-
+    def __init__(self , username ,password , email, admin):
+        self.username = username
+        self.set_password(password)
+        self.email = email
+        self.admin = admin
+    
+    def set_password(self , password):
+        self.password = generate_password_hash(password)
     def __repr__(self):
-        return (
-            f"<User username={self.username}, email={self.email}, public_id={self.public_id}, admin={self.admin}>"
-        )
+            return '%r' % (self.username)
 
 #create user table
 db.create_all()
@@ -64,11 +67,11 @@ def add_user(username, email, admin, password):
         return click.echo("email already signed-up")
     user = User.query.filter_by(username=username).first()
     if not user:
-        new_user = User(username=username, email=email, admin=admin)
+        new_user = User(username=username, email=email, password=password, admin=admin)
         db.session.add(new_user)
         db.session.commit()
         user_type = "admin user" if admin else "user"
         message = f"Successfully added new {user_type}:\n {new_user}"
         click.secho(message, fg="blue", bold=True)
         return 0
-'''Next step: password hashing'''
+'''Next step: admin model'''
