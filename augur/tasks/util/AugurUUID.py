7aab25811da78bcd31b6b74160f2b49d23d08830
Ulincsys <28362836a@gmail.com>
2022-07-26 19:16:42 -0500

    Create GithubUUID class

--- augur/tasks/util/AugurUUID.py
+++ augur/tasks/util/AugurUUID.py
@@ -2,14 +2,14 @@ from __future__ import annotations
 import uuid, typing
 
 class AugurUUID:
-    def __init__(self, platform: int=0, owner: int=0, repo: int=0):
+    struct = {
+        "platform": {"start": 0, "size": 1}
+    }
+    def __init__(self, platform: int=0):
         self.bytes = list(int(0).to_bytes(16, "big"))
+
         if platform != 0:
             self.set_platform_id(platform)
-        if owner != 0:
-            self.set_owner_id(owner)
-        if repo != 0:
-            self.set_repo_id(repo)
 
     def is_valid_index(self, index: int)-> bool:
         return index >= 0 and index < len(self.bytes)
@@ -21,7 +21,26 @@ class AugurUUID:
             raise ValueError(f"Too many bytes for start index, available: {len(self.bytes) - start_byte}, required: {len(byte_array)}")
 
         for i in range(start_byte, start_byte + len(byte_array)):
-            self.bytes[i] = byte_array[i - start_byte]
+            self.set_byte(i, byte_array[i - start_byte])
+
+    def write_int(self, source: int, start_byte: int, num_bytes: int):
+        try:
+            source = int(source)
+            source_bytes = source.to_bytes(num_bytes, "big")
+        except ValueError as e:
+            raise ValueError("The source must be an integer") from None
+        except OverflowError as e:
+            raise ValueError(f"The source must be at most {num_bytes} bytes in size: {source} is too big") from None
+
+        self.set_bytes(source_bytes, start_byte)
+
+    def get_int(self, start_byte: int, num_bytes: int):
+        source_bytes = []
+
+        for i in range(start_byte, start_byte + num_bytes):
+            source_bytes.append(self.get_byte(i))
+
+        return int.from_bytes(source_bytes, "big")
 
     def set_platform_id(self, platform_id: int):
         try:
@@ -34,38 +53,19 @@ class AugurUUID:
 
         self.set_bytes(platform_bytes, 0)
 
-    def set_owner_id(self, owner_id: int):
-        try:
-            owner_id = int(owner_id)
-            owner_bytes = owner_id.to_bytes(4, "big")
-        except ValueError as e:
-            raise ValueError("The owner_id must be an integer") from None
-        except OverflowError as e:
-            raise ValueError(f"The owner_id must be at most 4 bytes in size: {owner_id} is too big") from None
-
-        self.set_bytes(owner_bytes, 1)
-
-    def set_repo_id(self, repo_id: int):
-        try:
-            repo_id = int(repo_id)
-            repo_bytes = repo_id.to_bytes(4, "big")
-        except ValueError as e:
-            raise ValueError("The repo_id must be an integer") from None
-        except OverflowError as e:
-            raise ValueError(f"The repo_id must be at most 4 bytes in size: {repo_id} is too big") from None
-
-        self.set_bytes(repo_bytes, 5)
-
     def to_UUID(self)-> uuid.UUID:
         return uuid.UUID(bytes=bytes(self.bytes))
 
     def __int__(self)-> int:
         return int.from_bytes(self.bytes, "big")
 
-    def __getitem__(self, index: int)-> int:
-        return self.bytes[index]
+    def get_byte(self, index: int)-> int:
+        try:
+            return self.bytes[index]
+        except:
+            raise IndexError("UUID index out of range") from None
 
-    def __setitem__(self, index: int, value: int):
+    def set_byte(self, index: int, value: int):
         try:
             value = int(value)
             value_byte = value.to_bytes(1, "big")
@@ -77,6 +77,14 @@ class AugurUUID:
             raise IndexError(f"Index {index} out of bounds")
 
         self.bytes[index] = value_byte[0]
+
+    def __getitem__(self, key: str)-> int:
+        structure = self.struct[key]
+        return self.get_int(structure["start"], structure["size"])
+
+    def __setitem__(self, key: str, value: int):
+        structure = self.struct[key]
+        self.write_int(value, structure["start"], structure["size"])
 
     # Referencing a class type within itself like this requies the annotations import from above
     def __eq__(self, other: AugurUUID)-> bool:
@@ -91,27 +99,65 @@ class AugurUUID:
     def __len__(self)-> int:
         return len(self.bytes)
 
+    def __dict__(self)-> dict:
+        result = {}
+
+        for key in self.struct.keys():
+            structure = self.struct[key]
+            result[key] = self.get_int(structure["start"], structure["size"])
+
+        return result
+
     def __str__(self)-> str:
-        return str(self.to_UUID())
+        return str(self.__dict__())
 
     def __iter__(self):
         return (byte for byte in self.bytes)
 
+class GithubUUID(AugurUUID):
+    struct = {
+        "platform": {"start": 0, "size": 1},
+        "user": {"start": 1, "size": 4},
+        "repo": {"start": 5, "size": 3},
+        "issue": {"start": 8, "size": 4},
+        "event": {"start": 12, "size": 4},
+        "metadata": {"start": 12, "size": 4}
+    }
+
+    def __init__(self):
+        super().__init__(platform = 1)
+
+class UnresolvableUUID(GithubUUID):
+    def __init__(self):
+        super(GithubUUID, self).__init__(platform = 0)
+
 if __name__ == "__main__":
     # Some examples of using this class:
-    id = AugurUUID()
+    id = GithubUUID()
+
+    id["user"] = 15
+    id["repo"] = 65535
+    id["issue"] = 297
 
     print(id)
 
-    id[0] = 17
+    uid = UnresolvableUUID()
 
-    id.set_owner_id(1)
+    print(uid)
 
-    print(int(id))
-
-    # for byte in id:
-    #     print(byte)
-
-    print(id[0])
-
-    print(AugurUUID(17, 1, 1).bytes)
+    # id = AugurUUID()
+    #
+    # print(id)
+    #
+    # id[0] = 17
+    #
+    # id.set_owner_id(1)
+    #
+    # print(int(id))
+    #
+    # # for byte in id:
+    # #     print(byte)
+    #
+    # print(id[0])
+    #
+    # print(AugurUUID(17, 1, 1).bytes)
