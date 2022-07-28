e9abeed328e87a4f7f459681f4d2ef66ad786459
Isaac Milarsky <imilarsky@gmail.com>
2022-07-28 13:30:32 -0500

    it should work?
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- augur/tasks/git/util/facade_worker/contributor_interfaceable/contributor_interface.py
+++ augur/tasks/git/util/facade_worker/contributor_interfaceable/contributor_interface.py
@@ -111,7 +111,7 @@ def insert_alias(session, contributor, email):
 
     # Insert new alias
     
-    session.insert_data(alias, ContributorsAlias, ['alias_email','canonical_email'])
+    session.insert_data(alias, ContributorsAlias, ['canonical_email'])
     
 
     return
