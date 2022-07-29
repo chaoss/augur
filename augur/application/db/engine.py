9855c0000bdd04e71737b357ccc2502809837f45
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-07-28 20:28:54 -0500

    Prioritize env over json config
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur/application/db/engine.py
+++ augur/application/db/engine.py
@@ -11,15 +11,17 @@ if not augur_db_environment_var and not db_json_exists:
     raise Exception("AUGUR_DB environment variable is not set, please set with command below\n\t  export AUGUR_DB=postgresql+psycopg2://<user>:<password>@<host>:<port>/<db_name>")
 
 # if db.json exists it creates the engine from it, if it doesn't exist it creates it from the environment variable
-if db_json_exists:
+if augur_db_environment_var:
+    engine = create_engine(augur_db_environment_var)
+
+else:
     with open("db.json", 'r') as f:
         db_config = json.load(f)
 
         db_conn_string = f"postgresql+psycopg2://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database_name']}"
         engine = create_engine(db_conn_string)
 
-elif augur_db_environment_var:
-    engine = create_engine(augur_db_environment_var)
+
 
 @event.listens_for(engine, "connect", insert=True)
 def set_search_path(dbapi_connection, connection_record):
