f31969b6499c423c92f0b0c3c423008e39cc656b
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-08-04 17:31:27 -0500

    Fix tasks that need to be run
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur/tasks/start_tasks.py
+++ augur/tasks/start_tasks.py
@@ -1,7 +1,6 @@
 from augur.tasks.github.issue_tasks import *
 
 from augur.tasks.init.celery_app import celery_app as celery
-from augur.application.db.session import DatabaseSession
 
 pr_numbers = [70, 106, 170, 190, 192, 208, 213, 215, 216, 218, 223, 224, 226, 230, 237, 238, 240, 241, 248, 249, 250, 252, 253, 254, 255, 256, 257, 261, 268, 270, 273, 277, 281, 283, 288, 291, 303, 306, 309, 310, 311, 323, 324, 325, 334, 335, 338, 343, 346, 348, 350, 353, 355, 356, 357, 359, 360, 365, 369, 375, 381, 382, 388, 405, 408, 409, 410, 414, 418, 419, 420, 421, 422, 424, 425, 431, 433, 438, 445, 450, 454, 455, 456, 457, 460, 463, 468, 469, 470, 474, 475, 476, 477, 478, 479, 480, 481, 482, 484, 485, 486, 487, 488, 490, 491, 492, 493, 494, 495, 496, 497, 498, 499, 500, 501, 502, 504, 506, 507, 508, 509, 510, 512, 514]
 
@@ -12,7 +11,6 @@ def start_task(repo_git: str):
     owner, repo = get_owner_repo(repo_git)
     
     logger = logging.getLogger(start_task.__name__)
-    session = DatabaseSession(logger)
 
     logger.info(f"Collecting data for {owner}/{repo}")
  
@@ -23,7 +21,6 @@ def start_task(repo_git: str):
 
     start_tasks_group = group(start_task_list)
     
-
     secondary_task_list = []
     secondary_task_list.append(pull_request_reviews.s(owner, repo, pr_numbers))
     secondary_task_list.append(collect_events.si(repo_git))
