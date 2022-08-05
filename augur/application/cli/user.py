605db3d1e2740264c2a23854f1d6b2752175178e
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-08-05 13:07:50 -0500

    Change cli/user.py to sqlalchemy syntax
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur/application/cli/user.py
+++ augur/application/cli/user.py
@@ -5,48 +5,14 @@ Add Admin command: augur user add <username> <email> --admin
 '''
 
 import os
-from flask import Flask
 import click
-from flask_sqlalchemy import SQLAlchemy 
+import logging
 from werkzeug.security import generate_password_hash
-from uuid import uuid4
+from sqlalchemy.orm import Session
+from augur.application.db.engine import engine
 
-app = Flask(__name__)
 
-# SQLAlchemy Settings
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3' 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  
-# Construct the DB Object (SQLAlchemy interface)                                                                  
-db = SQLAlchemy (app)
-db.app = app 
-db.init_app(app)
-
-#User Class
-class User(db.Model):
-    """User model for storing login credentials and other details."""
-
-    __tablename__ = "user"
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(255), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column('password' , db.String(250))
-    admin = db.Column(db.Boolean, default=False)
-    public_id = db.Column(db.String(36), unique=True, default=lambda: str(uuid4()))
-
-    def __init__(self , username ,password , email, admin):
-        self.username = username
-        self.set_password(password)
-        self.email = email
-        self.admin = admin
-    
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
-    def __repr__(self):
-            return '%r' % (self.username)
-
-#create user table
-db.create_all()
+logger = logging.getLogger(__name__)
 
 #user subcommand
 @click.group('user', short_help='Support for adding regular users or administrative users')
@@ -55,19 +21,30 @@ def cli():
 @cli.command('add', short_help="Add a new user")
 @click.argument("username")
 @click.argument("email")
+@click.argument("firstname")
+@click.argument("lastname")
+@click.option(
+    "--phone-number", default=None, help="User phone number"
+)
 @click.option(
     "--admin", is_flag=True, default=False, help="New user has administrator role"
 )
 @click.password_option(help="Set password")
-def add_user(username, email, admin, password):
+def add_user(username, email, firstname, lastname, phone_number, admin, password):
     """Add a new user to the database with email address = EMAIL."""
-    if User.query.filter(User.username == username).first() is not None:
+
+    session = Session(engine)
+
+    if session.query(User).filter(User.login_name == username).first() is not None:
         return click.echo("username already taken")
-    if User.query.filter(User.email == email).first() is not None:
+
+    if session.query(User).filter(User.email == email).first() is not None:
         return click.echo("email already signed-up")
-    user = User.query.filter_by(username=username).first()
+
+    user = session.query(User).filter(User.login_name == username).first()
     if not user:
-        new_user = User(username=username, email=email, password=password, admin=admin)
+        password = generate_password_hash(password)
+        new_user = User(login_name=username, login_hashword=password, email=email, text_phone=phone_number, first_name=firstname, last_name=lastname, admin=admin, tool_source="User CLI", tool_version=None, data_source="CLI")
         db.session.add(new_user)
         db.session.commit()
         user_type = "admin user" if admin else "user"
