055b76f26b930d69a012327b14afb025b84545cd
Isaac Milarsky <imilarsky@gmail.com>
2022-08-05 12:40:53 -0500

    slight improvements to AugurTaskRoutine. Also added some tests for it
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- tests/test_tasks/runner.py
+++ tests/test_tasks/runner.py
@@ -17,3 +17,16 @@ time.sleep(2)
 subprocess.Popen(["augur", "backend", "kill"], stdout=FNULL, stderr=subprocess.STDOUT)
 print("Server successfully shutdown.")
 sys.exit(process.returncode)
+
+@pytest.fixture
+def celery_instance():
+    FNULL = open(os.devnull, "w")
+
+
+    start = subprocess.Popen(["celery", "-A", "tasks.celery_init.celery_app worker ", "--loglevel=info", "--concurrency=1"], stdout=FNULL, stderr=subprocess.STDOUT)
+    print("Waiting for the celery to start...")
+    time.sleep(5)
+
+    yield start
+
+    start.terminate()
--- tests/test_workers/test_start_tasks/test_task_routine.py
+++
@@ -1,3 +0,0 @@
-#SPDX-License-Identifier: MIT
-from augur.tasks.init.celery_app import celery_app as celery
-from augur.tasks.github.issue_tasks import *
