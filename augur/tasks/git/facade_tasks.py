16005fba92887c4d909d9278880470c235bea3a0
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-08-10 15:12:32 -0500

    Update query
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur/tasks/git/facade_tasks.py
+++ augur/tasks/git/facade_tasks.py
@@ -699,7 +699,14 @@ def link_commits_to_contributor(contributorQueue):
                         "cntrb_id": cntrb["cntrb_id"]
                     }
 
-                    query = s.sql.text("""UPDATE commits SET cmt_ght_author_id=:cntrb_id WHERE cmt_committer_email=:cntrb_email""")
+                    query = s.sql.text("""
+                            UPDATE commits 
+                            SET cmt_ght_author_id=:cntrb_id
+                            WHERE cmt_committer_email=:cntrb_email
+                            OR cmt_author_raw_email=:cntrb_email
+                            OR cmt_author_email=:cntrb_email
+                            OR cmt_committer_raw_email=:cntrb_email
+                    """)
 
                     engine.execute(query, **data)            
             
