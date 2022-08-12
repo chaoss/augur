6da3411a4a34e38af23820040763c3f40b85848d
Isaac Milarsky <imilarsky@gmail.com>
2022-08-12 12:26:01 -0500

    Make backend docker container work with augur-new
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -6,13 +6,10 @@ if [[ "$AUGUR_DB_SCHEMA_BUILD" == "1" ]]; then
     augur db create-schema
 fi
 
-augur config init \
-    --db_name "${AUGUR_DB_NAME:-augur}" \
-    --db_host "${AUGUR_DB_HOST:-database}" \
-    --db_port "${AUGUR_DB_PORT:-5432}" \
-    --db_user "${AUGUR_DB_DB_USER:-augur}" \
-    --db_password "$AUGUR_DB_PASSWORD" \
-    --github_api_key "$AUGUR_GITHUB_API_KEY"
+target='docker'
+AUGUR_FACADE_REPO_DIRECTORY=/augur/facade/
+
+./scripts/install/config.sh $target
 
 if [[ -f /repo_groups.csv ]]; then
     augur db add-repo-groups /repo_groups.csv
