f3dc912ca1f21bb555963a43a62e7f5eabc1d01d
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-08-15 15:42:44 -0500

    Continuous collection changes
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur/tasks/init/celery_app.py
+++ augur/tasks/init/celery_app.py
@@ -39,6 +39,11 @@ def split_tasks_into_groups(tasks):
     return grouped_tasks
 
 
+@celery_app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+    from augur.tasks.start_tasks import start_task
+    sender.add_periodic_task(3600/3, start_task.s())
+
 
 #Load logging config once at task definition
 @after_setup_logger.connect
@@ -51,4 +56,3 @@ def setup_loggers(*args,**kwargs):
     
     loggingConfig = TaskLogConfig(split_tasks_into_groups(tasks), logLevel=logging.DEBUG)
 
-    
