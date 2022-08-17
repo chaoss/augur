cefad5dbc146acfc894ac1e668dacec2660cc142
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-08-17 12:36:13 -0500

    Lint session.py
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur/application/db/session.py
+++ augur/application/db/session.py
@@ -1,32 +1,21 @@
 import os
-from sqlalchemy.dialects import postgresql as pg_dialects
-import sqlalchemy as s
-import pandas as pd
-import json
-import httpx
-from sqlalchemy.inspection import inspect
-from psycopg2.errors import DeadlockDetected
-from sqlalchemy.exc import OperationalError
-import logging
-
 import re
 import time
 import sys
 import random
+import logging
+import json
+import httpx
+import sqlalchemy as s
+import pandas as pd
 
-sys.path.append("..")
-from sqlalchemy.event import listen
-from sqlalchemy.event import listens_for
+from psycopg2.errors import DeadlockDetected
 
 from augur.tasks.util.random_key_auth import RandomKeyAuth
-# import psycopg2 
 from augur.application.db.engine import create_database_engine
 from augur.application.config import AugurConfig
-
 from augur.application.db.models import Platform
-sys.path.pop()
-#TODO: setup github headers in a method here.
-#Encapsulate data for celery task worker api
+
 
 class DatabaseSession(s.orm.Session):
 
@@ -55,38 +44,40 @@ class DatabaseSession(s.orm.Session):
             return connection.execute(sql_text)
 
 
-    def insert_data(self, data: [dict], table, natural_keys: [str], return_columns: [str] = []) -> None:
+    def insert_data(self, data: [dict], table, natural_keys: [str], return_columns: [str] = None) -> None:
         
-        if type(data) != list:
+        if isinstance(data, list) is False:
             
             # if a dict is passed to data then 
             # convert it to a list with one value
-            if type(data) == dict:
+            if isinstance(data, dict) is True:
                 data = [data]
             
             else:
                 self.logger.info("Data must be a list or a dict")
-                return
+                return None
 
         if len(data) == 0:
             # self.logger.info("Gave no data to insert, returning...")
-            return
+            return None
 
-        if type(data[0]) != dict:
+        if isinstance(data[0], dict) is False: 
             self.logger.info("Must be list of dicts")
-            return
+            return None
 
         # creates list of arguments to tell sqlalchemy what columns to return after the data is inserted
-        returning_args = []
-        for column in return_columns:
-            argument = getattr(table, column)
-            returning_args.append(argument)
+
+        if return_columns:
+            returning_args = []
+            for column in return_columns:
+                argument = getattr(table, column)
+                returning_args.append(argument)
 
         # creates insert on table
         # that returns cols specificed in returning_args
         # and inserts the data specified in data
         # NOTE: if return_columns does not have an values this still works
-        stmnt = pg_dialects.insert(table).returning(*returning_args).values(data)
+        stmnt = s.dialects.postgresql.insert(table).returning(*returning_args).values(data)
 
         # create a dict that the on_conflict_do_update method requires to be able to map updates whenever there is a conflict. See sqlalchemy docs for more explanation and examples: https://docs.sqlalchemy.org/en/14/dialects/postgresql.html#updating-using-the-excluded-insert-values
         setDict = {}
@@ -102,12 +93,14 @@ class DatabaseSession(s.orm.Session):
         )
 
 
-        # print(str(stmnt.compile(dialect=pg_dialects.dialect())))
+        # print(str(stmnt.compile(dialect=s.dialects.postgresql.dialect())))
         attempts = 0
-        sleep_time_list = [x for x in range(1,11)]
+        # creates list from 1 to 10
+        sleep_time_list = list(range(1,11))
         deadlock_detected = False
-        # if there is no data to return then it executes the insert the returns nothing
-        if len(return_columns) == 0:
+
+        # if there is no data to return then it executes the insert then returns nothing
+        if not return_columns:
 
             while attempts < 10:
                 print("")
@@ -115,7 +108,7 @@ class DatabaseSession(s.orm.Session):
                     with self.engine.connect() as connection:
                         connection.execute(stmnt)
                         break
-                except OperationalError as e:
+                except s.exc.OperationalError as e:
                     # print(str(e).split("Process")[1].split(";")[0])
                     if isinstance(e.orig, DeadlockDetected):
                         deadlock_detected = True
@@ -125,45 +118,45 @@ class DatabaseSession(s.orm.Session):
 
                         attempts += 1
                         continue
-                    else:
-                        raise OperationalError(f"An OperationalError other than DeadlockDetected occurred: {e}") 
+                    
+                    raise e
 
             else:
-                self.logger.error(f"Unable to insert data in 10 attempts")
+                self.logger.error("Unable to insert data in 10 attempts")
                 return
 
-            if deadlock_detected == True:
-                self.logger.error(f"Made it through even though Deadlock was detected")
+            if deadlock_detected is True:
+                self.logger.error("Made it through even though Deadlock was detected")
                     
-            return
+            return None
         
-        # else it get the requested return columns and returns them as a list of dicts
+
+        # othewise it gets the requested return columns and returns them as a list of dicts
+        while attempts < 10:
+            try:
+                with self.engine.connect() as connection:
+                    return_data_tuples = connection.execute(stmnt).fetchall()
+                    break
+            except s.exc.OperationalError as e:
+                if isinstance(e.orig, DeadlockDetected):
+                    sleep_time = random.choice(sleep_time_list)
+                    self.logger.debug(f"Deadlock detected on {table.__table__} table...trying again in {round(sleep_time)} seconds: transaction size: {len(data)}")
+                    time.sleep(sleep_time)
+
+                    attempts += 1
+                    continue   
+
+                raise e     
+
         else:
-            while attempts < 10:
-                try:
-                    with self.engine.connect() as connection:
-                        return_data_tuples = connection.execute(stmnt).fetchall()
-                        break
-                except OperationalError as e:
-                    if isinstance(e.orig, DeadlockDetected):
-                        sleep_time = random.choice(sleep_time_list)
-                        self.logger.debug(f"Deadlock detected on {table.__table__} table...trying again in {round(sleep_time)} seconds: transaction size: {len(data)}")
-                        time.sleep(sleep_time)
+            self.logger.error("Unable to insert and return data in 10 attempts")
+            return None
 
-                        attempts += 1
-                        continue     
-                    else:
-                        raise OperationalError(f"An OperationalError other than DeadlockDetected occurred: {e}")          
+        if deadlock_detected is True:
+            self.logger.error("Made it through even though Deadlock was detected")
 
-            else:
-                self.logger.error(f"Unable to insert and return data in 10 attempts")
-                return []
+        return_data = []
+        for data_tuple in return_data_tuples:
+            return_data.append(dict(data_tuple))
 
-            if deadlock_detected == True:
-                self.logger.error(f"Made it through even though Deadlock was detected")
-
-            return_data = []
-            for data in return_data_tuples:
-                return_data.append(dict(data))
-
-            return return_data
+        return return_data
