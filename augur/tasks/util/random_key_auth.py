17d7398cf8dd1da1b8a86966f12b77c10e72c521
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-08-17 12:01:53 -0500

    Document random key auth classes
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur/tasks/util/random_key_auth.py
+++ augur/tasks/util/random_key_auth.py
@@ -1,15 +1,24 @@
+"""This module defines the RandomKeyAuth class"""
+
 from httpx import Auth
 from random import choice
 
 
 class RandomKeyAuth(Auth):
+    """Custom Auth class for httpx that randomly assigns an api key to each request
+
+    Attributes:
+        list_of_keys ([str]): list of keys which are randomly selected from on each request
+        header_name (str): name of header that the keys need to be set to 
+        key_format (str): format string that defines the structure of the key and leaves a {} for the key to be inserted
+    """
     
     # pass a list of keys that are strings
     # pass the name of the header that you would like to be set on the request
     # Optionally pass the key_format. This is a string that contains a {} so the key can be added and applied to the header in the correct way.
     # For example on github the keys are formatted like "token asdfasfdasf" where asdfasfdasf is the key. So for github 
     # the key_format="token {0}"
-    def __init__(self, list_of_keys: [str], header_name, key_format=None):
+    def __init__(self, list_of_keys: [str], header_name: str, key_format: str=None):
         self.list_of_keys = list_of_keys
         self.header_name = header_name
         self.key_format = key_format
