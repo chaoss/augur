1b41aaedf1eeaa15fe658012d89988c00d118e86
Isaac Milarsky <imilarsky@gmail.com>
2022-09-09 10:15:21 -0500

    make detect_repo_move not take years by putting it in a celery group
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- augur/tasks/github/detect_move/tasks.py
+++ augur/tasks/github/detect_move/tasks.py
@@ -4,11 +4,9 @@ from augur.tasks.init.celery_app import celery_app as celery
 
 
 @celery.task
-def detect_github_repo_move():
+def detect_github_repo_move(repo_git: str) -> None:
     logger = logging.getLogger(detect_github_repo_move.__name__)
 
     with GithubTaskSession(logger) as session:
-        repos = session.query(Repo).all()
-
-        for repo in repos:
-            ping_github_for_repo_move(session, repo)
+        repo = session.query(Repo).filter(Repo.repo_git == repo_git).one()
+        ping_github_for_repo_move(session, repo)
