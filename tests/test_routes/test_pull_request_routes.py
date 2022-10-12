0898ea3f0d1253b1cfe4f0bd769c1833de528b60
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-10-12 12:47:24 -0500

    Remove metrics from test def
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- tests/test_routes/test_pull_request_routes.py
+++ tests/test_routes/test_pull_request_routes.py
@@ -2,21 +2,21 @@
 import requests
 import pytest
 
-def test_pull_requests_merge_contributor_new_by_group(metrics):
+def test_pull_requests_merge_contributor_new_by_group():
     response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/pull-requests-merge-contributor-new')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
     assert data[0]["count"] > 0
 
-def test_pull_requests_merge_contributor_new_by_repo(metrics):
+def test_pull_requests_merge_contributor_new_by_repo():
     response = requests.get('http://localhost:5000/api/unstable/repo-groups/10/repos/25430/pull-requests-merge-contributor-new')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
     assert data[0]["count"] > 0
 
-def test_pull_requests_closed_no_merge(metrics):
+def test_pull_requests_closed_no_merge():
     response = requests.get('http://localhost:5000/api/unstable/repos/25430/pull-requests-closed-no-merge')
     data = response.json()
     assert response.status_code == 200
