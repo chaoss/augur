761d25bd01417d229cd255fb2df17d1de65f0f38
Ulincsys <ulincsys@gmail.com>
2022-10-12 00:37:55 -0500

    Fix another typo: missing space before 'import' keyword

--- augur/application/cli/db.py
+++ augur/application/cli/db.py
@@ -137,7 +137,7 @@ def add_github_org(organization_name):
     Create new repo groups in Augur's database
     """
     from augur.tasks.github.util.github_task_session import GithubTaskSession
-    from augur.util.repo_load_controllerimport RepoLoadController
+    from augur.util.repo_load_controller import RepoLoadController
 
     with GithubTaskSession(logger) as session:
 
