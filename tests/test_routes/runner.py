75bf9e9e7e9d334d567dbc8032122397d94aeb0e
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2022-10-12 12:49:57 -0500

    Give more time to start augur
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- tests/test_routes/runner.py
+++ tests/test_routes/runner.py
@@ -9,10 +9,10 @@ FNULL = open(os.devnull, "w")
 
 start = subprocess.Popen(["augur", "backend", "start", "--disable-collection"])
 print("Waiting for the server to start...")
-time.sleep(5)
+time.sleep(12)
 
 process = subprocess.run(["pytest", "tests/test_routes/"])
-time.sleep(2)
+time.sleep(5)
 
 subprocess.Popen(["augur", "backend", "kill"])
 print("Server successfully shutdown.")
