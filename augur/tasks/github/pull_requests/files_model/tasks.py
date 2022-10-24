011a6d5604a3d8b041b93d16997e045c93813d3d
Isaac Milarsky <imilarsky@gmail.com>
2022-10-24 15:45:12 -0500

    Better logging
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- augur/tasks/github/pull_requests/files_model/tasks.py
+++ augur/tasks/github/pull_requests/files_model/tasks.py
@@ -1,4 +1,5 @@
 import logging
+import traceback
 from augur.tasks.github.util.github_task_session import GithubTaskSession
 from augur.tasks.github.pull_requests.files_model.core import *
 from augur.tasks.init.celery_app import celery_app as celery
@@ -9,4 +10,8 @@ def process_pull_request_files(repo_git: str) -> None:
 
     with GithubTaskSession(logger) as session:
         repo = session.query(Repo).filter(Repo.repo_git == repo_git).one()
-        pull_request_files_model(repo.repo_id, logger)
+        try:
+            pull_request_files_model(repo.repo_id, logger)
+        except Exception as e:
+            logger.error(f"Could not complete pull_request_files_model!\n Reason: {e} \n Traceback: {''.join(traceback.format_exception(None, e, e.__traceback__))}")
+            raise e
