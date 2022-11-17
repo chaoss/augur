49bfaebc3f9cfc3ac56fc08a3af6a521ae08957f
jafk5w <jafk5w@mail.missouri.edu>
2022-11-17 15:03:23 -0600

    Add test stubs for new metrics and routes

---
+++ tests/test_routes/test_iota_routes.py
@@ -0,0 +1,30 @@
+#SPDX-License-Identifier: MIT
+import requests
+import pytest
+
+def test_merged_closed_pull_request_ratio(metrics):
+	# unimplemented
+    #response = requests.get('route-url')
+    #data = response.json()
+    #assert response.status_code == 200
+    #assert len(data) >= 1
+    #assert data[0]["net"] >= 0
+    assert True
+
+def test_net_contributions(metrics):
+	# unimplemented
+    #response = requests.get('route-url')
+    #data = response.json()
+    #assert response.status_code == 200
+    #assert len(data) >= 1
+    #assert data[0]["net"] > 0
+    assert True
+
+def test_issue_pull_request_messages_by_contributor(metrics):
+	# unimplemented
+    #response = requests.get('route-url')
+    #data = response.json()
+    #assert response.status_code == 200
+    #assert len(data) >= 1
+    #assert data[0]["net"] > 0
+    assert True
