3a8246624bc9679b82b8aea98adbc2392d24387c
D <nnalienx@gmail.com>
2022-11-17 21:26:36 -0600

    Update

--- sprint_2_queries.sql
+++ sprint_2_queries.sql
@@ -16,10 +16,11 @@ INNER JOIN (SELECT pr_augur_contributor_id
 		   
 		   
 -- repo_id field will match repo id passed in from other metric 		   
--- returns the name and number of pull requests associated with each company that has contributed to this repo
-select cntrb_company as organization_name, count(pr_merged_at) as number_of_contributions
+-- returns the number of pull requests, date of first pull request, and date of latest pull request associated with each company
+select contributors.cntrb_company as organization_name, count(pr_merged_at) as number_of_contributions, 
+min(pr_merged_at) as first_contribution, max(pr_merged_at) as last_contribution
 from augur_data.pull_requests
 inner join augur_data.contributors on pull_requests.pr_augur_contributor_id = contributors.cntrb_id
-where cntrb_company is not null and pr_merged_at is not null and repo_id = '26285'
+where cntrb_company is not null and pr_merged_at is not null
+and repo_id = '26285'
 group by cntrb_company;
-
