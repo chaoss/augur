9b488f8cf92140a2b1998ce97e5d7e42d2c9e1e3
D <nnalienx@gmail.com>
2022-11-17 20:51:16 -0600

    count pull requests by each org

--- sprint_2_queries.sql
+++ sprint_2_queries.sql
@@ -15,3 +15,11 @@ INNER JOIN (SELECT pr_augur_contributor_id
 		   ON contributors.cntrb_id=pr_cntrb_id.pr_augur_contributor_id
 		   
 		   
+-- repo_id field will match repo id passed in from other metric 		   
+-- returns the name and number of pull requests associated with each company that has contributed to this repo
+select cntrb_company as organization_name, count(pr_merged_at) as number_of_contributions
+from augur_data.pull_requests
+inner join augur_data.contributors on pull_requests.pr_augur_contributor_id = contributors.cntrb_id
+where cntrb_company is not null and pr_merged_at is not null and repo_id = '26285'
+group by cntrb_company;
+
