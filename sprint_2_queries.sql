4253ac5e439ad1ef3e98fa65b730957226fbde96
NathanaelHagemeyer <60619235+NathanaelHagemeyer@users.noreply.github.com>
2022-11-17 20:46:05 -0600

    Update sprint_2_queries.sql
    Added initial Organization vs. Volunteer Driven query. Returns count of all pull requests where the associated contributor has a listed company

--- sprint_2_queries.sql
+++ sprint_2_queries.sql
@@ -3,3 +3,15 @@ SELECT COUNT(*)
 FILTER (WHERE pr_src_author_association = 'MEMBER'
 	   AND repo_id = '26285') as maintainer_count
 FROM augur_data.pull_requests
+
+
+-- repo_id field will match repo id passed in from other metric
+-- returns count of contributions made by a user with a company affiliation
+SELECT COUNT(cntrb_company) AS organization_cntrb
+FROM augur_data.contributors
+INNER JOIN (SELECT pr_augur_contributor_id
+		   FROM augur_data.pull_requests 
+		   WHERE repo_id = 26285) AS pr_cntrb_id
+		   ON contributors.cntrb_id=pr_cntrb_id.pr_augur_contributor_id
+		   
+		   
