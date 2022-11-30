5dbc27baf44f1d22cc3ee2170657a8bb1b7c1119
Isaac Wengler <iwengler3@gmail.com>
2022-11-30 15:14:00 -0600

    feat: add SQL for complexity/project_lines endpoint

--- tests/test_routes/test_complexity_routes.py
+++ tests/test_routes/test_complexity_routes.py
@@ -21,7 +21,7 @@ def test_complexity_lines(metrics):
     assert len(data) >= 1
 
 def test_complexity_comments(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/complexity/project_comments_lines')
+    response = requests.get('http://localhost:5000/api/unstable/complexity/project_comment_lines')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
