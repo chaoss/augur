a05d8e2afa06e0241dda36f5fddcb706dd05e124
jafk5w <112605144+jafk5w@users.noreply.github.com>
2022-12-06 14:57:57 -0600

    Update release.py

--- augur/api/metrics/release.py
+++ augur/api/metrics/release.py
@@ -153,5 +153,54 @@ def tag_only_releases(repo_group_id, repo_id=None, period='day', begin_date=None
                                       'begin_date': begin_date, 'end_date': end_date})
         return results
 
+@register_metric()
+def release_frequency(repo_group_id, repo_id=None, period='day', begin_date=None, end_date=None):
+    """ Returns a timeseries of releases occurring within a specific period
+
+    :param repo_group_id: The repository's repo_group_id
+    :param repo_id: The repository's repo_id, defaults to None
+    :param period: To set the periodicity to 'day', 'week', 'month' or 'year', defaults to 'day'
+    :param begin_date: Specifies the begin date, defaults to '1970-1-1 00:00:00'
+    :param end_date: Specifies the end date, defaults to datetime.now()
+    :return: DataFrame of new releases/period
+    """
+    if not begin_date:
+        begin_date = '1970-1-1'
+    if not end_date:
+        end_date = datetime.datetime.now().strftime('%Y-%m-%d')
+
+    if not repo_id:
+        releases_SQL = s.sql.text("""
+            SELECT
+    			COUNT(release_published_at) AS releases_over_time
+    		FROM
+    			releases
+    		WHERE release_published_at BETWEEN :start_date AND :end_date
+    		AND repo_id IN (SELECT repo_id FROM repo WHERE repo_group_id=:repo_group_id )
+    		GROUP BY repo_id;
+        """)
+
+        results = pd.read_sql(releases_SQL, engine,
+                              params={'period': period, 'repo_group_id': repo_group_id,
+                                      'begin_date': begin_date, 'end_date': end_date })
+        return results
+
+    else:
+        releases_SQL = s.sql.text("""
+            SELECT
+    			COUNT(release_published_at) AS releases_over_time
+    		FROM
+    			releases
+    		WHERE release_published_at BETWEEN :start_date AND :end_date
+    		AND repo_id = :repo_id
+    		GROUP BY repo_id;
+
+        """)
+
+        results = pd.read_sql(releases_SQL, engine,
+                              params={'period': period, 'repo_id': repo_id,
+                                      'begin_date': begin_date, 'end_date': end_date})
+        return results
+
 def create_release_metrics(metrics):
     add_metrics(metrics, __name__)
