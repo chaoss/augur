82b976cfe04220a70f1fda8ca3b6cfdbbb24ea31
Sean P. Goggins <s@goggins.com>
2022-12-06 16:58:40 -0600

    API Fix

--- augur/api/metrics/contributor.py
+++ augur/api/metrics/contributor.py
@@ -130,7 +130,7 @@ def contributors(repo_group_id, repo_id=None, period='day', begin_date=None, end
                                                                 'begin_date': begin_date, 'end_date': end_date})
     else:
         contributorsSQL = s.sql.text("""
-           SELECT uuid.int(id)                           AS user_id,
+           SELECT id::text                          AS user_id,
                 SUM(commits)                 AS commits,
                 SUM(issues)                  AS issues,
                 SUM(commit_comments)         AS commit_comments,
