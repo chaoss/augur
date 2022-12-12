4eca9b2ffa521dea3b4cbb773a23df06b2488478
Isaac Wengler <iwengler3@gmail.com>
2022-12-12 17:06:52 -0600

    feat: project languages sql

--- augur/api/routes/complexity.py
+++ augur/api/routes/complexity.py
@@ -13,7 +13,44 @@ def create_routes(server):
 
     @server.app.route('/{}/complexity/project_languages'.format(AUGUR_API_VERSION), methods=["GET"])
     def get_project_languages():
-        return Response(response="[{'repo_name': 'augur', languages: { 'python': 100 }}]",
+        project_languages_sql = s.sql.text("""
+            SELECT
+                    e.repo_id,
+                    augur_data.repo.repo_git,
+                    augur_data.repo.repo_name,
+                    e.programming_language,
+                    e.code_lines,
+                    e.files
+                FROM
+                    augur_data.repo,
+                (SELECT 
+                    d.repo_id,
+                    d.programming_language,
+                    SUM(d.code_lines) AS code_lines,
+                    COUNT(*)::int AS files
+                FROM
+                    (SELECT
+                            augur_data.repo_labor.repo_id,
+                            augur_data.repo_labor.programming_language,
+                            augur_data.repo_labor.code_lines
+                        FROM
+                            augur_data.repo_labor,
+                            ( SELECT 
+                                    augur_data.repo_labor.repo_id,
+                                    MAX ( data_collection_date ) AS last_collected
+                                FROM 
+                                    augur_data.repo_labor
+                                GROUP BY augur_data.repo_labor.repo_id) recent 
+                        WHERE
+                            augur_data.repo_labor.repo_id = recent.repo_id
+                            AND augur_data.repo_labor.data_collection_date > recent.last_collected - (5 * interval '1 minute')) d
+                GROUP BY d.repo_id, d.programming_language) e
+                WHERE augur_data.repo.repo_id = e.repo_id
+                ORDER BY e.repo_id
+        """)
+        results = pd.read_sql(project_languages_sql,  server.engine)
+        data = results.to_json(orient="records", date_format='iso', date_unit='ms')
+        return Response(response=data,
                     status=200,
                     mimetype="application/json")
 
