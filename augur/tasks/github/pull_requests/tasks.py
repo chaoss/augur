f4b9f8c3cafa76ebf75a227089e3ca6ecfee2f0c
Isaac Milarsky <krabs@tilde.team>
2023-01-25 12:44:54 -0600

    facade changes and skeleton of scaling changes
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

--- augur/tasks/github/pull_requests/tasks.py
+++ augur/tasks/github/pull_requests/tasks.py
@@ -18,27 +18,27 @@ platform_id = 1
 
 
 @celery.task
-def collect_pull_requests(repo_git_identifiers: [str]) -> None:
+def collect_pull_requests(repo_git: str) -> None:
 
     logger = logging.getLogger(collect_pull_requests.__name__)
 
     with DatabaseSession(logger, engine) as session:
 
-        for repo_git in repo_git_identifiers:
-            try:
+    
+        try:
 
-                repo_id = session.query(Repo).filter(
-                Repo.repo_git == repo_git).one().repo_id
+            repo_id = session.query(Repo).filter(
+            Repo.repo_git == repo_git).one().repo_id
 
-                owner, repo = get_owner_repo(repo_git)
-                pr_data = retrieve_all_pr_data(repo_git, logger)
+            owner, repo = get_owner_repo(repo_git)
+            pr_data = retrieve_all_pr_data(repo_git, logger)
 
-                if pr_data:
-                    process_pull_requests(pr_data, f"{owner}/{repo}: Pr task", repo_id, logger)
-                else:
-                    logger.info(f"{owner}/{repo} has no pull requests")
-            except Exception as e:
-                logger.error(f"Could not collect pull requests for {repo_git}\n Reason: {e} \n Traceback: {''.join(traceback.format_exception(None, e, e.__traceback__))}")
+            if pr_data:
+                process_pull_requests(pr_data, f"{owner}/{repo}: Pr task", repo_id, logger)
+            else:
+                logger.info(f"{owner}/{repo} has no pull requests")
+        except Exception as e:
+            logger.error(f"Could not collect pull requests for {repo_git}\n Reason: {e} \n Traceback: {''.join(traceback.format_exception(None, e, e.__traceback__))}")
         
     
 # TODO: Rename pull_request_reviewers table to pull_request_requested_reviewers
