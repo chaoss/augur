3df640054428fa59fe97808aee69a952204aceb6
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2023-02-03 21:05:26 -0600

    Update frontend with flask changes
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur/api/view/augur_view.py
+++ augur/api/view/augur_view.py
@@ -5,8 +5,8 @@ from .url_converters import *
 from .init import logger
 
 # from .server import User
+from ..server import app, session as db_session
 from augur.application.db.models import User, UserSessionToken
-from augur.application.db.session import DatabaseSession
 from augur.api.routes import AUGUR_API_VERSION
 from augur.api.util import get_bearer_token
 
@@ -14,100 +14,93 @@ import time
 
 login_manager = LoginManager()
 
-def create_routes(server):
+login_manager.init_app(app)
 
-    login_manager.init_app(server.app)
+app.secret_key = getSetting("session_key")
 
-    server.app.secret_key = getSetting("session_key")
+app.url_map.converters['list'] = ListConverter
+app.url_map.converters['bool'] = BoolConverter
+app.url_map.converters['json'] = JSONConverter
 
-    server.app.url_map.converters['list'] = ListConverter
-    server.app.url_map.converters['bool'] = BoolConverter
-    server.app.url_map.converters['json'] = JSONConverter
+# Code 404 response page, for pages not found
+@app.errorhandler(404)
+def page_not_found(error):
+    if AUGUR_API_VERSION in str(request.url_rule):
+        return jsonify({"status": "Not Found"}), 404
 
-    # Code 404 response page, for pages not found
-    @server.app.errorhandler(404)
-    def page_not_found(error):
-        if AUGUR_API_VERSION in str(request.url_rule):
-            return jsonify({"status": "Not Found"}), 404
+    return render_template('index.j2', title='404', api_url=getSetting('serving')), 404
 
-        return render_template('index.j2', title='404', api_url=getSetting('serving')), 404
+@app.errorhandler(405)
+def unsupported_method(error):
 
-    @server.app.errorhandler(405)
-    def unsupported_method(error):
+    if AUGUR_API_VERSION in str(request.url_rule):
+        return jsonify({"status": "Unsupported method"}), 405
+    
+    return render_message("405 - Method not supported", "The resource you are trying to access does not support the request method used"), 405
 
-        if AUGUR_API_VERSION in str(request.url_rule):
-            return jsonify({"status": "Unsupported method"}), 405
-        
-        return render_message("405 - Method not supported", "The resource you are trying to access does not support the request method used"), 405
+@login_manager.unauthorized_handler
+def unauthorized():
 
-    @login_manager.unauthorized_handler
-    def unauthorized():
+    if AUGUR_API_VERSION in str(request.url_rule):
 
-        if AUGUR_API_VERSION in str(request.url_rule):
+        token_str = get_bearer_token()
+        token = db_session.query(UserSessionToken).filter(UserSessionToken.token == token_str).first()
+        if not token:
+            return jsonify({"status": "Session expired"})
 
-            with DatabaseSession(logger) as db_session:
+        return jsonify({"status": "Login required"})
 
-                token_str = get_bearer_token()
-                token = db_session.query(UserSessionToken).filter(UserSessionToken.token == token_str).first()
-                if not token:
-                    return jsonify({"status": "Session expired"})
+    session["login_next"] = url_for(request.endpoint, **request.args)
+    return redirect(url_for('user_login'))
 
-            return jsonify({"status": "Login required"})
+@login_manager.user_loader
+def load_user(user_id):
 
-        session["login_next"] = url_for(request.endpoint, **request.args)
-        return redirect(url_for('user_login'))
-
-    @login_manager.user_loader
-    def load_user(user_id):
-
-        db_session = DatabaseSession(logger)
-
-        user = User.get_user(db_session, user_id)
-        if not user:
-            return None
-
-        groups = user.groups
-        tokens = user.tokens
-        applications = user.applications
-        for application in applications:
-            sessions = application.sessions
-        for group in groups:
-            repos = group.repos
-        for token in tokens:
-            application = token.application
-        db_session.expunge(user)
-
-
-        # The flask_login library sets a unique session["_id"]
-        # when login_user() is called successfully
-        if session.get("_id") is not None:
-        
-            user._is_authenticated = True
-            user._is_active = True
-
-        return user
-
-    @login_manager.request_loader
-    def load_user_request(request):
-
-        print(f"Current time of user request: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}")
-        token = get_bearer_token()
-        session = DatabaseSession(logger)
-
-        current_time = int(time.time())
-        token = session.query(UserSessionToken).filter(UserSessionToken.token == token, UserSessionToken.expiration >= current_time).first()
-        if token:
-
-            print("Valid user")
-
-            user = token.user
-            user._is_authenticated = True
-            user._is_active = True
-
-            return user
-            
+    user = User.get_user(db_session, user_id)
+    if not user:
         return None
 
-    @server.app.template_filter('as_datetime')
-    def as_datetime(seconds):
-        time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(seconds))
+    groups = user.groups
+    tokens = user.tokens
+    applications = user.applications
+    for application in applications:
+        sessions = application.sessions
+    for group in groups:
+        repos = group.repos
+    for token in tokens:
+        application = token.application
+    db_session.expunge(user)
+
+
+    # The flask_login library sets a unique session["_id"]
+    # when login_user() is called successfully
+    if session.get("_id") is not None:
+    
+        user._is_authenticated = True
+        user._is_active = True
+
+    return user
+
+@login_manager.request_loader
+def load_user_request(request):
+
+    print(f"Current time of user request: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}")
+    token = get_bearer_token()
+
+    current_time = int(time.time())
+    token = db_session.query(UserSessionToken).filter(UserSessionToken.token == token, UserSessionToken.expiration >= current_time).first()
+    if token:
+
+        print("Valid user")
+
+        user = token.user
+        user._is_authenticated = True
+        user._is_active = True
+
+        return user
+        
+    return None
+
+@app.template_filter('as_datetime')
+def as_datetime(seconds):
+    time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(seconds))
