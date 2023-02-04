b32fff8fbc1fd61b29273f5023b958597cc0ca29
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2023-02-03 20:56:05 -0600

    Fix auggie routes'
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- augur/api/routes/auggie.py
+++ augur/api/routes/auggie.py
@@ -14,6 +14,8 @@ import os
 import requests
 import slack
 
+from ..server import app
+
 
 AUGUR_API_VERSION = 'api/unstable'
 
@@ -242,109 +244,174 @@ AUGUR_API_VERSION = 'api/unstable'
    
 #     return filteredUser
 
-def create_routes(server):
 
+@app.route('/auggie/get_user', methods=['POST'])
+def get_auggie_user():
+    # arg = [request.json]
+    # response = server.transform(metrics.get_auggie_user, args=arg)
+    # return Response(response=response, status=200, mimetype="application/json")
+    ## From Method
+    profile_name = 'augur'
+    if os.environ.get('AUGUR_IS_PROD'):
+        profile_name = 'default'
+    client = boto3.Session(region_name='us-east-1', profile_name=profile_name).client('dynamodb')
+    response = client.get_item(
+        TableName="auggie-users",
+                Key={
+                    "email": {"S":'{}:{}'.format(body["email"],body["teamID"])}
+                }
+    )
+    user = response['Item']
 
-    @app.route('/auggie/get_user', methods=['POST'])
-    def get_auggie_user():
-        # arg = [request.json]
-        # response = server.transform(metrics.get_auggie_user, args=arg)
-        # return Response(response=response, status=200, mimetype="application/json")
-        ## From Method
+    filteredUser = {
+        "interestedRepos":user["interestedRepos"],
+        "interestedGroups":user["interestedGroups"],
+        "host":user["host"]
+    }
+    
+    return filteredUser
+
+@app.route('/auggie/update_tracking', methods=['POST'])
+def update_auggie_user_tracking():
+    # arg = [request.json]
+    # response = server.transform(metrics.update_tracking, args=arg)
+    # return Response(response=response, status=200, mimetype="application/json")
+    ## From Method
+    profile_name = 'augur'
+    if os.environ.get('AUGUR_IS_PROD'):
+        profile_name = 'default'
+    client = boto3.Session(region_name='us-east-1', profile_name=profile_name).client('dynamodb')
+    response = client.update_item(
+        TableName="auggie-users",
+        Key={
+            "email": {"S": '{}:{}'.format(body["email"], body["teamID"])}
+        },
+        UpdateExpression="SET interestedGroups = :valGroup, interestedRepos = :valRepo, maxMessages = :valMax, host = :valHost, interestedInsightTypes = :valInterestedInsights",
+        ExpressionAttributeValues={
+            ":valGroup": {
+                "L": body["groups"]
+            },
+            ":valRepo": {
+                "L": body["repos"]
+            },
+            ":valMax": {
+                "N": body["maxMessages"]
+            },
+            ":valHost": {
+                "S": body["host"]
+            },
+            ":valInterestedInsights": {
+                "L": body["insightTypes"]
+            }
+        },
+        ReturnValues="ALL_NEW"
+    )
+
+    updated_values = response['Attributes']
+
+    filtered_values = {
+        "interestedRepos": updated_values["interestedRepos"],
+        "interestedGroups": updated_values["interestedGroups"],
+        "host": updated_values["host"]
+    }
+
+    return filtered_values
+
+@app.route('/auggie/slack_login', methods=['POST'])
+def slack_login():
+    # arg = [request.json]
+    # response = server.transform(metrics.slack_login, args=arg)
+    # return Response(response=response, status=200, mimetype="application/json")
+    # From Method
+    print("slack_login")
+
+    r = requests.get(
+        url=f'https://slack.com/api/oauth.v2.access?code={body["code"]}&client_id={os.environ["AUGGIE_CLIENT_ID"]}&client_secret={os.environ["AUGGIE_CLIENT_SECRET"]}&redirect_uri=http%3A%2F%2Flocalhost%3A8080')
+    data = r.json()
+
+    if (data["ok"]):
+        print(data)
+        token = data["authed_user"]["access_token"]
+        team_id = data["team"]["id"]
+        webclient = slack.WebClient(token=token)
+
+        user_response = webclient.users_identity()
+        print(user_response)
+        email = user_response["user"]["email"]
+
         profile_name = 'augur'
         if os.environ.get('AUGUR_IS_PROD'):
             profile_name = 'default'
-        client = boto3.Session(region_name='us-east-1', profile_name=profile_name).client('dynamodb')
+        print("Making Boto3 Session")
+        client = boto3.Session(region_name='us-east-1',
+                            profile_name=profile_name).client('dynamodb')
         response = client.get_item(
             TableName="auggie-users",
-                 Key={
-                     "email": {"S":'{}:{}'.format(body["email"],body["teamID"])}
-                 }
-        )
-        user = response['Item']
-
-        filteredUser = {
-            "interestedRepos":user["interestedRepos"],
-            "interestedGroups":user["interestedGroups"],
-            "host":user["host"]
-        }
-       
-        return filteredUser
-
-    @app.route('/auggie/update_tracking', methods=['POST'])
-    def update_auggie_user_tracking():
-        # arg = [request.json]
-        # response = server.transform(metrics.update_tracking, args=arg)
-        # return Response(response=response, status=200, mimetype="application/json")
-        ## From Method
-        profile_name = 'augur'
-        if os.environ.get('AUGUR_IS_PROD'):
-            profile_name = 'default'
-        client = boto3.Session(region_name='us-east-1', profile_name=profile_name).client('dynamodb')
-        response = client.update_item(
-            TableName="auggie-users",
             Key={
-                "email": {"S": '{}:{}'.format(body["email"], body["teamID"])}
-            },
-            UpdateExpression="SET interestedGroups = :valGroup, interestedRepos = :valRepo, maxMessages = :valMax, host = :valHost, interestedInsightTypes = :valInterestedInsights",
-            ExpressionAttributeValues={
-                ":valGroup": {
-                    "L": body["groups"]
-                },
-                ":valRepo": {
-                    "L": body["repos"]
-                },
-                ":valMax": {
-                    "N": body["maxMessages"]
-                },
-                ":valHost": {
-                    "S": body["host"]
-                },
-                ":valInterestedInsights": {
-                    "L": body["insightTypes"]
-                }
-            },
-            ReturnValues="ALL_NEW"
+                "email": {"S": '{}:{}'.format(email, team_id)}
+            }
         )
 
-        updated_values = response['Attributes']
+        if ('Item' in response):
+            user = response['Item']
+            print(user)
 
-        filtered_values = {
-            "interestedRepos": updated_values["interestedRepos"],
-            "interestedGroups": updated_values["interestedGroups"],
-            "host": updated_values["host"]
-        }
+            filteredUser = {
+                "interestedRepos": user["interestedRepos"],
+                "interestedGroups": user["interestedGroups"],
+                "host": user["host"],
+                "maxMessages": user["maxMessages"],
+                "interestedInsights": user["interestedInsightTypes"]
+            }
 
-        return filtered_values
+            user_body = json.dumps({
+                'team_id': team_id,
+                'email': email,
+                'user': filteredUser
+            })
 
-    @app.route('/auggie/slack_login', methods=['POST'])
-    def slack_login():
-        # arg = [request.json]
-        # response = server.transform(metrics.slack_login, args=arg)
-        # return Response(response=response, status=200, mimetype="application/json")
-        # From Method
-        print("slack_login")
+            print(user_body)
 
-        r = requests.get(
-            url=f'https://slack.com/api/oauth.v2.access?code={body["code"]}&client_id={os.environ["AUGGIE_CLIENT_ID"]}&client_secret={os.environ["AUGGIE_CLIENT_SECRET"]}&redirect_uri=http%3A%2F%2Flocalhost%3A8080')
-        data = r.json()
+            return user_body
+        else:
+            client.put_item(
+                TableName="auggie-users",
+                Item={
+                    'botToken': {'S': 'null'},
+                    'currentMessages': {'N': "0"},
+                    'maxMessages': {'N': "0"},
+                    'email': {'S': '{}:{}'.format(email, team_id)},
+                    'host': {'S': 'null'},
+                    'interestedGroups': {'L': []},
+                    'interestedRepos': {'L': []},
+                    'interestedInsightTypes': {'L': []},
+                    'teamID': {'S': team_id},
+                    'thread': {'S': 'null'},
+                    'userID': {'S': user_response['user']['id']}
+                }
+            )
 
-        if (data["ok"]):
-            print(data)
-            token = data["authed_user"]["access_token"]
-            team_id = data["team"]["id"]
-            webclient = slack.WebClient(token=token)
+            # users_response = webclient.users_list()
+            # for user in users_response["members"]:
+            #     if "api_app_id" in user["profile"] and user["profile"]["api_app_id"] == "ASQKB8JT0":
+            #         im_response = webclient.conversations_open(
+            #             users=user["id"]
+            #         )
+            #         print("Hopefully IM is opened")
+            #         channel = im_response["channel"]["id"]
 
-            user_response = webclient.users_identity()
-            print(user_response)
-            email = user_response["user"]["email"]
+            #         message_response = webclient.chat_postMessage(
+            #             channel=channel,
+            #             text="what repos?",
+            #             as_user="true")
+            #         print(message_response)
 
-            profile_name = 'augur'
-            if os.environ.get('AUGUR_IS_PROD'):
-                profile_name = 'default'
-            print("Making Boto3 Session")
-            client = boto3.Session(region_name='us-east-1',
-                                profile_name=profile_name).client('dynamodb')
+            #         ts = message_response["ts"]
+            #         webclient.chat_delete(
+            #             channel=channel,
+            #             ts=ts
+            #         )
+
             response = client.get_item(
                 TableName="auggie-users",
                 Key={
@@ -352,92 +419,25 @@ def create_routes(server):
                 }
             )
 
-            if ('Item' in response):
-                user = response['Item']
-                print(user)
+            user = response['Item']
+            print(user)
 
-                filteredUser = {
-                    "interestedRepos": user["interestedRepos"],
-                    "interestedGroups": user["interestedGroups"],
-                    "host": user["host"],
-                    "maxMessages": user["maxMessages"],
-                    "interestedInsights": user["interestedInsightTypes"]
-                }
+            filteredUser = {
+                "interestedRepos": user["interestedRepos"],
+                "interestedGroups": user["interestedGroups"],
+                "host": user["host"],
+                "maxMessages": user["maxMessages"],
+                "interestedInsights": user["interestedInsightTypes"]
+            }
 
-                user_body = json.dumps({
-                    'team_id': team_id,
-                    'email': email,
-                    'user': filteredUser
-                })
+            user_body = json.dumps({
+                'team_id': team_id,
+                'email': email,
+                'user': filteredUser
+            })
 
-                print(user_body)
+            print(user_body)
 
-                return user_body
-            else:
-                client.put_item(
-                    TableName="auggie-users",
-                    Item={
-                        'botToken': {'S': 'null'},
-                        'currentMessages': {'N': "0"},
-                        'maxMessages': {'N': "0"},
-                        'email': {'S': '{}:{}'.format(email, team_id)},
-                        'host': {'S': 'null'},
-                        'interestedGroups': {'L': []},
-                        'interestedRepos': {'L': []},
-                        'interestedInsightTypes': {'L': []},
-                        'teamID': {'S': team_id},
-                        'thread': {'S': 'null'},
-                        'userID': {'S': user_response['user']['id']}
-                    }
-                )
-
-                # users_response = webclient.users_list()
-                # for user in users_response["members"]:
-                #     if "api_app_id" in user["profile"] and user["profile"]["api_app_id"] == "ASQKB8JT0":
-                #         im_response = webclient.conversations_open(
-                #             users=user["id"]
-                #         )
-                #         print("Hopefully IM is opened")
-                #         channel = im_response["channel"]["id"]
-
-                #         message_response = webclient.chat_postMessage(
-                #             channel=channel,
-                #             text="what repos?",
-                #             as_user="true")
-                #         print(message_response)
-
-                #         ts = message_response["ts"]
-                #         webclient.chat_delete(
-                #             channel=channel,
-                #             ts=ts
-                #         )
-
-                response = client.get_item(
-                    TableName="auggie-users",
-                    Key={
-                        "email": {"S": '{}:{}'.format(email, team_id)}
-                    }
-                )
-
-                user = response['Item']
-                print(user)
-
-                filteredUser = {
-                    "interestedRepos": user["interestedRepos"],
-                    "interestedGroups": user["interestedGroups"],
-                    "host": user["host"],
-                    "maxMessages": user["maxMessages"],
-                    "interestedInsights": user["interestedInsightTypes"]
-                }
-
-                user_body = json.dumps({
-                    'team_id': team_id,
-                    'email': email,
-                    'user': filteredUser
-                })
-
-                print(user_body)
-
-                return user_body
-        else:
-            return data
+            return user_body
+    else:
+        return data
