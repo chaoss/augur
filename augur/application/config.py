f32a53804993cd6ad893286199c30ec1e9879e20
Isaac Milarsky <imilarsky@gmail.com>
2023-02-07 11:45:16 -0600

    initial changes
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- augur/application/config.py
+++ augur/application/config.py
@@ -100,7 +100,7 @@ default_config = {
             },
             "Task_Routine": {
                 "prelim_phase": 1,
-                "repo_collect_phase": 1,
+                "primary_repo_collect_phase": 1,
                 "machine_learning_phase": 0
             }
         }
