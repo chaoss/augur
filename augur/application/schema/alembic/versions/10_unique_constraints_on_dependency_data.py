e7443b863f4b147004be50246c42fc9f2a5c3206
Isaac Milarsky <krabs@tilde.team>
2023-02-27 20:47:19 -0600

    alembic syntax
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

--- augur/application/schema/alembic/versions/10_unique_constraints_on_dependency_data.py
+++ augur/application/schema/alembic/versions/10_unique_constraints_on_dependency_data.py
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '8'
-down_revision = '7'
+revision = '10'
+down_revision = '9'
 branch_labels = None
 depends_on = None
 
