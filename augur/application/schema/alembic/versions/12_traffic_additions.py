70009c2fd7a88d28eff42713cb0644a32ca48da3
Sean P. Goggins <s@goggins.com>
2023-03-18 10:26:37 -0500

    alembic tweaking for traffic.

--- augur/application/schema/alembic/versions/12_traffic_additions.py
+++ augur/application/schema/alembic/versions/12_traffic_additions.py
@@ -5,9 +5,12 @@ Revises: 11
 Create Date: 2022-12-30 19:23:17.997570
 
 """
+from alembic.autogenerate import renderers
+
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
+from sqlalchemy.schema import Sequence
 
 # revision identifiers, used by Alembic.
 revision = '12'
@@ -29,10 +32,27 @@ def downgrade():
 
 def add_repo_clone_data_table_1(upgrade = True):
 
+    clone_sequence = Sequence("augur_data.repo_clones_data_id_seq")
+    add_sequence_to_model(clone_sequence, **{'schema': None})
+
+    @renderers.dispatch_for(CreateSequenceOp)
+    def render_create_sequence(autogen_context, op):
+        return "op.create_sequence(%r, **%r)" % (
+            op.sequence_name,
+            {"schema": op.schema}
+        )
+
+
+    @renderers.dispatch_for(DropSequenceOp)
+    def render_drop_sequence(autogen_context, op):
+        return "op.drop_sequence(%r, **%r)" % (
+            op.sequence_name,
+            {"schema": op.schema}
+        ) 
+
     if upgrade:
 
-
-        op.execute(CreateSequence(Sequence('augur_data.repo_clones_data_id_seq')))
+        op.create_sequence('repo_clones_data_id_seq', **{'schema': 'augur_data'})
         op.create_table('repo_clones_data',
         sa.Column('repo_clone_data_id', sa.BigInteger(), server_default=sa.text("nextval('augur_data.repo_clones_data_id_seq'::regclass)"), nullable=False),
         sa.Column('repo_id', sa.BigInteger(), nullable=False),
@@ -53,3 +73,6 @@ def add_repo_clone_data_table_1(upgrade = True):
         op.create_foreign_key('user_repos_repo_id_fkey', 'user_repos', 'repo', ['repo_id'], ['repo_id'], source_schema='augur_operations')
         op.drop_table('repo_clones_data', schema='augur_data')
         op.execute(DropSequence(Sequence('augur_data.repo_clones_data_id_seq')))
+        add_sequence_to_model(clone_sequence, 'augur_data')
+        op.drop_sequence('repo_clones_data_id_seq', **{'schema': 'augur_data'})
+
