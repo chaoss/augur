42de1f137c3b0a45bc37100639b80f53b7190503
Sean P. Goggins <s@goggins.com>
2023-03-18 09:49:39 -0500

    Removed update to releases table, since that's already done. Add a unique constraint on repo_id in the clones table.

--- augur/application/schema/alembic/versions/12_traffic_additions.py
+++ augur/application/schema/alembic/versions/12_traffic_additions.py
@@ -41,12 +41,7 @@ def add_repo_clone_data_table_1(upgrade = True):
         sa.PrimaryKeyConstraint('repo_clone_data_id'),
         schema='augur_data'
         )
-        op.alter_column('releases', 'release_id',
-                existing_type=sa.CHAR(length=256),
-                type_=sa.CHAR(length=128),
-                existing_nullable=False,
-                existing_server_default=sa.text('nextval(\'"augur_data".releases_release_id_seq\'::regclass)'),
-                schema='augur_data')
+        sa.create_unique_constraint('repo_clone_unique', 'repo_clones_data', ['repo_id'])
         op.drop_constraint('user_repos_repo_id_fkey', 'user_repos', schema='augur_operations', type_='foreignkey')
         op.create_foreign_key(None, 'user_repos', 'repo', ['repo_id'], ['repo_id'], source_schema='augur_operations', referent_schema='augur_data')
     
@@ -54,10 +49,4 @@ def add_repo_clone_data_table_1(upgrade = True):
         
         op.drop_constraint(None, 'user_repos', schema='augur_operations', type_='foreignkey')
         op.create_foreign_key('user_repos_repo_id_fkey', 'user_repos', 'repo', ['repo_id'], ['repo_id'], source_schema='augur_operations')
-        op.alter_column('releases', 'release_id',
-                existing_type=sa.CHAR(length=128),
-                type_=sa.CHAR(length=256),
-                existing_nullable=False,
-                existing_server_default=sa.text('nextval(\'"augur_data".releases_release_id_seq\'::regclass)'),
-                schema='augur_data')
         op.drop_table('repo_clones_data', schema='augur_data')
