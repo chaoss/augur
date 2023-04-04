6b05bbcfaee6a185f7454d2faed0ace0c274f1dd
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2023-04-04 18:49:12 -0500

    Start defining database fixutres
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- conftest.py
+++ conftest.py
@@ -3,10 +3,17 @@ import pytest
 import re
 import logging
 import sqlalchemy as s
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import uuid
+from sqlalchemy.pool import StaticPool
+
 
 from augur.application.db.session import DatabaseSession
 from augur.application.config import AugurConfig
-from augur.application.db.engine import get_database_string
+from augur.application.db.engine import get_database_string, create_database_engine
+
 
 logger = logging.getLogger(__name__)
 
@@ -21,6 +28,71 @@ def create_full_routes(routes):
         route = "http://localhost:5000/api/unstable/" + route
         full_routes.append(route)
     return full_routes
+
+@pytest.fixture
+def database():
+
+    test_db_name = "test_db_" + uuid.uuid4().hex
+
+    db_string = get_database_string()
+
+    # remove database_name
+    db_string = db_string[:db_string.rfind("/")+1]
+    test_db_string = db_string + test_db_name
+
+    match = re.match(r"postgresql\+psycopg2:\/\/([a-zA-Z0-9_]+):([^@]+)@([^:]+):(\d+)\/", db_string)
+
+    # Connect to the default 'postgres' database
+    conn = psycopg2.connect(
+        host=match.group(3),
+        port=match.group(4),
+        user=match.group(1),
+        password=match.group(2),
+        dbname='postgres'
+    )
+
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = conn.cursor()
+    
+    print("Creating database")
+    cursor.execute(sql.SQL("CREATE DATABASE {};").format(sql.Identifier(test_db_name)))
+
+    with open('tests/base_db.sql', 'r') as file:
+        sql = file.read()
+
+    cursor.execute(sql)
+
+    # Commit the changes to the database
+    conn.commit()
+
+    
+    print("Creating engine")
+    engine = create_database_engine(test_db_string, poolclass=StaticPool)
+
+    yield engine
+
+    # dispose engine
+    engine.dispose()
+
+    # ensure connections are removed
+    cursor.execute(sql.SQL("SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='{}';".format(test_db_name)))
+
+    # drop database
+    cursor.execute(sql.SQL("DROP DATABASE {};").format(sql.Identifier(test_db_name)))
+
+
+    # Close the cursor and the connection
+    cursor.close()
+    conn.close()
+
+
+
+
+
+
+
+
+
     
 @pytest.fixture
 def test_db_engine():
@@ -60,3 +132,9 @@ def test_db_config(test_db_session):
 # def client(augur_app):
 #     flask_client = initialize_components(augur_app, disable_housekeeper=True).load()
 #     return flask_client.test_client()
+
+
+
+import time
+
+
