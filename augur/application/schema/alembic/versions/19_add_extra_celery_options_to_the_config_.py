10a16cbe992de04c89ee31685f4d4851d27000b9
Isaac Milarsky <imilarsky@gmail.com>
2023-05-15 12:30:09 -0500

    Add alembic revision for new version of the config
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

---
+++ augur/application/schema/alembic/versions/19_add_extra_celery_options_to_the_config_.py
@@ -0,0 +1,50 @@
+"""Add extra celery options to the config if they do not exist
+
+Revision ID: 19
+Revises: 18
+Create Date: 2023-05-15 12:03:57.171011
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from augur.application.db.session import DatabaseSession
+from augur.application.config import *
+from sqlalchemy.sql import text
+
+# revision identifiers, used by Alembic.
+revision = '19'
+down_revision = '18'
+branch_labels = None
+depends_on = None
+
+logger = logging.getLogger(__name__)
+
+def upgrade():
+
+    with DatabaseSession(logger) as session:
+        config = AugurConfig(logger,session)
+        config_dict = config.load_config()
+        
+        section = config_dict.get("Celery")
+
+        if section:
+            if 'worker_process_vmem_cap' not in section.keys():
+                section['worker_process_vmem_cap'] = 0.25
+            
+            if 'refresh_materialized_views_interval_in_days' not in section.keys():
+                section['refresh_materialized_views_interval_in_days'] = 7
+        else:
+            section = config.default_config["Celery"]
+        
+        config.add_section_from_json("Celery", section)
+
+
+
+def downgrade():
+
+    conn = op.get_bind()
+
+    con.execute(text(f"""
+        DELETE FROM augur_operations.config
+        WHERE section_name='Celery' AND (setting_name='worker_process_vmem_cap' OR setting_name='refresh_materialized_views_interval_in_days');
+    """))
