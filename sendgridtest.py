005d3452b67c660fab35c66752e1a35f1fc56ae3
Sean P. Goggins <gogginss@missouri.edu>
2023-07-09 00:05:02 +0000

    email test
    Signed-off-by: Sean P. Goggins <gogginss@missouri.edu>

--- sendgridtest.py
+++ sendgridtest.py
@@ -7,7 +7,7 @@ from sendgrid.helpers.mail import Mail
 message = Mail(
     from_email='metrix@goggins.com',
     to_emails='gogginss@missouri.edu',
-    subject='Sending with Twilio SendGrid is Fun',
+    subject='Hi, Attached is your report of recommendations provided as a part of your DEI Badge! Contained are specific suggestions you may consider for making your project more open and welcoming to newcomers from diverse backgrounds. .... etc.',
     html_content='<strong>and easy to do anywhere, even with Python</strong>')
 try:
     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
