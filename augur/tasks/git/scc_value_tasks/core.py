618824abbc54d359f0b616b37f1dbd5424c64e73
Isaac Milarsky <krabs@tilde.team>
2023-08-21 13:22:07 -0500

    start implementing new value worker functionality
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

---
+++ augur/tasks/git/scc_value_tasks/core.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import logging
+import requests
+import json
+import os
+import subprocess
+import re
+import traceback
+from augur.application.db.models import *
+from augur.application.db.session import DatabaseSession
+from augur.application.config import AugurConfig
+from augur.tasks.github.util.github_api_key_handler import GithubApiKeyHandler
+from augur.application.db.util import execute_session_query
+
+def value_model(session,repo_id, path):
+    """Runs scc on repo and stores data in database
+        :param repo_id: Repository ID
+        :param path: URL path of the Repostiory
+    """
+
+    session.logger.info('Generating value data for repo')
+    session.logger.info(f"Repo ID: {repo_id}, Path: {path}")
