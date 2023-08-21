6fe820cce06c5cf6b62fbe649ede1e725d24e35f
Isaac Milarsky <krabs@tilde.team>
2023-08-21 14:10:45 -0500

    add method to parse json from subprocess call because we now do like the exact same thing in two places
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

--- augur/tasks/git/scc_value_tasks/core.py
+++ augur/tasks/git/scc_value_tasks/core.py
@@ -11,6 +11,7 @@ from augur.application.db.session import DatabaseSession
 from augur.application.config import AugurConfig
 from augur.tasks.github.util.github_api_key_handler import GithubApiKeyHandler
 from augur.application.db.util import execute_session_query
+from augur.tasks.util.worker_util import parse_json_from_subprocess_call
 
 def value_model(session,repo_id, path):
     """Runs scc on repo and stores data in database
@@ -24,15 +25,7 @@ def value_model(session,repo_id, path):
 
     path_to_scc = os.environ['HOME'] + '/scc'
 
-    p = subprocess.run(['./scc', '-f','json', path], cwd=path_to_scc, capture_output=True, text=True, timeout=None)
-    session.logger.info('scc has completed... ')
-    output = p.stdout
-
-    try:
-        required_data = json.loads(output)
-    except json.decoder.JSONDecodeError as e:
-        session.logger.error(f"Could not parse required output! \n output: {output} \n Error: {e}")
-        return
+    required_output = parse_json_from_subprocess_call(session.logger,['./scc', '-f','json', path], cwd=path_to_scc)
     
     session.logger.info('adding scc data to database... ')
     session.logger.debug(f"output: {required_output}")
