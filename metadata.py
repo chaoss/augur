ff4adc9d7b4bcb85c76d4743adec380bfeb75a51
Sean P. Goggins <s@goggins.com>
2023-09-05 09:04:49 -0500

    version update
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- metadata.py
+++ metadata.py
@@ -5,8 +5,8 @@ __url__ = "https://github.com/chaoss/augur"
 
 __short_description__ = "Python 3 package for free/libre and open-source software community metrics, models & data collection"
 
-__version__ = "0.52.0"
-__release__ = "v0.52.0 (I am Supposed to be Writing a Grant)"
+__version__ = "0.53.0"
+__release__ = "v0.53.0 (Shamrock Shake)"
 
 __license__ = "MIT"
 __copyright__ = "University of Missouri, University of Nebraska-Omaha, CHAOSS, Brian Warner & Augurlabs 2023"
