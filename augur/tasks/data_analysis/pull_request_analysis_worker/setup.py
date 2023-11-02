c21c8b37635bc5ab05d3425d5f48a84b1b623324
Sean P. Goggins <gogginss@missouri.edu>
2023-11-02 17:43:30 +0000

    updating joblib version requirement in pr analysis
    Signed-off-by: Sean P. Goggins <gogginss@missouri.edu>

--- augur/tasks/data_analysis/pull_request_analysis_worker/setup.py
+++ augur/tasks/data_analysis/pull_request_analysis_worker/setup.py
@@ -32,7 +32,7 @@ setup(
         'numpy==1.22.0',
         'pandas==1.5.3',
         'emoji==1.2.0',
-        'joblib==1.0.1',
+        'joblib==1.2.0',
         'xgboost==1.4.2',
         'scipy==1.10.0'
     ],
