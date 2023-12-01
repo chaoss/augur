9729bceec18277d074814ff2e833b5b5c769d860
Andrew Brain <andrewbrain2019@gmail.com>
2023-12-01 15:19:14 -0600

    Remove commented code
    Signed-off-by: Andrew Brain <andrewbrain2019@gmail.com>

--- augur/tasks/gitlab/gitlab_paginator.py
+++ augur/tasks/gitlab/gitlab_paginator.py
@@ -76,7 +76,6 @@ def process_dict_response(logger: logging.Logger, response: httpx.Response, page
     Returns:
         A string explaining what happened is returned if what happened is determined, otherwise None is returned.
     """
-    #logger.info("Request returned a dict: {}\n".format(page_data))
 
     status_code = response.status_code
     if status_code == 429:
@@ -93,81 +92,6 @@ def process_dict_response(logger: logging.Logger, response: httpx.Response, page
         time.sleep(key_reset_time)
 
         return GitlabApiResult.RATE_LIMIT_EXCEEDED
-
-    # message = page_data.get('message')
-    # errors = page_data.get('errors')
-
-    # if not message and not errors:
-    #     return GitlabApiResult.SUCCESS
-
-    # if message == "Not Found":
-    #     logger.error(
-    #         "Gitlab repo was not found or does not exist for endpoint: "
-    #         f"{response.url}"
-    #     )
-    #     return GitlabApiResult.REPO_NOT_FOUND
-
-    # if message and "You have exceeded a secondary rate limit. Please wait a few minutes before you try again" in message:
-
-    #     # sleeps for the specified amount of time that github says to retry after
-    #     retry_after = int(response.headers["Retry-After"])
-    #     logger.info(
-    #         f'\n\n\n\nSleeping for {retry_after} seconds due to secondary rate limit issue.\n\n\n\n')
-    #     time.sleep(retry_after)
-
-    #     return GitlabApiResult.SECONDARY_RATE_LIMIT
-    #     # return "do_not_increase_attempts"
-    
-    # if message and "API rate limit exceeded for user" in message:
-
-    #     current_epoch = int(time.time())
-    #     epoch_when_key_resets = int(response.headers["X-RateLimit-Reset"])
-    #     key_reset_time =  epoch_when_key_resets - current_epoch
-        
-    #     if key_reset_time < 0:
-    #         logger.error(f"Key reset time was less than 0 setting it to 0.\nThe current epoch is {current_epoch} and the epoch that the key resets at is {epoch_when_key_resets}")
-    #         key_reset_time = 0
-            
-    #     logger.info(f"\n\n\nAPI rate limit exceeded. Sleeping until the key resets ({key_reset_time} seconds)")
-    #     time.sleep(key_reset_time)
-
-    #     return GitlabApiResult.RATE_LIMIT_EXCEEDED
-
-    # if message and "You have triggered an abuse detection mechanism." in message:
-    #     # self.update_rate_limit(response, temporarily_disable=True,platform=platform)
-        
-
-    #     # sleeps for the specified amount of time that github says to retry after
-    #     retry_after = int(response.headers["Retry-After"])
-    #     logger.info(f"Abuse mechanism detected sleeping for {retry_after} seconds")
-    #     time.sleep(retry_after)
-
-    #     return GitlabApiResult.ABUSE_MECHANISM_TRIGGERED
-
-    # if message == "Bad credentials":
-    #     logger.error("\n\n\n\n\n\n\n Bad Token Detected \n\n\n\n\n\n\n")
-    #     # self.update_rate_limit(response, bad_credentials=True, platform=platform)
-    #     return GitlabApiResult.BAD_CREDENTIALS
-    
-    # if errors:
-    #     for error in errors:
-    #         if "API rate limit exceeded for user" in error['message']:
-    #             current_epoch = int(time.time())
-    #             epoch_when_key_resets = int(response.headers["X-RateLimit-Reset"])
-    #             key_reset_time =  epoch_when_key_resets - current_epoch
-                
-    #             if key_reset_time < 0:
-    #                 logger.error(f"Key reset time was less than 0 setting it to 0.\nThe current epoch is {current_epoch} and the epoch that the key resets at is {epoch_when_key_resets}")
-    #                 key_reset_time = 0
-                    
-    #             logger.info(f"\n\n\nAPI rate limit exceeded. Sleeping until the key resets ({key_reset_time} seconds)")
-    #             time.sleep(key_reset_time)
-    #             return GitlabApiResult.RATE_LIMIT_EXCEEDED
-            
-    #         err_type = error.get('type')
-
-    #         if err_type and 'NOT_FOUND' in err_type:
-    #             return GitlabApiResult.REPO_NOT_FOUND
 
 
     return GitlabApiResult.NEW_RESULT
@@ -221,36 +145,6 @@ class GitlabPaginator(collections.abc.Sequence):
         Returns:
             The value at the index
         """
-        # if isinstance(index, slice) is True:
-
-        #     data_slice = index
-        #     start = data_slice.start
-        #     stop = data_slice.stop
-        #     step = data_slice.step
-
-        #     first_item_page = (start // 100) + 1
-        #     end_item_page = (stop // 100) + 1
-
-        #     all_data: List[dict] = []
-
-        #     for page_number in range(first_item_page, end_item_page+1):
-
-        #         # create url to query
-        #         params = {"page": items_page}
-        #         url = add_query_params(self.url, params)
-
-        #         data, _ = self.retrieve_data(url)
-
-        #         all_data += data
-
-        #     first_page_index = start % 100
-
-        #     needed_data = []
-        #     for index in range(start, stop, step):
-        #         needed_data.append(all_data[index])
-
-        #     return needed_data
-
                 
         # get the page the item is on
         items_page = (index // 100) + 1
