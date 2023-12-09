a57e1567da3fb92be080ba94e137738f285dcb80
Andrew Brain <andrewbrain2019@gmail.com>
2023-12-09 13:20:27 -0600

    Add processing for mr files
    Signed-off-by: Andrew Brain <andrewbrain2019@gmail.com>

--- augur/tasks/gitlab/merge_request_task.py
+++ augur/tasks/gitlab/merge_request_task.py
@@ -4,9 +4,9 @@ from augur.tasks.init.celery_app import celery_app as celery
 from augur.tasks.init.celery_app import AugurCoreRepoCollectionTask
 from augur.tasks.gitlab.gitlab_api_handler import GitlabApiHandler
 from augur.tasks.gitlab.gitlab_task_session import GitlabTaskManifest
-from augur.application.db.data_parse import extract_needed_pr_data_from_gitlab_merge_request, extract_needed_merge_request_assignee_data, extract_needed_mr_label_data, extract_needed_pr_reviewer_data, extract_needed_pr_commit_data
+from augur.application.db.data_parse import extract_needed_pr_data_from_gitlab_merge_request, extract_needed_merge_request_assignee_data, extract_needed_mr_label_data, extract_needed_pr_reviewer_data, extract_needed_pr_commit_data, extract_needed_mr_file_data
 from augur.tasks.github.util.util import get_owner_repo, add_key_value_pair_to_dicts
-from augur.application.db.models import PullRequest, PullRequestAssignee, PullRequestLabel, PullRequestReviewer, PullRequestCommit, Repo
+from augur.application.db.models import PullRequest, PullRequestAssignee, PullRequestLabel, PullRequestReviewer, PullRequestCommit, PullRequestFile, Repo
 from augur.application.db.util import execute_session_query
 
 
@@ -264,6 +264,7 @@ def process_mr_commits(data, task_name, repo_id, logger, augur_db):
             all_commits.append(extract_needed_pr_commit_data(commit, repo_id, pull_request_id, tool_source, tool_version, data_source))
 
 
+    logger.info(f"{task_name}: Inserting {len(all_commits)} merge request commits")
     pr_commits_natural_keys = ["pull_request_id", "repo_id", "pr_cmt_sha"]
     augur_db.insert_data(all_commits,PullRequestCommit,pr_commits_natural_keys)
             
@@ -277,15 +278,43 @@ def collect_merge_request_files(mr_ids, repo_git) -> int:
     logger = logging.getLogger(collect_merge_request_comments.__name__) 
     with GitlabTaskManifest(logger) as manifest:
 
+        augur_db = manifest.augur_db
+
+        query = augur_db.session.query(Repo).filter(Repo.repo_git == repo_git)
+        repo_obj = execute_session_query(query, 'one')
+        repo_id = repo_obj.repo_id
+
         url = "https://gitlab.com/api/v4/projects/{owner}%2f{repo}/merge_requests/{id}/changes".format(owner=owner, repo=repo, id="{id}")
         files = retrieve_merge_request_data(mr_ids, url, "files", owner, repo, manifest.key_auth, logger, response_type="dict")
 
         if files:
             logger.info(f"Length of merge request files: {len(files)}")
-            logger.info(f"Mr file: {files[0]}")
-            #issue_ids = process_issues(issue_data, f"{owner}/{repo}: Gitlab Issue task", repo_id, logger, augur_db)
+            process_mr_files(files, f"{owner}/{repo}: Mr files task", repo_id, logger, augur_db)
         else:
             logger.info(f"{owner}/{repo} has no gitlab merge request files")
+
+def process_mr_files(data, task_name, repo_id, logger, augur_db):
+
+    tool_source = "Mr files Task"
+    tool_version = "2.0"
+    data_source = "Gitlab API"
+
+    # create mapping from mr number to pull request id of current mrs
+    mr_number_to_id_map = {}
+    mrs = augur_db.session.query(PullRequest).filter(PullRequest.repo_id == repo_id).all()
+    for mr in mrs:
+        mr_number_to_id_map[mr.pr_src_number] = mr.pull_request_id
+
+    all_files = []
+    for id, gitlab_file_data in data.items():
+
+        pull_request_id = mr_number_to_id_map[id]
+
+        all_files.extend(extract_needed_mr_file_data(gitlab_file_data, repo_id, pull_request_id, tool_source, tool_version, data_source))
+
+    logger.info(f"{task_name}: Inserting {len(all_files)} merge request files")
+    pr_file_natural_keys = ["pull_request_id", "repo_id", "pr_file_path"]
+    augur_db.insert_data(all_files, PullRequestFile, pr_file_natural_keys)
     
 
 def retrieve_merge_request_data(ids, url, name, owner, repo, key_auth, logger, response_type):
