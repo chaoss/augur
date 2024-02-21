57784100b8ca4304d5c098c323c4892d0a72e864
Sean Goggins <outdoors@acm.org>
2024-02-21 10:27:00 -0600

    library deversioning for cross-python compatability
    Signed-off-by: Sean Goggins <outdoors@acm.org>

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         "pandas==1.5.3", # 1.4.3
         "numpy==1.26.0", # 1.23.2
         "requests==2.28.0", # 2.28.1
-        "psycopg2-binary==2.9.3", #2.9.3 what is pscopg-binary 3.0.16
+        "psycopg2-binary", #==2.9.3", #2.9.3 what is pscopg-binary 3.0.16
         "click==8.0.3", # 8.1.3
         "psutil==5.8.0", # 5.9.1
         "gunicorn==20.1.0", # 20.1.0
