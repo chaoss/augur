64c4196e8bae334f3b5622960241063d7ef11d84
Isaac Milarsky <imilarsky@gmail.com>
2024-03-13 14:24:57 -0500

    fix conflicting namespace
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- augur/application/cli/db.py
+++ augur/application/cli/db.py
@@ -12,7 +12,7 @@ import sqlalchemy as s
 import pandas as pd
 import json
 import re
-import stat
+import stat as stat_module
 
 from augur.application.cli import test_connection, test_db_connection, with_database, DatabaseContext
 
@@ -443,13 +443,13 @@ def check_pgpass_credentials(config):
     if not path.isfile(pgpass_file_path):
         print("~/.pgpass does not exist, creating.")
         open(pgpass_file_path, "w+")
-        chmod(pgpass_file_path, stat.S_IWRITE | stat.S_IREAD)
+        chmod(pgpass_file_path, stat_module.S_IWRITE | stat_module.S_IREAD)
 
     pgpass_file_mask = oct(os.stat(pgpass_file_path).st_mode & 0o777)
 
     if pgpass_file_mask != "0o600":
         print("Updating ~/.pgpass file permissions.")
-        chmod(pgpass_file_path, stat.S_IWRITE | stat.S_IREAD)
+        chmod(pgpass_file_path, stat_module.S_IWRITE | stat_module.S_IREAD)
 
     with open(pgpass_file_path, "a+") as pgpass_file:
         end = pgpass_file.tell()
