effdd41783fc2511ed443354934776d51937ab7f
Andrew Brain <andrewbrain2019@gmail.com>
2024-04-13 08:37:15 -0500

    Start reducing query usage in github tasks
    Signed-off-by: Andrew Brain <andrewbrain2019@gmail.com>

--- augur/tasks/github/detect_move/tasks.py
+++ augur/tasks/github/detect_move/tasks.py
@@ -4,7 +4,7 @@ from augur.tasks.github.util.github_task_session import GithubTaskManifest
 from augur.tasks.github.detect_move.core import *
 from augur.tasks.init.celery_app import celery_app as celery
 from augur.tasks.init.celery_app import AugurCoreRepoCollectionTask, AugurSecondaryRepoCollectionTask
-from augur.application.db.util import execute_session_query
+from augur.application.db.lib import get_repo_by_repo_git
 
 
 
@@ -14,14 +14,15 @@ def detect_github_repo_move_core(repo_git : str) -> None:
     logger = logging.getLogger(detect_github_repo_move_core.__name__)
 
     logger.info(f"Starting repo_move operation with {repo_git}")
+
+    repo = get_repo_by_repo_git(repo_git)
+
+    logger.info(f"Pinging repo: {repo_git}")
+
     with GithubTaskManifest(logger) as manifest:
-        augur_db = manifest.augur_db
         #Ping each repo with the given repo_git to make sure
         #that they are still in place. 
-        query = augur_db.session.query(Repo).filter(Repo.repo_git == repo_git)
-        repo = execute_session_query(query, 'one')
-        logger.info(f"Pinging repo: {repo_git}")
-        ping_github_for_repo_move(augur_db, manifest.key_auth, repo, logger)
+        ping_github_for_repo_move(manifest.augur_db, manifest.key_auth, repo, logger)
 
 
 @celery.task(base=AugurSecondaryRepoCollectionTask)
@@ -30,11 +31,12 @@ def detect_github_repo_move_secondary(repo_git : str) -> None:
     logger = logging.getLogger(detect_github_repo_move_secondary.__name__)
 
     logger.info(f"Starting repo_move operation with {repo_git}")
+
+    repo = get_repo_by_repo_git(repo_git)
+
+    logger.info(f"Pinging repo: {repo_git}")
+
     with GithubTaskManifest(logger) as manifest:
-        augur_db = manifest.augur_db
         #Ping each repo with the given repo_git to make sure
         #that they are still in place. 
-        query = augur_db.session.query(Repo).filter(Repo.repo_git == repo_git)
-        repo = execute_session_query(query, 'one')
-        logger.info(f"Pinging repo: {repo_git}")
-        ping_github_for_repo_move(augur_db, manifest.key_auth, repo, logger,collection_hook='secondary')
+        ping_github_for_repo_move(manifest.augur_db, manifest.key_auth, repo, logger,collection_hook='secondary')
