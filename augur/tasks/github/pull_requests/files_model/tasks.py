0ffc592e228247ba811325717c7cc908e18dbc10
Andrew Brain <andrewbrain2019@gmail.com>
2024-04-15 19:04:43 -0500

    Reduce usage of github task manifest
    Signed-off-by: Andrew Brain <andrewbrain2019@gmail.com>

--- augur/tasks/github/pull_requests/files_model/tasks.py
+++ augur/tasks/github/pull_requests/files_model/tasks.py
@@ -1,18 +1,22 @@
 import logging
 from augur.tasks.github.pull_requests.files_model.core import *
-from augur.tasks.github.util.github_task_session import GithubTaskManifest
+from augur.application.db.session import DatabaseSession
 from augur.tasks.init.celery_app import celery_app as celery
 from augur.tasks.init.celery_app import AugurSecondaryRepoCollectionTask
 from augur.application.db.lib import get_repo_by_repo_git
+from augur.application.db import get_engine
+
 
 
 @celery.task(base=AugurSecondaryRepoCollectionTask)
 def process_pull_request_files(repo_git: str) -> None:
 
+    engine = get_engine()
+
     logger = logging.getLogger(process_pull_request_files.__name__)
 
     repo = get_repo_by_repo_git(repo_git)
 
-    with GithubTaskManifest(logger) as manifest:
+    with DatabaseSession(logger, engine=engine) as session:
 
-        pull_request_files_model(repo, logger, manifest.key_auth)
+        pull_request_files_model(repo, logger, session)
