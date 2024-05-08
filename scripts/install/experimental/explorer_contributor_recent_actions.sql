f2653ddc90c12efcc50aa0989777b57856644157
Sean P. Goggins <s@goggins.com>
2024-05-08 15:20:29 -0500

    added performance enhancing indices to new materialized views
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- scripts/install/experimental/explorer_contributor_recent_actions.sql
+++ scripts/install/experimental/explorer_contributor_recent_actions.sql
@@ -102,4 +102,6 @@ AS SELECT a.id AS cntrb_id,
 WITH DATA;
 
 -- View indexes:
-CREATE UNIQUE INDEX explorer_contributor_recent_actions_cntrb_id_created_at_repo_id_ac_idx ON augur_data.explorer_contributor_recent_actions USING btree (cntrb_id, created_at, repo_id, action, repo_name, login, rank);
+CREATE UNIQUE INDEX explorer_contributor_recent_actions_unique_idx ON augur_data.explorer_contributor_recent_actions USING btree (cntrb_id, created_at, repo_id, action, repo_name, login, rank);
+CREATE INDEX explorer_contributor_recent_actions_cntrb_id_idx ON augur_data.explorer_contributor_recent_actions USING btree (cntrb_id);
+CREATE INDEX explorer_contributor_recent_actions_repo_id_idx ON augur_data.explorer_contributor_recent_actions USING btree (repo_id DESC);
