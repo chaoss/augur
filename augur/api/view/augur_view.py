2315a2125a37a4807260350ae0a8090dcb3a0dfd
Ulincsys <ulincsys@gmail.com>
2024-06-24 11:02:57 -0500

    implement config updating and user table view
    Signed-off-by: Ulincsys <ulincsys@gmail.com>

--- augur/api/view/augur_view.py
+++ augur/api/view/augur_view.py
@@ -64,6 +64,11 @@ def internal_server_error(error):
     
     return render_message("500 - Internal Server Error", "An error occurred while trying to service your request. Please try again, and if the issue persists, please file a GitHub issue with the below error message:", error=stacktrace), 500
 
+@app.template_filter("escape_ID")
+def escape_HTML_ID(data: str) -> str:
+    data = data.replace(".", "\\.")
+    return data
+
 @login_manager.unauthorized_handler
 def unauthorized():
     if AUGUR_API_VERSION in str(request.path):
@@ -76,16 +81,6 @@ def unauthorized():
 
     session["login_next"] = url_for(request.endpoint, **request.args)
     return redirect(url_for('user_login'))
-
-def admin_required(func):
-    @login_required
-    @wraps(func)
-    def inner_function(*args, **kwargs):
-        if current_user.admin:
-            return func(*args, **kwargs)
-        else:
-            return forbidden(None)
-    return inner_function
 
 @login_manager.user_loader
 def load_user(user_id):
