3ee5e4854187b1c7d2ac6563d5c766923d5d69fc
Andrew Brain <andrewbrain2019@gmail.com>
2024-06-25 19:50:29 -0500

    Pass repo_id
    Signed-off-by: Andrew Brain <andrewbrain2019@gmail.com>

--- augur/tasks/github/pull_requests/tasks.py
+++ augur/tasks/github/pull_requests/tasks.py
@@ -40,7 +40,7 @@ def collect_pull_requests(repo_git: str, full_collection: bool) -> int:
         if full_collection:
             core_data_last_collected = None
         else:
-            core_data_last_collected = get_core_data_last_collected().date()
+            core_data_last_collected = get_core_data_last_collected(repo_id).date()
 
         total_count = 0
         all_data = []
