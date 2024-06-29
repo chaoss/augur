da49d9ebf75b328d8003f3aa7d756a1dea266386
Andrew Brain <andrewbrain2019@gmail.com>
2024-06-29 17:58:11 -0500

    Move issues tasks out of folder
    Signed-off-by: Andrew Brain <andrewbrain2019@gmail.com>

--- augur/tasks/github/__init__.py
+++ augur/tasks/github/__init__.py
@@ -1,6 +1,6 @@
 from augur.tasks.github.contributors import *
 from augur.tasks.github.events import *
-from augur.tasks.github.issues.tasks import *
+from augur.tasks.github.issues import *
 from augur.tasks.github.messages.tasks import *
 from augur.tasks.github.pull_requests.tasks import *
 from augur.tasks.github.repo_info.tasks import *
