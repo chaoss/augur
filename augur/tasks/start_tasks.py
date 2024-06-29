1f7723e74825ccd0c6365fa26f4acd2902bb25f1
Andrew Brain <andrewbrain2019@gmail.com>
2024-06-29 18:02:54 -0500

    Move traffic tasks out of folder
    Signed-off-by: Andrew Brain <andrewbrain2019@gmail.com>

--- augur/tasks/start_tasks.py
+++ augur/tasks/start_tasks.py
@@ -15,7 +15,7 @@ from augur.tasks.github.repo_info.tasks import collect_repo_info, collect_linux_
 from augur.tasks.github.pull_requests.files_model.tasks import process_pull_request_files
 from augur.tasks.github.pull_requests.commits_model.tasks import process_pull_request_commits
 from augur.tasks.git.dependency_tasks.tasks import process_ossf_dependency_metrics
-from augur.tasks.github.traffic.tasks import collect_github_repo_clones_data
+from augur.tasks.github.tasks import collect_github_repo_clones_data
 from augur.tasks.gitlab.merge_request_task import collect_gitlab_merge_requests, collect_merge_request_metadata, collect_merge_request_commits, collect_merge_request_files, collect_merge_request_comments
 from augur.tasks.gitlab.issues_task import collect_gitlab_issues, collect_gitlab_issue_comments
 from augur.tasks.gitlab.events_task import collect_gitlab_issue_events, collect_gitlab_merge_request_events
