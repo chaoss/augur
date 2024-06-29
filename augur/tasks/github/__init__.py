0d193984062ef8f5da6118500caffcd2552315f8
Andrew Brain <andrewbrain2019@gmail.com>
2024-06-29 17:55:09 -0500

    Move contributors tasks out of folder
    Signed-off-by: Andrew Brain <andrewbrain2019@gmail.com>

--- augur/tasks/github/__init__.py
+++ augur/tasks/github/__init__.py
@@ -1,4 +1,4 @@
-from augur.tasks.github.contributors.tasks import *
+from augur.tasks.github.contributors import *
 from augur.tasks.github.events.tasks import *
 from augur.tasks.github.issues.tasks import *
 from augur.tasks.github.messages.tasks import *
