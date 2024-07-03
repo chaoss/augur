2a30cbe661f14cf83d818ab46767a83416b95870
Andrew Brain <andrewbrain2019@gmail.com>
2024-07-03 17:34:58 -0500

    Simplify metadata exception
    Signed-off-by: Andrew Brain <andrewbrain2019@gmail.com>

--- augur/tasks/util/metadata_exception.py
+++ augur/tasks/util/metadata_exception.py
@@ -2,7 +2,5 @@ class MetadataException(Exception):
     def __init__(self, original_exception, additional_metadata):
         self.original_exception = original_exception
         self.additional_metadata = additional_metadata
-        super().__init__(self._build_message())
-
-    def _build_message(self):
-        return f"{str(self.original_exception)} | Additional metadata: {self.additional_metadata}"
+        
+        super().__init__(f"{str(self.original_exception)} | Additional metadata: {self.additional_metadata}")
