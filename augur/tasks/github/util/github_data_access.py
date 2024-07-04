1059e6da698e25f1197ada9913be0ecc3029670d
Andrew Brain <andrewbrain2019@gmail.com>
2024-07-04 14:38:29 -0500

    Fully collect github events
    Signed-off-by: Andrew Brain <andrewbrain2019@gmail.com>
    Signed-off-by: Ulincsys <ulincsys@gmail.com>

--- augur/tasks/github/util/github_data_access.py
+++ augur/tasks/github/util/github_data_access.py
@@ -65,11 +65,15 @@ class GithubDataAccess:
 
         return 
     
-    def is_pagination_limited_by_max_github_pages(self, url):
+    def does_pagination_contain_all_data(self, url):
         
         page_count = self.get_resource_page_count(url)
 
-        return page_count <= 299
+        if page_count > 300:
+            raise Exception(f"Either github raised the paginator page limit for things like events and messages, or 
+                            is_pagination_limited_by_max_github_pages is being used on a resource that does not have a page limit. Url: {url}")
+
+        return page_count == 300
     
     def get_resource_page_count(self, url):
 
