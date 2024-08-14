d736a1ba19fc3a1840d0245ae47b502ffcd011a6
Sean P. Goggins <s@goggins.com>
2024-08-14 18:01:44 -0500

    Working to get more information on this error: Traceback (most recent call last):   File '/home/ubuntu/github/virtualenvs/hosted/lib/python3.11/site-packages/billiard/pool.py', line 366, in workloop     put((READY, (job, i, result, inqW_fd)))   File '/home/ubuntu/github/virtualenvs/hosted/lib/python3.11/site-packages/billiard/queues.py', line 366, in put     self.send_payload(ForkingPickler.dumps(obj))                       ^^^^^^^^^^^^^^^^^^^^^^^^^   File '/home/ubuntu/github/virtualenvs/hosted/lib/python3.11/site-packages/billiard/reduction.py', line 56, in dumps     cls(buf, protocol).dump(obj) billiard.pool.MaybeEncodingError: Error sending result: ''(1, <ExceptionInfo: MetadataException('\'checks\' | Additional metadata: required_output: {}')>, None)''. Reason: ''PicklingError('Can\'t pickle <class \'augur.tasks.util.metadata_exception.MetadataException\'>: it\'s not the same object as augur.tasks.util.metadata_exception.MetadataException')''.
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- augur/tasks/git/dependency_tasks/tasks.py
+++ augur/tasks/git/dependency_tasks/tasks.py
@@ -21,4 +21,24 @@ def process_ossf_dependency_metrics(self, repo_git):
     
     logger = logging.getLogger(process_ossf_dependency_metrics.__name__)
 
-    generate_scorecard(logger, repo_git)
+    try: 
+        generate_scorecard(logger, repo_git)
+    except Exception as e: 
+        logger.warning(f'Exception generating scorecard: {e}')
+        raise MetadataException 
+    
+    """
+        This try/except block is an attempt to get more information about this occasional error: 
+        
+        ```bash
+        Traceback (most recent call last):
+        File "/home/ubuntu/github/virtualenvs/hosted/lib/python3.11/site-packages/billiard/pool.py", line 366, in workloop
+            put((READY, (job, i, result, inqW_fd)))
+        File "/home/ubuntu/github/virtualenvs/hosted/lib/python3.11/site-packages/billiard/queues.py", line 366, in put
+            self.send_payload(ForkingPickler.dumps(obj))
+                            ^^^^^^^^^^^^^^^^^^^^^^^^^
+        File "/home/ubuntu/github/virtualenvs/hosted/lib/python3.11/site-packages/billiard/reduction.py", line 56, in dumps
+            cls(buf, protocol).dump(obj)
+        billiard.pool.MaybeEncodingError: Error sending result: ''(1, <ExceptionInfo: MetadataException("\'checks\' | Additional metadata: required_output: {}")>, None)''. Reason: ''PicklingError("Can\'t pickle <class \'augur.tasks.util.metadata_exception.MetadataException\'>: it\'s not the same object as augur.tasks.util.metadata_exception.MetadataException")''.
+        ```
+    """
