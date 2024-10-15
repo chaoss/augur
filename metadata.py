055fa7cabcc98947cb87a89e9e4740355fcf3c30
Sean P. Goggins <s@goggins.com>
2024-10-15 18:23:20 -0500

    update version ID
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- metadata.py
+++ metadata.py
@@ -5,8 +5,8 @@ __url__ = "https://github.com/chaoss/augur"
 
 __short_description__ = "Python 3 package for free/libre and open-source software community metrics, models & data collection"
 
-__version__ = "0.76.3"
-__release__ = "v0.76.3 (Pumpkin Laser)"
+__version__ = "0.76.4"
+__release__ = "v0.76.4 (Pumpkin Core)"
 
 __license__ = "MIT"
 __copyright__ = "University of Missouri, University of Nebraska-Omaha, CHAOSS, Brian Warner & Augurlabs 2112"
