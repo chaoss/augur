f63d9c21af357f5df94ad8e6cdbb97e53df2f1de
Sean P. Goggins <outdoors@acm.org>
2025-01-23 16:33:46 -0600

    Update metadata.py
    Signed-off-by: Sean P. Goggins <outdoors@acm.org>

--- metadata.py
+++ metadata.py
@@ -5,8 +5,8 @@ __url__ = "https://github.com/chaoss/augur"
 
 __short_description__ = "Python 3 package for free/libre and open-source software community metrics, models & data collection"
 
-__version__ = "0.76.7"
-__release__ = "v0.76.7 (Captain Tuttle)"
+__version__ = "0.80.0"
+__release__ = "v0.80.0 (Data Monster)"
 
 __license__ = "MIT"
 __copyright__ = "University of Missouri, University of Nebraska-Omaha, CHAOSS, Brian Warner & Augurlabs 2112"
