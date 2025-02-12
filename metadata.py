a8be064cad200c3045e538f3bd319dce578f7eb9
Sean P. Goggins <s@goggins.com>
2025-02-12 09:48:30 -0600

    version bump
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- metadata.py
+++ metadata.py
@@ -5,8 +5,8 @@ __url__ = "https://github.com/chaoss/augur"
 
 __short_description__ = "Python 3 package for free/libre and open-source software community metrics, models & data collection"
 
-__version__ = "0.80.1"
-__release__ = "v0.80.1 (Data Monster)"
+__version__ = "0.81.0"
+__release__ = "v0.81.0 (Super Soaker)"
 
 __license__ = "MIT"
-__copyright__ = "University of Missouri, University of Nebraska-Omaha, CHAOSS, Brian Warner & Augurlabs 2112"
+__copyright__ = "University of Missouri, University of Nebraska-Omaha, CHAOSS, Sean Goggins, Brian Warner & Augurlabs 2112"
