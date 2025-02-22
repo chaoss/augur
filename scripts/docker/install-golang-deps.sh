8ce6439e2f044ac44ea6d8035f11ee99bc5d4466
Adrian Edwards <adrian@adriancedwards.com>
2025-02-22 18:30:42 -0500

    specify versions of scc and scorecard
    This fixes an issue with scc where the latest version requires a golang update to build.
    
    This also prevents the same happening to scorecard in future
    
    Signed-off-by: Adrian Edwards <adrian@adriancedwards.com>

--- scripts/docker/install-golang-deps.sh
+++ scripts/docker/install-golang-deps.sh
@@ -7,7 +7,9 @@ CURRENT_DIR=$PWD;
 # Install scc
 SCC_DIR="$HOME/scc"
 echo "Cloning Sloc Cloc and Code (SCC) to generate value data ..."
-git clone https://github.com/boyter/scc "$SCC_DIR"
+# this needs to be done from source. the latest version doesnt seem to exist on the package repo
+# however, the latest version (v3.5.0) requires bumping the golang version in the Dockerfile
+git clone --depth 1 --branch v3.4.0 https://github.com/boyter/scc "$SCC_DIR"
 cd $SCC_DIR
 go build;
 echo "scc build done"
@@ -17,7 +19,8 @@ cd $CURRENT_DIR
 # Install scorecard
 SCORECARD_DIR="$HOME/scorecard"
 echo "Cloning OSSF Scorecard to generate scorecard data ..."
-git clone https://github.com/ossf/scorecard $SCORECARD_DIR
+# lock version to prevent future issues if the golang version is bumped
+git clone --depth 1 --branch v5.1.1 https://github.com/ossf/scorecard $SCORECARD_DIR
 cd $SCORECARD_DIR
 go build;
 echo "scorecard build done"
