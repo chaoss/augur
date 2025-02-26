e9ed2c46bddb84e51364853d16f0dd626e705c14
Sean P. Goggins <s@goggins.com>
2025-02-26 10:23:46 -0600

    Release Versions
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- metadata.py
+++ metadata.py
@@ -6,7 +6,7 @@ __url__ = "https://github.com/chaoss/augur"
 __short_description__ = "Python 3 package for free/libre and open-source software community metrics, models & data collection"
 
 __version__ = "0.85.0"
-__release__ = "v0.85.0 (Basketball Jones)"
+__release__ = "v0.85.0 (Eurovision)"
 
 __license__ = "MIT"
 __copyright__ = "University of Missouri, University of Nebraska-Omaha, CHAOSS, Sean Goggins, Brian Warner & Augurlabs 2025"
