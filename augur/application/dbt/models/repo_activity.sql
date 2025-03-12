98deb0edd7d0a7445720f40b37ea1224e977cf75
Allen Hu <hu16@umail.ucsb.edu>
2025-03-11 21:31:37 -0700

    Add a hello world model and test for dbt
    Signed-off-by: Allen Hu <huallen28@gmail.com>

---
+++ augur/application/dbt/models/repo_activity.sql
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: MIT
+''' 
+"Hello World" for DBT Analytical Transformation Workflows
+This model calculates the total number of commits and issues for each repository
+'''
+{{ config(materialized='table') }}
+
+WITH commits AS (
+    SELECT repo_id, COUNT(*) AS total_commits 
+    FROM augur_data.commits 
+    GROUP BY repo_id
+),
+issues AS (
+    SELECT repo_id, COUNT(*) AS total_issues 
+    FROM augur_data.issues 
+    GROUP BY repo_id
+)
+
+SELECT 
+    c.repo_id, 
+    c.total_commits, 
+    COALESCE(i.total_issues, 0) AS total_issues
+FROM commits c
+LEFT JOIN issues i 
+    ON c.repo_id = i.repo_id
