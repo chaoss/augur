91ebd95fec180c162745707437be50b1cbe7a88e
KhanRayyan3622 <rayyan.khan@chaoss.community>
2025-04-08 04:29:46 +0500

    fix: Fix spelling in comments (issueing->issuing, analyses->analyzes, dependancy->dependency)
    Signed-off-by: KhanRayyan3622 <rayyan.khan@chaoss.community>

---
+++ augur/augur/application/db/models/augur_data.py
@@ -0,0 +1,7 @@
+dep_name = Column(String, comment="Name of the dependency found in project. ")
+dep_count = Column(Integer, comment="Number of times the dependency was found. ")
+dep_language = Column(String, comment="Language of the dependency. ")
+
+comment="This column is used to indicate analyzes run by a worker during the same execution period, and is useful for grouping, and time series analysis.  ",
+
+comment='This is a representation of the repo:branch information in the pull request. Head is issuing the pull request and base is taking the pull request. For example:  (We do not store all of this)\n\n "head": {\n      "label": "chaoss:pull-request-worker",\n      "ref": "pull-request-worke' 
