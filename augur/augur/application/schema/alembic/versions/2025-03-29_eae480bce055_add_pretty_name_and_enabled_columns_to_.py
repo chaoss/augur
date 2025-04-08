ccb1ab195b6360118862b2a1a2322c33836581b3
KhanRayyan3622 <rayyan.khan@chaoss.community>
2025-04-08 22:33:08 +0500

    Fix spelling and formatting in migration files: - Fix 'analyzes' to 'analyses' - Complete 'pull-request-worker' string - Improve formatting and comments - Reorder drop statements in downgrade()

--- augur/augur/application/schema/alembic/versions/2025-03-29_eae480bce055_add_pretty_name_and_enabled_columns_to_.py
+++ augur/augur/application/schema/alembic/versions/2025-03-29_eae480bce055_add_pretty_name_and_enabled_columns_to_.py
@@ -1,1 +1,32 @@
- 
+"""add pretty name and enabled columns to platform_default
+
+Revision ID: eae480bce055
+Revises: None
+Create Date: 2025-03-29 00:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eae480bce055'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('platform_features', sa.Column('worker_run_id', sa.BIGINT(), autoincrement=False, nullable=True, comment='This column is used to indicate analyses run by a worker during the same execution period, and is useful for grouping, and time series analysis.'))
+    op.add_column('platform_features', sa.Column('dep_name', sa.VARCHAR(), autoincrement=False, nullable=True, comment='Name of the dependency.'))
+    op.add_column('platform_features', sa.Column('dep_count', sa.INTEGER(), autoincrement=False, nullable=True, comment='Number of times the dependency was found.'))
+    op.add_column('platform_features', sa.Column('dep_language', sa.VARCHAR(), autoincrement=False, nullable=True, comment='Language of the dependency.'))
+    op.add_column('platform_features', sa.Column('pull_request_info', sa.Text(), nullable=True, comment='This is a representation of the repo:branch information in the pull request. Head is issuing the pull request and base is taking the pull request. For example: (We do not store all of this)\n\n "head": {\n      "label": "chaoss:pull-request-worker",\n      "ref": "pull-request-worker"'))
+
+
+def downgrade():
+    op.drop_column('platform_features', 'pull_request_info')
+    op.drop_column('platform_features', 'dep_language')
+    op.drop_column('platform_features', 'dep_count')
+    op.drop_column('platform_features', 'dep_name')
+    op.drop_column('platform_features', 'worker_run_id') 
--- augur/augur/application/schema/alembic/versions/2025-03-30_96e37a338676_add_platform_features_column_to_.py
+++ augur/augur/application/schema/alembic/versions/2025-03-30_96e37a338676_add_platform_features_column_to_.py
@@ -1,16 +1,32 @@
+"""add platform features column to platform_default
+
+Revision ID: 96e37a338676
+Revises: eae480bce055
+Create Date: 2025-03-30 00:00:00.000000
+
+"""
 from alembic import op
 import sqlalchemy as sa
 
+
+# revision identifiers, used by Alembic.
+revision = '96e37a338676'
+down_revision = 'eae480bce055'
+branch_labels = None
+depends_on = None
+
+
 def upgrade():
-    op.add_column('platform_features', sa.Column('worker_run_id', sa.BIGINT(), autoincrement=False, nullable=True, comment='This column is used to indicate analyzes run by a worker during the same execution period, and is useful for grouping, and time series analysis.'))
-    op.add_column('platform_features', sa.Column('dep_name', sa.VARCHAR(), autoincrement=False, nullable=True, comment='Name of the dependency found in project.'))
+    op.add_column('platform_features', sa.Column('worker_run_id', sa.BIGINT(), autoincrement=False, nullable=True, comment='This column is used to indicate analyses run by a worker during the same execution period, and is useful for grouping, and time series analysis.'))
+    op.add_column('platform_features', sa.Column('dep_name', sa.VARCHAR(), autoincrement=False, nullable=True, comment='Name of the dependency.'))
     op.add_column('platform_features', sa.Column('dep_count', sa.INTEGER(), autoincrement=False, nullable=True, comment='Number of times the dependency was found.'))
     op.add_column('platform_features', sa.Column('dep_language', sa.VARCHAR(), autoincrement=False, nullable=True, comment='Language of the dependency.'))
-    op.add_column('platform_features', sa.Column('pull_request_info', sa.Text(), nullable=True, comment='This is a representation of the repo:branch information in the pull request. Head is issuing the pull request and base is taking the pull request. For example: (We do not store all of this)\n\n "head": {\n      "label": "chaoss:pull-request-worker",\n      "ref": "pull-request-worke'))
+    op.add_column('platform_features', sa.Column('pull_request_info', sa.Text(), nullable=True, comment='This is a representation of the repo:branch information in the pull request. Head is issuing the pull request and base is taking the pull request. For example: (We do not store all of this)\n\n "head": {\n      "label": "chaoss:pull-request-worker",\n      "ref": "pull-request-worker"'))
+
 
 def downgrade():
-    op.drop_column('platform_features', 'worker_run_id')
-    op.drop_column('platform_features', 'dep_name')
-    op.drop_column('platform_features', 'dep_count')
+    op.drop_column('platform_features', 'pull_request_info')
     op.drop_column('platform_features', 'dep_language')
-    op.drop_column('platform_features', 'pull_request_info') 
+    op.drop_column('platform_features', 'dep_count')
+    op.drop_column('platform_features', 'dep_name')
+    op.drop_column('platform_features', 'worker_run_id') 
