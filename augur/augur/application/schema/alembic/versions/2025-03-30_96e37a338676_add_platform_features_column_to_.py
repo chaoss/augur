a16060904b5749f443fd12a2dd2296269dfb12ff
KhanRayyan3622 <rayyan.khan@chaoss.community>
2025-04-08 18:00:37 +0500

    fix: Correct spelling errors in migration file
    Signed-off-by: KhanRayyan3622 <rayyan.khan@chaoss.community>

---
+++ augur/augur/application/schema/alembic/versions/2025-03-30_96e37a338676_add_platform_features_column_to_.py
@@ -0,0 +1,16 @@
+from alembic import op
+import sqlalchemy as sa
+
+def upgrade():
+    op.add_column('platform_features', sa.Column('worker_run_id', sa.BIGINT(), autoincrement=False, nullable=True, comment='This column is used to indicate analyzes run by a worker during the same execution period, and is useful for grouping, and time series analysis.'))
+    op.add_column('platform_features', sa.Column('dep_name', sa.VARCHAR(), autoincrement=False, nullable=True, comment='Name of the dependency found in project.'))
+    op.add_column('platform_features', sa.Column('dep_count', sa.INTEGER(), autoincrement=False, nullable=True, comment='Number of times the dependency was found.'))
+    op.add_column('platform_features', sa.Column('dep_language', sa.VARCHAR(), autoincrement=False, nullable=True, comment='Language of the dependency.'))
+    op.add_column('platform_features', sa.Column('pull_request_info', sa.Text(), nullable=True, comment='Information about dependency analyzes'))
+
+def downgrade():
+    op.drop_column('platform_features', 'worker_run_id')
+    op.drop_column('platform_features', 'dep_name')
+    op.drop_column('platform_features', 'dep_count')
+    op.drop_column('platform_features', 'dep_language')
+    op.drop_column('platform_features', 'pull_request_info') 
