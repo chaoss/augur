b9d0ff00ba8114fa4cde0dc3d2ec68cf9680f09b
Sean P. Goggins <s@goggins.com>
2025-04-08 09:43:45 -0500

    updated
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- augur/tasks/git/util/facade_worker/facade_worker/utilitymethods.py
+++ augur/tasks/git/util/facade_worker/facade_worker/utilitymethods.py
@@ -148,7 +148,13 @@ def get_repo_commit_count(logger, facade_helper, repo_git):
 
 	# Check if the .git directory exists
 	if not os.path.exists(repo_loc):
-		raise FileNotFoundError(f"The directory {absolute_path} does not exist.")
+		try:
+			logger.error(f"Ran into an error with {repo_loc}. Checking another strategy without .git extension.")
+			repo_loc = absolute_path 
+			if not os.path.exists(repo_loc):
+				raise FileNotFoundError(f"The directory {repo_loc} does not exist.")
+		except Exception as e: 
+			raise FileNotFoundError(f"The directory {absolute_path} does not exist, also {repo_loc} does not exist.")
 	
 	# if there are no branches then the repo is empty
 	if count_branches(repo_loc) == 0:
