3415a60afbe863a9c88101bf2d7c213b35487fa7
KhanRayyan3622 <rayyan.khan@chaoss.community>
2025-04-08 22:40:01 +0500

    Fix spelling of 'dependency' in comments across multiple files

---
+++ augur/augur/application/db/models/augur_data.py
@@ -0,0 +1,3 @@
+dep_name = Column(String, comment="Name of the dependency found in project. ")
+dep_count = Column(Integer, comment="Number of times the dependency was found. ")
+dep_language = Column(String, comment="Language of the dependency. ") 
---
+++ augur/augur/application/schema/alembic/versions/legacy/80.2-create-tables.sql
@@ -0,0 +1,3 @@
+COMMENT ON COLUMN "augur_data"."repo_dependencies"."dep_name" IS 'Name of the dependency found in project. ';
+COMMENT ON COLUMN "augur_data"."repo_dependencies"."dep_count" IS 'Number of times the dependency was found. ';
+COMMENT ON COLUMN "augur_data"."repo_dependencies"."dep_language" IS 'Language of the dependency. '; 
