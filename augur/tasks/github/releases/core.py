0def0c092582e2acd22fac559edff3ec1dcd0ded
Sean P. Goggins <s@goggins.com>
2025-06-08 10:29:15 -0500

    handling NULL descriptions in relesaes
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- augur/tasks/github/releases/core.py
+++ augur/tasks/github/releases/core.py
@@ -26,7 +26,7 @@ def get_release_inf(repo_id, release, tag_only):
             'release_id': release['id'],
             'repo_id': repo_id,
             'release_name': release['name'],
-            'release_description': release['description'],
+            'release_description': release['description'] if release['description'] is not None else '',
             'release_author': author,
             'release_created_at': release['createdAt'],
             'release_published_at': release['publishedAt'],
