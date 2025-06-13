7a82d62828aba91e16f4cd9933e1293e9df4f9b2
Sean P. Goggins <s@goggins.com>
2025-06-13 15:44:01 -0500

    version update
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- metadata.py
+++ metadata.py
@@ -5,8 +5,8 @@ __url__ = "https://github.com/chaoss/augur"
 
 __short_description__ = "Python 3 package for free/libre and open-source software community metrics, models & data collection"
 
-__version__ = "0.89.3"
-__release__ = "v0.89.3 (Midnight Sun)"
+__version__ = "0.89.5"
+__release__ = "v0.89.5 (Midnight Sun)"
 
 __license__ = "MIT"
 __copyright__ = "University of Missouri, University of Nebraska-Omaha, CHAOSS, Sean Goggins, Brian Warner & Augurlabs 2025"
