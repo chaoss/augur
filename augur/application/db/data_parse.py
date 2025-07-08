9c462c42ca09a3bddac2fad85d0964f76f965a2b
Andrew Brain <andrewbrain2019@gmail.com>
2025-07-07 20:18:20 -0500

    extract out diff_refs and author assignment to make it clearer which object is None sometimes

--- augur/application/db/data_parse.py
+++ augur/application/db/data_parse.py
@@ -1088,17 +1088,21 @@ def extract_needed_mr_metadata(mr_dict, repo_id, pull_request_id, tool_source, t
     Returns:
         List of dicts of parsed mr metadata
     """
-    head = {'sha': mr_dict['diff_refs']['head_sha'],
+
+    diff_refs = mr_dict['diff_refs']
+    author = mr_dict['author']
+
+    head = {'sha': diff_refs['head_sha'],
             'ref': mr_dict['target_branch'],
             'label': str(mr_dict['target_project_id']) + ':' + mr_dict['target_branch'],
-            'author': mr_dict['author']['username'],
+            'author': author['username'],
             'repo': str(mr_dict['target_project_id'])
             }
 
-    base = {'sha': mr_dict['diff_refs']['base_sha'],
+    base = {'sha': diff_refs['base_sha'],
             'ref': mr_dict['source_branch'],
             'label': str(mr_dict['source_project_id']) + ':' + mr_dict['source_branch'],
-            'author': mr_dict['author']['username'],
+            'author': author['username'],
             'repo': str(mr_dict['source_project_id'])
             }
 
