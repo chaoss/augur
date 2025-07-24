18f11eb350b59995da6b63193dc96043070adaa1
Ulincsys <ulincsys@gmail.com>
2025-07-24 18:24:31 -0500

    cache redis connection on first retrieval
    Signed-off-by: Ulincsys <ulincsys@gmail.com>

--- augur/tasks/init/redis_connection.py
+++ augur/tasks/init/redis_connection.py
@@ -4,9 +4,11 @@ from augur.tasks.init import get_redis_conn_values
 
 
 def get_redis_connection():
+    global redis_connection
 
-    redis_db_number, redis_conn_string = get_redis_conn_values()
-
-    redis_connection= redis.from_url(f'{redis_conn_string}{redis_db_number+2}', decode_responses=True)
+    # Only load the redis connection values once
+    if "redis_connection" not in globals():
+        redis_db_number, redis_conn_string = get_redis_conn_values()
+        redis_connection = redis.from_url(f'{redis_conn_string}{redis_db_number+2}', decode_responses=True)
 
     return redis_connection
