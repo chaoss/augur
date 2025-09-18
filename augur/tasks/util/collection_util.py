ee866979f46a8abb5691897f473116f041e5cd52
Sean P. Goggins <s@goggins.com>
2025-09-17 19:25:16 -0500

    updated
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- augur/tasks/util/collection_util.py
+++ augur/tasks/util/collection_util.py
@@ -604,27 +604,56 @@ class AugurTaskRoutine:
                 if "github" in repo.repo_git:
                     # phases = col_hook.phases # SPG variable scoping test
                     # use default platform name
-                    pass
+
+                    augur_collection_sequence = []
+                    for job in col_hook.phases: # SPG variable scoping test
+                        #Add the phase to the sequence in order as a celery task.
+                        #The preliminary task creates the larger task chain 
+                        augur_collection_sequence.append(job(repo_git, full_collection))
+
+                    #augur_collection_sequence.append(core_task_success_util.si(repo_git))
+                    #Link all phases in a chain and send to celery
+                    augur_collection_chain = chain(*augur_collection_sequence)
+                    task_id = augur_collection_chain.apply_async().task_id
+
+                    self.logger.info(f"Setting {platform_name} repo {col_hook.name} status to collecting for repo: {repo_git}")
+
+                    #yield the value of the task_id to the calling method so that the proper collectionStatus field can be updated
+                    yield repo_git, task_id, col_hook.name
 
                 elif "gitlab" in repo.repo_git:
                     platform_name = "gitlab"
                     if col_hook.gitlab_phases is None:
                         return
                     phases = col_hook.gitlab_phases
-           
-                augur_collection_sequence = []
-                for job in col_hook.phases: # SPG variable scoping test
-                    #Add the phase to the sequence in order as a celery task.
-                    #The preliminary task creates the larger task chain 
-                    augur_collection_sequence.append(job(repo_git, full_collection))
+                    augur_collection_sequence = []
+                    for job in col_hook.phases: # SPG variable scoping test
+                        #Add the phase to the sequence in order as a celery task.
+                        #The preliminary task creates the larger task chain 
+                        augur_collection_sequence.append(job(repo_git, full_collection))
 
-                #augur_collection_sequence.append(core_task_success_util.si(repo_git))
-                #Link all phases in a chain and send to celery
-                augur_collection_chain = chain(*augur_collection_sequence)
-                task_id = augur_collection_chain.apply_async().task_id
+                    #augur_collection_sequence.append(core_task_success_util.si(repo_git))
+                    #Link all phases in a chain and send to celery
+                    augur_collection_chain = chain(*augur_collection_sequence)
+                    task_id = augur_collection_chain.apply_async().task_id
 
-                self.logger.info(f"Setting {platform_name} repo {col_hook.name} status to collecting for repo: {repo_git}")
+                    self.logger.info(f"Setting {platform_name} repo {col_hook.name} status to collecting for repo: {repo_git}")
 
-                #yield the value of the task_id to the calling method so that the proper collectionStatus field can be updated
-                yield repo_git, task_id, col_hook.name
+                    #yield the value of the task_id to the calling method so that the proper collectionStatus field can be updated
+                    yield repo_git, task_id, col_hook.name
+                # augur_collection_sequence = []
+                # for job in col_hook.phases: # SPG variable scoping test
+                #     #Add the phase to the sequence in order as a celery task.
+                #     #The preliminary task creates the larger task chain 
+                #     augur_collection_sequence.append(job(repo_git, full_collection))
+
+                # #augur_collection_sequence.append(core_task_success_util.si(repo_git))
+                # #Link all phases in a chain and send to celery
+                # augur_collection_chain = chain(*augur_collection_sequence)
+                # task_id = augur_collection_chain.apply_async().task_id
+
+                # self.logger.info(f"Setting {platform_name} repo {col_hook.name} status to collecting for repo: {repo_git}")
+
+                # #yield the value of the task_id to the calling method so that the proper collectionStatus field can be updated
+                # yield repo_git, task_id, col_hook.name
                 
