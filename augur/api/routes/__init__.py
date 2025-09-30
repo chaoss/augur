f07aa9288282c2c97466f2985371e70b846af184
Adrian Edwards <adredwar@redhat.com>
2025-09-30 19:41:56 -0400

    remove the report routes
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- augur/api/routes/__init__.py
+++ augur/api/routes/__init__.py
@@ -4,10 +4,8 @@ from .application import *
 from .batch import *
 from .collection_status import *
 from .config import *
-from .contributor_reports import *
 from .manager import *
 from .nonstandard_metrics import *
-from .pull_request_reports import *
 from .user import *
 from .dei import *
 from .util import *
