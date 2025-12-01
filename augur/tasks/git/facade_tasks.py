35ec526badfa4aae2dfa769ab194ee9233c55ff1
Adrian Edwards <adredwar@redhat.com>
2025-12-01 15:29:49 -0500

    Pylint and other style fixes
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>
    Signed-off-by: PredictiveManish <manish.tiwari.09@zohomail.in>

--- augur/tasks/git/facade_tasks.py
+++ augur/tasks/git/facade_tasks.py
@@ -255,7 +255,7 @@ def analyze_commits_in_parallel(repo_git, multithreaded: bool)-> None:
     facade_helper.log_activity('Debug',f"Commits missing from repo {repo_id}: {len(missing_commits)}")
 
     
-    if not len(missing_commits) or repo_id is None:
+    if missing_commits or repo_id is None:
         #session.log_activity('Info','Type of missing_commits: %s' % type(missing_commits))
         return
     
