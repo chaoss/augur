d5b30eade34e6190f38293ac5e507ccc1902203e
Adrian Edwards <adredwar@redhat.com>
2025-12-03 10:17:27 -0500

    remove duplicate FK
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- augur/application/db/models/augur_data.py
+++ augur/application/db/models/augur_data.py
@@ -1315,15 +1315,7 @@ class Commit(Base):
             onupdate="CASCADE",
             initially="DEFERRED",
             deferrable=True,
-        ),
-        ForeignKey(
-            "augur_data.contributors.cntrb_login",
-            name="fk_commits_contributors_4",
-            ondelete="CASCADE",
-            onupdate="CASCADE",
-            initially="DEFERRED",
-            deferrable=True,
-        ),
+        )
     )
     tool_source = Column(String)
     tool_version = Column(String)
