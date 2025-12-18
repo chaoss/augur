b39a2c7597d68d523d0f38c0fd0ae88878eaa30e
PredictiveManish <manish.tiwari.09@zohomail.in>
2025-12-18 14:48:24 +0530

    Update
    Signed-off-by: PredictiveManish <manish.tiwari.09@zohomail.in>

--- augur/tasks/git/dependency_libyear_tasks/libyear_util/pypi_parser.py
+++ augur/tasks/git/dependency_libyear_tasks/libyear_util/pypi_parser.py
@@ -1,11 +1,6 @@
 import re, os
 import json
-import sys
-if sys.version_info >= (3, 11):
-    import tomllib
-else:
-    import tomli as tomllib
-   
+import tomli     
 import logging
 import yaml
 
@@ -101,13 +96,15 @@ def map_dependencies_pipfile(packages, type):
 
 
 #def parse_pipfile(file_handle):
-#    manifest = tomllib.load(file_handle)
+#    manifest = tomli.load(file_handle)
 #    return map_dependencies_pipfile(manifest['packages'],'runtime') + #map_dependencies_pipfile(manifest['dev-packages'], 'develop')
 ## Erro handling Means that the parse_pipfile(...) old function is assuming the presence of a dev-packages key in the parsed Pipfile, but that key does not exist in some cases.
 
 def parse_pipfile(file_handle):
+    import tomli
+
     try:
-        manifest = tomllib.load(file_handle)
+        manifest = tomli.load(file_handle)
     except Exception as e:
         logging.warning(
             f"Failed to parse Pipfile: {getattr(file_handle, 'name', 'unknown')}, error: {e}"
@@ -165,8 +162,8 @@ def parse_setup_py(file_handle):
 def parse_poetry(file_handle, repo_id=None, path=None):
     file_name = getattr(file_handle, 'name', 'unknown')
     try:
-        manifest = tomllib.load(file_handle)
-    except tomllib.TomlDecodeError as e:
+        manifest = tomli.load(file_handle)
+    except tomli.TomlDecodeError as e:
         logging.warning(f"[Repo ID: {repo_id}] Skipping malformed TOML file: {file_name} at {path}, error: {e}")
         return []
     except Exception as e:
@@ -183,7 +180,7 @@ def parse_poetry(file_handle, repo_id=None, path=None):
 
 
 def parse_poetry_lock(file_handle):
-    manifest = tomllib.load(file_handle)
+    manifest = tomli.load(file_handle)
     deps = list()
     group = 'runtime'
     for package in manifest['package']:
