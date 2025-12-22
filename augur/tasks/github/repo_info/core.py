2262e4ccb26e87bc3c75493d8f58fa2584e555d4
Himasree Kolathur <himasrikolathur@gmail.com>
2025-12-22 21:26:08 +0530

    docs: replace hardcoded tool_version with shared metadata version (Phase 1)
    Signed-off-by: Himasree Kolathur <himasrikolathur@gmail.com>

--- augur/tasks/github/repo_info/core.py
+++ augur/tasks/github/repo_info/core.py
@@ -11,6 +11,7 @@ from augur.application.db.lib import execute_sql
 from augur.tasks.github.util.github_task_session import *
 from augur.application.db.models.augur_data import RepoBadging
 from urllib.parse import quote
+from augur.metadata import __version__
 
 def query_committers_count(key_auth, logger, owner, repo):
 
@@ -223,7 +224,7 @@ def repo_info_model(key_auth, repo_orm_obj, logger):
         'pull_requests_closed': data['pr_closed']['totalCount'] if data['pr_closed'] else None,
         'pull_requests_merged': data['pr_merged']['totalCount'] if data['pr_merged'] else None,
         'tool_source': 'Repo_info Model',
-        'tool_version': '0.50.0',
+        'tool_version': __version__,
         'data_source': "Github"
     }
 
@@ -281,7 +282,6 @@ def badges_model(logger,repo_git,repo_id,db):
 
     #Hit cii api with no api key.
     response = hit_api(None, url, logger)
-
     try:
         response_data = response.json()
     except:
