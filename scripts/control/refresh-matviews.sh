abd5242f446c432f9e571481072c80b15ade25b8
1steve78 <yasinsteve4@gmail.com>
2026-01-10 13:57:36 +0530

    Remove obsolete explorer_libyear_all matview refresh
    Signed-off-by: 1steve78 <yasinsteve4@gmail.com>

--- scripts/control/refresh-matviews.sh
+++ scripts/control/refresh-matviews.sh
@@ -5,6 +5,5 @@ psql -U augur -h localhost -p 5432 -d padres -c 'REFRESH MATERIALIZED VIEW augur
 psql -U augur -h localhost -p 5432 -d padres -c 'REFRESH MATERIALIZED VIEW augur_data.api_get_all_repos_commits with data;'
 psql -U augur -h localhost -p 5432 -d padres -c 'REFRESH MATERIALIZED VIEW augur_data.augur_new_contributors with data;'
 psql -U augur -h localhost -p 5432 -d padres -c 'REFRESH MATERIALIZED VIEW augur_data.explorer_contributor_actions with data;'
-psql -U augur -h localhost -p 5432 -d padres -c 'REFRESH MATERIALIZED VIEW augur_data.explorer_libyear_all with data;'
 psql -U augur -h localhost -p 5432 -d padres -c 'REFRESH MATERIALIZED VIEW augur_data.explorer_new_contributors with data;'
 psql -U augur -h localhost -p 5432 -d padres -c 'REFRESH MATERIALIZED VIEW augur_data.explorer_entry_list with data;'
