aa145c08785016e4055ca786695a9df00628f409
Adrian Edwards <adredwar@redhat.com>
2026-03-04 14:00:29 -0500

    use central function for parsing the contributor data
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- augur/tasks/github/facade_github/core.py
+++ augur/tasks/github/facade_github/core.py
@@ -4,6 +4,7 @@ from augur.tasks.github.util.github_task_session import *
 from augur.application.db.models import *
 from augur.tasks.util.AugurUUID import GithubUUID
 from augur.application.db.lib import bulk_insert_dicts, batch_insert_contributors
+from augur.application.db.data_parse import extract_needed_contributor_data as extract_github_contributor
 from augur.tasks.github.util.github_data_access import GithubDataAccess
 
 
@@ -62,53 +63,7 @@ def query_github_contributors(logger, key_auth, github_url):
 
             contributor = github_data_access.get_resource(cntrb_url)
 
-            # Extract all available profile fields using .get() so we never miss or crash on
-            # optional keys. Email is only present when the user has made it public or when
-            # the request is appropriately authenticated (see GitHub API docs).
-            email = contributor.get('email')
-            canonical_email = email
-            company = contributor.get('company')
-            location = contributor.get('location')
-            full_name = contributor.get('name')
-            created_at = contributor.get('created_at')
-            updated_at = contributor.get('updated_at')
-
-            # TODO get and store an owner id
-
-            # Generate ID for cntrb table
-            cntrb_id = GithubUUID()
-            cntrb_id["user"] = int(contributor['id'])
-            cntrb_id["platform"] = platform_id
-
-            cntrb = {
-                "cntrb_id": cntrb_id.to_UUID(),
-                "cntrb_login": contributor['login'],
-                "cntrb_created_at": created_at,
-                "cntrb_email": email,
-                "cntrb_company": company,
-                "cntrb_location": location,
-                "cntrb_canonical": canonical_email,
-                "cntrb_full_name": full_name,
-                "cntrb_last_used": updated_at,
-                "gh_user_id": contributor['id'],
-                "gh_login": contributor['login'],
-                "gh_url": contributor.get('url'),
-                "gh_html_url": contributor.get('html_url'),
-                "gh_node_id": contributor.get('node_id'),
-                "gh_avatar_url": contributor.get('avatar_url'),
-                "gh_gravatar_id": contributor.get('gravatar_id'),
-                "gh_followers_url": contributor.get('followers_url'),
-                "gh_following_url": contributor.get('following_url'),
-                "gh_gists_url": contributor.get('gists_url'),
-                "gh_starred_url": contributor.get('starred_url'),
-                "gh_subscriptions_url": contributor.get('subscriptions_url'),
-                "gh_organizations_url": contributor.get('organizations_url'),
-                "gh_repos_url": contributor.get('repos_url'),
-                "gh_events_url": contributor.get('events_url'),
-                "gh_received_events_url": contributor.get('received_events_url'),
-                "gh_type": contributor.get('type'),
-                "gh_site_admin": contributor.get('site_admin'),
-            }
+            cntrb = extract_github_contributor(contributor)
 
             #insert cntrb to table.
             #session.logger.info(f"Contributor:  {cntrb}  \n")
