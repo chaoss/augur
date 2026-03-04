b4ef48ca19d32bd8921418fdba57240ae800c3a9
Cali Dolfi <47504866+cdolfi@users.noreply.github.com>
2026-03-04 18:33:28 -0500

    reverting query changes, will move to separate pr

--- augur/tasks/github/facade_github/tasks.py
+++ augur/tasks/github/facade_github/tasks.py
@@ -167,7 +167,8 @@ def link_commits_to_contributor(logger, facade_helper, contributorQueue):
                 UPDATE commits 
                 SET cmt_ght_author_id=:cntrb_id
                 WHERE 
-                cmt_author_email=:cntrb_email
+                (cmt_author_raw_email=:cntrb_email
+                OR cmt_author_email=:cntrb_email)
                 AND cmt_ght_author_id is NULL
         """).bindparams(cntrb_id=cntrb["cntrb_id"],cntrb_email=cntrb["email"])
 
